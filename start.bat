53314318c6b6dac736b20858b7a3e14bb25b9cfe
nemosminer <slbaker@hotmail.co.nz>
2017-06-20 10:46:27 +1200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle maximized -command "&.\NemosMiner-v2.0.ps1 -Wallet 1QGADhdMRpp9Pk5u5zG1TrHKRrdK5R81TE -Location US -PoolName zpool -Type nvidia -Algorithm bitcore,hmq1725,x17,blakecoin,Nist5,MyriadGroestl,Lyra2RE2,neoscrypt,blake2s
+powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle maximized -command "&.\NemosMiner-v2.0.ps1 -Wallet 1QGADhdMRpp9Pk5u5zG1TrHKRrdK5R81TE -Location US -PoolName zpool -Type nvidia -Algorithm bitcore,hmq1725,x17,blakecoin,Nist5,MyriadGroestl,Lyra2RE2,neoscrypt,blake2s -Donate 10
