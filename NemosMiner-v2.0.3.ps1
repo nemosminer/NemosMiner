eb155783e2480474fa54c401a0514c3cf408e482
nemosminer <slbaker@hotmail.co.nz>
2017-06-23 19:45:39 +1200

    Update and rename NemosMiner-v2.0.2.ps1 to NemosMiner-v2.0.3.ps1

--- NemosMiner-v2.0.3.ps1
+++ NemosMiner-v2.0.3.ps1
@@ -4,7 +4,7 @@
     [Parameter(Mandatory=$false)]
     [String]$UserName, 
     [Parameter(Mandatory=$false)]
-    [String]$WorkerName = "ID=NemosMiner-2.0.2", 
+    [String]$WorkerName = "ID=NemosMiner-2.0.3", 
     [Parameter(Mandatory=$false)]
     [Int]$API_ID = 0, 
     [Parameter(Mandatory=$false)]
@@ -59,7 +59,7 @@ if(Test-Path "Stats"){Get-ChildItemContent "Stats" | ForEach {$Stat = Set-Stat $
 $LastDonated = (Get-Date).AddDays(-1).AddHours(1)
 $WalletDonate = "1MsrCoAt8qM53HUMsUxvy9gMj3QVbHLazH"
 $UserNameDonate = "1MsrCoAt8qM53HUMsUxvy9gMj3QVbHLazH"
-$WorkerNameDonate = "NemosMiner-2.0.2"
+$WorkerNameDonate = "NemosMiner-2.0.3"
 $WalletBackup = $Wallet
 $UserNameBackup = $UserName
 $WorkerNameBackup = $WorkerName
