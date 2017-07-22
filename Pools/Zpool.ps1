2ec195b3f8617aea3d912162b472a213decb230f
nemosminer <slbaker@hotmail.co.nz>
2017-07-23 06:47:44 +1200

    update to be paid in any currency eg. BTC DNR KMD

--- Pools/Zpool.ps1
+++ Pools/Zpool.ps1
@@ -46,7 +46,7 @@ $Zpool_Request | Get-Member -MemberType NoteProperty | Select -ExpandProperty Na
             Host = $Zpool_Host
             Port = $Zpool_Port
             User = $Wallet
-            Pass = $WorkerName
+            Pass = $WorkerName,c=$Currency
             Location = $Location
             SSL = $false
         }
