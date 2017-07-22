728ae50af434bf590b5bf059d741b184d7ea6262
nemosminer <slbaker@hotmail.co.nz>
2017-07-22 17:00:13 +1200

    Update Zpool.ps1

--- Pools/Zpool.ps1
+++ Pools/Zpool.ps1
@@ -46,7 +46,7 @@ $Zpool_Request | Get-Member -MemberType NoteProperty | Select -ExpandProperty Na
             Host = $Zpool_Host
             Port = $Zpool_Port
             User = $Wallet
-            Pass = $WorkerName,c=$Currency
+            Pass = c="+$Currency[0]
             Location = $Location
             SSL = $false
         }
