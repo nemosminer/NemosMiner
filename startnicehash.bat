38bb8282c427b9a8ddc356bebdab2e40a5b95c92
nemosminer <slbaker@hotmail.co.nz>
2017-08-19 20:56:14 +1200

    Create startnicehash.bat

---
+++ startnicehash.bat
@@ -0,0 +1,1 @@
+powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle maximized -command "&.\NemosMiner-v2.1.ps1 -SelGPUEWBF '0 1' -SelGPUCC '0,1' -Currency BTC -interval 60 -Wallet 1QGADhdMRpp9Pk5u5zG1TrHKRrdK5R81TE -Location US -PoolName nicehash -Type nvidia -Algorithm keccak,skunk,lbry,equihash,Nist5,Lyra2RE2,neoscrypt,blake2s -Donate 5
