2ebe65f6d406fd5453955d9484608a1924dd9879
nemosminer <slbaker@hotmail.co.nz>
2017-08-21 13:45:14 +1200

    Update CcminerPalgin.ps1

--- Miners/CcminerPalgin.ps1
+++ Miners/CcminerPalgin.ps1
@@ -1,7 +1,58 @@
 . .\Include.ps1
 
 $Path = ".\Bin\NVIDIA-Palgin\ccminer.exe"
-$Uri = "https://github.com/palginpav/ccminer/releases/download/1.1.1/palginmod_1.1_x86.zip"
+$Uri = "https://github.com/palginpav/ccminer/r. .\Include.ps1
+
+$Path = ".\Bin\NVIDIA-Alexis78\ccminer.exe"
+$Uri = "https://github.com/nemosminer/ccminer-Alexis78/releases/download/ccminer-alexis78/ccminer-alexis78-ms2013-cuda7.5.7z"
+
+$Commands = [PSCustomObject]@{
+    #"bitcore" = "" #Bitcore
+    #"blake2s" = " -d $SelGPUCC" #Blake2s
+    #"blakecoin" = " -d $SelGPUCC" #Blakecoin
+    #"vanilla" = "" #BlakeVanilla
+    #"cryptonight" = "" #Cryptonight
+    #"decred" = "" #Decred
+    #"equihash" = "" #Equihash
+    #"ethash" = "" #Ethash
+    #"groestl" = "" #Groestl
+    #"hmq1725" = "" #hmq1725
+    "keccak" = " -i 29" #Keccak
+    #"lbry" = " -d $SelGPUCC" #Lbry
+    #"lyra2v2" = " -d $SelGPUCC" #Lyra2RE2
+    #"lyra2z" = "" #Lyra2z
+    "myr-gr" = " -d $SelGPUCC" #MyriadGroestl
+    #"neoscrypt" = " -i 15 -d $SelGPUCC" #NeoScrypt
+    "nist5" = "" #Nist5
+    #"pascal" = "" #Pascal
+    #"qubit" = "" #Qubit
+    #"scrypt" = "" #Scrypt
+    #"sia" = "" #Sia
+    "sib" = " -i 21 -d $SelGPUCC" #Sib
+    "X11Gost" = " -i 21 -d $SelGPUCC" #X11Gost
+    "skein" = " -d $SelGPUCC" #Skein
+    #"timetravel" = "" #Timetravel
+    "c11" = " -i 21 -d $SelGPUCC" #C11
+    #"x11evo" = "" #X11evo
+    "x17" = " -i 21 -d $SelGPUCC" #X17
+    #"yescrypt" = "" #Yescrypt
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $Wallet -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
+        API = "Ccminer"
+        Port = 4068
+        Wrap = $false
+        URI = $Uri
+    }
+}
+eleases/download/1.1.1/palginmod_1.1_x86.zip"
 
 $Commands = [PSCustomObject]@{
     "bastion" = ""
