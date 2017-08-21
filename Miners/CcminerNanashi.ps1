64aed818984750eb3981fe54e07f0d4db2fd47ab
nemosminer <slbaker@hotmail.co.nz>
2017-08-21 13:52:32 +1200

    Update CcminerNanashi.ps1

--- Miners/CcminerNanashi.ps1
+++ Miners/CcminerNanashi.ps1
@@ -1,81 +1,47 @@
-﻿. .\Include.ps1
+. .\Include.ps1
 
-$Path = ".\Bin\ccm-2-2-mod-r2-cuda9\ccminer.exe"
-$URI = "https://github.com/Nanashi-Meiyo-Meijin/ccminer/releases/download/v2.2-mod-r2/2.2-mod-r2-CUDA9.binary.zip"
-
-
-$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+$Path = ".\Bin\NVIDIA-Alexis78\ccminer.exe"
+$Uri = "https://github.com/nemosminer/ccminer-Alexis78/releases/download/ccminer-alexis78/ccminer-alexis78-ms2013-cuda7.5.7z"
 
 $Commands = [PSCustomObject]@{
-
-
-    "bastion" = ""
-
-    #"bitcore" = "" 
-    #"blake" = ""
-    #"blakecoin" = ""
-    #"blake2s" = "" 
-    #"bmw" = ""
-    #"c11" = " -i 21 " 
-    #"cryptolight" = ""
-    #"cryptonight" = ""  
-    # "decred" = "" 
-    #"deep" = ""
-    #"dmd-gr" = "" 
-    #"fresh" = ""
-    #"fugue256" = ""
-    #"groestl" = "" 
-    #"heavy" = "" 
-    #"hmq1725" = "" 
-    #"jha" = "" 
-    #"keccak" = "" 
-    #"lbry" = "" 
-    #"luffa" = ""
-    #"lyra2RE" = "" 
-    #"lyra2REv2" = "" 
-    #"lyra2z" = ""
-    #"m7m" = ""
-    #"mjollnir" = "" 
-    #"myr-gr" = "" 
-    #"neoscrypt" = "" 
-    #"nist5" = "" 
-    #"pascal" = "" 
-    #"penta" = ""
-    #"quark" = "" 
-    #"qubit" = "" 
-    #"s3" = "" 
-    #"scrypt" = "" 
-    #"scrypt:N" = "" 
-    #"scrypt-jane" = "" 
-    #"sia" = ""  
-    #"sib" = " -i 21 " 
-    #"skein" = "" 
-    #"skein2" = ""
-    "skunk" = " -d $SelGPUCC" 
-    #"timetravel" = ""
-    #"tribus" = ""
-    #"vanilla" = "" 
-    #"veltor" = ""
-    #"whirlpool" = ""
-    #"whirlpoolx" = ""
-    #"wildkeccak" = ""
-    #"x11" = "" 
-    #"x11evo" = "" 
-    #"x11gost" = ""
-    #"x13" = "" 
-    #"x14" = ""
-    #"x15" = "" 
-    #"x17" = ""
-    #"zr5" = ""
+    #"bitcore" = "" #Bitcore
+    #"blake2s" = " -d $SelGPUCC" #Blake2s
+    #"blakecoin" = " -d $SelGPUCC" #Blakecoin
+    #"vanilla" = "" #BlakeVanilla
+    #"cryptonight" = "" #Cryptonight
+    #"decred" = "" #Decred
+    #"equihash" = "" #Equihash
+    #"ethash" = "" #Ethash
+    #"groestl" = "" #Groestl
+    #"hmq1725" = "" #hmq1725
+    "keccak" = " -i 29" #Keccak
+    #"lbry" = " -d $SelGPUCC" #Lbry
+    #"lyra2v2" = " -d $SelGPUCC" #Lyra2RE2
+    #"lyra2z" = "" #Lyra2z
+    "myr-gr" = " -d $SelGPUCC" #MyriadGroestl
+    #"neoscrypt" = " -i 15 -d $SelGPUCC" #NeoScrypt
+    "nist5" = "" #Nist5
+    #"pascal" = "" #Pascal
+    #"qubit" = "" #Qubit
+    #"scrypt" = "" #Scrypt
+    #"sia" = "" #Sia
+    "sib" = " -i 21 -d $SelGPUCC" #Sib
+    "X11Gost" = " -i 21 -d $SelGPUCC" #X11Gost
+    "skein" = " -d $SelGPUCC" #Skein
+    #"timetravel" = "" #Timetravel
+    "c11" = " -i 21 -d $SelGPUCC" #C11
+    #"x11evo" = "" #X11evo
+    "x17" = " -i 21 -d $SelGPUCC" #X17
+    #"yescrypt" = "" #Yescrypt
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
-$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = " -R 5 -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $Wallet -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $Wallet -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
         API = "Ccminer"
         Port = 4068
