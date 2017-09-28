9efd67e7acfd5630495885003de29df02466cb05
nemosminer <slbaker@hotmail.co.nz>
2017-09-28 20:13:04 +1300

    fix "no pools alert zpool"

--- NemosMiner-v2.1.ps1
+++ NemosMiner-v2.1.ps1
@@ -96,7 +96,7 @@ while($true)
         Where Location -EQ $Location | 
         Where SSL -EQ $SSL | 
         Where {$PoolName.Count -eq 0 -or (Compare $PoolName $_.Name -IncludeEqual -ExcludeDifferent | Measure).Count -gt 0}}
-    if($AllPools.Count -eq 0){"No Pools!" | Out-Host; sleep 1; continue}
+    if($AllPools.Count -eq 0){"No Pools!" | Out-Host; sleep 10; continue}
     $Pools = [PSCustomObject]@{}
     $Pools_Comparison = [PSCustomObject]@{}
     $AllPools.Algorithm | Select -Unique | ForEach {$Pools | Add-Member $_ ($AllPools | Where Algorithm -EQ $_ | Sort Price -Descending | Select -First 1)}
