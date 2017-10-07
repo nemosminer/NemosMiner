48257eb30bf14589315f90053418570abbc25d1d
nemosminer <slbaker@hotmail.co.nz>
2017-10-07 19:47:30 +1300

    Create CcminerXevan.ps1

---
+++ Miners/CcminerXevan.ps1
@@ -0,0 +1,51 @@
+. .\Include.ps1
+
+$Path = ".\Bin\NVIDIA-Xevan\ccminer.exe"
+$Uri = "https://github.com/nemosminer/ccminer-xevan/releases/download/ccminer-xevan/ccminer_x86.exe"
+
+$Commands = [PSCustomObject]@{
+    #"bitcore" = "" #Bitcore
+    #"blake2s" = "" #Blake2s
+    #"blakecoin" = "" #Blakecoin
+    #"vanilla" = "" #BlakeVanilla
+    #"cryptonight" = "" #Cryptonight
+    #"decred" = "" #Decred
+    #"equihash" = "" #Equihash
+    #"ethash" = "" #Ethash
+    #"groestl" = "" #Groestl
+    #"hmq1725" = "" #hmq1725
+    #"keccak" = "" #Keccak
+    #"lbry" = "" #Lbry
+    #"lyra2v2" = "" #Lyra2RE2
+    #"lyra2z" = "" #Lyra2z
+    #"myr-gr" = "" #MyriadGroestl
+    #"neoscrypt" = " -i 15" #NeoScrypt
+    #"nist5" = "" #Nist5
+    #"pascal" = "" #Pascal
+    #"qubit" = "" #Qubit
+    #"scrypt" = "" #Scrypt
+    #"sia" = "" #Sia
+    #"sib" = " -i 21" #Sib
+    #"skein" = "" #Skein
+    #"timetravel" = "" #Timetravel
+	#"veltor" = "" #Veltor
+    #"c11" = " -i 21" #C11
+    "xevan" = "" #Xevan
+    #"x17" = " -i 21" #X17
+    #"yescrypt" = "" #Yescrypt
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
+        API = "Ccminer"
+        Port = 4068
+        Wrap = $false
+        URI = $Uri
+    }
+}
