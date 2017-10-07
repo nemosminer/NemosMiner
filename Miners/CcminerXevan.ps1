91d1e610b78769de61b4cc2761125f86c18ba95c
nemosminer <slbaker@hotmail.co.nz>
2017-10-07 20:16:05 +1300

    Update CcminerXevan.ps1

--- Miners/CcminerXevan.ps1
+++ Miners/CcminerXevan.ps1
@@ -1,6 +1,6 @@
 . .\Include.ps1
 
-$Path = ".\Bin\NVIDIA-Xevan\ccminer.exe"
+$Path = ".\Bin\NVIDIA-Xevan\ccminer_x86.exe"
 $Uri = "https://github.com/nemosminer/ccminer-xevan/releases/download/ccminer-xevan/ccminer_x86.7z"
 
 $Commands = [PSCustomObject]@{
