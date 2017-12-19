7cc52d0ce2c8fae6ebe9e358596e66258b0815c1
nemosminer <slbaker@hotmail.co.nz>
2017-12-20 08:51:07 +1300

    Update DSTM.ps1

--- Miners/DSTM.ps1
+++ Miners/DSTM.ps1
@@ -1,10 +1,10 @@
-﻿. .\Include.ps1
+. .\Include.ps1
 
 $Path = ".\Bin\NVIDIA-DSTM\zm.exe"
-# Uri = "https://bitcointalk.org/index.php?topic=2021765.0"
+$Uri = "https://github.com/nemosminer/DSTM-equihash-miner/releases/download/0.5.7/zm_0.5.7_win.zip"
 
 $Commands = [PSCustomObject]@{
-    "equihash" = " -d $SelGPUCC" #Equihash
+    "equihash" = " -d $SelCC" #Equihash
 }
 $Port = 2222
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -13,7 +13,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = "--telemetry=127.0.0.1:$Port --server $($Pools.(Get-Algorithm($_)).Host) --port $($Pools.(Get-Algorithm($_)).Port) --fee 0 --solver 0 --eexit 1 --user $($Pools.(Get-Algorithm($_)).User) --pass $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        Arguments = "--telemetry=127.0.0.1:$Port --server $($Pools.(Get-Algorithm($_)).Host) --port $($Pools.(Get-Algorithm($_)).Port) --user $($Pools.(Get-Algorithm($_)).User) --pass $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
         API = "DSTM"
         Port = $Port
