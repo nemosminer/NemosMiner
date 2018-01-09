bbd2530746dea8172b89e4529fd20483a87c1e32
Yuriy Kuzmenko <ykuzmenko73.work@gmail.com>
2018-01-09 20:17:43 +0700

    Revert "changed wallet and interval"
    This reverts commit 59b8018e2938c63e9667d0d08139ce2d28546428.

--- miner.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-set DIR=%~dp0
-
-cd %DIR%
-powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle minimized -command "&.\NemosMiner-v2.3.ps1 -SelGPUDSTM '0 1' -SelGPUCC '0,1' -Currency USD -Passwordcurrency BTC -interval 30 -Wallet 1G1384gnsaswY3ddHukcCv4rEGbhsEnrrg -Location US -PoolName ahashpool -Type nvidia -Algorithm xevan,hsr,phi,tribus,c11,lbry,skein,timetravel,sib,bitcore,x17,Nist5,MyriadGroestl,Lyra2RE2,neoscrypt,blake2s,skunk,Groestl,HMQ1725,Keccak,Scrypt -Donate 0 -WorkerName server
--- restart.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call C:\Tools\Mining\Nemos\stop.bat
-
-C:\Windows\System32\Taskkill /f /im tail.exe
-timeout 2
-del C:\Tools\Mining\Nemos\Logs\miner.log
-
-call C:\Tools\Mining\Nemos\start.bat
