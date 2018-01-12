b86d4317db21275ed947058d26f48045fa624691
nemosminer <slbaker@hotmail.co.nz>
2018-01-12 13:49:48 +1300

    Update NemosMiner-v2.3.ps1

--- NemosMiner-v2.3.ps1
+++ NemosMiner-v2.3.ps1
@@ -10,9 +10,9 @@ param(
     [Parameter(Mandatory=$false)]
     [String]$API_Key = "", 
     [Parameter(Mandatory=$false)]
-    [Int]$Interval = 60, #seconds before between cycles after the first has passed 
+    [Int]$Interval = 180, #seconds before between cycles after the first has passed 
     [Parameter(Mandatory=$false)]
-    [Int]$FirstInterval = 180, #seconds of the first cycle of activated or started first time miner
+    [Int]$FirstInterval = 240, #seconds of the first cycle of activated or started first time miner
     [Parameter(Mandatory=$false)]
     [String]$Location = "europe", #europe/us/asia
     [Parameter(Mandatory=$false)]
