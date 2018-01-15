5b25d2916a11e15255e255cc09c3c36bdf85a887
nemosminer <slbaker@hotmail.co.nz>
2018-01-15 15:59:41 +1300

    Update miner.cmd

--- miner.cmd
+++ miner.cmd
@@ -2,6 +2,6 @@
 set DIR=%~dp0
 
 cd %DIR%
-powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle minimized -command "&.\NemosMiner-v2.3.ps1 -PoolName ahashpool -Algorithm xevan,hsr,phi,tribus,c11,lbry,skein,timetravel,sib,bitcore,x17,Nist5,MyriadGroestl,Lyra2RE2,neoscrypt,blake2s,skunk,Groestl,HMQ1725,Keccak,Scrypt -WorkerName server
+powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle minimized -command "&.\NemosMiner-v2.3.ps1 -PoolName ahashpool -Algorithm xevan,hsr,phi,tribus,c11,lbry,skein,timetravel,sib,bitcore,x17,Nist5,MyriadGroestl,Lyra2RE2,neoscrypt,blake2s,skunk,Groestl,HMQ1725,Keccak,Scrypt -WorkerName NemosMiner
 
-; powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle minimized -command "&.\NemosMiner-v2.3.ps1 -SelGPUDSTM '0 1' -SelGPUCC '0,1' -Currency USD -Passwordcurrency BTC -interval 30 -Wallet 1G1384gnsaswY3ddHukcCv4rEGbhsEnrrg -Location US -PoolName ahashpool -Type nvidia -Algorithm xevan,hsr,phi,tribus,c11,lbry,skein,timetravel,sib,bitcore,x17,Nist5,MyriadGroestl,Lyra2RE2,neoscrypt,blake2s,skunk,Groestl,HMQ1725,Keccak,Scrypt -Donate 0 -WorkerName server
+; powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle minimized -command "&.\NemosMiner-v2.3.ps1 -SelGPUDSTM '0 1' -SelGPUCC '0,1' -Currency USD -Passwordcurrency BTC -interval 30 -Wallet 1QGADhdMRpp9Pk5u5zG1TrHKRrdK5R81TE -Location US -PoolName ahashpool -Type nvidia -Algorithm xevan,hsr,phi,tribus,c11,lbry,skein,timetravel,sib,bitcore,x17,Nist5,MyriadGroestl,Lyra2RE2,neoscrypt,blake2s,skunk,Groestl,HMQ1725,Keccak,Scrypt -Donate 5 -WorkerName NemosMiner
