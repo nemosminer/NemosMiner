a65249e6ebe7613ee2e153c37696053c2503ef56
nemosminer <slbaker@hotmail.co.nz>
2018-02-05 08:45:38 +1300

    Update restart.bat

--- restart.bat
+++ restart.bat
@@ -2,7 +2,9 @@
 call C:\Tools\Mining\Nemos\stop.bat
 
 C:\Windows\System32\Taskkill /f /im tail.exe
-timeout 2
+
+timeout 1
 del C:\Tools\Mining\Nemos\Logs\miner.log
+timeout 1
 
 call C:\Tools\Mining\Nemos\start.bat
