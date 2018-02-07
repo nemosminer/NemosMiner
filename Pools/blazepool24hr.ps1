134cf145135b584223267256da5052b03377d09e
nemosminer <slbaker@hotmail.co.nz>
2018-02-08 09:04:21 +1300

    add blazepool24hr support

---
+++ Pools/blazepool24hr.ps1
@@ -0,0 +1,47 @@
+. .\Include.ps1
+
+try {
+    $blazepool_Request = Invoke-WebRequest "http://api.blazepool.com/status" -UseBasicParsing -Headers @{"Cache-Control"="no-cache"} | ConvertFrom-Json } catch { return }
+
+if (-not $blazepool_Request) {return}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Location = "US"
+
+$blazepool_Request | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    $blazepool_Host = "$_.mine.blazepool.com"
+    $blazepool_Port = $blazepool_Request.$_.port
+    $blazepool_Algorithm = Get-Algorithm $blazepool_Request.$_.name
+    $blazepool_Coin = ""
+
+    $Divisor = 1000000000
+	
+        switch ($blazepool_Algorithm) {
+
+        "equihash"{$Divisor /= 1000}
+        "blake2s"{$Divisor *= 1000}
+        "blakecoin"{$Divisor *= 1000}
+        "decred"{$Divisor *= 1000}
+    }
+
+    if ((Get-Stat -Name "$($Name)_$($blazepool_Algorithm)_Profit") -eq $null) {$Stat = Set-Stat -Name "$($Name)_$($blazepool_Algorithm)_Profit" -Value ([Double]$blazepool_Request.$_.actual_last24h / $Divisor)}
+    else {$Stat = Set-Stat -Name "$($Name)_$($blazepool_Algorithm)_Profit" -Value ([Double]$blazepool_Request.$_.actual_last24h / $Divisor)}
+	
+    if ($Wallet) {
+        [PSCustomObject]@{
+            Algorithm     = $blazepool_Algorithm
+            Info          = $blazepool_Coin
+            Price         = $Stat.Live
+            StablePrice   = $Stat.Week
+            MarginOfError = $Stat.Week_Fluctuation
+            Protocol      = "stratum+tcp"
+            Host          = $blazepool_Host
+            Port          = $blazepool_Port
+            User          = $Wallet
+            Pass          = "$WorkerName,c=$Passwordcurrency"
+            Location      = $Location
+            SSL           = $false
+        }
+    }
+}
