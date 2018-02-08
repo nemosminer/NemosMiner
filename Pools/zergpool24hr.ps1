978a2c837400531205a20e90e0dc8deaa63252a6
nemosminer <slbaker@hotmail.co.nz>
2018-02-08 14:01:43 +1300

    Update zergpool24hr.ps1

--- Pools/zergpool24hr.ps1
+++ Pools/zergpool24hr.ps1
@@ -26,8 +26,8 @@ $zergpool_Request | Get-Member -MemberType NoteProperty | Select -ExpandProperty
 	"keccak"{$Divisor *= 1000}
     }
 
-    if((Get-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit") -eq $null){$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.actual_last24h / $Divisor *(1-($zergpool_Request.$_.fees/100)))}
-    else{$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.actual_last24h / $Divisor *(1-($zergpool_Request.$_.fees/100)))}
+    if((Get-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit") -eq $null){$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.actual_last24h / $Divisor}
+    else{$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.actual_last24h / $Divisor}
 	
     if($Wallet)
     {
