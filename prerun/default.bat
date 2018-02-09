c47af2dc67d97bd69835dd70dfbcb13b73c1196b
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2018-02-09 16:53:53 +0100

    Update default.bat

--- prerun/default.bat
+++ prerun/default.bat
@@ -2,3 +2,7 @@ REM This file will run for any algo if algo.bat does not exist
 REM Place yout code below
 
 echo prerun default file
+
+REM Example clock settings using nvidiaInspector
+REM nvidiaInspector.exe -setBaseClockOffset:0,0,150 -setMemoryClockOffset:0,0,250 -setVoltageOffset:0,0,0 -setPowerTarget:0,115 -setTempTarget:0,0,92 
+
