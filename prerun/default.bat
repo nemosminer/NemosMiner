f35e6d9440134578e9224b681322af2b5cc8c975
nemosminer <slbaker@hotmail.co.nz>
2018-02-13 09:33:51 +1300

    add more info about prerun
    REM you need to download nvidiaInspector and place nvidiaInspector.exe in the prerun folder the remove REM from "REM nvidiaInspector.exe" below and this example OC will work note: it +75core +505Memory 71%tdp 90temptarget  (standard 1070 OC)

--- prerun/default.bat
+++ prerun/default.bat
@@ -4,5 +4,8 @@ REM Place your code below
 echo prerun default file
 
 REM Example clock settings using nvidiaInspector
+
+REM you need to download nvidiaInspector and place nvidiaInspector.exe in the prerun folder the remove REM from "REM nvidiaInspector.exe" below and this defalut OC will work
+
 REM nvidiaInspector.exe -setBaseClockOffset:0,0,75 -setMemoryClockOffset:0,0,505 -setVoltageOffset:0,0,0 -setPowerTarget:0,75 -setTempTarget:0,0,90 
 
