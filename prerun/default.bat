f29d0ac91122b0616ea24529637d04d68f5d843d
nemosminer <slbaker@hotmail.co.nz>
2018-02-13 09:19:06 +1300

    Update default.bat

--- prerun/default.bat
+++ prerun/default.bat
@@ -4,5 +4,5 @@ REM Place your code below
 echo prerun default file
 
 REM Example clock settings using nvidiaInspector
-REM nvidiaInspector.exe -setBaseClockOffset:0,0,75 -setMemoryClockOffset:0,0,505 -setVoltageOffset:0,0,0 -setPowerTarget:0,75 -setTempTarget:0,0,82 
+REM nvidiaInspector.exe -setBaseClockOffset:0,0,75 -setMemoryClockOffset:0,0,505 -setVoltageOffset:0,0,0 -setPowerTarget:0,75 -setTempTarget:0,0,90 
 
