ab2e8954e25530ebaf40be43f8cc7f63714d59d9
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2018-02-17 12:08:06 +0100

    Update zergpoolplus.ps1

--- Pools/zergpoolplus.ps1
+++ Pools/zergpoolplus.ps1
@@ -16,18 +16,18 @@ $zergpool_Request | Get-Member -MemberType NoteProperty | Select -ExpandProperty
     $zergpool_Algorithm = Get-Algorithm $zergpool_Request.$_.name
     $zergpool_Coin = ""
 
-    $Divisor = 1000000
+    $Divisor = 1000000000
 	
     switch ($zergpool_Algorithm) {
-        "equihash" {$Divisor /= 1000}
-        "blake2s" {$Divisor *= 1000}
-	"blakecoin" {$Divisor *= 1000}
-        "decred" {$Divisor *= 1000}
-	"keccak" {$Divisor *= 1000}
+        "equihash"{$Divisor /= 1000}
+        "blake2s"{$Divisor *= 1000}
+        "blakecoin"{$Divisor *= 1000}
+        "decred"{$Divisor *= 1000}
+	"keccak"{$Divisor *= 1000}
     }
 
-    if((Get-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit") -eq $null){$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.actual_last24h/$Divisor)}
-    else{$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.actual_last24h/$Divisor)}
+    if((Get-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit") -eq $null){$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.actual_last24h / $Divisor)}
+    else{$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.actual_last24h / $Divisor)}
 	
     if($Wallet)
     {
