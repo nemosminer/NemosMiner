376d2717d7f870a96ce1bd75ba2cc0d80d89538f
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2018-02-17 12:07:35 +0100

    Update zpoolplus.ps1

--- Pools/zpoolplus.ps1
+++ Pools/zpoolplus.ps1
@@ -4,46 +4,45 @@ $PlusPath = ((split-path -parent (get-item $script:MyInvocation.MyCommand.Path).
 Try{
 	$Zpool_Request = get-content $PlusPath | ConvertFrom-Json } catch { return }
 
-if(-not $Zpool_Request){return}
+if (-not $Zpool_Request) {return}
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Location = "US"
 
-$Zpool_Request | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {
+$Zpool_Request | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
     $Zpool_Host = "$_.mine.zpool.ca"
     $Zpool_Port = $Zpool_Request.$_.port
     $Zpool_Algorithm = Get-Algorithm $Zpool_Request.$_.name
     $Zpool_Coin = ""
 
-    $Divisor = 1000000
+    $Divisor = 1000000000
 	
     switch ($Zpool_Algorithm) {
-        "equihash" {$Divisor /= 1000}
-        "blake2s" {$Divisor *= 1000}
-	"blakecoin" {$Divisor *= 1000}
-        "decred" {$Divisor *= 1000}
-	"keccak" {$Divisor *= 1000}
+        "equihash"{$Divisor /= 1000}
+        "blake2s"{$Divisor *= 1000}
+        "blakecoin"{$Divisor *= 1000}
+        "decred"{$Divisor *= 1000}
+	"keccak"{$Divisor *= 1000}
     }
 
-    if((Get-Stat -Name "$($Name)_$($Zpool_Algorithm)_Profit") -eq $null){$Stat = Set-Stat -Name "$($Name)_$($Zpool_Algorithm)_Profit" -Value ([Double]$Zpool_Request.$_.estimate_last24h/$Divisor *(1-($Zpool_Request.$_.fees/100)))}
-    else{$Stat = Set-Stat -Name "$($Name)_$($Zpool_Algorithm)_Profit" -Value ([Double]$Zpool_Request.$_.estimate_current/$Divisor *(1-($Zpool_Request.$_.fees/100)))}
+    if ((Get-Stat -Name "$($Name)_$($Zpool_Algorithm)_Profit") -eq $null) {$Stat = Set-Stat -Name "$($Name)_$($Zpool_Algorithm)_Profit" -Value ([Double]$Zpool_Request.$_.actual_last24h / $Divisor *(1-($Zpool_Request.$_.fees/100)))}
+    else {$Stat = Set-Stat -Name "$($Name)_$($Zpool_Algorithm)_Profit" -Value ([Double]$Zpool_Request.$_.actual_last24h / $Divisor *(1-($Zpool_Request.$_.fees/100)))}
 	
-    if($Wallet)
-    {
+    if ($Wallet) {
         [PSCustomObject]@{
-            Algorithm = $Zpool_Algorithm
-            Info = $Zpool
-            Price = $Stat.Live
-            StablePrice = $Stat.Week
-            MarginOfError = $Stat.Fluctuation
-            Protocol = "stratum+tcp"
-            Host = $Zpool_Host
-            Port = $Zpool_Port
-            User = $Wallet
-            Pass = "$WorkerName,c=$Passwordcurrency"
-            Location = $Location
-            SSL = $false
+            Algorithm     = $Zpool_Algorithm
+            Info          = $Zpool_Coin
+            Price         = $Stat.Live
+            StablePrice   = $Stat.Week
+            MarginOfError = $Stat.Week_Fluctuation
+            Protocol      = "stratum+tcp"
+            Host          = $Zpool_Host
+            Port          = $Zpool_Port
+            User          = $Wallet
+            Pass          = "$WorkerName,c=$Passwordcurrency"
+            Location      = $Location
+            SSL           = $false
         }
     }
 }
