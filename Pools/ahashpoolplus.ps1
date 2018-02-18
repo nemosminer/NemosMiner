a7cdef3dbcdf2f82f603a4650a054726d0f239eb
ahashpool <31766130+ahashpool@users.noreply.github.com>
2018-02-18 12:55:51 +0800

    Update ahashpoolplus.ps1

--- Pools/ahashpoolplus.ps1
+++ Pools/ahashpoolplus.ps1
@@ -21,9 +21,16 @@ $ahashpool_Request | Get-Member -MemberType NoteProperty | Select-Object -Expand
         switch ($ahashpool_Algorithm) {
 
         "equihash"{$Divisor /= 1000}
+        "yescrypt"{$Divisor /= 1000}
+        "sha256"{$Divisor *= 1000}
+        "sha256t"{$Divisor *= 1000}
+        "blake"{$Divisor *= 1000}
         "blake2s"{$Divisor *= 1000}
-        "blakecoin"{$Divisor *= 1000}
+	"blakecoin"{$Divisor *= 1000}
         "decred"{$Divisor *= 1000}
+        "keccak"{$Divisor *= 1000}
+        "keccakc"{$Divisor *= 1000}
+        "vanilla"{$Divisor *= 1000}
     }
 
     if ((Get-Stat -Name "$($Name)_$($ahashpool_Algorithm)_Profit") -eq $null) {$Stat = Set-Stat -Name "$($Name)_$($ahashpool_Algorithm)_Profit" -Value ([Double]$ahashpool_Request.$_.actual_last24h / $Divisor *(1-($ahashpool_Request.$_.fees/100)))}
