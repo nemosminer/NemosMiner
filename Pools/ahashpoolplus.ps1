c134aa4bec2a41e5d821936a2115c1a4349e5a86
nemosminer <slbaker@hotmail.co.nz>
2018-02-20 11:50:22 +1300

    Update ahashpoolplus.ps1

--- Pools/ahashpoolplus.ps1
+++ Pools/ahashpoolplus.ps1
@@ -1,7 +1,8 @@
 . .\Include.ps1
 
-try {
-    $ahashpool_Request = Invoke-WebRequest "http://mytestenv.alwaysdata.net/servefiles/BrainToNemos6.json" -UseBasicParsing -Headers @{"Cache-Control"="no-cache"} | ConvertFrom-Json } catch { return }
+$PlusPath = ((split-path -parent (get-item $script:MyInvocation.MyCommand.Path).Directory)+"\BrainPlus\ahashpoolplus\ahashpoolplus.json")
+Try{
+	$ahashpool_Request = get-content $PlusPath | ConvertFrom-Json } catch { return }
 
 if (-not $ahashpool_Request) {return}
 
@@ -19,10 +20,16 @@ $ahashpool_Request | Get-Member -MemberType NoteProperty | Select-Object -Expand
 	
         switch ($ahashpool_Algorithm) {
 
-        "equihash"{$Divisor /= 1000}
+        "sha256"{$Divisor *= 1000000}
+        "sha256t"{$Divisor *= 1000000}
+        "blake"{$Divisor *= 1000}
         "blake2s"{$Divisor *= 1000}
-        "blakecoin"{$Divisor *= 1000}
+	"blakecoin"{$Divisor *= 1000}
         "decred"{$Divisor *= 1000}
+        "vanilla"{$Divisor *= 1000}
+	"x11"{$Divisor *= 1000}
+	"equihash"{$Divisor /= 1000}
+        "yescrypt"{$Divisor /= 1000}
     }
 
     if ((Get-Stat -Name "$($Name)_$($ahashpool_Algorithm)_Profit") -eq $null) {$Stat = Set-Stat -Name "$($Name)_$($ahashpool_Algorithm)_Profit" -Value ([Double]$ahashpool_Request.$_.actual_last24h / $Divisor *(1-($ahashpool_Request.$_.fees/100)))}
@@ -39,7 +46,7 @@ $ahashpool_Request | Get-Member -MemberType NoteProperty | Select-Object -Expand
             Host          = $ahashpool_Host
             Port          = $ahashpool_Port
             User          = $Wallet
-            Pass          = "$WorkerName,c=$Passwordcurrency"
+            Pass          = "$WorkerName,c=BTC"
             Location      = $Location
             SSL           = $false
         }
