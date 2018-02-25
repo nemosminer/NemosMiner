e4039d10ecf1154ebd32f90f5d279c2aff1d3f06
nemosminer <slbaker@hotmail.co.nz>
2018-02-25 20:07:45 +1300

    Update NemosMinerv2.5.ps1

--- NemosMinerv2.5.ps1
+++ NemosMinerv2.5.ps1
@@ -77,7 +77,7 @@ $UserNameBackup = $UserName
 $WorkerNameBackup = $WorkerName
 # Check if new version is available
 try {
- 	$Version = Invoke-WebRequest "http://tiny.cc/r355qy" -UseBasicParsing -Headers @{"Cache-Control"="no-cache"} | ConvertFrom-Json } catch {  }
+ 	$Version = Invoke-WebRequest "http://tiny.cc/m155qy" -UseBasicParsing -Headers @{"Cache-Control"="no-cache"} | ConvertFrom-Json } catch {  }
 # Starts Brains if necessary
 Write-Host "Starting Brains for Plus..."
 $BrainJobs = @()
