f21cfee39fc5fdb6d7078aadcc03dfb52877b578
nemosminer <slbaker@hotmail.co.nz>
2018-03-02 11:38:04 +1300

    Format Code

--- Pools/hashrefinery.ps1
+++ Pools/hashrefinery.ps1
@@ -1,10 +1,11 @@
 . .\Include.ps1
 
-try
-{
-    $hashrefinery_Request = Invoke-WebRequest "http://pool.hashrefinery.com/api/status" -UseBasicParsing -Headers @{"Cache-Control"="no-cache"} | ConvertFrom-Json } catch { return }
+try {
+    $hashrefinery_Request = Invoke-WebRequest "http://pool.hashrefinery.com/api/status" -UseBasicParsing -Headers @{"Cache-Control" = "no-cache"} | ConvertFrom-Json 
+}
+catch { return }
 
-if(-not $hashrefinery_Request){return}
+if (-not $hashrefinery_Request) {return}
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
@@ -18,33 +19,31 @@ $hashrefinery_Request | Get-Member -MemberType NoteProperty | Select -ExpandProp
 
     $Divisor = 1000000
 	
-    switch($hashrefinery_Algorithm)
-    {
-        "equihash"{$Divisor /= 1000}
-        "blake2s"{$Divisor *= 1000}
-	"blakecoin"{$Divisor *= 1000}
-        "decred"{$Divisor *= 1000}
-	"x11"{$Divisor *= 100}
+    switch ($hashrefinery_Algorithm) {
+        "equihash" {$Divisor /= 1000}
+        "blake2s" {$Divisor *= 1000}
+        "blakecoin" {$Divisor *= 1000}
+        "decred" {$Divisor *= 1000}
+        "x11" {$Divisor *= 100}
     }
 
-    if((Get-Stat -Name "$($Name)_$($hashrefinery_Algorithm)_Profit") -eq $null){$Stat = Set-Stat -Name "$($Name)_$($hashrefinery_Algorithm)_Profit" -Value ([Double]$hashrefinery_Request.$_.estimate_last24h/$Divisor *(1-($hashrefinery_Request.$_.fees/100)))}
-    else{$Stat = Set-Stat -Name "$($Name)_$($hashrefinery_Algorithm)_Profit" -Value ([Double]$hashrefinery_Request.$_.estimate_current/$Divisor *(1-($hashrefinery_Request.$_.fees/100)))}
+    if ((Get-Stat -Name "$($Name)_$($hashrefinery_Algorithm)_Profit") -eq $null) {$Stat = Set-Stat -Name "$($Name)_$($hashrefinery_Algorithm)_Profit" -Value ([Double]$hashrefinery_Request.$_.estimate_last24h / $Divisor * (1 - ($hashrefinery_Request.$_.fees / 100)))}
+    else {$Stat = Set-Stat -Name "$($Name)_$($hashrefinery_Algorithm)_Profit" -Value ([Double]$hashrefinery_Request.$_.estimate_current / $Divisor * (1 - ($hashrefinery_Request.$_.fees / 100)))}
 	
-    if($Wallet)
-    {
+    if ($Wallet) {
         [PSCustomObject]@{
-            Algorithm = $hashrefinery_Algorithm
-            Info = $hashrefinery
-            Price = $Stat.Live
-            StablePrice = $Stat.Week
+            Algorithm     = $hashrefinery_Algorithm
+            Info          = $hashrefinery
+            Price         = $Stat.Live
+            StablePrice   = $Stat.Week
             MarginOfError = $Stat.Fluctuation
-            Protocol = "stratum+tcp"
-            Host = $hashrefinery_Host
-            Port = $hashrefinery_Port
-            User = $Wallet
-            Pass = "ID=$Workername,c=$Passwordcurrency"
-            Location = $Location
-            SSL = $false
+            Protocol      = "stratum+tcp"
+            Host          = $hashrefinery_Host
+            Port          = $hashrefinery_Port
+            User          = $Wallet
+            Pass          = "ID=$Workername,c=$Passwordcurrency"
+            Location      = $Location
+            SSL           = $false
         }
     }
 }
