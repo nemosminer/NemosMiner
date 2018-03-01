c4febae6ac64162fc5444aec1b95c54fbeca5ca1
nemosminer <slbaker@hotmail.co.nz>
2018-03-01 20:26:18 +1300

    disabling the "quickedit" property
     disabling the "quickedit" property for the miner CMD window
    https://github.com/nemosminer/NemosMiner-v2.5.1/issues/329

--- NemosMinerv2.5.1.ps1
+++ NemosMinerv2.5.1.ps1
@@ -50,6 +50,78 @@ param(
     [Parameter(Mandatory=$false)]
     [Bool]$TrackEarnings = $True # Display earnings information
 )
+$QuickEditCodeSnippet=@" 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+
+public static class DisableConsoleQuickEdit
+{
+
+const uint ENABLE_QUICK_EDIT = 0x0040;
+
+// STD_INPUT_HANDLE (DWORD): -10 is the standard input device.
+const int STD_INPUT_HANDLE = -10;
+
+[DllImport("kernel32.dll", SetLastError = true)]
+static extern IntPtr GetStdHandle(int nStdHandle);
+
+[DllImport("kernel32.dll")]
+static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+
+[DllImport("kernel32.dll")]
+static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
+
+public static bool SetQuickEdit(bool SetEnabled)
+{
+
+    IntPtr consoleHandle = GetStdHandle(STD_INPUT_HANDLE);
+
+    // get current console mode
+    uint consoleMode;
+    if (!GetConsoleMode(consoleHandle, out consoleMode))
+    {
+        // ERROR: Unable to get console mode.
+        return false;
+    }
+
+    // Clear the quick edit bit in the mode flags
+    if (SetEnabled)
+    {
+        consoleMode &= ~ENABLE_QUICK_EDIT;
+    }
+    else
+    {
+        consoleMode |= ENABLE_QUICK_EDIT;
+    }
+
+    // set the new mode
+    if (!SetConsoleMode(consoleHandle, consoleMode))
+    {
+        // ERROR: Unable to set console mode
+        return false;
+    }
+
+    return true;
+}
+}
+
+"@
+
+$QuickEditMode=add-type -TypeDefinition $QuickEditCodeSnippet -Language CSharp
+
+
+function Set-QuickEdit() 
+{
+[CmdletBinding()]
+param(
+[Parameter(Mandatory=$false, HelpMessage="This switch will disable Console QuickEdit option")]
+    [switch]$DisableQuickEdit=$false
+)}
 $CurrentProduct = "NemosMiner"
 $CurrentVersion = [Version]"2.5.1"
 $ScriptStartDate = Get-Date
@@ -524,7 +596,7 @@ while($true)
                 {
                     for($i = 0; $i -lt $_.Algorithms.Count; $i++)
                     {
-                        $Stat = Set-Stat -Name "$($_.Name)_$($_.Algorithms | Select -Index $i)_HashRate" -Value ($Miner_HashRates | Select -Index $i)
+                        $Stat = Set-Stat -Name "$($_.Name)_$($_.Algorithms | Select-Object -Index $i)_HashRate" -Value ($Miner_HashRates | Select -Index $i)
                     }
                     $_.New = $false
                     $_.Hashrate_Gathered = $true
@@ -538,9 +610,9 @@ while($true)
 #        {
 #            for($i = 0; $i -lt $_.Algorithms.Count; $i++)
 #            {
-#                if((Get-Stat "$($_.Name)_$($_.Algorithms | Select -Index $i)_HashRate") -eq $null)
+#                if((Get-Stat "$($_.Name)_$($_.Algorithms | Select-Object -Index $i)_HashRate") -eq $null)
 #                {
-#                    $Stat = Set-Stat -Name "$($_.Name)_$($_.Algorithms | Select -Index $i)_HashRate" -Value 0
+#                    $Stat = Set-Stat -Name "$($_.Name)_$($_.Algorithms | Select-Object -Index $i)_HashRate" -Value 0
 #                }
 #            }
 #        }
