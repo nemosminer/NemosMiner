348efb5207e5876bb29e0d43a9a4a89fa6b26e25
nemosminer <slbaker@hotmail.co.nz>
2018-03-02 11:40:21 +1300

    Format Code

--- Pools/blazepool.ps1
+++ Pools/blazepool.ps1
@@ -1,7 +1,9 @@
 . .\Include.ps1
 
 try {
-    $blazepool_Request = Invoke-WebRequest "http://api.blazepool.com/status" -UseBasicParsing -Headers @{"Cache-Control"="no-cache"} | ConvertFrom-Json } catch { return }
+    $blazepool_Request = Invoke-WebRequest "http://api.blazepool.com/status" -UseBasicParsing -Headers @{"Cache-Control" = "no-cache"} | ConvertFrom-Json 
+}
+catch { return }
 
 if (-not $blazepool_Request) {return}
 
@@ -18,23 +20,23 @@ $blazepool_Request | Get-Member -MemberType NoteProperty | Select-Object -Expand
     $Divisor = 1000000
 	
     switch ($blazepool_Algorithm) {
-		"blake"{$Divisor *= 1000}
-		"blake2s"{$Divisor *= 1000}
-		"blakecoin"{$Divisor *= 1000}
-		"decred"{$Divisor *= 1000}
-		"keccak"{$Divisor *= 1000}
-		"keccakc"{$Divisor *= 1000}
-		"quark"{$Divisor *= 1000}
-		"qubit"{$Divisor *= 1000}
-		"vanilla"{$Divisor *= 1000}
-		"scrypt"{$Divisor *= 1000}
-		"x11"{$Divisor *= 1000}
-		"equihash"{$Divisor /= 1000}
-		"yescrypt"{$Divisor /= 1000}
-	}
+        "blake" {$Divisor *= 1000}
+        "blake2s" {$Divisor *= 1000}
+        "blakecoin" {$Divisor *= 1000}
+        "decred" {$Divisor *= 1000}
+        "keccak" {$Divisor *= 1000}
+        "keccakc" {$Divisor *= 1000}
+        "quark" {$Divisor *= 1000}
+        "qubit" {$Divisor *= 1000}
+        "vanilla" {$Divisor *= 1000}
+        "scrypt" {$Divisor *= 1000}
+        "x11" {$Divisor *= 1000}
+        "equihash" {$Divisor /= 1000}
+        "yescrypt" {$Divisor /= 1000}
+    }
 
-    if ((Get-Stat -Name "$($Name)_$($blazepool_Algorithm)_Profit") -eq $null) {$Stat = Set-Stat -Name "$($Name)_$($blazepool_Algorithm)_Profit" -Value ([Double]$blazepool_Request.$_.estimate_last24h / $Divisor *(1-($blazepool_Request.$_.fees/100)))}
-    else {$Stat = Set-Stat -Name "$($Name)_$($blazepool_Algorithm)_Profit" -Value ([Double]$blazepool_Request.$_.estimate_current / $Divisor *(1-($blazepool_Request.$_.fees/100)))}
+    if ((Get-Stat -Name "$($Name)_$($blazepool_Algorithm)_Profit") -eq $null) {$Stat = Set-Stat -Name "$($Name)_$($blazepool_Algorithm)_Profit" -Value ([Double]$blazepool_Request.$_.estimate_last24h / $Divisor * (1 - ($blazepool_Request.$_.fees / 100)))}
+    else {$Stat = Set-Stat -Name "$($Name)_$($blazepool_Algorithm)_Profit" -Value ([Double]$blazepool_Request.$_.estimate_current / $Divisor * (1 - ($blazepool_Request.$_.fees / 100)))}
 	
     if ($Wallet) {
         [PSCustomObject]@{
