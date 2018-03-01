bd1d22080acb11e2568b745bc742830bc3d7be82
nemosminer <slbaker@hotmail.co.nz>
2018-03-02 11:47:24 +1300

    Format Code

--- Miners/XMRig.ps1
+++ Miners/XMRig.ps1
@@ -5,17 +5,17 @@ $Uri = "https://github.com/xmrig/xmrig-nvidia/releases/download/v2.4.5/xmrig-nvi
 
 $Commands = [PSCustomObject]@{
     "cryptonight" = " --cuda-devices $SelGPUCC --cuda-launch=8x120" #Cryptonight
-	"cryptonight-lite" = " --cuda-devices $SelGPUCC --cuda-launch=8x120" #Cryptonight-lite
+    "cryptonight-lite" = " --cuda-devices $SelGPUCC --cuda-launch=8x120" #Cryptonight-lite
 }
 $Port = 2222
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
-$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
-   [PSCustomObject]@{
-		Type = "NVIDIA"
-		Path = $Path
-		Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) --api-port $port --donate-level 1"
-        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Live *.99} # substract 1% devfee
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) --api-port $port --donate-level 1"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Live * .99} # substract 1% devfee
         API = "XMRig"
         Port = $Port
         Wrap = $false
