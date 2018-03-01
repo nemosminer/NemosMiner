e29e44a8431bb78c39d744de930f9b0754621a33
nemosminer <slbaker@hotmail.co.nz>
2018-03-02 11:33:41 +1300

    Format Code

--- Pools/zergpool.ps1
+++ Pools/zergpool.ps1
@@ -1,10 +1,11 @@
 . .\Include.ps1
 
-try
-{
-    $zergpool_Request = Invoke-WebRequest "http://api.zergpool.com:8080/api/status" -UseBasicParsing -Headers @{"Cache-Control"="no-cache"} | ConvertFrom-Json } catch { return }
+try {
+    $zergpool_Request = Invoke-WebRequest "http://api.zergpool.com:8080/api/status" -UseBasicParsing -Headers @{"Cache-Control" = "no-cache"} | ConvertFrom-Json 
+}
+catch { return }
 
-if(-not $zergpool_Request){return}
+if (-not $zergpool_Request) {return}
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
@@ -21,29 +22,28 @@ $zergpool_Request | Get-Member -MemberType NoteProperty | Select -ExpandProperty
     switch ($zergpool_Algorithm) {
         "equihash" {$Divisor /= 1000}
         "blake2s" {$Divisor *= 1000}
-	"blakecoin" {$Divisor *= 1000}
+        "blakecoin" {$Divisor *= 1000}
         "decred" {$Divisor *= 1000}
-	"keccak" {$Divisor *= 1000}
+        "keccak" {$Divisor *= 1000}
     }
 
-    if((Get-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit") -eq $null){$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.estimate_last24h/$Divisor)}
-    else{$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.estimate_current/$Divisor)}
+    if ((Get-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit") -eq $null) {$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.estimate_last24h / $Divisor)}
+    else {$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.estimate_current / $Divisor)}
 	
-    if($Wallet)
-    {
+    if ($Wallet) {
         [PSCustomObject]@{
-            Algorithm = $zergpool_Algorithm
-            Info = $zergpool
-            Price = $Stat.Live
-            StablePrice = $Stat.Week
+            Algorithm     = $zergpool_Algorithm
+            Info          = $zergpool
+            Price         = $Stat.Live
+            StablePrice   = $Stat.Week
             MarginOfError = $Stat.Fluctuation
-            Protocol = "stratum+tcp"
-            Host = $zergpool_Host
-            Port = $zergpool_Port
-            User = $Wallet
-            Pass = "$WorkerName,c=$Passwordcurrency"
-            Location = $Location
-            SSL = $false
+            Protocol      = "stratum+tcp"
+            Host          = $zergpool_Host
+            Port          = $zergpool_Port
+            User          = $Wallet
+            Pass          = "$WorkerName,c=$Passwordcurrency"
+            Location      = $Location
+            SSL           = $false
         }
     }
 }
