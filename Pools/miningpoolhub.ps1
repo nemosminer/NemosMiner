519573efb2fc197aa935ccf7153671ba227f0ef7
nemosminer <slbaker@hotmail.co.nz>
2018-03-02 11:35:53 +1300

    Format Code

--- Pools/miningpoolhub.ps1
+++ Pools/miningpoolhub.ps1
@@ -1,9 +1,10 @@
 . .\Include.ps1
 
-try { $MiningPoolHub_Request = Invoke-WebRequest "https://miningpoolhub.com/index.php?page=api&action=getautoswitchingandprofitsstatistics" -UseBasicParsing -Headers @{"Cache-Control"="no-cache"} | ConvertFrom-Json } catch { return }
+try { $MiningPoolHub_Request = Invoke-WebRequest "https://miningpoolhub.com/index.php?page=api&action=getautoswitchingandprofitsstatistics" -UseBasicParsing -Headers @{"Cache-Control" = "no-cache"} | ConvertFrom-Json 
+}
+catch { return }
 
-if(-not $MiningPoolHub_Request.success)
-{
+if (-not $MiningPoolHub_Request.success) {
     return
 }
 
@@ -11,42 +12,42 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Locations = 'Europe', 'US', 'Asia'
 
-$Locations | ForEach {
+$Locations | ForEach-Object {
     $Location = $_
 
-    $MiningPoolHub_Request.return | ForEach {
+    $MiningPoolHub_Request.return | ForEach-Object {
         $Algorithm = $_.algo -replace "-"
         $Coin = (Get-Culture).TextInfo.ToTitleCase(($_.current_mining_coin -replace "-", " ")) -replace " "
 
-        $Stat = Set-Stat -Name "$($Name)_$($Algorithm)_Profit" -Value ([decimal]$_.profit/1000000000)
-        $Price = (($Stat.Live*(1-[Math]::Min($Stat.Day_Fluctuation,1)))+($Stat.Day*(0+[Math]::Min($Stat.Day_Fluctuation,1))))
+        $Stat = Set-Stat -Name "$($Name)_$($Algorithm)_Profit" -Value ([decimal]$_.profit / 1000000000)
+        $Price = (($Stat.Live * (1 - [Math]::Min($Stat.Day_Fluctuation, 1))) + ($Stat.Day * (0 + [Math]::Min($Stat.Day_Fluctuation, 1))))
         
         [PSCustomObject]@{
-            Algorithm = $Algorithm
-            Info = $Coin
-            Price = $Price
+            Algorithm   = $Algorithm
+            Info        = $Coin
+            Price       = $Price
             StablePrice = $Stat.Week
-            Protocol = 'stratum+tcp'
-            Host = $_.all_host_list.split(";") | Sort -Descending {$_ -ilike "$Location*"} | Select -First 1
-            Port = $_.algo_switch_port
-            User = '$UserName.$WorkerName'
-            Pass = 'x'
-            Location = $Location
-            SSL = $false
+            Protocol    = 'stratum+tcp'
+            Host        = $_.all_host_list.split(";") | Sort-Object -Descending {$_ -ilike "$Location*"} | Select-Object -First 1
+            Port        = $_.algo_switch_port
+            User        = '$UserName.$WorkerName'
+            Pass        = 'x'
+            Location    = $Location
+            SSL         = $false
         }
         
         [PSCustomObject]@{
-            Algorithm = $Algorithm
-            Info = $Coin
-            Price = $Price
+            Algorithm   = $Algorithm
+            Info        = $Coin
+            Price       = $Price
             StablePrice = $Stat.Week
-            Protocol = 'stratum+ssl'
-            Host = $_.all_host_list.split(";") | Sort -Descending {$_ -ilike "$Location*"} | Select -First 1
-            Port = $_.algo_switch_port
-            User = '$UserName.$WorkerName'
-            Pass = 'x'
-            Location = $Location
-            SSL = $true
+            Protocol    = 'stratum+ssl'
+            Host        = $_.all_host_list.split(";") | Sort-Object -Descending {$_ -ilike "$Location*"} | Select-Object -First 1
+            Port        = $_.algo_switch_port
+            User        = '$UserName.$WorkerName'
+            Pass        = 'x'
+            Location    = $Location
+            SSL         = $true
         }
     }
 }
