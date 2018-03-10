1668211f094c16889f7f7c9b2f19fe070b973ab7
nemosminer <slbaker@hotmail.co.nz>
2018-03-11 09:42:43 +1300

    improve zergpool eu support

--- Pools/zergpool24hr.ps1
+++ Pools/zergpool24hr.ps1
@@ -9,42 +9,46 @@ if (-not $zergpool_Request) {return}
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
-$Location = "US"
+$Locations = "US", "Europe"
 
-$zergpool_Request | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | foreach {
-    $zergpool_Host = "mine.zergpool.com"
-    $zergpool_Port = $zergpool_Request.$_.port
-    $zergpool_Algorithm = Get-Algorithm $zergpool_Request.$_.name
-    $zergpool_Coin = ""
+$Locations | ForEach {
+    $Location = $_
 
-    $Divisor = 1000000000
-	
-    switch ($zergpool_Algorithm) {
-        "equihash" {$Divisor /= 1000}
-        "blake2s" {$Divisor *= 1000}
-        "blakecoin" {$Divisor *= 1000}
-        "decred" {$Divisor *= 1000}
-        "keccak" {$Divisor *= 1000}
-	"keccakc" {$Divisor *= 1000}
-    }
+	$zergpool_Request | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | foreach {
+	    $zergpool_Host = If ($Location -eq "Europe"){$Location + ".mine.zergpool.com"}else{"mine.zergpool.com"}
+	    $zergpool_Port = $zergpool_Request.$_.port
+	    $zergpool_Algorithm = Get-Algorithm $zergpool_Request.$_.name
+	    $zergpool_Coin = ""
 
-    if ((Get-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit") -eq $null) {$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.actual_last24h / $Divisor)}
-    else {$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.actual_last24h / $Divisor)}
-	
-    if ($Wallet) {
-        [PSCustomObject]@{
-            Algorithm     = $zergpool_Algorithm
-            Info          = $zergpool
-            Price         = $Stat.Live
-            StablePrice   = $Stat.Week
-            MarginOfError = $Stat.Fluctuation
-            Protocol      = "stratum+tcp"
-            Host          = $zergpool_Host
-            Port          = $zergpool_Port
-            User          = $Wallet
-            Pass          = "$WorkerName,c=$Passwordcurrency"
-            Location      = $Location
-            SSL           = $false
-        }
-    }
+	    $Divisor = 1000000000
+
+	    switch ($zergpool_Algorithm) {
+		"equihash" {$Divisor /= 1000}
+		"blake2s" {$Divisor *= 1000}
+		"blakecoin" {$Divisor *= 1000}
+		"decred" {$Divisor *= 1000}
+		"keccak" {$Divisor *= 1000}
+		"keccakc" {$Divisor *= 1000}
+	    }
+
+	    if ((Get-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit") -eq $null) {$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.actual_last24h / $Divisor)}
+	    else {$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.actual_last24h / $Divisor)}
+
+	    if ($Wallet) {
+		[PSCustomObject]@{
+		    Algorithm     = $zergpool_Algorithm
+		    Info          = $zergpool
+		    Price         = $Stat.Live
+		    StablePrice   = $Stat.Week
+		    MarginOfError = $Stat.Fluctuation
+		    Protocol      = "stratum+tcp"
+		    Host          = $zergpool_Host
+		    Port          = $zergpool_Port
+		    User          = $Wallet
+		    Pass          = "$WorkerName,c=$Passwordcurrency"
+		    Location      = $Location
+		    SSL           = $false
+		}
+	    }
+	}
 }
