1e00e7f3e8a02fb1abdeb6e2d87a0cdf05e5e8ea
nemosminer <slbaker@hotmail.co.nz>
2018-03-16 08:43:22 +1300

    Update ccminerAlexis78.ps1

--- Miners/ccminerAlexis78.ps1
+++ Miners/ccminerAlexis78.ps1
@@ -32,7 +32,7 @@ $Commands = [PSCustomObject]@{
     #"timetravel" = "" #Timetravel
     "c11" = " -i 21 -d $SelGPUCC --api-remote" #C11
     #"x11evo" = "" #X11evo
-    "x11gost" = " -i 21 -d $SelGPUCC --api-remote" #X11gost
+    #"x11gost" = " -i 21 -d $SelGPUCC --api-remote" #X11gost
     "x17" = " -i 20 -d $SelGPUCC --api-remote" #X17
     #"yescrypt" = "" #Yescrypt
 }
