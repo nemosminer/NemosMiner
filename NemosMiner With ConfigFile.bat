8baa48fb94314ad4714be8fe5186c2699c2ce4d8
nemosminer <slbaker@hotmail.co.nz>
2018-03-15 14:23:11 +1300

    Add files via upload

---
+++ NemosMiner With ConfigFile.bat
@@ -0,0 +1,1 @@
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -version 5.0 -executionpolicy bypass -windowstyle normal -command ".\NemosMiner-v3.0.ps1 -ConfigFile '.\Config\config.json'"
