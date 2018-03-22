98575b6e8247065c31c731e3adcccc114421077d
nemosminer <slbaker@hotmail.co.nz>
2018-03-23 09:39:06 +1300

    Update Brain-2.1.ps1

--- BrainPlus/zergpoolplus/Brain-2.1.ps1
+++ BrainPlus/zergpoolplus/Brain-2.1.ps1
@@ -1,7 +1,6 @@
 <#
 This file is part of NemosMiner
 Copyright (c) 2018 MrPlus
-Copyright (c) 2018 Nemo
 
 NemosMiner is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
