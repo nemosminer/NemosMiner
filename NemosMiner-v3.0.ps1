5d201ee1328b9346abfb67985f4dc7ad05f6168d
nemosminer <slbaker@hotmail.co.nz>
2018-03-23 09:09:42 +1300

    Update NemosMiner-v3.0.ps1

--- NemosMiner-v3.0.ps1
+++ NemosMiner-v3.0.ps1
@@ -1,3 +1,22 @@
+<#
+This file is part of NemossMiner
+Copyright (c) 2018 Nemo
+Copyright (c) 2018 MrPlus
+
+NemosMiner is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+NemosMiner is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#>
+
 param(
     [Parameter(Mandatory = $false)]
     [String]$Wallet = "1QGADhdMRpp9Pk5u5zG1TrHKRrdK5R81TE", 
