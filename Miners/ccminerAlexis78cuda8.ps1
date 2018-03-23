01382dc1807afe8a95cf270a8bbaabbf75271dad
nemosminer <slbaker@hotmail.co.nz>
2018-03-23 20:35:20 +1300

    Update ccminerAlexis78cuda8.ps1

--- Miners/ccminerAlexis78cuda8.ps1
+++ Miners/ccminerAlexis78cuda8.ps1
@@ -43,7 +43,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
         Type = "NVIDIA"
         Path = $Path
         Arguments = " -b $($Variables.MinerAPITCPPort) -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Live}
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Hour}
         API = "Ccminer"
         Port = $Variables.MinerAPITCPPort
         Wrap = $false
