c911424667a82bb9099fa13e34a0391dc4b17e62
nemosminer <slbaker@hotmail.co.nz>
2018-04-11 11:36:22 +1200

    Update ccminerCryptonight.ps1

--- Miners/ccminerCryptonight.ps1
+++ Miners/ccminerCryptonight.ps1
@@ -50,7 +50,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
         Arguments = " -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
         API = "Wrapper"
-        Port = $Variables.MinerAPITCPPort
+        Port = 4068
         Wrap = $true
         URI = $Uri
         User = $Pools.(Get-Algorithm($_)).User
