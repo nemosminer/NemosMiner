d8cc0d774eaf0abe56d98e853634a5d0e7986e8c
nemosminer <slbaker@hotmail.co.nz>
2018-04-11 12:54:08 +1200

    Update Wrapper.ps1

--- Wrapper.ps1
+++ Wrapper.ps1
@@ -37,24 +37,13 @@ do {
             $HashRate = [Decimal]$Words[$Words.IndexOf(($Words -like "*/s" | Select-Object -Last 1)) - 1]
 
             switch ($Words[$Words.IndexOf(($Words -like "*/s" | Select-Object -Last 1))]) {
+	         "h/s" {$HashRate *= [Math]::Pow(1000, 0)}
                 "kh/s" {$HashRate *= [Math]::Pow(1000, 1)}
                 "mh/s" {$HashRate *= [Math]::Pow(1000, 2)}
                 "gh/s" {$HashRate *= [Math]::Pow(1000, 3)}
                 "th/s" {$HashRate *= [Math]::Pow(1000, 4)}
                 "ph/s" {$HashRate *= [Math]::Pow(1000, 5)}
             }
-
-            $HashRate | Set-Content ".\cryptonightV7Hashrate.txt"
-        } elseif ($Line -like "*total speed:*" -or $Line -like "*accepted:*") {
-            $Words = $Line -split " "
-            $HashRate = [Decimal]($Words -like "*H/s*" -replace ',', '' -replace "[^0-9.]",'' | Select-Object -Last 1)
-
-            switch ($Words -like "*H/s*" -replace "[0-9.,]",'' | Select-Object -Last 1) {
-                "KH/s" {$HashRate *= [Math]::Pow(1000, 1)}
-                "mH/s" {$HashRate *= [Math]::Pow(1000, 2)}
-                "MH/s" {$HashRate *= [Math]::Pow(1000, 2)}
-            }
-			$HashRate = [int]$HashRate
             $HashRate | Set-Content ".\cryptonightV7Hashrate.txt"
         }
 
