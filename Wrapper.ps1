42fa16fd76f9494eb0623e7bebe2492621cd2e8e
nemosminer <slbaker@hotmail.co.nz>
2018-04-12 10:11:55 +1200

    Update Wrapper.ps1

--- Wrapper.ps1
+++ Wrapper.ps1
@@ -32,11 +32,12 @@ do {
     $PowerShell.Streams.Verbose.ReadAll() | ForEach-Object {
         $Line = $_
 
-        if ($Line -like "*total speed:*" -or $Line -like "*accepted:*") {
+        if ($Line -like "*accepted:*") {
             $Words = $Line -split " "
             $HashRate = [Decimal]$Words[$Words.IndexOf(($Words -like "*/s" | Select-Object -Last 1)) - 1]
 
             switch ($Words[$Words.IndexOf(($Words -like "*/s" | Select-Object -Last 1))]) {
+	         "h/s" {$HashRate *= [Math]::Pow(1000, 0)}
                 "kh/s" {$HashRate *= [Math]::Pow(1000, 1)}
                 "mh/s" {$HashRate *= [Math]::Pow(1000, 2)}
                 "gh/s" {$HashRate *= [Math]::Pow(1000, 3)}
@@ -45,11 +46,12 @@ do {
             }
 
             $HashRate | Set-Content ".\cryptonightV7Hashrate.txt"
-        } elseif ($Line -like "*overall speed is*") {
+        } elseif ($Line -like "*accepted:*") {
             $Words = $Line -split " "
             $HashRate = [Decimal]($Words -like "*H/s*" -replace ',', '' -replace "[^0-9.]",'' | Select-Object -Last 1)
 
             switch ($Words -like "*H/s*" -replace "[0-9.,]",'' | Select-Object -Last 1) {
+	         "H/s" {$HashRate *= [Math]::Pow(1000, 0)}
                 "KH/s" {$HashRate *= [Math]::Pow(1000, 1)}
                 "mH/s" {$HashRate *= [Math]::Pow(1000, 2)}
                 "MH/s" {$HashRate *= [Math]::Pow(1000, 2)}
