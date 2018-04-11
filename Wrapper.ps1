75a07f282321b7c9071e8ebaccaaf77bebf75076
nemosminer <slbaker@hotmail.co.nz>
2018-04-11 12:15:14 +1200

    Update Wrapper.ps1

--- Wrapper.ps1
+++ Wrapper.ps1
@@ -22,7 +22,7 @@ if ($ArgumentList -ne "") {$Command += " $ArgumentList"}
 $PowerShell.AddScript("$Command 2>&1 | Write-Verbose -Verbose") | Out-Null
 $Result = $PowerShell.BeginInvoke()
 
-Write-Host "NemosMinerv3.1 Wrapper Started" -BackgroundColor Yellow -ForegroundColor Black
+Write-Host "NemosMinerv2.4.2 Wrapper Started" -BackgroundColor Yellow -ForegroundColor Black
 
 do {
     Start-Sleep 1
@@ -33,6 +33,26 @@ do {
         if ($Line -like "*total speed:*" -or $Line -like "*accepted:*") {
             $Words = $Line -split " "
             $HashRate = [Decimal]$Words[$Words.IndexOf(($Words -like "*/s" | Select-Object -Last 1)) - 1]
+
+            switch ($Words[$Words.IndexOf(($Words -like "*/s" | Select-Object -Last 1))]) {
+                "kh/s" {$HashRate *= [Math]::Pow(1000, 1)}
+                "mh/s" {$HashRate *= [Math]::Pow(1000, 2)}
+                "gh/s" {$HashRate *= [Math]::Pow(1000, 3)}
+                "th/s" {$HashRate *= [Math]::Pow(1000, 4)}
+                "ph/s" {$HashRate *= [Math]::Pow(1000, 5)}
+            }
+
+            $HashRate | Set-Content ".\Wrapper_$Id.txt"
+        } elseif ($Line -like "*overall speed is*") {
+            $Words = $Line -split " "
+            $HashRate = [Decimal]($Words -like "*H/s*" -replace ',', '' -replace "[^0-9.]",'' | Select-Object -Last 1)
+
+            switch ($Words -like "*H/s*" -replace "[0-9.,]",'' | Select-Object -Last 1) {
+                "KH/s" {$HashRate *= [Math]::Pow(1000, 1)}
+                "mH/s" {$HashRate *= [Math]::Pow(1000, 2)}
+                "MH/s" {$HashRate *= [Math]::Pow(1000, 2)}
+            }
+			$HashRate = [int]$HashRate
             $HashRate | Set-Content ".\Wrapper_$Id.txt"
         }
 
