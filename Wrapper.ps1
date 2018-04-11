723e179a54cc12c8036c8d18d5500b2d19d05dc6
nemosminer <slbaker@hotmail.co.nz>
2018-04-12 10:21:37 +1200

    Update Wrapper.ps1

--- Wrapper.ps1
+++ Wrapper.ps1
@@ -24,7 +24,7 @@ if ($ArgumentList -ne "") {$Command += " $ArgumentList"}
 $PowerShell.AddScript("$Command 2>&1 | Write-Verbose -Verbose") | Out-Null
 $Result = $PowerShell.BeginInvoke()
 
-Write-Host "NemosMinerv3.1 Wrapper Started" -BackgroundColor Yellow -ForegroundColor Black
+Write-Host "NemosMiner-v3.1 Wrapper Started" -BackgroundColor Yellow -ForegroundColor Black
 
 do {
     Start-Sleep 1
@@ -32,12 +32,13 @@ do {
     $PowerShell.Streams.Verbose.ReadAll() | ForEach-Object {
         $Line = $_
 
-        if ($Line -like "*accepted:*") {
+        if ($Line -like "*total speed:*" -or $Line -like "*accepted:*") {
             $Words = $Line -split " "
             $HashRate = [Decimal]$Words[$Words.IndexOf(($Words -like "*/s" | Select-Object -Last 1)) - 1]
 
             switch ($Words[$Words.IndexOf(($Words -like "*/s" | Select-Object -Last 1))]) {
-	         "h/s" {$HashRate *= [Math]::Pow(1000, 0)}
+                 "h/s" {$HashRate *= [Math]::Pow(1000, 0)}
+                 "H/s" {$HashRate *= [Math]::Pow(1000, 0)}
                 "kh/s" {$HashRate *= [Math]::Pow(1000, 1)}
                 "mh/s" {$HashRate *= [Math]::Pow(1000, 2)}
                 "gh/s" {$HashRate *= [Math]::Pow(1000, 3)}
@@ -46,17 +47,19 @@ do {
             }
 
             $HashRate | Set-Content ".\cryptonightV7Hashrate.txt"
-        } elseif ($Line -like "*accepted:*") {
+        }
+        elseif ($Line -like "*total speed:*" -or $Line -like "*accepted:*") {
             $Words = $Line -split " "
-            $HashRate = [Decimal]($Words -like "*H/s*" -replace ',', '' -replace "[^0-9.]",'' | Select-Object -Last 1)
+            $HashRate = [Decimal]($Words -like "*H/s*" -replace ',', '' -replace "[^0-9.]", '' | Select-Object -Last 1)
 
-            switch ($Words -like "*H/s*" -replace "[0-9.,]",'' | Select-Object -Last 1) {
-	         "H/s" {$HashRate *= [Math]::Pow(1000, 0)}
+            switch ($Words -like "*H/s*" -replace "[0-9.,]", '' | Select-Object -Last 1) {
+                 "h/s" {$HashRate *= [Math]::Pow(1000, 0)}
+                 "H/s" {$HashRate *= [Math]::Pow(1000, 0)}
                 "KH/s" {$HashRate *= [Math]::Pow(1000, 1)}
                 "mH/s" {$HashRate *= [Math]::Pow(1000, 2)}
                 "MH/s" {$HashRate *= [Math]::Pow(1000, 2)}
             }
-			$HashRate = [int]$HashRate
+            $HashRate = [int]$HashRate
             $HashRate | Set-Content ".\cryptonightV7Hashrate.txt"
         }
 
