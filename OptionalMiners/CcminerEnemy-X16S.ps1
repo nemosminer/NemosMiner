f031efb67c5fcc9878e2b74e02362cfccf031feb
nemosminer <slbaker@hotmail.co.nz>
2018-04-21 08:13:14 +1200

    Create CcminerEnemy-X16S.ps1

---
+++ OptionalMiners/CcminerEnemy-X16S.ps1
@@ -0,0 +1,23 @@
+$Path = '.\Bin\NVIDIA-Enemy(X16S)\ccminer.exe'
+$Uri = 'http://kudaraidee.thaiddns.com:69/DechoKocharin/Miner/x16s.zip'
+
+$Commands = [PSCustomObject]@{
+    X16S = ' -i 20'
+       
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Live}
+        API = "Ccminer"
+        Port = 4068
+        Wrap = $false
+        URI = $Uri
+		User = $Pools.(Get-Algorithm($_)).User
+    }
+}
