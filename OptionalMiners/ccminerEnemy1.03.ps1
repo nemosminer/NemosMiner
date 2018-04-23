4f4dfd56e75cb05994b52120b2c56282740a9fb4
nemosminer <slbaker@hotmail.co.nz>
2018-04-24 09:25:54 +1200

    Update ccminerEnemy1.03.ps1

--- OptionalMiners/ccminerEnemy1.03.ps1
+++ OptionalMiners/ccminerEnemy1.03.ps1
@@ -36,7 +36,7 @@ $Commands = [PSCustomObject]@{
     #"veltor" = "" #Veltor
     #"x11evo" = " -d $SelGPUCC" #X11evo
     "x17" = " -d $SelGPUCC -N 1" #X17
-    "x16r" = " -d $SelGPUCC -N 3" #X16r
+    #"x16r" = " -d $SelGPUCC -N 3" #X16r
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
