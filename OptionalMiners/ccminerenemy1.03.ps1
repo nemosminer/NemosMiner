f320e9a33e919c1f1c9841c80b9d94629b33b0ca
nemosminer <slbaker@hotmail.co.nz>
2018-04-27 14:00:44 +1200

    Update ccminerenemy1.03.ps1

--- OptionalMiners/ccminerenemy1.03.ps1
+++ OptionalMiners/ccminerenemy1.03.ps1
@@ -35,7 +35,7 @@ $Commands = [PSCustomObject]@{
     #"x11" = "" #X11
     #"veltor" = "" #Veltor
     #"x11evo" = " -d $SelGPUCC" #X11evo
-    "x17" = " -d $SelGPUCC -N 1" #X17
+    "x17" = " -d $SelGPUCC -N 1" #X17(fastest)
     #"x16r" = " -d $SelGPUCC -N 3" #X16r
 }
 
