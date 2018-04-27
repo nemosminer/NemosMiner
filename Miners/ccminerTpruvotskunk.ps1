3aea95337aad540b7464c185f388f6c240398815
nemosminer <slbaker@hotmail.co.nz>
2018-04-27 14:54:48 +1200

    Update ccminerTpruvotskunk.ps1

--- Miners/ccminerTpruvotskunk.ps1
+++ Miners/ccminerTpruvotskunk.ps1
@@ -33,7 +33,7 @@ $Commands = [PSCustomObject]@{
     #"x11evo" = "" #X11evo
     #"x17" = "" #X17
     #"yescrypt" = "" #Yescrypt
-    "skunk" = " -d $SelGPUCC" #Skunk(tied with spmodskunk as fastest)
+    "skunk" = " -d $SelGPUCC" #Skunk(spmodskunk is fastesr)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
