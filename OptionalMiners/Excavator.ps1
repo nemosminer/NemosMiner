b743da193db72a857b0ae1949b6b0dc9a6e648a8
nemosminer <slbaker@hotmail.co.nz>
2018-04-27 19:16:12 +1200

    Update Excavator.ps1

--- OptionalMiners/Excavator.ps1
+++ OptionalMiners/Excavator.ps1
@@ -6,14 +6,14 @@ $Path = ".\Bin\Excavator\excavator.exe"
 $Uri = "https://github.com/nicehash/excavator/releases/download/v1.4.4a/excavator_v1.4.4a_NVIDIA_Win64.zip"
 
 $Commands = [PSCustomObject]@{
-    "blake2s" = @() #Blake2s
+    "blake2s" = @() #Blake2s(alexis78 faster)
     #"decred" = @() #Decred
-    "daggerhashimoto" = @() #Ethash
-    "equihash" = @() #Equihash
+    "daggerhashimoto" = @() #Ethash(claymore faster)
+    "equihash" = @() #Equihash(dstm faster)
     #"lbry" = @() #Lbry
-    "lyra2rev2" = @() #Lyra2RE2
-    "neoscrypt" = @() #NeoScrypt
-    "nist5" = @() #nist5
+    "lyra2rev2" = @() #Lyra2RE2(fastest tied with alexis78)
+    "neoscrypt" = @() #NeoScrypt(fastest)
+    "nist5" = @() #nist5(alexis78 faster)
     #"pascal" = @() #Pascal
 }
 
