418c533a6cd204529edfdde5cddacc6fbce52429
nemosminer <slbaker@hotmail.co.nz>
2018-04-27 13:40:46 +1200

    Update XMRig.ps1

--- Miners/XMRig.ps1
+++ Miners/XMRig.ps1
@@ -4,9 +4,9 @@ $Path = ".\Bin\NVIDIA-XMRig\xmrig-nvidia.exe"
 $Uri = "https://github.com/xmrig/xmrig-nvidia/releases/download/v2.6.0-beta1/xmrig-nvidia-2.6.0-beta1-cuda9-win64.zip"
 
 $Commands = [PSCustomObject]@{
-    #"cryptonight" = " --cuda-devices $SelGPUCC" #Cryptonight
-    #"cryptonight-lite" = " --cuda-devices $SelGPUCC" #Cryptonight-lite
-    #"cryptonightV7" = " --cuda-devices $SelGPUCC" #CryptonightV7
+    #"cryptonight" = " --cuda-devices $SelGPUCC" #Cryptonight(max 3 gpus)
+    #"cryptonight-lite" = " --cuda-devices $SelGPUCC" #Cryptonight-lite(max 3 gpus)
+    #"cryptonightV7" = " --cuda-devices $SelGPUCC" #CryptonightV7(max 3 gpus)
 }
 $Port = $Variables.MinerAPITCPPort #2222
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
