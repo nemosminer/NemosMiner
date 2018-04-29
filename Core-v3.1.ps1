19e170f01ac2ccd6ef9a3af2715d48ac847890ba
nemosminer <slbaker@hotmail.co.nz>
2018-04-30 11:17:39 +1200

    Update Core-v3.1.ps1

--- Core-v3.1.ps1
+++ Core-v3.1.ps1
@@ -44,7 +44,7 @@ Function InitApplication {
     #Randomly sets donation minutes per day between (0,(3..5)) minutes if set to less than 3
     $Variables | Add-Member -Force @{DonateRandom = [PSCustomObject]@{}}
     $Variables | Add-Member -Force @{LastDonated = (Get-Date).AddDays(-1).AddHours(1)}
-    If ($Config.Donate -lt 3) {$Config.Donate = (0, (3..5)) | Get-Random}
+    If ($Config.Donate -lt 3) {$Config.Donate = (0, (3..8)) | Get-Random}
     $Variables | Add-Member -Force @{WalletBackup = $Config.Wallet}
     $Variables | Add-Member -Force @{UserNameBackup = $Config.UserName}
     $Variables | Add-Member -Force @{WorkerNameBackup = $Config.WorkerName}
