c895ae332c32c21e78341f4eb111e3304d421d4c
nemosminer <slbaker@hotmail.co.nz>
2018-05-03 09:24:12 +1200

    coins info zerg

--- Pools/zergpool24hr.ps1
+++ Pools/zergpool24hr.ps1
@@ -9,6 +9,59 @@ catch { return }
 if (-not $zergpool_Request) {return}
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+. .\Include.ps1
+
+try {
+    $zergpool_Request = Invoke-WebRequest "http://api.zergpool.com:8080/api/status" -UseBasicParsing -Headers @{"Cache-Control" = "no-cache"} | ConvertFrom-Json 
+}
+catch { return }
+
+if (-not $zergpool_Request) {return}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Location = "US"
+
+$zergpool_Request | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | foreach {
+    $zergpool_Host = "$_.mine.zergpool.com"
+    $zergpool_Port = $zergpool_Request.$_.port
+    $zergpool_Algorithm = Get-Algorithm $zergpool_Request.$_.name
+    $zergpool_Coin = $zergpool_Request.$_.coins
+
+    $Divisor = 1000000000
+
+    switch ($zergpool_Algorithm) {
+        "equihash" {$Divisor /= 1000}
+        "blake2s" {$Divisor *= 1000}
+        "blakecoin" {$Divisor *= 1000}
+        "decred" {$Divisor *= 1000}
+        "keccak" {$Divisor *= 1000}
+        "keccakc" {$Divisor *= 1000}
+    }
+
+    if ((Get-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit") -eq $null) {$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.actual_last24h / $Divisor)}
+    else {$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.actual_last24h / $Divisor * (1 - ($zergpool_Request.$_.fees / 100)))}
+
+    $ConfName = if ($Config.PoolsConfig.$Name -ne $Null) {$Name}else {"default"}
+    $PwdCurr = if ($Config.PoolsConfig.$ConfName.PwdCurrency) {$Config.PoolsConfig.$ConfName.PwdCurrency}else {$Config.Passwordcurrency}
+
+    if ($Config.PoolsConfig.default.Wallet) {
+        [PSCustomObject]@{
+            Algorithm     = $zergpool_Algorithm
+            Info          = "$zergpool_Coin - Coin(s)"
+            Price         = $Stat.Live * $Config.PoolsConfig.$ConfName.PricePenaltyFactor
+            StablePrice   = $Stat.Week
+            MarginOfError = $Stat.Fluctuation
+            Protocol      = "stratum+tcp"
+            Host          = $zergpool_Host
+            Port          = $zergpool_Port
+            User          = $Config.PoolsConfig.$ConfName.Wallet
+            Pass          = "$($Config.PoolsConfig.$ConfName.WorkerName),c=$($PwdCurr)"
+            Location      = $Location
+            SSL           = $false
+        }
+    }
+}
 
 $Location = "US"
 
