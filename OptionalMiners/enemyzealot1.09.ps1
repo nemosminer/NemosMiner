ca040ace1001a461b639573f8467c69373f96939
nemosminer <slbaker@hotmail.co.nz>
2018-05-06 14:46:11 +1200

    disable

--- OptionalMiners/enemyzealot1.09.ps1
+++ OptionalMiners/enemyzealot1.09.ps1
@@ -6,10 +6,10 @@ $Uri = "http://nemos.dx.am/opt/nemos/enemyz1.09.7z"
 $Commands = [PSCustomObject]@{
     #"polytimos" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Polytimos
     #"hsr" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Hsr
-    "phi" = " -d $SelGPUCC -i 20" #Phi(sp-mod faster)
-    "bitcore" = " -d $SelGPUCC -i 20" #Bitcore(sp-mod faster)
-    "x16r" = " -d $SelGPUCC -N 180 -i 20" #X16r(sp-hash faster + no dev fee)
-    "x16s" = " -d $SelGPUCC -N 180 -i 20" #X16s(enemy1.08faster sp-hash close)
+    #"phi" = " -d $SelGPUCC -i 20" #Phi(sp-mod faster)
+    #"bitcore" = " -d $SelGPUCC -i 20" #Bitcore(sp-mod faster)
+    #"x16r" = " -d $SelGPUCC -N 180 -i 20" #X16r(sp-hash faster + no dev fee)
+    #"x16s" = " -d $SelGPUCC -N 180 -i 20" #X16s(enemy1.08faster sp-hash close)
     #"blake2s" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Blake2s
     #"blakecoin" = " -d $SelGPUCC" #Blakecoin
     #"vanilla" = "" #BlakeVanilla
@@ -39,7 +39,7 @@ $Commands = [PSCustomObject]@{
     #"c11" = " -d $SelGPUCC --api-remote --api-allow=0/0" #C11
     #"veltor" = "" #Veltor
     #"x11evo" = " -d $SelGPUCC" #X11evo
-    "x17" = " -d $SelGPUCC" #X17(enoemy1.03faster + no dev fee)
+    #"x17" = " -d $SelGPUCC" #X17(enemy1.03faster + no dev fee)
     #"yescrypt" = "" #Yescrypt
 }
 
