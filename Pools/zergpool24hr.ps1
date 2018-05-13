a7b28060545fb6a9a673152884db4273d8e6908f
nemosminer <slbaker@hotmail.co.nz>
2018-05-14 09:22:02 +1200

    add yescrypt & yescryptR16 support

--- Pools/zergpool24hr.ps1
+++ Pools/zergpool24hr.ps1
@@ -28,6 +28,8 @@ $zergpool_Request | Get-Member -MemberType NoteProperty | Select-Object -ExpandP
         "decred" {$Divisor *= 1000}
         "keccak" {$Divisor *= 1000}
         "keccakc" {$Divisor *= 1000}
+        "yescrypt"{$Divisor /= 1000}
+        "yescryptr16"{$Divisor /= 1000}
     }
 
     if ((Get-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit") -eq $null) {$Stat = Set-Stat -Name "$($Name)_$($zergpool_Algorithm)_Profit" -Value ([Double]$zergpool_Request.$_.actual_last24h / $Divisor)}
