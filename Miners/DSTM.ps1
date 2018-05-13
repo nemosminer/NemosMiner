a2b531154bf325274dcec096735a2e600e948a18
nemosminer <slbaker@hotmail.co.nz>
2018-05-13 19:57:49 +1200

    update dstm from v0.6 to v0.6.1

--- Miners/DSTM.ps1
+++ Miners/DSTM.ps1
@@ -1,7 +1,7 @@
 . .\Include.ps1
 
 $Path = ".\Bin\NVIDIA-DSTM\zm.exe"
-$Uri = "https://github.com/nemosminer/DSTM-equihash-miner/releases/download/DSTM-0.6/zm_0.6_win.zip"
+$Uri = "http://nemos.dx.am/opt/nemos/zm_0.6.1_win.7z"
 
 $Commands = [PSCustomObject]@{
     "equihash" = " -d $SelGPUDSTM" #Equihash(fastest)
