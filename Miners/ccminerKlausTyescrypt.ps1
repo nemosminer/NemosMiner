8da28a5f75fc93f6e66c8e1e92c4ba96dcff5688
nemosminer <slbaker@hotmail.co.nz>
2018-05-14 08:43:04 +1200

    add yescrypt & yescryptR16 support

---
+++ Miners/ccminerKlausTyescrypt.ps1
@@ -0,0 +1,56 @@
+. .\Include.ps1
+
+$Path = ".\Bin\NVIDIA-KlausTyescrypt\ccminer.exe"
+$Uri = "https://github.com/iwtym/iwtym-yescrypt/archive/master.zip"
+
+$Commands = [PSCustomObject]@{
+    #"bitcore" = "" #Bitcore
+    #"blake2s" = "" #Blake2s
+    #"blakecoin" = "" #Blakecoin
+    #"vanilla" = "" #BlakeVanilla
+    #"cryptonight" = "" #Cryptonight
+    #"decred" = "" #Decred
+    #"equihash" = "" #Equihash
+    #"ethash" = "" #Ethash
+    #"groestl" = " -d $SelGPUCC" #Groestl
+    #"hmq1725" = "" #hmq1725
+    #"keccak" = "" #Keccak
+    #"lbry" = "" #Lbry
+    #"lyra2v2" = " -d $SelGPUCC" #Lyra2RE2
+    #"lyra2z" = "" #Lyra2z
+    #"myr-gr" = " -d $SelGPUCC" #MyriadGroestl
+    #"neoscrypt" = " -b 4068 -d $SelGPUCC" #NeoScrypt
+    #"nist5" = " -d $SelGPUCC" #Nist5
+    #"pascal" = "" #Pascal
+    #"qubit" = "" #Qubit
+    #"scrypt" = "" #Scrypt
+    #"sia" = "" #Sia
+    #"sib" = "" #Sib
+    #"skein" = "" #Skein
+    #"timetravel" = "" #Timetravel
+    #"x11" = "" #X11
+    #"veltor" = "" #Veltor
+    #"x11evo" = "" #X11evo
+    #"x17" = "" #X17
+    "yescrypt" = "" #yescrypt
+    "yescryptR8" = ""
+    "yescryptR16" = "" #YescryptR16 #Yenten
+    "yescryptR16v2" = "" #PPNP
+    #"keccakc" = " -d $SelGPUCC" #Keccakc(fastest)
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-b $($Variables.MinerAPITCPPort) -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
+        API = "Ccminer"
+        Port = $Variables.MinerAPITCPPort
+        Wrap = $false
+        URI = $Uri
+		User = $Pools.(Get-Algorithm($_)).User
+    }
+}
