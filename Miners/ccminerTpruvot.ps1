b6f3c1a89cfa6d9a63d63c0941d73171b6079f6d
nemosminer <slbaker@hotmail.co.nz>
2018-05-21 10:09:29 +1200

    Update ccminerTpruvot.ps1

--- Miners/ccminerTpruvot.ps1
+++ Miners/ccminerTpruvot.ps1
@@ -6,9 +6,9 @@ $Uri = "https://github.com/tpruvot/ccminer/releases/download/2.2.5-tpruvot/ccmin
 $Commands = [PSCustomObject]@{
     #"polytimos" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Polytimos
     #"hsr" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Hsr
-    "phi" = " -N 1 -d $SelGPUCC" #Phi
-    "bitcore" = " -d $SelGPUCC" #Bitcore(spmodbitcore faster)
-    "jha" = " -d $SelGPUCC" #Jha
+    "phi" = " -r 0 -N 1 -d $SelGPUCC" #Phi
+    "bitcore" = " -r 0 -d $SelGPUCC" #Bitcore(spmodbitcore faster)
+    "jha" = " -r 0 -d $SelGPUCC" #Jha
     #"blake2s" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Blake2s
     #"blakecoin" = " -d $SelGPUCC" #Blakecoin
     #"vanilla" = "" #BlakeVanilla
@@ -17,24 +17,24 @@ $Commands = [PSCustomObject]@{
     #"equihash" = "" #Equihash
     #"ethash" = "" #Ethash
     #"groestl" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Groestl
-    "hmq1725" = " -d $SelGPUCC" #hmq1725
+    "hmq1725" = " -r 0 -d $SelGPUCC" #hmq1725
     #"keccakc" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Keccakc
     #"lbry" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Lbry
     #"lyra2v2" = " -N 1 -d $SelGPUCC --api-remote --api-allow=0/0" #Lyra2RE2
-    "lyra2z" = "  -d $SelGPUCC --submit-stale" #Lyra2z
+    "lyra2z" = "  -r 0 -d $SelGPUCC --submit-stale" #Lyra2z
     #"myr-gr" = "" #MyriadGroestl
     #"neoscrypt" = " -d $SelGPUCC" #NeoScrypt
     #"nist5" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Nist5
     #"pascal" = "" #Pascal
     #"qubit" = "" #Qubit
     #"scrypt" = "" #Scrypt
-    "sha256t" = " -d $SelGPUCC" #Sha256t
+    "sha256t" = " -r 0 -d $SelGPUCC" #Sha256t
     #"sia" = "" #Sia
     #"sib" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Sib
     #"skein" = "" #Skein
     #"skunk" = " -d $SelGPUCC" #Skunk
-    "timetravel" = " -d $SelGPUCC" #Timetravel
-    "tribus" = " -d $SelGPUCC" #Tribus
+    "timetravel" = " -r 0 -d $SelGPUCC" #Timetravel
+    "tribus" = " -r 0 -d $SelGPUCC" #Tribus
     #"c11" = " -d $SelGPUCC --api-remote --api-allow=0/0" #C11
     #"veltor" = "" #Veltor
     #"x11evo" = " -d $SelGPUCC" #X11evo (Alexis78 faster)
