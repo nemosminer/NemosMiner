91dd05c2021fc65c5958e6895128eb158ae44235
nemosminer <slbaker@hotmail.co.nz>
2018-05-21 10:13:33 +1200

    Update ccminerenemy1.03.ps1

--- OptionalMiners/ccminerenemy1.03.ps1
+++ OptionalMiners/ccminerenemy1.03.ps1
@@ -35,8 +35,8 @@ $Commands = [PSCustomObject]@{
     #"x11" = "" #X11
     #"veltor" = "" #Veltor
     #"x11evo" = " -d $SelGPUCC" #X11evo
-    "x17" = " -d $SelGPUCC -N 1" #X17(Alexis 78-v1.2 is faster)
-    #"x16r" = " -d $SelGPUCC -N 3" #X16r(poolparty faster)
+    "x17" = " -r 0 -i 19 -d $SelGPUCC -N 1" #X17(Alexis 78-v1.2 faster)
+    #"x16r" = " -d $SelGPUCC -N 3" #X16r(ccminerx16r faster)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
