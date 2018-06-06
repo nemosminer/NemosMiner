322375900e5565b6556a062769b4d7f2af54b071
nemosminer <slbaker@hotmail.co.nz>
2018-06-07 09:27:20 +1200

    Create ccminercool.ps1

---
+++ OptionalMiners/ccminercool.ps1
@@ -0,0 +1,24 @@
+. .\Include.ps1
+
+$Path = ".\Bin\NVIDIA-ccminercool\coolMiner-x64.exe"
+$Uri = "http://nemos.dx.am/opt/nemos/coolMiner-x64-v1-1.7z"
+
+$Commands = [PSCustomObject]@{
+    "lyra2z" = " -d $SelGPUCC -i 20 -r 0" #Lyra2z
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-b 4068 -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week * .99} # substract 1% devfee
+        API = "Ccminer"
+        Port = 4068
+        Wrap = $false
+        URI = $Uri
+        User = $Pools.(Get-Algorithm($_)).User
+    }
+}
