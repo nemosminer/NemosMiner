0849fd183d1228827dbcc88446d73e3685e6e93e
nemosminer <slbaker@hotmail.co.nz>
2018-06-08 09:25:51 +1200

    Delos vs the world results

--- OptionalMiners/DelosMiner.ps1
+++ OptionalMiners/DelosMiner.ps1
@@ -5,9 +5,9 @@ $Uri = "http://nemos.dx.am/opt/nemos/DelosMiner1.3.0-x86-cuda91.7z"
 
 $Commands = [PSCustomObject]@{
     #"polytimos" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Polytimos
-    "hsr" = " -d $SelGPUCC -r 0" #Hsr
-    "phi" = " -d $SelGPUCC -r 0" #Phi
-    "bitcore" = " -r 0 -d $SelGPUCC" #Bitcore
+    #"hsr" = " -d $SelGPUCC -r 0" #Hsr(Alexis78v1.2 faster)
+    "phi" = " -d $SelGPUCC -r 0" #Phi(fastest)
+    "bitcore" = " -r 0 -d $SelGPUCC" #Bitcore(fastest)
     #"jha" = " -r 0 -d $SelGPUCC" #Jha
     #"blake2s" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Blake2s
     #"blakecoin" = " -d $SelGPUCC" #Blakecoin
@@ -17,10 +17,10 @@ $Commands = [PSCustomObject]@{
     #"equihash" = "" #Equihash
     #"ethash" = "" #Ethash
     #"groestl" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Groestl
-    "hmq1725" = " -r 0 -d $SelGPUCC" #hmq1725
+    #"hmq1725" = " -r 0 -d $SelGPUCC" #hmq1725(ccminerx16sv0.5 faster)
     #"keccakc" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Keccakc
     #"lbry" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Lbry
-    "lyra2v2" = " -d $SelGPUCC -r 0" #Lyra2RE2
+    #"lyra2v2" = " -d $SelGPUCC -r 0" #Lyra2RE2(Alexis78v1.2 faster)
     #"lyra2z" = "  -r 0 -d $SelGPUCC --submit-stale" #Lyra2z
     #"myr-gr" = "" #MyriadGroestl
     #"neoscrypt" = " -d $SelGPUCC" #NeoScrypt
@@ -31,16 +31,16 @@ $Commands = [PSCustomObject]@{
     #"sha256t" = " -r 0 -d $SelGPUCC" #Sha256t
     #"sia" = "" #Sia
     #"sib" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Sib
-    "skein" = " -d $SelGPUCC -r 0" #Skein
-    "skunk" = " -d $SelGPUCC -r 0" #Skunk
+    #"skein" = " -d $SelGPUCC -r 0" #Skein(Alexi78v1.2 faster)
+    "skunk" = " -d $SelGPUCC -r 0" #Skunk(fastest)
     #"timetravel" = " -r 0 -d $SelGPUCC" #Timetravel
-    "tribus" = " -r 0 -d $SelGPUCC" #Tribus
-    "c11" = " -d $SelGPUCC -r 0" #C11
+    #"tribus" = " -r 0 -d $SelGPUCC" #Tribus(close but spmodtribusv2 is slighty faster + no dev fee)
+    #"c11" = " -d $SelGPUCC -r 0" #C11(Alexis78v1.2 faster)
     #"veltor" = "" #Veltor
     #"x11evo" = " -d $SelGPUCC" #X11evo
-    "x17" = " -d $SelGPUCC -r 0" #X17
-    "x16r" = " -d $SelGPUCC -r 0" #X16r
-    "x16s" = " -d $SelGPUCC -r 0" #X16s
+    "x17" = " -d $SelGPUCC -r 0" #X17(fastest)
+    #"x16r" = " -d $SelGPUCC -r 0" #X16r(ccminerx16r faster)
+    "x16s" = " -d $SelGPUCC -r 0" #X16s(fastest)
     #"yescrypt" = "" #Yescrypt
 }
 
