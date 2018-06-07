ced955d531c0193e60e645fe0938e9c163b0f4a3
nemosminer <slbaker@hotmail.co.nz>
2018-06-07 15:30:17 +1200

    divisor field
    Change other pools which implement the divisor field as well

--- Pools/ahashpoolplus.ps1
+++ Pools/ahashpoolplus.ps1
@@ -20,21 +20,7 @@ $ahashpool_Request | Get-Member -MemberType NoteProperty | Select-Object -Expand
     $ahashpool_Coin = $ahashpool_Request.$_.coins
     $ahashpool_Coinname = $ahashpoolCoins_Request.$_.name
 
-    $Divisor = 1000000000
-	
-    switch ($ahashpool_Algorithm) {
-
-        "sha256" {$Divisor *= 1000000}
-        "sha256t" {$Divisor *= 1000000}
-        "blake" {$Divisor *= 1000}
-        "blake2s" {$Divisor *= 1000}
-        "blakecoin" {$Divisor *= 1000}
-        "decred" {$Divisor *= 1000}
-        "vanilla" {$Divisor *= 1000}
-        "x11" {$Divisor *= 1000}
-        "equihash" {$Divisor /= 1000}
-        "yescrypt" {$Divisor /= 1000}
-    }
+    $Divisor = 1000000000 * [Double]$AHashPool_Request.$_.mbtc_mh_factor
 
     if ((Get-Stat -Name "$($Name)_$($ahashpool_Algorithm)_Profit") -eq $null) {$Stat = Set-Stat -Name "$($Name)_$($ahashpool_Algorithm)_Profit" -Value ([Double]$ahashpool_Request.$_.actual_last24h / $Divisor * (1 - ($ahashpool_Request.$_.fees / 100)))}
     else {$Stat = Set-Stat -Name "$($Name)_$($ahashpool_Algorithm)_Profit" -Value ([Double]$ahashpool_Request.$_.actual_last24h / $Divisor * (1 - ($ahashpool_Request.$_.fees / 100)))}
