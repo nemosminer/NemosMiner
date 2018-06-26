33c0f9f9c16f6c5ad68030df2688a39548d04673
nemosminer <slbaker@hotmail.co.nz>
2018-06-26 12:07:25 +1200

    update version

--- NemosMiner-v3.1.bat
+++
@@ -1,1 +0,0 @@
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -version 5.0 -executionpolicy bypass -windowstyle normal -command ".\NemosMiner-v3.1.ps1 -ConfigFile '.\Config\config.json'"
