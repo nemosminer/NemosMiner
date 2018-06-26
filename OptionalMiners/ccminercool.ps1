4828ea627c97e1881e228cf501e9468926989e3f
platinum4 <platinumstephen@gmail.com>
2018-06-26 01:20:12 -0500

    Quiet

--- OptionalMiners/ccminercool.ps1
+++ OptionalMiners/ccminercool.ps1
@@ -4,7 +4,7 @@ $Path = ".\Bin\NVIDIA-ccminercool\coolMiner-x64.exe"
 $Uri = "http://nemos.dx.am/opt/nemos/coolMiner-x64-v1-4.7z"
 
 $Commands = [PSCustomObject]@{
-    "lyra2z" = " -d $SelGPUCC --submit-stale -i 20.5" #Lyra2z(fastest)
+    "lyra2z" = " -i 20.5 -d $SelGPUCC" #Lyra2z(fastest)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -13,7 +13,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = "-b 4068 -R 1 -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        Arguments = "-b 4068 -R 1 -q --submit-stale -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week * .99} # substract 1% devfee
         API = "Ccminer"
         Port = 4068
