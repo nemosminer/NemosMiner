45f3d180fed9712bc1a8925b5048745a83a2cdc2
platinum4 <platinumstephen@gmail.com>
2018-07-01 22:14:17 -0500

    Add Skein, Skunk

--- OptionalMiners/CryptoDredge.ps1
+++ OptionalMiners/CryptoDredge.ps1
@@ -4,12 +4,14 @@ $Path = ".\Bin\NVIDIA-CryptoDredge\CryptoDredge.exe"
 $Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.6.0/CryptoDredge_0.6.0.zip"
 
 $Commands = [PSCustomObject]@{
-    "allium" = " -d $SelGPUCC" #Allium(testing)
-    "lyra2v2" = " -d $SelGPUCC" #Lyra2RE2(testing)
-    "lyra2z" = " -d $SelGPUCC" #Lyra2z(testing)
-    "neoscrypt" = " -d $SelGPUCC" #NeoScrypt(testing)
-    "phi" = " -d $SelGPUCC" #Phi (testing)
-    "phi2" = " -d $SelGPUCC" #Phi2 (testing)
+    "allium" = " -d $SelGPUCC" #Allium
+    "lyra2v2" = " -d $SelGPUCC" #Lyra2RE2
+    "lyra2z" = " -d $SelGPUCC" #Lyra2z
+    "neoscrypt" = " -d $SelGPUCC" #NeoScrypt
+    "phi" = " -d $SelGPUCC" #Phi
+    "phi2" = " -d $SelGPUCC" #Phi2
+    "skein" = " -d $SelGPUCC" #Skein
+    "skunk" = " -d $SelGPUCC" #Skunk
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
