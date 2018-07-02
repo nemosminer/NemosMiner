392a2d50f362dd7a4bc6e91aef846bd93ffa58b1
nemosminer <slbaker@hotmail.co.nz>
2018-07-02 14:58:06 +1200

    increase GPU count from 6 to 12

--- OptionalMiners/CuBalloon.ps1
+++ OptionalMiners/CuBalloon.ps1
@@ -13,7 +13,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = " -b $($Variables.MinerAPITCPPort) -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) --cuda_devices 0,1,2,3,4,5, --cuda_threads 64,64,64,64,64,64 --cuda_blocks 48,48,48,48,48,48 --cuda_sync 0 -t 0"
+        Arguments = " -b $($Variables.MinerAPITCPPort) -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) --cuda_devices 0,1,2,3,4,5, --cuda_threads 64,64,64,64,64,64,64,64,64,64,64,64 --cuda_blocks 48,48,48,48,48,4848,48,48,48,48,48 --cuda_sync 0 -t 0"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week * .96} # substract about 3% fee
         API = "Ccminer"
         Port = $Variables.MinerAPITCPPort
