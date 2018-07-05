93febabde6e7c0c19fe0b1be8df387ccd71c9dbd
nemosminer <slbaker@hotmail.co.nz>
2018-07-05 14:24:50 +1200

    Update CuBalloon.ps1

--- OptionalMiners/CuBalloon.ps1
+++ OptionalMiners/CuBalloon.ps1
@@ -1,4 +1,4 @@
-. .\Include.ps1
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
 
 $Path = ".\Bin\NVIDIA-CuBalloon\cuballoon.exe"
 $Uri = "https://github.com/Belgarion/cuballoon/files/2143221/CuBalloon.1.0.2.Windows.zip"
@@ -13,10 +13,10 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = " -b $($Variables.MinerAPITCPPort) -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) --cuda_devices 0,1,2,3,4,5,6,7,8,9,10,11 --cuda_threads 128,128,128,128,128,128,128,128,128,128,128,128 --cuda_blocks 48,48,48,48,48,48,48,48,48,48,48,48 --cuda_sync 0 -t 0"
+        Arguments = " -b $($Variables.NVIDIAMinerAPITCPPort) -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) --cuda_devices 0,1,2,3,4,5,6,7,8,9,10,11 --cuda_threads 128,128,128,128,128,128,128,128,128,128,128,128 --cuda_blocks 48,48,48,48,48,48,48,48,48,48,48,48 --cuda_sync 0 -t 0"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week * .96} # substract about 3% fee
         API = "Ccminer"
-        Port = $Variables.MinerAPITCPPort
+        Port = $Variables.NVIDIAMinerAPITCPPort
         Wrap = $false
         URI = $Uri
         User = $Pools.(Get-Algorithm($_)).User
