fc2c1262b374287daf3e7d0461fdc84321135557
nemosminer <slbaker@hotmail.co.nz>
2018-07-05 14:03:38 +1200

    Update EthashClaymore.ps1

--- Miners/EthashClaymore.ps1
+++ Miners/EthashClaymore.ps1
@@ -1,9 +1,9 @@
-. .\Include.ps1
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
 
 $Path = ".\\Bin\\Ethash-Claymore\\EthDcrMiner64.exe"
 $Uri = "https://github.com/nemosminer/Claymores-Dual-Ethereum/releases/download/v11.8/Claymore.s.Dual.Ethereum.v11.8.zip"
 $Commands = [PSCustomObject]@{
-    "ethash" = " -di $($SelGPUCC.Replace(',',''))" #Ethash(fastest)
+    "ethash" = " -di $($($Config.SelGPUCC).Replace(',',''))" #Ethash(fastest)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -12,10 +12,10 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = "-esm 3 -allpools 1 -allcoins 1 -platform 2 -mode 1 -mport -$($Variables.MinerAPITCPPort) -epool $($Pools.Ethash.Host):$($Pools.Ethash.Port) -ewal $($Pools.Ethash.User) -epsw $($Pools.Ethash.Pass)$($Commands.$_)"
+        Arguments = "-esm 3 -allpools 1 -allcoins 1 -platform 2 -mode 1 -mport -$($Variables.NVIDIAMinerAPITCPPort) -epool $($Pools.Ethash.Host):$($Pools.Ethash.Port) -ewal $($Pools.Ethash.User) -epsw $($Pools.Ethash.Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week * .99} # substract 1% devfee
         API = "Claymore"
-        Port = $Variables.MinerAPITCPPort #3333
+        Port = $Variables.NVIDIAMinerAPITCPPort #3333
         Wrap = $false
         URI = $Uri
         User = $Pools.(Get-Algorithm($_)).User
