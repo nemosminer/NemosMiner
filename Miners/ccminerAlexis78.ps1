610626432ee9cbf5a4e9b73b97b26c46c1480fdc
nemosminer <slbaker@hotmail.co.nz>
2018-07-05 14:07:39 +1200

    Update ccminerAlexis78.ps1

--- Miners/ccminerAlexis78.ps1
+++ Miners/ccminerAlexis78.ps1
@@ -1,41 +1,41 @@
-. .\Include.ps1
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
 
 $Path = ".\Bin\NVIDIA-Alexis78\ccminer.exe"
 $Uri = "https://github.com/nemosminer/ccminerAlexis78/releases/download/Alexis78-v1.2/ccminerAlexis78v1.2x32.7z"
 
 $Commands = [PSCustomObject]@{
-    "hsr" = " -N 1 -d $SelGPUCC" #Hsr(fastest)
-    "poly" = " -N 1 -d $SelGPUCC" #polytimos(fastest)
+    "hsr" = " -N 1 -d $($Config.SelGPUCC)" #Hsr(fastest)
+    "poly" = " -N 1 -d $($Config.SelGPUCC)" #polytimos(fastest)
     #"bitcore" = "" #Bitcore
-    #"blake2s" = " -r 0 -d $SelGPUCC" #Blake2s(fastest)
-    "x13" = " -d $SelGPUCC -i 20 -N 1" #X13
+    #"blake2s" = " -r 0 -d $($Config.SelGPUCC)" #Blake2s(fastest)
+    "x13" = " -d $($Config.SelGPUCC) -i 20 -N 1" #X13
     #"vanilla" = "" #BlakeVanilla
     #"cryptonight" = "" #Cryptonight
-    "veltor" = " -i 23 -d $SelGPUCC" #Veltor(fastest)
+    "veltor" = " -i 23 -d $($Config.SelGPUCC)" #Veltor(fastest)
     #"decred" = "" #Decred
     #"equihash" = "" #Equihash
     #"ethash" = "" #Ethash
     #"groestl" = "" #Groestl
     #"hmq1725" = "" #hmq1725
-    "keccak" = " -N 1 -m 2 -i 29 -d $SelGPUCC" #Keccak(fastest)
-    "keccakc" = " -N 1 -i 29 -d $SelGPUCC" #Keccakc(fastest)
-    "lbry" = " -d $SelGPUCC" #Lbry
-    "lyra2v2" = " -d $SelGPUCC -N 1" #Lyra2RE2(fastest) 
+    "keccak" = " -N 1 -m 2 -i 29 -d $($Config.SelGPUCC)" #Keccak(fastest)
+    "keccakc" = " -N 1 -i 29 -d $($Config.SelGPUCC)" #Keccakc(fastest)
+    "lbry" = " -d $($Config.SelGPUCC)" #Lbry
+    "lyra2v2" = " -d $($Config.SelGPUCC) -N 1" #Lyra2RE2(fastest) 
     #"lyra2z" = "" #Lyra2z
-    "myr-gr" = " -d $SelGPUCC -N 1" #MyriadGroestl
-    #"neoscrypt" = " -i 15 -d $SelGPUCC" #NeoScrypt
-    #"nist5" = " -r 0 -d $SelGPUCC" #Nist5(fastest)
+    "myr-gr" = " -d $($Config.SelGPUCC) -N 1" #MyriadGroestl
+    #"neoscrypt" = " -i 15 -d $($Config.SelGPUCC)" #NeoScrypt
+    #"nist5" = " -r 0 -d $($Config.SelGPUCC)" #Nist5(fastest)
     #"pascal" = "" #Pascal
     #"qubit" = "" #Qubit
     #"scrypt" = "" #Scrypt
     #"sia" = "" #Sia
-    #"sib" = " -i 21 -d $SelGPUCC" #Sib(fastest)
-    "skein" = " -N 1 -d $SelGPUCC" #Skein(fastest)
+    #"sib" = " -i 21 -d $($Config.SelGPUCC)" #Sib(fastest)
+    "skein" = " -N 1 -d $($Config.SelGPUCC)" #Skein(fastest)
     #"timetravel" = "" #Timetravel
-    "c11" = " -N 1 -i 21 -d $SelGPUCC" #C11(fastest)
-    "x11evo" = " -N 1 -i 21 -d $SelGPUCC " #X11evo(fastest)
-    #"x11gost" = " -i 21 -d $SelGPUCC --api-remote" #X11gost
-    "x17" = " -N 1 -i 20 -d $SelGPUCC" #X17(fastest)
+    "c11" = " -N 1 -i 21 -d $($Config.SelGPUCC)" #C11(fastest)
+    "x11evo" = " -N 1 -i 21 -d $($Config.SelGPUCC) " #X11evo(fastest)
+    #"x11gost" = " -i 21 -d $($Config.SelGPUCC) --api-remote" #X11gost
+    "x17" = " -N 1 -i 20 -d $($Config.SelGPUCC)" #X17(fastest)
     #"yescrypt" = "" #Yescrypt
 }
 
@@ -45,10 +45,10 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = "-b $($Variables.MinerAPITCPPort) -R 1 -q -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        Arguments = "-b $($Variables.NVIDIAMinerAPITCPPort) -R 1 -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
         API = "Ccminer"
-        Port = $Variables.MinerAPITCPPort #4068
+        Port = $Variables.NVIDIAMinerAPITCPPort #4068
         Wrap = $false
         URI = $Uri
         User = $Pools.(Get-Algorithm($_)).User
