15284c94f97011a63221d5218237a49f789201a7
nemosminer <slbaker@hotmail.co.nz>
2018-07-05 14:02:18 +1200

    Update CryptoDredge.ps1

--- Miners/CryptoDredge.ps1
+++ Miners/CryptoDredge.ps1
@@ -1,17 +1,17 @@
-. .\Include.ps1
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
 
 $Path = ".\Bin\NVIDIA-CryptoDredge\CryptoDredge.exe"
 $Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.6.0/CryptoDredge_0.6.0.zip"
 
 $Commands = [PSCustomObject]@{
-    "allium" = " -d $SelGPUCC" #Allium
-    "lyra2v2" = " -d $SelGPUCC" #Lyra2RE2
-    "lyra2z" = " -d $SelGPUCC --submit-stale" #Lyra2z
-    "neoscrypt" = " -d $SelGPUCC" #NeoScrypt
-    "phi" = " -d $SelGPUCC" #Phi
-    "phi2" = " -d $SelGPUCC" #Phi2
-    "skein" = " -d $SelGPUCC" #Skein
-    "skunk" = " -d $SelGPUCC" #Skunk
+    "allium" = "" #Allium
+    "lyra2v2" = "" #Lyra2RE2
+    "lyra2z" = " --submit-stale" #Lyra2z
+    "neoscrypt" = "" #NeoScrypt
+    "phi" = "" #Phi
+    "phi2" = "" #Phi2
+    "skein" = "" #Skein
+    "skunk" = "" #Skunk
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -20,7 +20,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = " -b 127.0.0.1:4444 -a $_ --no-watchdog -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) -R 1 -q -N 1"
+        Arguments = " -b 127.0.0.1:4444 -d $($Config.SelGPUCC) -a $_ --no-watchdog -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) -R 1 -q -N 1"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week * .99} # substract 1% devfee
         API = "cryptodredge"
         Port = "4444"
