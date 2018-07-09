a76f8c233236e22fb859aea27263caaebac0818a
nemosminer <slbaker@hotmail.co.nz>
2018-07-10 08:55:01 +1200

    Revert

--- Miners/EthashPhoenix.ps1
+++ Miners/EthashPhoenix.ps1
@@ -3,7 +3,7 @@ if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps
 $Path = ".\\Bin\\Ethash-Phoenix\\PhoenixMiner.exe"
 $Uri = "http://nemos.dx.am/opt/nemos/PhoenixMiner_3.0c.7z"
 $Commands = [PSCustomObject]@{
-    "ethash" = " -gpus $($($Config.SelGPUCC).Replace(',',''))" #Ethash(fastest)
+    "ethash" = " -di $($($Config.SelGPUCC).Replace(',',''))" #Ethash(fastest)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -12,7 +12,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = "-rmode 1 -proto 4 -coin auto -nvidia -cdmport -$($Variables.NVIDIAMinerAPITCPPort) -pool $($Pools.Ethash.Host):$($Pools.Ethash.Port) -wal $($Pools.Ethash.User) -pass $($Pools.Ethash.Pass)$($Commands.$_)"
+        Arguments = "-esm 3 -allpools 1 -allcoins 1 -platform 2 -mport -$($Variables.NVIDIAMinerAPITCPPort) -epool $($Pools.Ethash.Host):$($Pools.Ethash.Port) -ewal $($Pools.Ethash.User) -epsw $($Pools.Ethash.Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week * .9935} # substract 0.65% devfee
         API = "Wrapper"
         Port = $Variables.NVIDIAMinerAPITCPPort #3333
