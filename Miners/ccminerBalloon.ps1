7c2628022bfeaf384954557f990b518da2db48dd
nemosminer <slbaker@hotmail.co.nz>
2018-07-12 09:44:08 +1200

    Create ccminerBalloon.ps1

---
+++ Miners/ccminerBalloon.ps1
@@ -0,0 +1,24 @@
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
+
+$Path = ".\Bin\NVIDIA-ccminerBalloon\ccminer.exe"
+$Uri = "https://github.com/monkins1010/ccminer--v2balloon/releases/download/v2.3/ccminer.balloon.v2.3.monkins1010.zip"
+
+$Commands = [PSCustomObject]@{
+    "balloon" = " -d $($Config.SelGPUCC) --cuda_threads 448 --cuda_blocks 48" #Balloon
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-b $($Variables.NVIDIAMinerAPITCPPort) -R 1 -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day}
+        API = "Ccminer"
+        Port = $Variables.NVIDIAMinerAPITCPPort
+        Wrap = $false
+        URI = $Uri
+        User = $Pools.(Get-Algorithm($_)).User
+    }
+}
