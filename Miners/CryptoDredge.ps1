f36a0fa4a83d98868da4a516e3264fd580748602
nemosminer <slbaker@hotmail.co.nz>
2018-08-07 18:26:42 +1200

    Update CryptoDredge.ps1

--- Miners/CryptoDredge.ps1
+++ Miners/CryptoDredge.ps1
@@ -11,7 +11,7 @@ $Commands = [PSCustomObject]@{
     "phi"       = "" #Phi
     "phi2"      = "" #Phi2
     #"skein"     = "" #Skein
-    #"skunk"     = "" #Skunk
+    "skunk"     = "" #Skunk
     "tribus"     = "" #Tribus
 }
 
