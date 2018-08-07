6698efef8ee3fd39aa2f331306808b468fb3e12e
nemosminer <slbaker@hotmail.co.nz>
2018-08-07 14:21:45 +1200

    Update ccminerx16r.ps1

--- Miners/ccminerx16r.ps1
+++ Miners/ccminerx16r.ps1
@@ -37,7 +37,7 @@ $Commands = [PSCustomObject]@{
     #"x11evo" = " -d $($Config.SelGPUCC)" #X11evo
     #"x17" = " -i 21.5 -d $($Config.SelGPUCC) --api-remote" #X17
     "x16r" = " -d $($Config.SelGPUCC)" #X16r(testing)
-    "x16s" = " -d $($Config.SelGPUCC)" #X16s
+    #"x16s" = " -d $($Config.SelGPUCC)" #X16s
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
