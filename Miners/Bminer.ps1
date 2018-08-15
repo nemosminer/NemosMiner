703a9d3e9c7c1f20981c7765e4af04d06b0eb64f
nemosminer <slbaker@hotmail.co.nz>
2018-08-16 09:58:25 +1200

    Update Bminer.ps1

--- Miners/Bminer.ps1
+++ Miners/Bminer.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
 $Path = ".\Bin\NVIDIA-Bminer\bminer.exe"
-$Uri = "https://www.bminercontent.com/releases/bminer-lite-v9.0.0-199ca8c-amd64.zip"
+$Uri = "https://www.bminercontent.com/releases/bminer-lite-v10.1.0-1323b4f-amd64.zip"
 $Commands = [PSCustomObject]@{
     "equihash" = " -max-temperature 94 -devices $($Config.SelGPUCC)" #Equihash(fastest no dev fee)
 }
