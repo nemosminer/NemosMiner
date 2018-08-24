ed76d51ce53be7d010450ee7097f70e7647feb34
nemosminer <slbaker@hotmail.co.nz>
2018-08-24 14:12:25 +1200

    Update enemyzealot1.17x32.ps1

--- OptionalMiners/enemyzealot1.17x32.ps1
+++ OptionalMiners/enemyzealot1.17x32.ps1
@@ -4,47 +4,23 @@ $Path = ".\Bin\NVIDIA-enemyz1.17x32\z-enemy.exe"
 $Uri = "http://nemos.dx.am/opt/nemos/z-enemy.1-17-cuda9.2_x32.7z"
 
 $Commands = [PSCustomObject]@{
-    "aeriumx" = " -i 21" #AeriumX(fastest)
-    "polytimos" = " -i 20" #Polytimos(not tested)
-    #"hsr" = " -i 20 " #Hsr
-    #phi2" = " -i 20" #Phi2 (cryptodredge faster)
-    #bitcore" = " -i 20" #Bitcore (trex faster
-    #x16r" = " -i 20" #X16r(trex faster)
-    #16s" = " -i 20" #X16s(trex faster)
-    #"blake2s" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Blake2s
-    #"blakecoin" = " -d $SelGPUCC" #Blakecoin
-    #"vanilla" = "" #BlakeVanilla
-    #"cryptonight" = " -i 10 -d $SelGPUCC" #Cryptonight
-    #"decred" = "" #Decred
-    #"equihash" = "" #Equihash
-    #"ethash" = "" #Ethash
-    #"groestl" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Groestl
-    #"hmq1725" = " -d $SelGPUCC" #hmq1725
-    #"keccakc" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Keccakc
-    #"lbry" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Lbry
-    #"lyra2v2" = " -N 1 -d $SelGPUCC --api-remote --api-allow=0/0" #Lyra2RE2
-    #"lyra2z" = "  -d $SelGPUCC --api-remote --api-allow=0/0 --submit-stale" #Lyra2z
-    #"myr-gr" = "" #MyriadGroestl
-    #"neoscrypt" = " -d $SelGPUCC" #NeoScrypt
-    #"nist5" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Nist5
-    #"pascal" = "" #Pascal
-    #"qubit" = "" #Qubit
-    #"scrypt" = "" #Scrypt
-    #"sha256t" = " -d $SelGPUCC" #Sha256t
-    #"sia" = "" #Sia
-    #"sib" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Sib
-    #"skein" = "" #Skein
-    #"skunk" = " -r 0 -d $SelGPUCC -i 20" #Skunk(Delos faster)
-    "sonoa" = " -i 21" #SonoA (fastest)
-    #"timetravel" = " -i 24" #Timetravel(x64 faster)
-    #tribus" = " -i 20" #Tribus(cryptodrege faster)
-    #c11" = " -i 20" #C11(trex faster)
-    #xevan" = " -i 20" #Xevan(xevan75/9 faster)
-    #"x11evo" = " -d $SelGPUCC" #X11evo
-    #x17" = " -i 20" #X17(trex faster)
-    "vitalium" = " -i 20" #Vitalium(not testest)
-    #"yescrypt" = "" #Yescrypt
-    "hex" = " -i 23" #Hex(fastest)
+"aergo" = " -i 21" #Aergo
+"aeriumx" = " -i 21" #AeriumX(fastest)
+#"hsr" = "" #Hsr
+#"phi" = "" #Phi
+#"phi2" = "" #Phi2 (cryptodredge faster)
+"poly" = "" #Polytimos(not tested)
+#"bitcore" = "" #Bitcore (t-rex faster)
+#"x16r" = "" #X16r (t-rex faster)
+#"x16s" = "" #X16s(t-rex faster)
+"sonoa" = " -i 21" #SonoA t-rex faster)
+#"skunk" = "" #Skunk(Delos faster)
+#"timetravel" = "" #Timetravel(fastest)
+#"tribus" = "" #Tribus(trex,cryptodredge faster)
+#"c11" = "" #C11(Alexis78v1.2 faster)
+#"xevan" = "" #Xevan(fastest Alexis78Xevan very cose)
+#"x17" = "" #X17(Alexis78,Delos,enemy1.03 faster)
+"hex" = " -i 23" #Hex(fastest)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
