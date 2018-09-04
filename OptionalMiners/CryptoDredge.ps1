f96ea493bb1f75682c2be57b0429652df6a43e23
nemosminer <slbaker@hotmail.co.nz>
2018-09-05 08:30:14 +1200

    Update CryptoDredge.ps1

--- OptionalMiners/CryptoDredge.ps1
+++ OptionalMiners/CryptoDredge.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
 $Path = ".\Bin\NVIDIA-CryptoDredge\CryptoDredge.exe"
-$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.9.0/CryptoDredge_0.9.0_cuda_9.2_windows.zip"
+$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.9.1/CryptoDredge_0.9.1_cuda_9.2_windows.zip"
 
 $Commands = [PSCustomObject]@{
     "allium"    = "" #Allium (fastetst)
