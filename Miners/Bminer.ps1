f85e0e10fd4f8dece312711794a005088aa61a12
nemosminer <slbaker@hotmail.co.nz>
2018-09-07 11:06:24 +1200

    Update Bminer.ps1

--- Miners/Bminer.ps1
+++ Miners/Bminer.ps1
@@ -1,6 +1,6 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
-$Path = ".\Bin\NVIDIA-BminerBTG\bminer.exe"
+$Path = ".\Bin\NVIDIA-Bminer\bminer.exe"
 $Uri = "https://www.bminercontent.com/releases/bminer-lite-v10.1.0-1323b4f-amd64.zip"
 $Commands = [PSCustomObject]@{
     "equihashBTG" = " -uri zhash://" #Equihash(testing)
