d110e12967be94663f5b020f60ae27c039522b28
nemosminer <slbaker@hotmail.co.nz>
2018-09-08 14:06:09 +1200

    No more Wrapper Fix API

--- Miners/Bminer.ps1
+++ Miners/Bminer.ps1
@@ -15,9 +15,9 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
         Path = $Path
         Arguments = "$($Commands.$_)$($Pools.(Get-Algorithm($_)).User):$($Pools.(Get-Algorithm($_)).Pass.ToString().replace(',','%2C'))@$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -max-temperature 94 -devices $($Config.SelGPUCC) -api 127.0.0.1:$Port -nofee"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week} 
-        API = "Wrapper"
+        API = "Bminer"
         Port = $Port
-        Wrap = $true
+        Wrap = $false
         URI = $Uri    
         User = $Pools.(Get-Algorithm($_)).User
     }
