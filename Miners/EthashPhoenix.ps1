3bb62b4efd867d29bcca92b04a7e4d564d86ee5b
nemosminer <slbaker@hotmail.co.nz>
2018-09-08 14:07:27 +1200

    Add phoenix to API disable Wrapper

--- Miners/EthashPhoenix.ps1
+++ Miners/EthashPhoenix.ps1
@@ -14,9 +14,9 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
         Path = $Path
         Arguments = "-esm 3 -allpools 1 -allcoins 1 -platform 2 -mport -$($Variables.NVIDIAMinerAPITCPPort) -epool $($Pools.Ethash.Host):$($Pools.Ethash.Port) -ewal $($Pools.Ethash.User) -epsw $($Pools.Ethash.Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week * .9935} # substract 0.65% devfee
-        API = "Wrapper"
+        API = "phoenix"
         Port = $Variables.NVIDIAMinerAPITCPPort #3333
-        Wrap = $true
+        Wrap = $false
         URI = $Uri
         User = $Pools.(Get-Algorithm($_)).User
     }
