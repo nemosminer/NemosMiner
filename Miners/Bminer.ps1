9e359d37afb29859effb6f4fabbea79719d63fef
nemosminer <slbaker@hotmail.co.nz>
2018-09-09 12:14:23 +1200

    Update Bminer.ps1

--- Miners/Bminer.ps1
+++ Miners/Bminer.ps1
@@ -16,7 +16,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
         Path = $Path
         Arguments = "$($Commands.$_)$($Pools.(Get-Algorithm($_)).User):$($Pools.(Get-Algorithm($_)).Pass.ToString().replace(',','%2C'))@$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -max-temperature 94 -devices $($Config.SelGPUCC) -api 127.0.0.1:$Port -nofee"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week} 
-        API = "Bminer"
+        API = "bminer"
         Port = $Port
         Wrap = $false
         URI = $Uri    
