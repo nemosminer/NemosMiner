7cbd4340862f1bb9c720f02006060de40ca0ef26
Grant Emsley <grant@emsley.ca>
2018-09-12 19:01:16 -0400

    Kill when pausing

--- NemosMiner.ps1
+++ NemosMiner.ps1
@@ -1224,6 +1224,9 @@ $ButtonPause.Add_Click( {
                             Sleep 1
                             # simply "Kill with power"
                             Stop-Process $_.Process -Force | Out-Null
+                            # Try to kill any process with the same path, in case it is still running but the process handle is incorrect
+                            $KillPath = $_.Path
+                            Get-Process | Where-Object {$_.Path -eq $KillPath} | Stop-Process -Force
                             Write-Host -ForegroundColor Yellow "closing miner"
                             Sleep 1
                             $_.Status = "Idle"
