6f17fe45bc861c7dcf9a7bbc335a56debc29742a
nemosminer <slbaker@hotmail.co.nz>
2018-09-14 12:22:45 +1200

    Update CryptoDredge.ps1

--- OptionalMiners/CryptoDredge.ps1
+++ OptionalMiners/CryptoDredge.ps1
@@ -4,18 +4,18 @@ $Path = ".\Bin\NVIDIA-CryptoDredge\CryptoDredge.exe"
 $Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.9.1/CryptoDredge_0.9.1_cuda_9.2_windows.zip"
 
 $Commands = [PSCustomObject]@{
-    "allium"            = " -a $_" #Allium (fastetst)
-    "lyra2v2"           = " -a $_" #Lyra2RE2 (fastest)
-    "lyra2z"            = " -a $_" #Lyra2z (fastest)
-    "neoscrypt"         = " -a $_" #NeoScrypt (fastest)
-    "phi"               = " -a $_" #Phi
-    "phi2"              = " -a $_" #Phi2 (fastest)
+    "allium"            = " -a allium" #Allium (fastetst)
+    "lyra2v2"           = " -a lyra2v2" #Lyra2RE2 (fastest)
+    "lyra2z"            = " -a lyra2z" #Lyra2z (fastest)
+    "neoscrypt"         = " -a neoscrypt" #NeoScrypt (fastest)
+    "phi"               = " -a phi" #Phi
+    "phi2"              = " -a phi2" #Phi2 (fastest)
     #"skein"     = "" #Skein
     #"skunk"     = "" #Skunk(trex faster)
-    "cryptonightheavy"  = " -a $_" # CryptoNightHeavy(fastest)
-    "cryptonightv7"     = " -a $_" # CryptoNightV7(fastest)
+    "cryptonightheavy"  = " -a cryptonightheavy" # CryptoNightHeavy(fastest)
+    "cryptonightv7"     = " -a cryptonightv7" # CryptoNightV7(fastest)
     "cryptonightmonero" = " -a cryptonightv7" # Cryptonightmonero(fastest)
-    "tribus"            = " -a $_" #Tribus (fastest)
+    "tribus"            = " -a tribus" #Tribus (fastest)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
