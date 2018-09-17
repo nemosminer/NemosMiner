53fde2439c1e0d2b24c2126eb37282a5acc87a9c
nemosminer <slbaker@hotmail.co.nz>
2018-09-17 12:32:32 +1200

    Update enemyzealotx32.ps1

--- OptionalMiners/enemyzealotx32.ps1
+++ OptionalMiners/enemyzealotx32.ps1
@@ -1,6 +1,6 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\Bin\NVIDIA-enemyzealotx32\z-enemy.exe"
+$Path = ".\Bin\NVIDIA-enemyzealot32120\z-enemy.exe"
 $Uri = "http://nemos.dx.am/opt/nemos/z-enemy.1-20-cuda9.2_x32_v2.zip"
 
 $Commands = [PSCustomObject]@{
