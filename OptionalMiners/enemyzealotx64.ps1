2286dcd524c231b4e6cf0d9bc13f67ecdd48e684
nemosminer <slbaker@hotmail.co.nz>
2018-09-29 18:31:41 +1200

    Update enemyzealotx64.ps1

--- OptionalMiners/enemyzealotx64.ps1
+++ OptionalMiners/enemyzealotx64.ps1
@@ -1,25 +1,26 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\Bin\NVIDIA-enemyzealot64121\z-enemy.exe"
-$Uri = "http://nemos.dx.am/opt/nemos/z-enemy.1-21-cuda9.2_x64.zip"
+$Path = ".\Bin\NVIDIA-enemyzealot64121a\z-enemy.exe"
+$Uri = "http://nemos.dx.am/opt/nemos/z-enemy.1-21a-cuda9.2_x64.zip"
 
 $Commands = [PSCustomObject]@{
-#"aeriumx" = " -i 21" #AeriumX(x32120 faster)
+"aeriumx" = " -i 20" #AeriumX(2080)
+"bcd" = " -i 20" #Bcd(2080)
 #"hsr" = "" #Hsr
 #"phi" = "" #Phi
 #"phi2" = "" #Phi2 (cryptodredge faster)
-#"poly" = "" #Polytimos(trex faster) 
-#"bitcore" = "" #Bitcore (trex faster)
-#"x16r" = "" #X16r (trex faster)
-#"x16s" = "" #X16s (trex faster)
-#"sonoa" = " -i 21" #SonoA (trex faster)
-#"skunk" = "" #Skunk (trex faster)
-#"timetravel" = "" #Timetravel (x32120 faster)
+"poly" = " -i 20" #Polytimos(2080) 
+"bitcore" = " -i 20" #Bitcore (2080)
+"x16r" = " -i 20" #X16r (2080)
+"x16s" = " -i 20" #X16s (2080)
+"sonoa" = " -i 20" #SonoA (2080)
+"skunk" = " -i 20" #Skunk (2080)
+"timetravel" = " -i 20" #Timetravel (x64120 faster)
 #"tribus" = "" #Tribus (cryptodredge faster)
-#"c11" = "" #C11 (trex faster)
-"xevan" = "" #Xevan (fastest on 1080ti rigs /ccminer7.5/9 faster on 1070ti rigs)
-#"x17" = "" #X17 (trex faster)
-"hex" = " -i 23" #Hex (x32120 faster)
+"c11" = " -i 20" #C11 (2080)
+"xevan" = " -i 20" #Xevan (x64120 faster)
+"x17" = " -i 20" #X17 (2080)
+"hex" = " -i 20" #Hex (2080)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
