f11f3985877fb73617cdeb1caa000663513b42fb
nemosminer <slbaker@hotmail.co.nz>
2018-10-08 10:49:56 +1300

    Update trex.ps1
    cuda 10

--- OptionalMiners/trex.ps1
+++ OptionalMiners/trex.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
 
 $Path = ".\Bin\NVIDIA-trex610\t-rex.exe"
-$Uri = "http://nemos.dx.am/opt/nemos/t-rex-0.6.10wincuda9.1.zip"
+$Uri = "http://nemos.dx.am/opt/nemos/t-rex-0.7.0-win-cuda10.0.zip"
 
 $Commands = [PSCustomObject]@{
 "balloon" = "" #Balloon(fastest)
