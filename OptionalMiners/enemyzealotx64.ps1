668e8fb6b5b5a5e16878d917cb5643294da4750a
nemosminer <slbaker@hotmail.co.nz>
2018-10-16 21:48:06 +1300

    Update enemyzealotx64.ps1

--- OptionalMiners/enemyzealotx64.ps1
+++ OptionalMiners/enemyzealotx64.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\Bin\NVIDIA-enemyzealot64122\z-enemy.exe"
-$Uri = "https://nemosminer.com/data/optional/z-enemy.1-22-cuda10.0_x64.7z"
+$Path = ".\Bin\NVIDIA-enemyzealot64122a\z-enemy.exe"
+$Uri = "https://nemosminer.com/data/optional/z-enemy.1-22a-cuda10.0_x64_v1.7z"
 
 $Commands = [PSCustomObject]@{
 #"aeriumx" = " -i 20" #AeriumX(x32)
