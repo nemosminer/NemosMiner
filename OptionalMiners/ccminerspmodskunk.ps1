6645709a12b7a28716a76f51ea3d394f53a89d96
nemosminer <slbaker@hotmail.co.nz>
2018-10-23 18:03:51 +1300

    Update ccminerspmodskunk.ps1

--- OptionalMiners/ccminerspmodskunk.ps1
+++ OptionalMiners/ccminerspmodskunk.ps1
@@ -1,1 +1,24 @@
 
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
+
+$Path = ".\Bin\NVIDIA-Spmodskunk\ccminer.exe"
+$Uri = ""
+
+$Commands = [PSCustomObject]@{
+    "skunk" = "" #Skunk
+
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-b $($Variables.NVIDIAMinerAPITCPPort) -R 1 -a $_ -o $($Pools.(Get-Algorithm $_).Protocol)://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm $_) = $Stats."$($Name)_$(Get-Algorithm $_)_HashRate".Week}
+        API = "ccminer"
+        Port = $Variables.NVIDIAMinerAPITCPPort #4068
+        URI = $Uri
+    }
+}
