c46e4fe0c67b7f5c7cb544a1a13083489dc04a60
nemosminer <slbaker@hotmail.co.nz>
2018-10-26 09:27:36 +1300

    Update xmrstak.ps1
    This release fixes a NVIDIA bug which reduce the hash rate for monero/cryptonight_v8 a little bit.
    The upcoming Graft fork on 31st Oct to the POW cryptonight_v8 is supported with this release.

--- OptionalMiners/xmrstak.ps1
+++ OptionalMiners/xmrstak.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
 $Path = ".\Bin\CryptoNight-FireIce251\xmr-stak.exe"
-$Uri = "https://github.com/fireice-uk/xmr-stak/releases/download/2.5.1/xmr-stak-win64-2.5.1.zip"
+$Uri = "https://github.com/fireice-uk/xmr-stak/releases/download/2.5.2/xmr-stak-win64-2.5.2.zip"
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
 $Port = 3335
