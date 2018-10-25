5f58387e5a979d805fa24d2659a910080aa1743e
nemosminer <slbaker@hotmail.co.nz>
2018-10-26 09:29:35 +1300

    Create CcminerZjazz-CUDA.ps1

---
+++ OptionalMiners/CcminerZjazz-CUDA.ps1
@@ -0,0 +1,25 @@
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
+
+$Path = '.\Bin\NVIDIA-Zjazz_CUDA\zjazz_cuda.exe'
+$Uri = 'https://github.com/zjazz/zjazz_cuda_miner/releases/download/1.2/zjazz_cuda_win64_1.2.zip'
+
+
+$Commands = [PSCustomObject]@{
+	x22i = '' #SUQA
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-b $($Variables.NVIDIAMinerAPITCPPort) -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Live}
+        API = "Ccminer"
+        Port = $Variables.NVIDIAMinerAPITCPPort
+        Wrap = $false
+        URI = $Uri
+		User = $Pools.(Get-Algorithm($_)).User
+    }
+}
