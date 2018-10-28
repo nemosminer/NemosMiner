2f934c064dfd0d99ba1727ca55a84411c6e95c86
nemosminer <slbaker@hotmail.co.nz>
2018-10-29 10:49:57 +1300

    Update ccminercool.ps1

--- OptionalMiners/ccminercool.ps1
+++ OptionalMiners/ccminercool.ps1
@@ -4,7 +4,7 @@ $Path = ".\Bin\NVIDIA-ccminercool177\coolMiner-x64.exe"
 $Uri = "https://nemosminer.com/data/optional/coolMiner-x64-v1-7.7z"
 
 $Commands = [PSCustomObject]@{
-    "lyra2z" = " -d $($Config.SelGPUCC) --submit-stale -N 1" #Lyra2z (compare speed and wattage draw compared to trex and cryptodreage as they all have 1%fee only require the best one not all 3..)
+    "lyra2z" = " -d $($Config.SelGPUCC) --submit-stale -N 1 --hide-diff -q" #Lyra2z (compare speed and wattage draw compared to trex and cryptodreage as they all have 1%fee only require the best one not all 3..)
 
 }
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
