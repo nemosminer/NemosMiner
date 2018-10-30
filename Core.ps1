ddc05e25c60e4ea4b6250e0c222e4562877a6f53
nemosminer <slbaker@hotmail.co.nz>
2018-10-31 11:36:14 +1300

    Update Core.ps1

--- Core.ps1
+++ Core.ps1
@@ -30,13 +30,13 @@ Function InitApplication {
     [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
     Set-Location (Split-Path $script:MyInvocation.MyCommand.Path)
     Get-ChildItem . -Recurse | Unblock-File
-	if (Get-MpComputerStatus -ErrorAction SilentlyContinue) {
-		Update-Status("INFO: Adding NemosMiner path to Windows Defender's exclusions..")
-		try {if ((Get-MpPreference).ExclusionPath -notcontains (Convert-Path .)) {Start-Process powershell -Verb runAs -ArgumentList "Add-MpPreference -ExclusionPath '$(Convert-Path .)'"}}catch {}
-	} 
-	else {
-		Update-Status("INFO: Windows Defender is disabled, make sure to exclude NemosMiner directory from your antivirus program")
-	}
+    if (Get-MpComputerStatus -ErrorAction SilentlyContinue) {
+        Update-Status("INFO: Adding NemosMiner path to Windows Defender's exclusions..")
+        try {if ((Get-MpPreference).ExclusionPath -notcontains (Convert-Path .)) {Start-Process powershell -Verb runAs -ArgumentList "Add-MpPreference -ExclusionPath '$(Convert-Path .)'"}}catch {}
+    } 
+    else {
+        Update-Status("INFO: Windows Defender is disabled, make sure to exclude NemosMiner directory from your antivirus program")
+    }
     if ($Proxy -eq "") {$PSDefaultParameterValues.Remove("*:Proxy")}
     else {$PSDefaultParameterValues["*:Proxy"] = $Proxy}
     Update-Status("Initializing Variables...")
@@ -198,15 +198,15 @@ Function NPMCycle {
     $Variables.StatusText = "Loading pool stats.."
     $PoolFilter = @()
     $Config.PoolName | foreach {$PoolFilter += ($_ += ".*")}
-	Do {
-		$AllPools = if(Test-Path "Pools"){Get-ChildItemContent "Pools" -Include $PoolFilter | ForEach {$_.Content | Add-Member @{Name = $_.Name} -PassThru} | 
-			Where {$_.SSL -EQ $Config.SSL -and ($Config.PoolName.Count -eq 0 -or ($_.Name -in $Config.PoolName)) -and (!$Config.Algorithm -or ((!($Config.Algorithm | ? {$_ -like "+*"}) -or $_.Algorithm -in ($Config.Algorithm | ? {$_ -like "+*"}).Replace("+","")) -and (!($Config.Algorithm | ? {$_ -like "-*"}) -or $_.Algorithm -notin ($Config.Algorithm | ? {$_ -like "-*"}).Replace("-",""))) )}
-		}
-			if ($AllPools.Count -eq 0) {
-			$Variables.StatusText = "! Error contacting pool retrying in 30 seconds.."
-			Sleep 30
-		}
-	} While ($AllPools.Count -eq 0)
+    Do {
+        $AllPools = if (Test-Path "Pools") {Get-ChildItemContent "Pools" -Include $PoolFilter | ForEach {$_.Content | Add-Member @{Name = $_.Name} -PassThru} | 
+                Where {$_.SSL -EQ $Config.SSL -and ($Config.PoolName.Count -eq 0 -or ($_.Name -in $Config.PoolName)) -and (!$Config.Algorithm -or ((!($Config.Algorithm | ? {$_ -like "+*"}) -or $_.Algorithm -in ($Config.Algorithm | ? {$_ -like "+*"}).Replace("+", "")) -and (!($Config.Algorithm | ? {$_ -like "-*"}) -or $_.Algorithm -notin ($Config.Algorithm | ? {$_ -like "-*"}).Replace("-", ""))) )}
+        }
+        if ($AllPools.Count -eq 0) {
+            $Variables.StatusText = "! Error contacting pool retrying in 30 seconds.."
+            Sleep 30
+        }
+    } While ($AllPools.Count -eq 0)
     $Variables.StatusText = "Computing pool stats.."
     # Use location as preference and not the only one
     $AllPools = ($AllPools | ? {$_.location -eq $Config.Location}) + ($AllPools | ? {$_.name -notin ($AllPools | ? {$_.location -eq $Config.Location}).Name})
@@ -268,13 +268,14 @@ Function NPMCycle {
     $Variables.StatusText = "Loading miners.."
     $Variables | Add-Member -Force @{Miners = @()}
     $StartPort = 4068
-    $Variables.Miners = if (Test-Path "Miners") {@(
-                Get-ChildItemContent "Miners"
-                if ($Config.IncludeOptionalMiners -and (Test-Path "OptionalMiners")) {Get-ChildItemContent "OptionalMiners"}
-                if (Test-Path "CustomMiners") { Get-ChildItemContent "CustomMiners"}
-            ) | ForEach {$_.Content | Add-Member @{Name = $_.Name} -PassThru} |
+    $Variables.Miners = if (Test-Path "Miners") {
+        @(
+            Get-ChildItemContent "Miners"
+            if ($Config.IncludeOptionalMiners -and (Test-Path "OptionalMiners")) {Get-ChildItemContent "OptionalMiners"}
+            if (Test-Path "CustomMiners") { Get-ChildItemContent "CustomMiners"}
+        ) | ForEach {$_.Content | Add-Member @{Name = $_.Name} -PassThru} |
             Where {$Config.Type.Count -eq 0 -or (Compare $Config.Type $_.Type -IncludeEqual -ExcludeDifferent | Measure).Count -gt 0} | 
-            Where {!($Config.Algorithm | ? {$_.StartsWith("+")}) -or (Compare (($Config.Algorithm | ? {$_.StartsWith("+")}).Replace("+","")) $_.HashRates.PSObject.Properties.Name -IncludeEqual -ExcludeDifferent | Measure).Count -gt 0} | 
+            Where {!($Config.Algorithm | ? {$_.StartsWith("+")}) -or (Compare (($Config.Algorithm | ? {$_.StartsWith("+")}).Replace("+", "")) $_.HashRates.PSObject.Properties.Name -IncludeEqual -ExcludeDifferent | Measure).Count -gt 0} | 
             Where {$Config.MinerName.Count -eq 0 -or (Compare $Config.MinerName $_.Name -IncludeEqual -ExcludeDifferent | Measure).Count -gt 0}
     }
     $Variables.Miners = $Variables.Miners | ForEach {
@@ -416,9 +417,6 @@ Function NPMCycle {
         if ($filtered.Count -eq 0) {
             if ($_.Process -eq $null) {
                 $_.Status = "Failed"
-                  # Try to kill any process with the same path, in case it is still running but the process handle is incorrect
-                $KillPath = $_.Path
-                Get-Process | Where-Object {$_.Path -eq $KillPath} | Stop-Process -Force
             }
             elseif ($_.Process.HasExited -eq $false) {
                 $_.Active += (Get-Date) - $_.Process.StartTime
@@ -426,11 +424,7 @@ Function NPMCycle {
                 Sleep 1
                 # simply "Kill with power"
                 Stop-Process $_.Process -Force | Out-Null
-                Sleep 1
-                # Kill any process with the same path, in case $_.Process is incorrect
-                $KillPath = $_.Path
-                Get-Process | Where-Object {$_.Path -eq $KillPath} | Stop-Process -Force
-                $Variables.StatusText = "closing current miner and switching"
+                Write-Host -ForegroundColor Yellow "closing current miner and switching"
                 Sleep 1
                 $_.Status = "Idle"
             }
