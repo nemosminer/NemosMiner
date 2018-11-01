84dcdfe29703739d9cf5397d694d336c00d3d994
nemosminer <slbaker@hotmail.co.nz>
2018-11-02 09:14:25 +1300

    Update CryptoDredge.ps1
    x17 bitcore testing

--- OptionalMiners/CryptoDredge.ps1
+++ OptionalMiners/CryptoDredge.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\Bin\NVIDIA-CryptoDredge094\CryptoDredge.exe"
-$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.9.4/CryptoDredge_0.9.4_cuda_10.0_windows.zip"
+$Path = ".\Bin\NVIDIA-CryptoDredge095\CryptoDredge.exe"
+$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.9.5/CryptoDredge_0.9.5_cuda_9.2_windows.zip"
 
 $Commands = [PSCustomObject]@{
     "allium"            = " -a allium" #Allium (fastetst)
@@ -14,7 +14,7 @@ $Commands = [PSCustomObject]@{
     "skein"             = " -a skein" #Skein
     "cryptonightheavy"  = " -a cryptonightheavy" # CryptoNightHeavy(fastest)
     "cryptonightv7"     = " -a cryptonightv7" # CryptoNightV7(fastest)
-    #"cryptonightmonero" = " -a cryptonightv7" # Cryptonightmonero(fastest)
+   #"cryptonightmonero" = " -a cryptonightv7" # Cryptonightmonero(fastest)
     "tribus"            = " -a tribus" #Tribus (fastest)
     "cnv8"              = " -a cnv8" #CryptoNightv8 
     "cryptonightmonero" = " -a cnv8" # Cryptonightmonero
@@ -22,6 +22,8 @@ $Commands = [PSCustomObject]@{
     "polytimos "        = " -a polytimos " #Polytimos 
     "skunk"             = " -a skunk" #Skunk
     "bcd"               = " -a bcd" #Bcd
+    "x17"               = " -a x17" #X17
+    "bitcore"           = " -a bitcore" #Bitcore
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -30,7 +32,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = " --retry-pause 1 -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) --no-watchdog -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) -R 1 -q -N 1"
+        Arguments = " --api-type ccminer-tcp --no-color --intensity 6 --cpu-priority 5 --no-crashreport --no-watchdog --retry-pause 1 -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) --no-watchdog -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) -r -1 -R 1"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week * .99} # substract 1% devfee
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort
