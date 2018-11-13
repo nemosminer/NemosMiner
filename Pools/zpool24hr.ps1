d42a529ff187153fe3e5e8a4b5b1cf305bae9b52
nemosminer <slbaker@hotmail.co.nz>
2018-11-14 10:39:14 +1300

    fix PriceField

--- Pools/zpool24hr.ps1
+++ Pools/zpool24hr.ps1
@@ -9,9 +9,8 @@ if (-not $Request) {return}
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 $HostSuffix = ".mine.zpool.ca"
-# $PriceField = "actual_last24h"
-$PriceField = "estimate_current"
-$DivisorMultiplier = 1000000
+$PriceField = "actual_last24h"
+$DivisorMultiplier = 1000000000
  
 $Locations = "eu", "us", "na", "sea"
 $Locations | ForEach-Object {
