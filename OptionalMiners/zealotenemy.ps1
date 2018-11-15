7971480182a09ce84ef9ae009f82df8b1a663cca
nemo <slbaker@hotmail.co.nz>
2018-11-16 05:16:35 +1300

    v2 to v3
    - Important update(1.24 ver.3): Fixed reduced poolside hashrate ( XDNA : hex algo)

--- OptionalMiners/zealotenemy.ps1
+++ OptionalMiners/zealotenemy.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\Bin\NVIDIA-zealotenemy124\z-enemy.exe"
-$Uri = "https://nemosminer.com/data/optional/z-enemy.1-24-cuda10.0_ver2.7z"
+$Path = ".\Bin\NVIDIA-zealotenemy124v3\z-enemy.exe"
+$Uri = "https://nemosminer.com/data/optional/z-enemy.1-24-cuda10.0_ver3.7z"
 
 $Commands = [PSCustomObject]@{
     "aeriumx"    = "" #AeriumX(RTX)
