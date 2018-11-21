5922a38ba397ec98593ca127339e1f445ca2b16d
nemo <slbaker@hotmail.co.nz>
2018-11-21 14:08:23 +1300

    Update EarningsTrackerJob.ps1

--- EarningsTrackerJob.ps1
+++ EarningsTrackerJob.ps1
@@ -17,8 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 <#
 Product:        NemosMiner
 File:           EarningsTrackerJob.ps1
-version:        3.5.2
-version date:   29 October 2018
+version:        3.5.3
+version date:   21 November 2018
 #>
 
 # param(
