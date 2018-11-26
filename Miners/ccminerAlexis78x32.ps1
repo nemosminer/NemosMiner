53853940b3ed81d86dbf38ea6c680f6aa4c91b14
nemo <slbaker@hotmail.co.nz>
2018-11-26 14:46:09 +1300

    Update and rename ccminerAlexis78.ps1 to ccminerAlexis78x32.ps1
    testing x32 vs x64

--- Miners/ccminerAlexis78x32.ps1
+++ Miners/ccminerAlexis78x32.ps1
@@ -1,11 +1,11 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\Bin\NVIDIA-Alexis78\ccminer.exe"
-$Uri = "https://github.com/nemosminer/ccminerAlexis78/releases/download/Alexis78-v1.2/ccminerAlexis78v1.2x32.7z"
+$Path = ".\Bin\NVIDIA-Alexis78x32\ccminer.exe"
+$Uri = "https://github.com/nemosminer/ccminerAlexis78/releases/download/Alexis78-v1.4/ccminerAlexis78v1.4x32.7z"
 
 $Commands = [PSCustomObject]@{
     #"hsr" = " -N 1 -d $($Config.SelGPUCC)" #Hsr(testing)
-    #"poly" = " -N 1 -d $($Config.SelGPUCC)" #polytimos(trex faster)
+    "poly" = " -N 1 -d $($Config.SelGPUCC)" #polytimos
     #"bitcore" = "" #Bitcore
     #"blake2s" = " -r 0 -d $($Config.SelGPUCC)" #Blake2s
     #"x13" = " -d $($Config.SelGPUCC) -i 20 -N 1" #X13
@@ -17,10 +17,10 @@ $Commands = [PSCustomObject]@{
     #"ethash" = "" #Ethash
     #"groestl" = "" #Groestl
     #"hmq1725" = "" #hmq1725
-    #"keccak" = " -N 1 -m 2 -i 29 -d $($Config.SelGPUCC)" #Keccak
-    #"keccakc" = " -N 1 -i 29 -d $($Config.SelGPUCC)" #Keccakc
+    "keccak" = " -N 1 -m 2 -i 29 -d $($Config.SelGPUCC)" #Keccak
+    "keccakc" = " -N 1 -i 29 -d $($Config.SelGPUCC)" #Keccakc
     #"lbry" = " -d $($Config.SelGPUCC)" #Lbry
-    #"lyra2v2" = " -d $($Config.SelGPUCC) -N 1" #Lyra2RE2(fastest open source/cryptodredge faster) 
+    "lyra2v2" = " -d $($Config.SelGPUCC) -N 1" #Lyra2RE2
     #"lyra2z" = "" #Lyra2z
     #"myr-gr" = " -d $($Config.SelGPUCC) -N 1" #MyriadGroestl
     #"neoscrypt" = " -i 15 -d $($Config.SelGPUCC)" #NeoScrypt
@@ -30,12 +30,12 @@ $Commands = [PSCustomObject]@{
     #"scrypt" = "" #Scrypt
     #"sia" = "" #Sia
     #"sib" = " -i 21 -d $($Config.SelGPUCC)" #Sib
-    #"skein" = " -N 1 -d $($Config.SelGPUCC)" #Skein(fastest)
+    "skein" = " -N 1 -d $($Config.SelGPUCC)" #Skein
     #"timetravel" = "" #Timetravel
-    #"c11" = " -N 1 -i 21 -d $($Config.SelGPUCC)" #C11(spmodgit10 & trex faster)
-    #"x11evo" = " -N 1 -i 21 -d $($Config.SelGPUCC) " #X11evo(fastest)
+    "c11" = " -N 1 -i 21 -d $($Config.SelGPUCC)" #C11
+    "x11evo" = " -N 1 -i 21 -d $($Config.SelGPUCC) " #X11evo
     #"x11gost" = " -i 21 -d $($Config.SelGPUCC) --api-remote" #X11gost
-    #"x17" = " -N 1 -i 20 -d $($Config.SelGPUCC)" #X17(trex faster)
+    "x17" = " -N 1 -i 20 -d $($Config.SelGPUCC)" #X17
     #"yescrypt" = "" #Yescrypt
 }
 
