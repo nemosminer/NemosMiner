ba04acfc2c2a145982ed367e4de4b14a8df47140
nemo <slbaker@hotmail.co.nz>
2018-12-02 10:11:46 +1300

    Equihash 144 based coins NiceHash

--- Miners/Bminer.ps1
+++ Miners/Bminer.ps1
@@ -6,6 +6,7 @@ $Commands = [PSCustomObject]@{
     #"equihashBTG" = " -uri zhash://" #EquihashBTG(testing)
     #"equihash" = " -uri stratum://" #Equihash(fastest)
     #"equihash144" = " -pers auto -uri equihash1445://" #Equihash144(testing)
+    "zhash" = " -pers auto -uri equihash1445://" #Zhash(testing)
     
     #"ethash" = " -uri ethstratum://" #Ethash(testing 2080)
 }
@@ -14,14 +15,14 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
     [PSCustomObject]@{
-        Type = "NVIDIA"
-        Path = $Path
+        Type      = "NVIDIA"
+        Path      = $Path
         Arguments = "$($Commands.$_)$($Pools.(Get-Algorithm($_)).User):$($Pools.(Get-Algorithm($_)).Pass.ToString().replace(',','%2C'))@$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -max-temperature 94 -devices $($Config.SelGPUCC) -api 127.0.0.1:$Port"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week} 
-        API = "bminer"
-        Port = $Port
-        Wrap = $false
-        URI = $Uri    
-        User = $Pools.(Get-Algorithm($_)).User
+        API       = "bminer"
+        Port      = $Port
+        Wrap      = $false
+        URI       = $Uri    
+        User      = $Pools.(Get-Algorithm($_)).User
     }
 }
