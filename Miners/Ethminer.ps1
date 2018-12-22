b837f0f9dcbcc0844588e5cf173549381e719a68
nemo <slbaker@hotmail.co.nz>
2018-12-22 14:27:33 +1300

    Update Ethminer.ps1
    rc2

--- Miners/Ethminer.ps1
+++ Miners/Ethminer.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
 $Path = ".\Bin\NVIDIA-Ethminer017a1\ethminer.exe"
-$Uri = "https://github.com/ethereum-mining/ethminer/releases/download/v0.17.0-alpha.1/ethminer-0.17.0-alpha.1-cuda10.0-windows-amd64.zip"
+$Uri = "https://github.com/ethereum-mining/ethminer/releases/download/v0.17.0-rc.2/ethminer-0.17.0-rc.2-cuda10.0-windows-amd64.zip"
 $Commands = [PSCustomObject]@{
     "ethash" = "" #Ethash(fastest)
 }
@@ -13,7 +13,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
         Type      = "NVIDIA"
         Path      = $Path
         Arguments = "--cuda-devices $($Config.SelGPUDSTM) --api-port -$Port -U -P stratum://$($Pools.(Get-Algorithm($_)).User):x@$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week} 
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day} 
         API       = "ethminer"
         Port      = $Port
         Wrap      = $false
