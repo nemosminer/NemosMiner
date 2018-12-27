bfe39498dc8776fce56bbff369a7cfc811a33e23
nemo <slbaker@hotmail.co.nz>
2018-12-28 09:33:51 +1300

    Create miniZ.ps1
    Test miniZ RTX support cuda 9.2 (API for get hashrate not done yet)

---
+++ OptionalMiners/miniZ.ps1
@@ -0,0 +1,23 @@
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
+ 
+$Path = ".\Bin\NVIDIA-miniZ1.0i\miner.exe"
+$Uri = "https://nemosminer.com/data/optional/miniZ_v1.0i_win-x64.7z"
+$Commands = [PSCustomObject]@{
+    #"zhash" = "" #Zhash(10/20 series cuda9.2 testing)
+}
+$Port = $Variables.NVIDIAMinerAPITCPPort
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    [PSCustomObject]@{
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "--latency --nocolor --tempunits C -cd $($Config.SelGPUDSTM) --telemetry $($Variables.NVIDIAMinerAPITCPPort) --pers auto --url $($Pools.(Get-Algorithm($_)).User)@$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) --pass $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day * .98} # substract 2% devfee
+        API       = "miniZ"
+        Port      = $Variables.NVIDIAMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri    
+        User      = $Pools.(Get-Algorithm($_)).User
+    }
+}
