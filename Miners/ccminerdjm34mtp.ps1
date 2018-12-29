72e2be3a363cbff225739b073959a3e7fe7fd2ba
nemo <slbaker@hotmail.co.nz>
2018-12-30 09:21:36 +1300

    thanks to krnlx for improvement
    implement new faster kernel from krnlx
    fix issue running on windows
    compiled with sm_52, sm_61 and sm_75
    fix high memory usage
    improved kernel for filling scratchpad on gpu

--- Miners/ccminerdjm34mtp.ps1
+++ Miners/ccminerdjm34mtp.ps1
@@ -1,8 +1,8 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
 
-$Path = ".\Bin\NVIDIA-djm34-mtp/ccmier.exe
-$Uri = "https://github.com/nemosminer/djm34mtpccminer/releases/download/1.1.4/djm34mtpccminer.7z"
+$Path = ".\Bin\NVIDIA-ccminermtp117/ccmier.exe
+$Uri = "https://github.com/nemosminer/djm34mtpccminer/releases/download/1.1.7/ccminerMTPkrnlx.7z"
 
 $Commands = [PSCustomObject]@{
     "mtp= "" #MTP
@@ -14,7 +14,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = "-b $($Variables.NVIDIAMinerAPITCPPort) -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        Arguments = "-d $($Config.SelGPUCC) -i 16 --quite -b $($Variables.NVIDIAMinerAPITCPPort) -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day
         API = "Ccminer"
         Port = $Variables.NVIDIAMinerAPITCPPort
