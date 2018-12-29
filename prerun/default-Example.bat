58127043a4690efa644c2118ce7ee7282d66bf54
nemo <slbaker@hotmail.co.nz>
2018-12-30 10:08:08 +1300

    Update default-Example.bat

--- prerun/default-Example.bat
+++ prerun/default-Example.bat
@@ -1,2 +1,5 @@
+REM This file will run for any algo if AlgoName.bat does not exist
+REM Place your code below and rename this file to default.bat
+
 REM nvidiaInspector.exe -setBaseClockOffset:0,0,50 -setMemoryClockOffset:0,0,450 -setVoltageOffset:0,0,0 -setPowerTarget:0,75 -setTempTarget:0,0,83 
 
