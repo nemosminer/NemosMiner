185d0916f4cd005b25cbf8d23f25796c1bd6c635
nemo <slbaker@hotmail.co.nz>
2018-12-30 05:55:34 +1300

    Update Gminer.ps1
    1.13

--- OptionalMiners/Gminer.ps1
+++ OptionalMiners/Gminer.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
-$Path = ".\Bin\NVIDIA-Gminer112\miner.exe"
-$Uri = "https://nemosminer.com/data/optional/gminer_1_12_minimal_windows64.7z"
+$Path = ".\Bin\NVIDIA-Gminer113\miner.exe"
+$Uri = "https://nemosminer.com/data/optional/gminer_1_13_minimal_windows64.7z"
 $Commands = [PSCustomObject]@{
     #"zhash" = "" #Zhash(working with 10series/ not RTX)
 }
