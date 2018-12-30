74db94a3a50a3b30f50035622f3d8f52fe05a6a1
nemo <slbaker@hotmail.co.nz>
2018-12-31 12:49:41 +1300

    Update CryptoDredge.ps1
    Improve MTP algorithm
    Stabilize MTP algorithm

--- OptionalMiners/CryptoDredge.ps1
+++ OptionalMiners/CryptoDredge.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\Bin\NVIDIA-CryptoDredge0151\CryptoDredge.exe"
-$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.15.1/CryptoDredge_0.15.1_cuda_10.0_windows.zip"
+$Path = ".\Bin\NVIDIA-CryptoDredge0152\CryptoDredge.exe"
+$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.15.2/CryptoDredge_0.15.2_cuda_10.0_windows.zip"
 
 $Commands = [PSCustomObject]@{
     "allium"            = " -a allium" #Allium (fastest)
