2c56000e14d4995d95393f0bafa72685be59bfe7
nemo <slbaker@hotmail.co.nz>
2018-12-31 14:57:11 +1300

    Update ethash-example.bat

--- prerun/ethash-example.bat
+++ prerun/ethash-example.bat
@@ -1,2 +1,2 @@
-REM nvidiaInspector.exe -setBaseClockOffset:0,0,25 -setMemoryClockOffset:0,0,950 -setVoltageOffset:0,0,0 -setPowerTarget:0,60 -setTempTarget:0,0,83 
+REM nvidiaInspector.exe -setBaseClockOffset:0,0,25 -setMemoryClockOffset:0,0,900 -setVoltageOffset:0,0,0 -setPowerTarget:0,60 -setTempTarget:0,0,83 
 
