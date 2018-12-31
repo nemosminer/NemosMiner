82edfd09c553b96c3d32b07bfbc813e62168c5ae
nemo <slbaker@hotmail.co.nz>
2018-12-31 21:29:32 +1300

    Update ccminermtp.ps1

--- Miners/ccminermtp.ps1
+++ Miners/ccminermtp.ps1
@@ -1,25 +1,25 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
 
-$Path = ".\Bin\NVIDIA-ccminermtp119/ccminer_sm61.exe
+$Path = ".\Bin\NVIDIA-ccminermtp119/ccminer_sm61.exe"
 $Uri = "https://github.com/nemosminer/djm34mtpccminer/releases/download/1.1.9/ccminermtp119.7z"
 
 $Commands = [PSCustomObject]@{
-    "mtp= "" #MTP
+    "mtp" = "" #MTP
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
     [PSCustomObject]@{
-        Type = "NVIDIA"
-        Path = $Path
+        Type      = "NVIDIA"
+        Path      = $Path
         Arguments = "-d $($Config.SelGPUCC) -i 16 --quite -b $($Variables.NVIDIAMinerAPITCPPort) -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day
-        API = "Ccminer"
-        Port = $Variables.NVIDIAMinerAPITCPPort
-        Wrap = $false
-        URI = $Uri
-	User = $Pools.(Get-Algorithm($_)).User
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
+        API       = "ccminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort #4068
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.(Get-Algorithm($_)).User
     }
 }
