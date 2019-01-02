ad326144b1d9d1479e4a827bfa2e2254e63ff8ca
nemo <slbaker@hotmail.co.nz>
2019-01-03 08:45:35 +1300

    Create ccminersupr.ps1
    add x16rt support

---
+++ Miners/ccminersupr.ps1
@@ -0,0 +1,25 @@
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
+
+$Path = ".\Bin\NVIDIA-ccminersupr20\ccminer.exe"
+$Uri = "https://github.com/ocminer/suprminer/releases/download/2.0/suprminer-2.0.7z"
+
+$Commands = [PSCustomObject]@{
+    "x16r" = " -d $($Config.SelGPUCC)" #X16r(testing)
+    "x16rt" = " -d $($Config.SelGPUCC)" #X16RT 
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-b $($Variables.NVIDIAMinerAPITCPPort) -R 1 -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day}
+        API = "ccminer"
+        Port = $Variables.NVIDIAMinerAPITCPPort
+        Wrap = $false
+        URI = $Uri
+        User = $Pools.(Get-Algorithm($_)).User
+    }
+}
