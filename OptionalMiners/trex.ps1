ffc0c45ab73ad7b70f30316ad265a23d200dfc44
nemo <slbaker@hotmail.co.nz>
2019-01-03 13:39:22 +1300

    Update trex.ps1

--- OptionalMiners/trex.ps1
+++ OptionalMiners/trex.ps1
@@ -1,72 +1,47 @@
-if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
 
 $Path = ".\Bin\NVIDIA-trex089\t-rex.exe"
 $Uri = "https://github.com/trexminer/T-Rex/releases/download/0.8.9/t-rex-0.8.9-win-cuda10.0.zip"
 
 $Commands = [PSCustomObject]@{
-<<<<<<< HEAD
 "balloon" = "" #Balloon(fastest)
 "astralhash" = "" #Astralhash
 "jeonghash" = "" #Jeonghash
 "padihash" = "" #Padihash
 "pawelhash" = "" #Pawelhash
-"polytimos" = "" #Poly 
-"bcd" = "" #Bcd (fastest)
-#"skunk" = "" #Skunk (CryptoDredge faster)
-"bitcore" = "" #Bitcore( fastest)
+"polytimos" = "" #Poly(fastest)
+"bcd" = "" #Bcd(fastest)
+"skunk" = "" #Skunk(fastest)
+"hsr" = "" #Hsr(Testing)
+"bitcore" = "" #Bitcore(fastest)
 "geek"= "" #Geekcash
-#"lyra2z" = "" #Lyra2z (Asic)
-#"tribus" = "" #Tribus (not profitable atm)
-"c11" = "" #C11 (fastest)
-"x17" = "" #X17 (fastest)
-"x16s" = "" #X16s (fastest)
-"x16r" = "" #X16r (fastest)
-"sonoa" = "" #SonoA (fastest)
-#"hmq1725" = "" #Hmq1725 (CryptoDredge faster)
-"dedal" = "" #Dedal (fastest)
-"sha256t" = "" #Sha256t (fastest)
-"x21s" = "" #X21s (fastest)
-"x22i" = "" #X22i (fastest)
-"timetravel" = "" #Timetravel (fastest)
-=======
-    "balloon"    = "" #Balloon(fastest)
-    "astralhash" = "" #Astralhash
-    "jeonghash"  = "" #Jeonghash
-    "padihash"   = "" #Padihash
-    "pawelhash"  = "" #Pawelhash
-    #"polytimos" = "" #Poly (zealotenemy faster)
-    "bcd"        = "" #Bcd (fastest)
-    #"skunk" = "" #Skunk (CryptoDredge faster)
-    "bitcore"    = "" #Bitcore( fastest)
-    "geek"       = "" #Geekcash
-    #"lyra2z" = "" #Lyra2z (Asic)
-    #"tribus" = "" #Tribus (not profitable atm)
-    "c11"        = "" #C11 (fastest)
-    "x17"        = "" #X17 (fastest)
-    "x16s"       = "" #X16s (fastest)
-    "x16r"       = "" #X16r (fastest)
-    "sonoa"      = "" #SonoA (fastest)
-    #"hmq1725" = "" #Hmq1725 (CryptoDredge faster)
-    "dedal"      = "" #Dedal (fastest)
-    "sha256t"    = "" #Sha256t (fastest)
-    "x21s"       = "" #X21s (fastest)
-    "x22i"       = "" #X22i (fastest)
-    "timetravel" = "" #Timetravel (fastest)
->>>>>>> parent of d33f8b9... Revert "tidy-up a little"
+"lyra2z" = "" #Lyra2z (cryptodredge faster)
+"tribus" = "" #Tribus(CryptoDredge faster)
+"c11" = "" #C11(fastest)
+"x17" = "" #X17(fastest)
+"x16s" = "" #X16s(fastest)
+"x16r" = "" #X16r(fastest)
+"sonoa" = "" #SonoA(fastest)
+"hmq1725" = "" #Hmq1725(fastest)
+"dedal" = "" #Dedal
+"sha256t" = "" #Sha256t(fastest)
+"x21s" = "" #X21s
+"x22i" = "" #X22i
+"timetravel" = "" #Timetravel(fastest)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
-$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
     [PSCustomObject]@{
-        Type      = "NVIDIA"
-        Path      = $Path
-        Arguments = "-b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) --quiet -r 10 --cpu-priority 5"
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) --api-bind-http 0 --quiet -r 10 --cpu-priority 5"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day * .99} # substract 1% devfee
-        API       = "ccminer"
-        Port      = $Variables.NVIDIAMinerAPITCPPort
-        Wrap      = $false
-        URI       = $Uri
-        User      = $Pools.(Get-Algorithm($_)).User
+        API = "ccminer"
+        Port = $Variables.NVIDIAMinerAPITCPPort
+        Wrap = $false
+        URI = $Uri
+        User = $Pools.(Get-Algorithm($_)).User
     }
 }
