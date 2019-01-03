eb42e1bee55f55defef79dd78855e3c66612ea86
nemosminer <slbaker@hotmail.co.nz>
2019-01-03 14:13:54 +1300

    Revert "Update zealotenemy.ps1"
    This reverts commit 96ad7f8df70ed625972c6363afc4db56f4583272.

--- OptionalMiners/zealotenemy.ps1
+++ OptionalMiners/zealotenemy.ps1
@@ -1,32 +1,25 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\Bin\NVIDIA-trex089\t-rex.exe"
-$Uri = "https://github.com/trexminer/T-Rex/releases/download/0.8.9/t-rex-0.8.9-win-cuda10.0.zip"
+$Path = ".\Bin\NVIDIA-zealotenemy128\z-enemy.exe"
+$Uri = "https://nemosminer.com/data/optional/z-enemy.1-28-cuda10.0.7z"
 
 $Commands = [PSCustomObject]@{
-    "balloon"    = "" #Balloon(fastest)
-    "astralhash" = "" #Astralhash
-    "jeonghash"  = "" #Jeonghash
-    "padihash"   = "" #Padihash
-    "pawelhash"  = "" #Pawelhash
-    "polytimos"  = "" #Poly 
-    "bcd"        = "" #Bcd (fastest)
-    #"skunk" = "" #Skunk (CryptoDredge faster)
-    "bitcore"    = "" #Bitcore( fastest)
-    "geek"       = "" #Geekcash
-    #"lyra2z" = "" #Lyra2z (Asic)
-    #"tribus" = "" #Tribus (not profitable atm)
-    "c11"        = "" #C11 (fastest)
-    "x17"        = "" #X17 (fastest)
-    "x16s"       = "" #X16s (fastest)
-    "x16r"       = "" #X16r (fastest)
-    "sonoa"      = "" #SonoA (fastest)
-    #"hmq1725" = "" #Hmq1725 (CryptoDredge faster)
-    "dedal"      = "" #Dedal (fastest)
-    "sha256t"    = "" #Sha256t (fastest)
-    "x21s"       = "" #X21s (fastest)
-    "x22i"       = "" #X22i (fastest)
-    "timetravel" = "" #Timetravel (fastest)
+    "aeriumx"    = "" #AeriumX(testing)
+    "bcd"        = "" #Bcd(trex faster/close tho)
+    #"phi"        = "" #Phi (CryptoDredge faster)
+    #"phi2"       = "" #Phi2 (CryptoDredge faster)
+    #"poly"       = "" #Polytimos(fastest) 
+    #"bitcore"    = "" #Bitcore (trex faster)
+    #"x16r"       = "" #X16r (trex faster)
+    #"x16s"       = "" #X16s (trex faster)
+    "sonoa"      = "" #SonoA (trex faster)
+    #"skunk"      = "" #Skunk (CryptoDredge faster)
+    #"timetravel" = "" #Timetravel (trex faster)
+    #"tribus"     = "" #Tribus (not profitable atm)
+    #"c11"        = "" #C11 (trex faster)
+    "xevan"      = "" #Xevan (fastest)
+    #"x17"        = "" #X17(trex faster)
+    "hex"        = "" #Hex (fastest)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -35,7 +28,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "-b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) --quiet -r 10 --cpu-priority 5"
+        Arguments = "-b $($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -R 1 -q -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day * .99} # substract 1% devfee
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort
