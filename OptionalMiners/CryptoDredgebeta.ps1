e052b4218015c6de89040adbc0805d6f3267d046
nemo <slbaker@hotmail.co.nz>
2019-01-05 11:46:09 +1300

    Create CryptoDredgebeta.ps1

---
+++ OptionalMiners/CryptoDredgebeta.ps1
@@ -0,0 +1,24 @@
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
+
+$Path = ".\Bin\NVIDIA-CryptoDredge0160\CryptoDredge.exe"
+$Uri = "https://nemosminer.com/data/optional/0.16.0-beta.7z"
+
+$Commands = [PSCustomObject]@{
+    "x16rt"            = " -a x16rt" #x16rt (testing)
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = " --api-type ccminer-tcp --no-color --intensity 6 --cpu-priority 5 --no-crashreport --no-watchdog -r -1 -R 1 -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day * .99} # substract 1% devfee
+        API       = "ccminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.(Get-Algorithm($_)).User
+    }
+}
