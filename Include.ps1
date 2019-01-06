80d8c6a566e90a2c30b9b74d9e7fd13ed8348615
nemo <slbaker@hotmail.co.nz>
2019-01-07 08:39:49 +1300

    Update Include.ps1

--- Include.ps1
+++ Include.ps1
@@ -14,8 +14,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 <#
 Product:        NemosMiner
 File:           include.ps1
-version:        3.6.2
-version date:   30 December 2018
+version:        3.6.3
+version date:   7 January 2019
 #>
 
 # New-Item -Path function: -Name ((Get-FileHash $MyInvocation.MyCommand.path).Hash) -Value {$true} -EA SilentlyContinue | out-null
