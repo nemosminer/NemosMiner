786354d4e33126f03ba474d41bd25e14cbc01003
nemo <slbaker@hotmail.co.nz>
2019-01-07 08:55:08 +1300

    Update ccminerKlausT.ps1
    YescryptR8 added to zpool already supported in previous version's of NemosMiner

--- Miners/ccminerKlausT.ps1
+++ Miners/ccminerKlausT.ps1
@@ -4,21 +4,6 @@ $Path = ".\Bin\NVIDIA-CcminerKlausTv10\ccminer.exe"
 $Uri = "https://github.com/nemosminer/ccminerKlausTyescrypt/releases/download/v10/ccminerKlausTyescryptv10.7z"
 
 $Commands = [PSCustomObject]@{
-    #"bitcore" = "" #Bitcore
-    #"blake2s" = "" #Blake2s
-    #"blakecoin" = " -d $($Config.SelGPUCC)" #Blakecoin
-    #"vanilla" = "" #BlakeVanilla
-    #"cryptonight" = "" #Cryptonight
-    #"decred" = "" #Decred
-    #"equihash" = "" #Equihash
-    #"ethash" = "" #Ethash
-    #"groestl" = " -r 0 -d $($Config.SelGPUCC)" #Groestl(fastest)
-    #"hmq1725" = "" #hmq1725
-    #"keccak" = " -d $($Config.SelGPUCC)" #Keccak
-    #"lbry" = "" #Lbry
-    #"lyra2v2" = " -d $($Config.SelGPUCC)" #Lyra2RE2
-    #"lyra2z" = "" #Lyra2z
-    #"myr-gr" = " -d $($Config.SelGPUCC)" #MyriadGroestl
     "yescrypt" = " -i 12.5 -d $($Config.SelGPUCC)" #Yescrypt
     "yescryptR8" = " -i 12.5 -d $($Config.SelGPUCC)" #YescryptR8
     "yescryptR16" = " -i 12.5 -d $($Config.SelGPUCC)" #YescryptR16
@@ -26,19 +11,7 @@ $Commands = [PSCustomObject]@{
    #"yescryptR32" = " -i 12.5 -d $($Config.SelGPUCC)" #YescryptR32 
     "yescryptR16v2" = " -i 12.5 -d $($Config.SelGPUCC)" #YescryptR16v2
     "neoscrypt" = " -i 17 -d $($Config.SelGPUCC)" #NeoScrypt
-    #"nist5" = " -d $($Config.SelGPUCC)" #Nist5
-    #"pascal" = "" #Pascal
-    #"qubit" = "" #Qubit
-    #"scrypt" = "" #Scrypt
-    #"sia" = "" #Sia
-    #"sib" = "" #Sib
-    #"skein" = " -i 28 -d $($Config.SelGPUCC)" #Skein
-    #"timetravel" = "" #Timetravel
-    #"x11" = "" #X11
-    #"veltor" = "" #Veltor
-    #"x11evo" = "" #X11evo
-    #"c11" = " -d $($Config.SelGPUCC)" #C11
-}
+  
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
