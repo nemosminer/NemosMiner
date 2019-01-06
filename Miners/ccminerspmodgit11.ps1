f64b0d01ca3259db9e59a9e634fa3f1c9674ac14
nemo <slbaker@hotmail.co.nz>
2019-01-07 08:51:55 +1300

    Update ccminerspmodgit11.ps1

--- Miners/ccminerspmodgit11.ps1
+++ Miners/ccminerspmodgit11.ps1
@@ -4,37 +4,7 @@ $Path = ".\Bin\NVIDIA-ccminerspmodgit11\ccminer.exe"
 $Uri = "https://github.com/sp-hash/suprminer/releases/download/spmod-git11/spmodgit11.7z"
 
 $Commands = [PSCustomObject]@{
-    #"phi" = " -d $($Config.SelGPUCC)" #Phi
-    #"bitcore" = " -d $($Config.SelGPUCC)" #Bitcore
-    #"jha" = " -d $($Config.SelGPUCC)" #Jha
-    #"blake2s" = " -d $($Config.SelGPUCC)" #Blake2s
-    #"blakecoin" = " -d $($Config.SelGPUCC)" #Blakecoin
-    #"vanilla" = "" #BlakeVanilla
-    #"cryptonight" = " -i 10.5 -l 8x120 --bfactor=8 -d $($Config.SelGPUCC) --api-remote" #Cryptonight
-    #"decred" = "" #Decred
-    #"equihash" = "" #Equihash
-    #"ethash" = "" #Ethash
-    #"groestl" = " -d $($Config.SelGPUCC)" #Groestl
-    #"hmq1725" = " -d $($Config.SelGPUCC)" #hmq1725
-    #"keccak" = "" #Keccak
-    #"lbry" = " -d $($Config.SelGPUCC)" #Lbry
-    #"lyra2v2" = "" #Lyra2RE2
-    #"lyra2z" = " -d $($Config.SelGPUCC) --api-remote --api-allow=0/0 --submit-stale" #Lyra2z
-    #"myr-gr" = "" #MyriadGroestl
-    #"neoscrypt" = " -d $($Config.SelGPUCC)" #NeoScrypt
-    #"nist5" = "" #Nist5
-    #"pascal" = "" #Pascal
-    #"qubit" = "" #Qubit
-    #"scrypt" = "" #Scrypt
-    #"sia" = "" #Sia
-    #"sib" = "" #Sib
-    #"skein" = "" #Skein
-    #"skunk" = " -d $($Config.SelGPUCC)" #Skunk
-    #"timetravel" = " -d $($Config.SelGPUCC)" #Timetravel
-    #"tribus" = " -d $($Config.SelGPUCC)" #Tribus
     "c11" = " -d $($Config.SelGPUCC)" #C11 (trex faster/ fastest open source)
-    #"veltor" = "" #Veltor
-    #"x11evo" = " -d $($Config.SelGPUCC)" #X11evo
     "x17" = " -d $($Config.SelGPUCC)" #X17 (trex faster/ fastest open source)
     "x16r" = " -d $($Config.SelGPUCC)" #X16r(trex faster/ fastest open source)
     "x16s" = " -d $($Config.SelGPUCC)" #X16s (trex faster/ fastest open source)
