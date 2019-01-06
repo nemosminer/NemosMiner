91ea3503563eb4903b71cd6fdaa0902aa78b8b48
nemo <slbaker@hotmail.co.nz>
2019-01-07 08:53:32 +1300

    Update ccminerTpruvot.ps1

--- Miners/ccminerTpruvot.ps1
+++ Miners/ccminerTpruvot.ps1
@@ -4,47 +4,16 @@ $Path = ".\Bin\NVIDIA-Tpruvotv2.3cuda10\ccminer.exe"
 $Uri = "https://github.com/nemosminer/ccminerTpruvot/releases/download/v2.3-cuda10/ccminertpruvotx32.7z"
 
 $Commands = [PSCustomObject]@{
-    #"polytimos"  = " -d $($Config.SelGPUCC)" #Polytimos
-    #"hsr" = " -d $($Config.SelGPUCC)" #Hsr
     "phi"        = " -d $($Config.SelGPUCC)" #Phi (testing)
-    #"phi2"       = " -d $($Config.SelGPUCC)" #Phi2 (testing)
     "allium"     = " -d $($Config.SelGPUCC)" #Allium
     "bitcore"    = " -d $($Config.SelGPUCC)" #Bitcore(spmodbitcore faster)
-    #"jha"        = " -d $($Config.SelGPUCC)" #Jha
-    #"blake2s" = " -d $($Config.SelGPUCC) --api-remote --api-allow=0/0" #Blake2s
-    #"x13" = " -d $($Config.SelGPUCC) -r 0 -i 20 -N 1" #X13
-    #"vanilla" = "" #BlakeVanilla
-    #"cryptonight" = " -i 10 -d $($Config.SelGPUCC)" #Cryptonight
-    #"decred" = "" #Decred
-    #"equihash" = "" #Equihash
-    #"ethash" = "" #Ethash
-    #"groestl"    = " -d $($Config.SelGPUCC)" #Groestl
     "hmq1725"    = " -d $($Config.SelGPUCC)" #hmq1725
-    #"keccakc"    = " -d $($Config.SelGPUCC) -i 29" #Keccakc
-    #"keccak"     = " -d $($Config.SelGPUCC) -i 29" #Keccak
-    #"lyra2v2"    = " -d $($Config.SelGPUCC)" #Lyra2RE2
-    #"lyra2z"     = " -d $($Config.SelGPUCC) --submit-stale" #Lyra2z
-    #"myr-gr"     = " -d $($Config.SelGPUCC)" #MyriadGroestl
-    #"neoscrypt"  = " -d $($Config.SelGPUCC)" #NeoScrypt
-    #"nist5" = " -d $($Config.SelGPUCC) --api-remote --api-allow=0/0" #Nist5
-    #"pascal" = "" #Pascal
-    #"qubit" = "" #Qubit
-    #"scrypt" = "" #Scrypt
     "sha256t"    = " -d $($Config.SelGPUCC) -i 29" #Sha256t
-    #"sia" = "" #Sia
-    #"sib"        = " -d $($Config.SelGPUCC)" #Sib
     "sonoa"      = " -d $($Config.SelGPUCC)" #Sonoa
     "skunk"      = " -d $($Config.SelGPUCC)" #Skunk
     "timetravel" = " -d $($Config.SelGPUCC)" #Timetravel
-    #"tribus"     = " -d $($Config.SelGPUCC)" #Tribus
-    #"c11"        = " -d $($Config.SelGPUCC)" #C11
-    #"veltor" = "" #Veltor
-    #"x11evo"     = " -d $($Config.SelGPUCC)" #X11evo (Alexis78 faster)
-    #"x17"        = " -d $($Config.SelGPUCC)" #X17(Enemy1.03 faster)
-    #"x16r"       = " -d $($Config.SelGPUCC)" #X16r
-    #"x16s"       = " -d $($Config.SelGPUCC)" #X16s
     "exosis"     = " -d $($Config.SelGPUCC)" #Exosis
-    #"yescrypt" = "" #Yescrypt
+
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
