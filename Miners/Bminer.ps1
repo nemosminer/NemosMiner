4e7dc4ee53acb1438f4f7e0a67c519e6afa95ebb
nemo <slbaker@hotmail.co.nz>
2019-01-11 19:48:32 +1300

    Update Bminer.ps1

--- Miners/Bminer.ps1
+++ Miners/Bminer.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
 $Path = ".\Bin\NVIDIA-Bminer114\bminer.exe"
-$Uri = "https://www.bminercontent.com/releases/bminer-lite-v11.4.0-5d7c753-CUDA-9.2-amd64.zipp"
+$Uri = "https://www.bminercontent.com/releases/bminer-lite-v11.4.0-5d7c753-CUDA-9.2-amd64.zip"
 $Commands = [PSCustomObject]@{
     #"equihashBTG" = " -uri zhash://" #EquihashBTG(miniZ faster)
     #"equihash" = " -uri stratum://" #Equihash(Asic)
