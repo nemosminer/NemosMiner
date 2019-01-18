ec1e1b1724e3f1685229fb451d5437f3df55102b
nemo <slbaker@hotmail.co.nz>
2019-01-18 19:54:41 +1300

    Update multiminer.ps1
    all done Happy Mining !

--- OptionalMiners/multiminer.ps1
+++ OptionalMiners/multiminer.ps1
@@ -4,7 +4,7 @@ $Path = ".\Bin\NVIDIA-multiminer1001201\multiminer.exe"
 $Uri = "https://github.com/bogdanadnan/multiminer/releases/download/v1.0.0/multiminer_v1.0.0_12.01.2019.zip"
 
 $Commands = [PSCustomObject]@{
-    #"argon2d-dyn" = "" #argon2d-dyn (testing)
+    "argon2d-dyn" = "" #argon2d-dyn (testing)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -13,7 +13,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "-o stratum+tcp://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -a $_ --use-gpu CUDA --gpu-batchsize=1024 -t 2 -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_)"
+        Arguments = "-b $($Variables.NVIDIAMinerAPITCPPort) -o stratum+tcp://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -a $_ --use-gpu CUDA --gpu-batchsize=1024 -t 2 -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day * .99} # substract 1% devfee
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort #4068
