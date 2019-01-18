a55a47cf1160f472ea0bde7d86aa24bbdc78af8a
nemo <slbaker@hotmail.co.nz>
2019-01-18 17:56:02 +1300

    Create multiminer.ps1

---
+++ OptionalMiners/multiminer.ps1
@@ -0,0 +1,26 @@
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
+
+$Path = ".\Bin\NVIDIA-multiminer1001201\cpuminer-aes-sse42.exe"
+$Uri = "https://github.com/bogdanadnan/multiminer/releases/download/v1.0.0/multiminer_v1.0.0_12.01.2019.zip"
+
+$Commands = [PSCustomObject]@{
+    #"argon2d-dyn" = " -a argon2d250" #argon2d-dyn (testing)
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    [PSCustomObject]@{
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "-o stratum+tcp://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm $_) = $Stats."$($Name)_$(Get-Algorithm $_)_HashRate".Day}
+        API       = "Ccminer"
+        Port      = $Variables.CPUMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.(Get-Algorithm($_)).User
+        Host      = $Pools.(Get-Algorithm $_).Host
+        Coin      = $Pools.(Get-Algorithm $_).Coin
+    }
+}
