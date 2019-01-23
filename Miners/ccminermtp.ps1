91d2cd1ef36910fbb44af39bcb2165c03b09fbc5
nemo <slbaker@hotmail.co.nz>
2019-01-24 08:55:38 +1300

    Update ccminermtp.ps1

--- Miners/ccminermtp.ps1
+++ Miners/ccminermtp.ps1
@@ -1,11 +1,11 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
 
-$Path = ".\Bin\NVIDIA-ccminermtp1110/ccminer_61.exe"
-$Uri = "https://github.com/nemosminer/djm34mtpccminer/releases/download/1.1.10/ccminermtp-v1.1.10.7z"
+$Path = ".\Bin\NVIDIA-ccminermtp1113/ccminer.exe"
+$Uri = "https://github.com/nemosminer/djm34mtpccminer/releases/download/v1.1.13/ccminermtpv1113.7z"
 
 $Commands = [PSCustomObject]@{
-    #"mtp" = "" #MTP
+    #"mtp" = " -i 20" #MTP (testing zergpool)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -14,7 +14,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "-d $($Config.SelGPUCC) -i 16 --quite -b $($Variables.NVIDIAMinerAPITCPPort) -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        Arguments = "-d $($Config.SelGPUCC) --quite -b $($Variables.NVIDIAMinerAPITCPPort) -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day}
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort #4068
