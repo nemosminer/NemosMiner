ed2ec9a6263e1d576dbf5070e0fcf67f969c2460
nemo <slbaker@hotmail.co.nz>
2019-01-24 17:10:55 +1300

    Update Gminer.ps1

--- OptionalMiners/Gminer.ps1
+++ OptionalMiners/Gminer.ps1
@@ -1,9 +1,9 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
-$Path = ".\Bin\NVIDIA-Gminer120\miner.exe"
-$Uri = "https://nemosminer.com/data/optional/gminer_1_20_minimal_windows64.7z"
+$Path = ".\Bin\NVIDIA-Gminer122\miner.exe"
+$Uri = "https://nemosminer.com/data/optional/gminer_1_22_minimal_windows64.7z"
 $Commands = [PSCustomObject]@{
-    #"cuckroo"      = " --devices $($Config.SelGPUDSTM) --algo grin29 --pers auto" #Equihash Cuckaroo29/GRIN (testing)
+    #"cuckaroo29"  = " --devices $($Config.SelGPUDSTM) --algo grin29 --pers auto" #Equihash Cuckaroo29/GRIN (testing)
     "equihash144"  = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Equihash144 (fastest)
     "zhash"        = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Zhash (fastest)
     "equihash192"  = " --devices $($Config.SelGPUDSTM) --algo 192_7 --pers ZERO_PoW" #Equihash192 (fastest)
