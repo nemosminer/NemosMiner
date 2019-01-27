e9acd27ad75e5204c2e413bd60dd70a17420166c
nemo <slbaker@hotmail.co.nz>
2019-01-28 10:29:21 +1300

    Update EnergiMiner.ps1
    running now

--- Miners/EnergiMiner.ps1
+++ Miners/EnergiMiner.ps1
@@ -4,7 +4,7 @@ $Path = ".\Bin\NVIDIA-EnergiMiner221\energiminer.exe"
 $Uri = "https://nemosminer.com/data/optional/energiminer-2.2.1-Windows.7z"
 
 $Commands = [PSCustomObject]@{
-    #"nrghash"        = "" #Nrghash (testing)
+    "nrghash" = "" #Nrghash (testing)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -13,7 +13,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "--cuda-devices $($Config.SelGPUDSTM) stratum+tcp://$($Pools.(Get-Algorithm($_)).User):c=BTC@nrghash.mine.zergpool.com:3100 -U --response-timeout 10 --cuda-parallel-hash 8 --cuda-block-size 256"
+        Arguments = "--cuda-devices $($Config.SelGPUDSTM) -U stratum://$($Pools.(Get-Algorithm($_)).User).$($Pools.(Get-Algorithm($_)).Pass)@nrghash.mine.zergpool.com:$($Pools.(Get-Algorithm($_)).Port)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day}
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort
