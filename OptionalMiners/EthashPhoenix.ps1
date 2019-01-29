9aad263b35fcdf9f4b53f932f89bf99b7d05b4e7
nemo <slbaker@hotmail.co.nz>
2019-01-30 09:11:04 +1300

    Update EthashPhoenix.ps1
    checking 20 series support

--- OptionalMiners/EthashPhoenix.ps1
+++ OptionalMiners/EthashPhoenix.ps1
@@ -1,8 +1,8 @@
 
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\\Bin\\Ethash-Phoenix41c\\PhoenixMiner.exe"
-$Uri = "https://nemosminer.com/data/optional/PhoenixMiner_4.1c_Windows.7z"
+$Path = ".\\Bin\\Ethash-Phoenix40b\\PhoenixMiner.exe"
+$Uri = "https://nemosminer.com/data/optional/PhoenixMiner_4.0b_Windows.7z"
 $Commands = [PSCustomObject]@{
     #"ethash" = " -di $($($Config.SelGPUCC).Replace(',',''))" #Ethash(9/10 series only no 20series support)
 }
