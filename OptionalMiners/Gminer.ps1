6b57b764c9d5e9f00e0a6c94e92455e7c3562034
nemo <slbaker@hotmail.co.nz>
2019-01-30 09:30:06 +1300

    Update Gminer.ps1

--- OptionalMiners/Gminer.ps1
+++ OptionalMiners/Gminer.ps1
@@ -9,7 +9,7 @@ $Commands = [PSCustomObject]@{
     "equihash192"  = " --devices $($Config.SelGPUDSTM) --algo 192_7 --pers ZERO_PoW" #Equihash192 (fastest)
     "equihash-btg" = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers BgoldPoW" # Equihash-btg (fastest)
     "equihash96"   = " --devices $($Config.SelGPUDSTM) --algo 96_5 --pers auto" #Equihash96 (fastest)
-    #"beam"        = " --devices $($Config.SelGPUDSTM) --algo 150_5 --pers Beam-PoW" #Equihash150 (Gminer Dev is onto it)
+    "beam"        = " --devices $($Config.SelGPUDSTM) --algo 150_5 --pers Beam-PoW" #Equihash150 (fastest)
 }
 $Port = $Variables.NVIDIAMinerAPITCPPort
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
