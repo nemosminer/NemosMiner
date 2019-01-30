45e1a9cfa591785339e5d93ce49f0f9be7e80025
nemo <slbaker@hotmail.co.nz>
2019-01-31 09:26:01 +1300

    Update ccminerKlausT-TR32.ps1

--- Miners/ccminerKlausT-TR32.ps1
+++ Miners/ccminerKlausT-TR32.ps1
@@ -1,6 +1,6 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\Bin\NVIDIA-CcminerKlausTv10-YR32\ccminer.exe"
+$Path = ".\Bin\NVIDIA-CcminerKlausT10R32\ccminer.exe"
 $Uri = "https://github.com/nemosminer/ccminerKlausTyescrypt/releases/download/v10/ccminerKlausTyescryptv10.7z"
 
 $Commands = [PSCustomObject]@{
