be535c51e0d04998a342c02b1228f4bb292617e1
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-01-30 17:55:25 +0100

    Add files via upload

---
+++ OptionalMiners/GMiner.ps1
@@ -0,0 +1,31 @@
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
+ 
+$Path = ".\Bin\NVIDIA-Gminer\miner.exe"
+$Uri = "https://nemosminer.com/data/optional/gminer_1_25_minimal_windows64.7z"
+$Commands = [PSCustomObject]@{
+    "equihash144"  = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Equihash144 (fastest)
+    "zhash"        = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Zhash (fastest)
+    "equihash192"  = " --devices $($Config.SelGPUDSTM) --algo 192_7 --pers ZERO_PoW" #Equihash192 (fastest)
+    "equihash-btg" = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers BgoldPoW" # Equihash-btg (fastest)
+    "equihash96"   = " --devices $($Config.SelGPUDSTM) --algo 96_5 --pers auto" #Equihash96 (fastest)
+    "beam"         = " --devices $($Config.SelGPUDSTM) --algo 150_5 --pers Beam-PoW" #Equihash150 (fastest)
+}
+$Port = $Variables.NVIDIAMinerAPITCPPort
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+	$Algo = Get-Algorithm($_)
+    [PSCustomObject]@{
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "-t 95 --watchdog 0 --api $($Variables.NVIDIAMinerAPITCPPort) --server $($Pools.($Algo).Host) --port $($Pools.($Algo).Port) --user $($Pools.($Algo).User) --pass $($Pools.($Algo).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * .98} # substract 2% devfee
+        API       = "gminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri    
+        User = $Pools.($Algo).User
+        Host = $Pools.($Algo).Host
+        Coin = $Pools.($Algo).Coin
+    }
+}
--- OptionalMiners/Zjazz.ps1
+++ OptionalMiners/Zjazz.ps1
@@ -4,24 +4,27 @@ $Path = ".\Bin\NVIDIA-zjazz12\zjazz_cuda.exe"
 $Uri = "https://github.com/zjazz/zjazz_cuda_miner/releases/download/1.2/zjazz_cuda_win64_1.2.zip"
 
 $Commands = [PSCustomObject]@{
-    #"bitcash" = " -a bitcash" #Bitcash (testing)
-    #"cuckoo"  = " -a bitcash --cuckoo-intensity 22" #Cuckoo (tested fastest/10series only not running on RTX)
+    # "bitcash" = " -a bitcash" #Bitcash (testing)
+    "cuckoo"  = " -a bitcash --cuckoo-intensity 22 " #Cuckoo (testing nlpool) --cuckoo-cpu-assist-min 
     #"x22i"    = " -a x22i" #SUQA (testing)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    $Algo = Get-Algorithm($_)
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "-d $($Config.SelGPUDSTM.Replace(' ', ' -d ')) --api-bind $($Variables.NVIDIAMinerAPITCPPort) -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day * .98} # substract 2% devfee
-        API       = "zjazz"
+        Arguments = "-d $($Config.SelGPUDSTM.Replace(' ', ' -d ')) --api-bind $($Variables.NVIDIAMinerAPITCPPort) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".week * .98 * 2000} # substract 2% devfee + Temp fix for nlpool wrong hashrate
+        API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort #4068
         Wrap      = $false
         URI       = $Uri
-        User      = $Pools.(Get-Algorithm($_)).User
+        User      = $Pools.($Algo).User
+        Host      = $Pools.($Algo).Host
+        Coin      = $Pools.($Algo).Coin
         PreventCPUMining      = $true
     }
 }
