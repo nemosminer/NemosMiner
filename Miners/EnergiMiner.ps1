503c8d3ad6548b75303955638e42045248478927
nemo <slbaker@hotmail.co.nz>
2019-02-02 10:30:48 +1300

    Update EnergiMiner.ps1

--- Miners/EnergiMiner.ps1
+++ Miners/EnergiMiner.ps1
@@ -1,6 +1,6 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\Bin\NVIDIA-EnergiMiner\energiminer.exe"
+$Path = ".\Bin\NVIDIA-EnergiMiner221\energiminer.exe"
 $Uri = "https://nemosminer.com/data/optional/energiminer-2.2.1-Windows.7z"
 
 $Commands = [PSCustomObject]@{
@@ -15,7 +15,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
         Type      = "NVIDIA"
         Path      = $Path
         Arguments = "--response-timeout 10 --cuda-parallel-hash 8 --cuda-block-size 256 --cuda-devices $($Config.SelGPUDSTM) -U stratum://$($Pools.($Algo).User):$($Pools.($Algo).Pass.ToString().replace(',','%2C'))@nrghash.mine.zergpool.com:$($Pools.($Algo).Port)"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Week}
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
         API       = "wrapper"
         Port      = $Variables.NVIDIAMinerAPITCPPort #4068
         Wrap      = $true
