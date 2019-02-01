a6016b7e3b09beb750c29d88717d790f4a783c0d
nemo <slbaker@hotmail.co.nz>
2019-02-02 11:00:42 +1300

    Update ccminerTpruvot.ps1

--- Miners/ccminerTpruvot.ps1
+++ Miners/ccminerTpruvot.ps1
@@ -46,7 +46,7 @@ $Commands = [PSCustomObject]@{
     #"x17"        = " -d $($Config.SelGPUCC)" #X17(Enemy1.03 faster)
     #"yescrypt" = "" #Yescrypt
     "sha256q"       = " -d $($Config.SelGPUCC)" #Sha256q
-    #"lyra2v3"       = " -d $($Config.SelGPUCC)" #Lyra2v3
+    "lyra2v3"       = " -d $($Config.SelGPUCC)" #Lyra2v3 (testing NiceHash)
     #"blake2b"       = " -d $($Config.SelGPUCC)" #blake2b
 }
 
