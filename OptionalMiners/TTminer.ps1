1181613a64dfc6db479214376b1ed4da6285f56f
nemo <slbaker@hotmail.co.nz>
2019-02-04 08:19:00 +1300

    add TTminer
    progpow support ZergPool

---
+++ OptionalMiners/TTminer.ps1
@@ -0,0 +1,27 @@
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
+
+$Path = ".\Bin\NVIDIA-TTMiner218\TT-Miner.exe"
+$Uri = "https://tradeproject.de/download/Miner/TT-Miner.zip"
+
+$Commands = [PSCustomObject]@{
+    "progpow"  = ""
+}
+ 
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    $Algo = Get-Algorithm($_)
+    [PSCustomObject]@{
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "-a $($Algo) -d $($Config.SelGPUDSTM) --api-bind 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".day * .98} # substract 2% devfee
+        API       = "TTminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort #4068
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.($Algo).User
+        Host      = $Pools.($Algo).Host
+        Coin      = $Pools.($Algo).Coin
+    }
+}
