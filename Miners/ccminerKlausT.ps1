68eced67eb0f42f2bf7f7037e44f47d55b755326
nemo <slbaker@hotmail.co.nz>
2019-02-03 18:02:18 +1300

    Update ccminerKlausT.ps1
    add lyra2v3 support fast

--- Miners/ccminerKlausT.ps1
+++ Miners/ccminerKlausT.ps1
@@ -1,15 +1,15 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\Bin\NVIDIA-CcminerKlausT10\ccminer.exe"
-$Uri = "https://github.com/nemosminer/ccminerKlausTyescrypt/releases/download/v10/ccminerKlausTyescryptv10.7z"
+$Path = ".\Bin\NVIDIA-CcminerKlausT\ccminer.exe"
+$Uri = "https://github.com/nemosminer/ccminer-KlausT-8.21-mod-r18-src-fix/releases/download/8.21-r18-fix%2Blyra2v3/ccminer-8.21-yescrypt-algos+lyra2v3.7z"
 
 $Commands = [PSCustomObject]@{
-    "neoscrypt" = " -i 17 -d $($Config.SelGPUCC)" #NeoScrypt
-    "yescrypt" = " -i 12.5 -d $($Config.SelGPUCC)" #Yescrypt
-    "yescryptR16" = " -i 12.5 -d $($Config.SelGPUCC)" #YescryptR16
-    "yescryptR16v2" = " -i 12.5 -d $($Config.SelGPUCC)" #YescryptR16v2
-    "yescryptR24" = " -i 12.5 -d $($Config.SelGPUCC)" #YescryptR24 
-    "yescryptR8" = " -i 12.5 -d $($Config.SelGPUCC)" #YescryptR8
+    "neoscrypt" = " -a neoscrypt -i 17 -d $($Config.SelGPUCC)" #NeoScrypt
+    "yescrypt" = " -a yescrypt -i 12.5 -d $($Config.SelGPUCC)" #Yescrypt
+    "yescryptR16" = " -a yescryptr16 -i 12.5 -d $($Config.SelGPUCC)" #YescryptR16
+    "yescryptR16v2" = " -a yescryptr16v2 -i 12.5 -d $($Config.SelGPUCC)" #YescryptR16v2
+    "yescryptR24" = " -a yescryptr24 -i 12.5 -d $($Config.SelGPUCC)" #YescryptR24 
+    "yescryptR8" = " -a yescryptr8 -i 12.5 -d $($Config.SelGPUCC)" #YescryptR8
     #"bitcore" = "" #Bitcore
     #"blake2s" = "" #Blake2s
     #"blakecoin" = " -d $($Config.SelGPUCC)" #Blakecoin
@@ -22,7 +22,8 @@ $Commands = [PSCustomObject]@{
     #"hmq1725" = "" #hmq1725
     #"keccak" = " -d $($Config.SelGPUCC)" #Keccak
     #"lbry" = "" #Lbry
-    #"lyra2v2" = " -d $($Config.SelGPUCC)" #Lyra2RE2
+    "lyra2v3" = " -a lyra2v3 -i 24 -d $($Config.SelGPUCC)" #Lyra2v3
+    "lyra2rev3" = " -a lyra2v3 -i 24 -d $($Config.SelGPUCC)" #Lyra2rev3
     #"lyra2z" = "" #Lyra2z
     #"myr-gr" = " -d $($Config.SelGPUCC)" #MyriadGroestl
     #"nist5" = " -d $($Config.SelGPUCC)" #Nist5
@@ -47,7 +48,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = "--cpu-priority 5 -b $($Variables.NVIDIAMinerAPITCPPort) -N 1 -R 1 -a $_ -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        Arguments = "--cpu-priority 5 -b $($Variables.NVIDIAMinerAPITCPPort) -N 1 -R 1 -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
         API = "ccminer"
         Port = $Variables.NVIDIAMinerAPITCPPort
