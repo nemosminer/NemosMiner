33bb41b7006e5d3ce9a41b0687e517a7a35df9d1
nemo <slbaker@hotmail.co.nz>
2019-02-04 08:11:39 +1300

    Yay
    Enable Beam and grincuckaroo29

--- OptionalMiners/Gminer.ps1
+++ OptionalMiners/Gminer.ps1
@@ -1,15 +1,15 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
-$Path = ".\Bin\NVIDIA-Gminer125\miner.exe"
-$Uri = "https://nemosminer.com/data/optional/gminer_1_25_minimal_windows64.7z"
+$Path = ".\Bin\NVIDIA-Gminer128\miner.exe"
+$Uri = "https://nemosminer.com/data/optional/gminer_1_28_minimal_windows64.7z"
 $Commands = [PSCustomObject]@{
     "equihash144"  = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Equihash144 (fastest)
     "zhash"        = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Zhash (fastest)
     "equihash192"  = " --devices $($Config.SelGPUDSTM) --algo 192_7 --pers ZERO_PoW" #Equihash192 (fastest)
     "equihash-btg" = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers BgoldPoW" # Equihash-btg (fastest)
     "equihash96"   = " --devices $($Config.SelGPUDSTM) --algo 96_5 --pers auto" #Equihash96 (fastest)
-    #"beam"         = " --devices $($Config.SelGPUDSTM) --algo 150_5 --pers Beam-PoW" #Equihash150 (Disable as Nicehash recommends)
-    #"grincuckaroo29"  = " --devices $($Config.SelGPUDSTM) --algo grin29 --pers auto" #Grincuckaroo29 (testing Nicehash)
+    "beam"         = " --devices $($Config.SelGPUDSTM) --algo 150_5 --pers Beam-PoW" #Equihash150 (fastest)
+    "grincuckaroo29"  = " --devices $($Config.SelGPUDSTM) --algo grin29 --pers auto" #Grincuckaroo29 (fastest)
 }
 $Port = $Variables.NVIDIAMinerAPITCPPort
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
