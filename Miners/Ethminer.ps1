9f8a3bf21cbc0bb783079ce19a00d930d2d2c204
nemo <slbaker@hotmail.co.nz>
2019-02-11 09:48:31 +1300

    Update Ethminer.ps1

--- Miners/Ethminer.ps1
+++ Miners/Ethminer.ps1
@@ -1,9 +1,9 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
-$Path = ".\Bin\NVIDIA-Ethminer0170a1\ethminer.exe"
-$Uri = "https://github.com/ethereum-mining/ethminer/releases/download/v0.17.0-alpha.1/ethminer-0.17.0-alpha.1-cuda10.0-windows-amd64.zip"
+$Path = ".\Bin\NVIDIA-Ethminer0170\ethminer.exe"
+$Uri = "https://github.com/ethereum-mining/ethminer/releases/download/v0.17.0/ethminer-0.17.0-cuda10.0-windows-amd64.zip"
 $Commands = [PSCustomObject]@{
-    #"ethash" = "" #Ethash(fastest)
+    #"ethash" = "" #Ethash
 }
 $Port = $Variables.NVIDIAMinerAPITCPPort
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -14,7 +14,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
         Type      = "NVIDIA"
         Path      = $Path
         Arguments = "--cuda-devices $($Config.SelGPUDSTM) --api-port -$Port -U -P stratum://$($Pools.($Algo).User):x@$($Pools.($Algo).Host):$($Pools.($Algo).Port)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Week} 
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day} 
         API       = "ethminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort
         Wrap      = $false
