fcd90f3a854798269b60853d68837c04e4ca422f
platinum4 <platinumstephen@gmail.com>
2019-02-10 06:56:48 -0600

    Ethminer 0.17.0 final release instead of alpha

--- Miners/Ethminer.ps1
+++ Miners/Ethminer.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
-$Path = ".\Bin\NVIDIA-Ethminer0170a1\ethminer.exe"
-$Uri = "https://github.com/ethereum-mining/ethminer/releases/download/v0.17.0-alpha.1/ethminer-0.17.0-alpha.1-cuda10.0-windows-amd64.zip"
+$Path = ".\Bin\NVIDIA-Ethminer0170\ethminer.exe"
+$Uri = "https://github.com/ethereum-mining/ethminer/releases/download/v0.17.0/ethminer-0.17.0-cuda10.0-windows-amd64.zip"
 $Commands = [PSCustomObject]@{
     #"ethash" = "" #Ethash(fastest)
 }
