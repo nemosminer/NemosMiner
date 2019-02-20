983d7494fbfb26c7dfce2451089f831367a9c37e
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-02-20 19:13:42 +0100

    Create ccminerMtp-NH.ps1
    Add NiceHash MTP support

---
+++ Miners/ccminerMtp-NH.ps1
@@ -0,0 +1,28 @@
+if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1")}
+
+$Path = ".\Bin\NVIDIA-ccminermtp-NH\ccminer.exe"
+$Uri = "https://github.com/MrPlusGH/ccminer-mtp/releases/download/1.1.12/ccminermtp.zip"
+
+$Commands = [PSCustomObject]@{
+    "mtp" = "" #mtp
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+	$Algo = Get-Algorithm($_)
+    If ($Pools.($Algo).Host -notlike "*nicehash*") {return}
+    [PSCustomObject]@{
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "-b $($Variables.NVIDIAMinerAPITCPPort) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -a $Algo -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Week}
+        API       = "ccminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort #4068
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.($Algo).User
+        Host      = $Pools.($Algo).Host
+        Coin      = $Pools.($Algo).Coin
+    }
+}
