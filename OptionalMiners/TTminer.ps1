9135d16dc1672a5facb2721a6a16aa7f359b296d
nemo <slbaker@hotmail.co.nz>
2019-02-21 10:00:54 +1300

    Update TTminer.ps1
    Enable MTP on NiceHash with TTminer

--- OptionalMiners/TTminer.ps1
+++ OptionalMiners/TTminer.ps1
@@ -5,9 +5,9 @@ $Uri = "https://tradeproject.de/download/Miner/TT-Miner-2.1.11-beta7.zip"
 
 $Commands = [PSCustomObject]@{  
     "mtp"     = "-A MTP-100" #(Supports NiceHash)  
-    "ethash"  = "-A ETHASH-100" #(Supports NiceHash)
-    "progpow" = "-A PROGPOW-100" #(Supports NiceHash)
-    "lyra2v3" = "-A LYRA2V3-100" #(Supports NiceHash)
+    #"ethash"  = "-A ETHASH-100" #(Supports NiceHash)
+    #"progpow" = "-A PROGPOW-100" #(Supports NiceHash)
+    #"lyra2v3" = "-A LYRA2V3-100" #(Supports NiceHash)
 }
  
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
