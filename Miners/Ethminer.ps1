6a40136f4b0630fe7e735d6d28d7ca73a0489a4d
nemo <slbaker@hotmail.co.nz>
2019-02-21 05:57:09 +1300

    Update Ethminer.ps1

--- Miners/Ethminer.ps1
+++ Miners/Ethminer.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
-$Path = ".\Bin\NVIDIA-Ethminer0170\ethminer.exe"
-$Uri = "https://github.com/ethereum-mining/ethminer/releases/download/v0.17.0/ethminer-0.17.0-cuda10.0-windows-amd64.zip"
+$Path = ".\Bin\NVIDIA-Ethminer0171\ethminer.exe"
+$Uri = "https://github.com/ethereum-mining/ethminer/releases/download/v0.17.1-rc.0/ethminer-0.17.1-rc.0-cuda10.0-windows-amd64.zip"
 $Commands = [PSCustomObject]@{
     #"ethash" = "" #Ethash
 }
