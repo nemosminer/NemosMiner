6ca20200a6bf1ab499a7c8c3adb0fde969cf7a32
nemo <slbaker@hotmail.co.nz>
2019-02-22 09:10:19 +1300

    Update Gminer.ps1
    beta tested Update to Official v1.34 Release

--- OptionalMiners/Gminer.ps1
+++ OptionalMiners/Gminer.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
 $Path = ".\Bin\NVIDIA-Gminer134\miner.exe"
-$Uri = "https://github.com/develsoftware/GMinerBetaRelease/releases/download/1.34/gminer_1_34_minimal_windows64.zip"
+$Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.34/gminer_1_34_minimal_windows64.zip"
 $Commands = [PSCustomObject]@{
     "equihash144"  = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Equihash144 (fastest)
     "zhash"        = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Zhash (fastest)
