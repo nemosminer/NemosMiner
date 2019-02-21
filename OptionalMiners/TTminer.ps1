1a86b121f53fc196e4baede627ab30fefbd0b6b5
nemo <slbaker@hotmail.co.nz>
2019-02-22 06:01:00 +1300

    Update TTminer.ps1

--- OptionalMiners/TTminer.ps1
+++ OptionalMiners/TTminer.ps1
@@ -1,6 +1,6 @@
-if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1")}
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\Bin\NVIDIA-TTMiner\TT-Miner.exe"
+$Path = ".\Bin\NVIDIA-TTMiner2111b\TT-Miner.exe"
 $Uri = "https://tradeproject.de/download/Miner/TT-Miner-2.1.11-beta7.zip"
 
 $Commands = [PSCustomObject]@{
