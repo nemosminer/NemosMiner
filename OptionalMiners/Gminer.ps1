c730c107d63d765b6b50ca96fda95c1757072a1e
Attila <borzak.attila@gmail.com>
2019-02-24 10:28:46 +0100

    Change --pers to auto
    I can lose hash if the pool switches to another coin within the same algo.

--- OptionalMiners/Gminer.ps1
+++ OptionalMiners/Gminer.ps1
@@ -5,10 +5,10 @@ $Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.34/gm
 $Commands = [PSCustomObject]@{
     "equihash144"  = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Equihash144 (fastest)
     "zhash"        = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Zhash (fastest)
-    "equihash192"  = " --devices $($Config.SelGPUDSTM) --algo 192_7 --pers ZERO_PoW" #Equihash192 (fastest)
-    "equihash-btg" = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers BgoldPoW" # Equihash-btg (fastest)
+    "equihash192"  = " --devices $($Config.SelGPUDSTM) --algo 192_7 --pers auto" #Equihash192 (fastest)
+    "equihash-btg" = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" # Equihash-btg (fastest)
     "equihash96"   = " --devices $($Config.SelGPUDSTM) --algo 96_5 --pers auto" #Equihash96 (fastest)
-    #"beam"         = " --devices $($Config.SelGPUDSTM) --algo 150_5 --pers Beam-PoW" #Equihash150 (Bminer faster)
+    #"beam"         = " --devices $($Config.SelGPUDSTM) --algo 150_5 --pers auto" #Equihash150 (Bminer faster)
     #"grincuckaroo29"  = " --devices $($Config.SelGPUDSTM) --algo grin29 --pers auto" #Grincuckaroo29 (fastest on 20 series, Bminer faster on 10series)
     #"grincuckatoo31"  = " --devices $($Config.SelGPUDSTM) --algo grin31 --pers auto" #Grincuckatoo31 requires 7.4GB VRam, will work on 8GB cards under Linux and Windows 7, will not work under Windows 10)
  
