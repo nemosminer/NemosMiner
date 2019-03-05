4720d0e1b37bf28c8b50ed0ff788f7b7bb67b5dc
nemo <slbaker@hotmail.co.nz>
2019-03-06 09:49:03 +1300

    v15.2.0
    Improve performance and stability of Cuckatoo31.
    Support RTX 2080 for Cuckatoo31.

--- Miners/Bminer.ps1
+++ Miners/Bminer.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
-$Path = ".\Bin\NVIDIA-Bminer1510\bminer.exe"
-$Uri = "https://www.bminercontent.com/releases/bminer-lite-v15.1.0-2d30b6f-amd64.zip"
+$Path = ".\Bin\NVIDIA-Bminer1520\bminer.exe"
+$Uri = "https://www.bminercontent.com/releases/bminer-lite-v15.2.0-c1d0f59-amd64.zip"
 $Commands = [PSCustomObject]@{
     #"equihashBTG" = " -uri zhash://" #EquihashBTG(miniZ faster)
     #"equihash" = " -uri stratum://" #Equihash(Asic)
