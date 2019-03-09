b67cedd34d32b0fffc3be5eadf0d55502a8dc5eb
nemo <slbaker@hotmail.co.nz>
2019-03-09 13:08:53 +1300

    add 6x RTX 2080 OC.bat

---
+++ prerun/nemos2080oc.bat
@@ -0,0 +1,6 @@
+nvidiaInspector.exe -setBaseClockOffset:0,0,125 -setMemoryClockOffset:0,0,250 -setVoltageOffset:0,0,0 -setPowerTarget:0,62 -setTempTarget:0,0,85 
+nvidiaInspector.exe -setBaseClockOffset:0,1,125 -setMemoryClockOffset:0,1,250 -setVoltageOffset:0,1,0 -setPowerTarget:1,62 -setTempTarget:0,1,85
+nvidiaInspector.exe -setBaseClockOffset:0,2,125 -setMemoryClockOffset:0,2,250 -setVoltageOffset:0,2,0 -setPowerTarget:2,62 -setTempTarget:0,2,85
+nvidiaInspector.exe -setBaseClockOffset:0,3,125 -setMemoryClockOffset:0,3,250 -setVoltageOffset:0,3,0 -setPowerTarget:3,62 -setTempTarget:0,3,85
+nvidiaInspector.exe -setBaseClockOffset:0,4,125 -setMemoryClockOffset:0,4,250 -setVoltageOffset:0,4,0 -setPowerTarget:4,62 -setTempTarget:0,4,85
+nvidiaInspector.exe -setBaseClockOffset:0,5,125 -setMemoryClockOffset:0,5,250 -setVoltageOffset:0,5,0 -setPowerTarget:5,62 -setTempTarget:0,5,85
--- prerun/skunk-example.bat
+++
@@ -1,2 +0,0 @@
-REM nvidiaInspector.exe -setBaseClockOffset:0,0,75 -setMemoryClockOffset:0,0,0 -setVoltageOffset:0,0,0 -setPowerTarget:0,65 -setTempTarget:0,0,83 
-
