c6e02a2ffaad3cd0497667959d62e69a83a18201
nemo <slbaker@hotmail.co.nz>
2019-03-10 13:11:46 +1300

    Update ccminerAlexis78.ps1

--- Miners/ccminerAlexis78.ps1
+++ Miners/ccminerAlexis78.ps1
@@ -9,6 +9,7 @@ $Commands = [PSCustomObject]@{
     #"lyra2v2" = " -d $($Config.SelGPUCC) -N 1" #Lyra2RE2
     #"poly" = " -N 1 -d $($Config.SelGPUCC)" #polytimos
     #"skein" = " -i 28 -N 1 -d $($Config.SelGPUCC)" #Skein
+    #"skein2" = " -i 28 -N 1 -d $($Config.SelGPUCC)" #Skein2
     #"x11evo" = " -N 1 -i 21 -d $($Config.SelGPUCC) " #X11evo
     #"bitcore" = "" #Bitcore
     #"blake2s" = " -r 0 -d $($Config.SelGPUCC)" #Blake2s
@@ -47,7 +48,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
         Type = "NVIDIA"
         Path = $Path
         Arguments = "-b $($Variables.NVIDIAMinerAPITCPPort) -R 1 -a $_ -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Week}
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
         API = "ccminer"
         Port = $Variables.NVIDIAMinerAPITCPPort #4068
         Wrap = $false
