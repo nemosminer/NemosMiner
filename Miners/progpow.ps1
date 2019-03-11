93989371eda29d1993a4baaf61679bd5a0595ddd
nemo <slbaker@hotmail.co.nz>
2019-03-11 14:14:21 +1300

    Update progpow.ps1

--- Miners/progpow.ps1
+++ Miners/progpow.ps1
@@ -4,7 +4,12 @@ $Path = ".\Bin\NVIDIA-progpowminer016\progpowminer-cuda.exe"
 $Uri = "https://nemosminer.com/data/optional/progpowminer0.16-FinalCuda10.7z"
 
 $Commands = [PSCustomObject]@{
-     "progpow" = ""
+       #"progpow"  = " -a PROGPOW-100" # (progpowminer faster + 0% fee)
+       #"mtp"      = " -a MTP-100 " # (ccminer faster + 0% fee)
+       #"ethash"   = " -a ETHASH-100 "# (Bminer/Ethminer faster + 0% fee)
+       #"ubqhash"  = " -a UBQHASH-100 "
+       #"myr-gr"   = " -a MYRGR-100 " 
+       #"lyra2v3"  = " -a LYRA2V3-100 " # (ccminer faster + 0%fee)
  
 }
 
