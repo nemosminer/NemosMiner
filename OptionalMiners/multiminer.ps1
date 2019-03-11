68cbd09605508f5944ca297d5f6d91020a2139d5
nemo <slbaker@hotmail.co.nz>
2019-03-12 09:42:02 +1300

    Update multiminer.ps1

--- OptionalMiners/multiminer.ps1
+++ OptionalMiners/multiminer.ps1
@@ -4,7 +4,7 @@ $Path = ".\Bin\NVIDIA-MultiMiner\multiminer.exe"
 $Uri = "https://github.com/bogdanadnan/multiminer/releases/download/v1.1.0/multiminer_v1.1.0_24.01.2019.zip"
 
 $Commands = [PSCustomObject]@{
-    "argon2d4096" = " --use-gpu=CUDA --gpu-batchsize=210 -t 4" #Argon2d4096 
+    "argon2d4096" = " --use-gpu=CUDA --gpu-batchsize=210 -t 1" #Argon2d4096 (--gpu-batchsize=256 -t 4)
 }
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
@@ -15,7 +15,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = " -q -b $($Variables.NVIDIAMinerAPITCPPort) -a $_ -o $($Pools.($Algo).Protocol)://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        Arguments = "-R 1 --gpu-id=$($Config.SelGPUCC) -q -b $($Variables.NVIDIAMinerAPITCPPort) -a $_ -o $($Pools.($Algo).Protocol)://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * .99} # substract 1% devfee
         API = "Ccminer"
         Port = $Variables.NVIDIAMinerAPITCPPort
