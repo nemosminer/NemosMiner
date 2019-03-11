07d896894fda9bc940fe2629de1789990026c36b
nemo <slbaker@hotmail.co.nz>
2019-03-11 13:42:03 +1300

    Update progpow.ps1

--- Miners/progpow.ps1
+++ Miners/progpow.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
 
 $Path = ".\Bin\NVIDIA-progpowminer016\progpowminer-cuda.exe"
-$Uri = "https://nemosminer.com/data/optional/progpowminer0.16-FinalCuda 10.7z"
+$Uri = "https://nemosminer.com/data/optional/progpowminer0.16-FinalCuda10.7z"
 
 $Commands = [PSCustomObject]@{
      "progpow" = ""
