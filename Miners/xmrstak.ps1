0e19c101eb0332d9ee7df60f10c6e9e8ff8652a8
nemo <slbaker@hotmail.co.nz>
2019-03-16 13:18:25 +1300

    Update xmrstak.ps1

--- Miners/xmrstak.ps1
+++ Miners/xmrstak.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\Bin\CryptoNight-FireIce2101\xmr-stak.exe"
-$Uri = "https://github.com/fireice-uk/xmr-stak/releases/download/2.10.1/xmr-stak-win64-2.10.1.7z"
+$Path = ".\Bin\CryptoNight-FireIce2102\xmr-stak.exe"
+$Uri = "https://github.com/fireice-uk/xmr-stak/releases/download/2.10.2/xmr-stak-win64-2.10.2.7z"
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
 $Port = 3335
