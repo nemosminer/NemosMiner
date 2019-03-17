7f3e4cbcf2835a8c3b6d285584e233f06ada4706
nemo <slbaker@hotmail.co.nz>
2019-03-18 10:28:35 +1300

    Update miniZ.ps1

--- OptionalMiners/miniZ.ps1
+++ OptionalMiners/miniZ.ps1
@@ -3,11 +3,11 @@ if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps
 $Path = ".\Bin\NVIDIA-miniZ12l\miniZ.exe"
 $Uri = "https://miniz.ch/?smd_process_download=1&download_id=1974"
 $Commands = [PSCustomObject]@{
-    #"equihash144"  = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Equihash144 (testing)
-    #"zhash"        = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Zhash (testing)
-    #"equihash192"  = " --devices $($Config.SelGPUDSTM) --algo 192_7 --pers auto" #Equihash192 (testing)
-    #"equihash-btg" = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers BgoldPoW " # Equihash-btg MPH (testing)
-    #"equihash96"   = " --devices $($Config.SelGPUDSTM) --algo 96_5 --pers auto" #Equihash96 (testing)
+    #"equihash144"  = " --algo 144,5 --pers auto" #Equihash144 (testing)
+    #"zhash"        = " --algo 144,5 --pers auto" #Zhash (testing)
+    #"equihash192"  = " --algo 192,7 --pers auto" #Equihash192 (testing)
+    #"equihash-btg" = " --algo 144,5 --pers BgoldPoW " # Equihash-btg MPH (testing)
+    #"equihash96"   = " --algo 96,5  --pers auto" #Equihash96 (testing)
 }
 $Port = $Variables.NVIDIAMinerAPITCPPort
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -16,7 +16,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "--intensity 100 --latency --nocolor --tempunits C -cd $($Config.SelGPUDSTM) --telemetry $($Variables.NVIDIAMinerAPITCPPort) --pers auto --url $($Pools.(Get-Algorithm($_)).User)@$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) --pass $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        Arguments = "--intensity 100 --latency --nocolor --tempunits C -cd $($Config.SelGPUDSTM) --telemetry $($Variables.NVIDIAMinerAPITCPPort) --url $($Pools.(Get-Algorithm($_)).User)@$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) --pass $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day * .98} # substract 2% devfee
         API       = "miniZ"
         Port      = $Variables.NVIDIAMinerAPITCPPort
