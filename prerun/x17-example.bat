0a4f6fc0ddff7d33f4dc59be6bc8f3e807940cd9
nemo <slbaker@hotmail.co.nz>
2019-03-18 22:15:41 +1300

    Create x17-example.bat

---
+++ prerun/x17-example.bat
@@ -0,0 +1,6 @@
+nvidiaInspector.exe -setBaseClockOffset:0,0,125 -setMemoryClockOffset:0,0,450 -setVoltageOffset:0,0,0 -setPowerTarget:0,60 -setTempTarget:0,0,85 
+nvidiaInspector.exe -setBaseClockOffset:0,1,125 -setMemoryClockOffset:0,1,450 -setVoltageOffset:0,1,0 -setPowerTarget:1,60 -setTempTarget:0,1,85
+nvidiaInspector.exe -setBaseClockOffset:0,2,125 -setMemoryClockOffset:0,2,450 -setVoltageOffset:0,2,0 -setPowerTarget:2,60 -setTempTarget:0,2,85
+nvidiaInspector.exe -setBaseClockOffset:0,3,125 -setMemoryClockOffset:0,3,450 -setVoltageOffset:0,3,0 -setPowerTarget:3,60 -setTempTarget:0,3,85
+nvidiaInspector.exe -setBaseClockOffset:0,4,125 -setMemoryClockOffset:0,4,450 -setVoltageOffset:0,4,0 -setPowerTarget:4,60 -setTempTarget:0,4,85
+nvidiaInspector.exe -setBaseClockOffset:0,5,125 -setMemoryClockOffset:0,5,450 -setVoltageOffset:0,5,0 -setPowerTarget:5,60 -setTempTarget:0,5,85
