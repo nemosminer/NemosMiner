72d1359f1feb6e1a4a96d288ce0d5b5bba24cb12
nemo <slbaker@hotmail.co.nz>
2019-03-18 23:25:10 +1300

    Update NemosMiner.bat

--- NemosMiner.bat
+++ NemosMiner.bat
@@ -1,1 +1,1 @@
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -version 5.0 -executionpolicy bypass -windowstyle normal -command ".\NemosMiner.ps1 -ConfigFile '.\Config\config.json'"
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -version 5.0 -executionpolicy bypass -windowstyle maximized -command ".\NemosMiner.ps1 -ConfigFile '.\Config\config.json'"
