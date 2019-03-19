b4f2748408d5d450cdb77e4f00b2f76ccac54bc5
nemo <slbaker@hotmail.co.nz>
2019-03-19 17:38:22 +1300

    Update ccminerverus.ps1

--- Miners/ccminerverus.ps1
+++ Miners/ccminerverus.ps1
@@ -4,7 +4,7 @@ $Path = ".\Bin\NVIDIA-Ccminerverus3.3\ccminer.exe"
 $Uri = "https://nemosminer.com/data/optional/CCMinerVerus-3.3.7z"
 
 $Commands = [PSCustomObject]@{
-     "verushash" = " -a verus -i 19 -d $($Config.SelGPUCC)" #verus (Testing)
+     "verushash" = " -a verus -i 18.75 -d $($Config.SelGPUCC)" #verus (Testing)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -14,7 +14,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = "--cpu-priority 4 -b $($Variables.NVIDIAMinerAPITCPPort) -N 1 -R 1 -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        Arguments = "--cpu-priority 4 -b $($Variables.NVIDIAMinerAPITCPPort) -N 2 -R 1 -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
         API = "ccminer"
         Port = $Variables.NVIDIAMinerAPITCPPort
