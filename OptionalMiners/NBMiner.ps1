f4ee03ec150bbfe281af8efb60dfe9e2e04acf79
nemo <slbaker@hotmail.co.nz>
2019-03-21 17:50:13 +1300

    Update NBMiner.ps1
    (8gb cards work win7,8, 8.1 & Linux. Win10 requires 10gb+vram)

--- OptionalMiners/NBMiner.ps1
+++ OptionalMiners/NBMiner.ps1
@@ -6,7 +6,7 @@ $Uri = "https://nemosminer.com/data/optional/NBMiner_Win213.7z"
 $Commands = [PSCustomObject]@{
     #"grincuckatoo31" = " -a cuckatoo -o stratum+tcp://" #grincuckatoo31 (8gb cards work win7,8, 8.1 & Linux. Win10 requires 10gb+vram)
      "grincuckaroo29" = " -a cuckaroo -o stratum+tcp://" #grincuckaroo29 (fastest)
-     "ethash" = " -a ethash -o ethnh+tcp://" #ethash (testing)
+     #"ethash" = " -a ethash -o ethnh+tcp://" #ethash
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
