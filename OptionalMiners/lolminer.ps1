508651c8b43d37bc0162f0c760c893fe592bd0fc
nemo <slbaker@hotmail.co.nz>
2019-03-21 18:29:50 +1300

    Update lolminer.ps1

--- OptionalMiners/lolminer.ps1
+++ OptionalMiners/lolminer.ps1
@@ -7,7 +7,7 @@ $Commands = [PSCustomObject]@{
     #"equihash96" = " --coin MNX" #Equihash 96,5
     #"Equihash21x9" = "--coin AION" #Equihash 210,9
     #"equihash144" = " --coin AUTO144_5" #Equihash 144,5
-    #"beam" = " --coin BEAM" #Equihash 150,5
+     "beam" = " --coin BEAM" #Equihash 150,5
     }
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
