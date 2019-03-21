696058ff68875c3f920f1f51822d7e9e31b2f4d1
nemo <slbaker@hotmail.co.nz>
2019-03-21 18:04:44 +1300

    Update lolminer.ps1
    miniZ faster

--- OptionalMiners/lolminer.ps1
+++ OptionalMiners/lolminer.ps1
@@ -4,10 +4,10 @@ $Path = ".\Bin\NVIDIA-AMD-lolMiner07\lolMiner.exe"
 $Uri = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/0.7/lolMiner_v07_Win64.zip"
 
 $Commands = [PSCustomObject]@{
-    "equihash96" = " --coin MNX" #Equihash 96,5
+    #"equihash96" = " --coin MNX" #Equihash 96,5
     #"Equihash21x9" = "--coin AION" #Equihash 210,9
-    "equihash144" = " --coin AUTO144_5" #Equihash 144,5
-    "beam" = " --coin BEAM" #Equihash 150,5
+    #"equihash144" = " --coin AUTO144_5" #Equihash 144,5
+    #"beam" = " --coin BEAM" #Equihash 150,5
     }
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
