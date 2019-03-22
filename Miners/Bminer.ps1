f96777e699d96ee5adf2c4ce55dc94ce44fa19e1
nemo <slbaker@hotmail.co.nz>
2019-03-22 23:20:40 +1300

    Update Bminer.ps1

--- Miners/Bminer.ps1
+++ Miners/Bminer.ps1
@@ -3,15 +3,14 @@ if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps
 $Path = ".\Bin\NVIDIA-Bminer1531\bminer.exe"
 $Uri = "https://www.bminercontent.com/releases/bminer-lite-v15.3.1-8887ee1-amd64.zip"
 $Commands = [PSCustomObject]@{
-    "equihashBTG" = " -uri zhash://" #EquihashBTG(miniZ faster)
-   #"equihash" = " -uri stratum://" #Equihash(Asic)
-    "equihash144" = " -pers auto -uri equihash1445://" #Equihash144(miniZ faster)
-    "zhash" = " -pers auto -uri equihash1445://" #Zhash(miniZ faster)
-    "ethash" = " -uri ethstratum://" #Ethash (Phoenix faster)
-   #"aeternity" = " -uri aeternity://" #aeternity(testing)
-    "beam" = " -uri beam://" #beam(fastest)
+    "equihashBTG" = " -uri zhash://" #EquihashBTG
+    "equihash144" = " -pers auto -uri equihash1445://" #Equihash144
+    "zhash" = " -pers auto -uri equihash1445://" #Zhash
+    "ethash" = " -uri ethstratum://" #Ethash 
+   #"aeternity" = " -uri aeternity://" #aeternity
+    "beam" = " -uri beam://" #beam
     "grincuckaroo29" = " -uri cuckaroo29://" #grincuckaroo29 
-    #"grincuckatoo31" = " -uri cuckatoo31://" #grincuckatoo31 (8gb cards work win7,8, 8.1 & Linux. Win10 requires 10gb+vram)
+   #"grincuckatoo31" = " -uri cuckatoo31://" #grincuckatoo31 (8gb cards work win7,8, 8.1 & Linux. Win10 requires 10gb+vram)
     
 }
 $Port = $Variables.NVIDIAMinerAPITCPPort
