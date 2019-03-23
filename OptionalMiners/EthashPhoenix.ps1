811d53d8086980192337727559aba370c19f1027
nemo <slbaker@hotmail.co.nz>
2019-03-24 09:25:46 +1300

    4.2c
    Added new Nvidia kernels v3 with slightly higher hashrate and lower power consumption. The new kernels are supported only on GTX1050/1060/1070/1080/Ti GPUs and are enabled by default on these GPUs. Note that you can't use these kernels for dual-mining, only -nvkernel 2 works for dual mining on Nvidia.
    Added parameter to enable or disable driver-specific optimizations on Nvidia GPUs. Use -nvdo 1 (the default is 0) to enable the optimizations. This won't change hashrate (or will change it only slightly) but may make the cards more stable depending on the concrete Nvidia driver.
    Small bugfixes

--- OptionalMiners/EthashPhoenix.ps1
+++ OptionalMiners/EthashPhoenix.ps1
@@ -1,8 +1,8 @@
 
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\Bin\Ethash-Phoenix42b\PhoenixMiner.exe"
-$Uri = "https://nemosminer.com/data/optional/PhoenixMiner_4.2b_Windows.7z"
+$Path = ".\Bin\Ethash-Phoenix42c\PhoenixMiner.exe"
+$Uri = "https://nemosminer.com/data/optional/PhoenixMiner_4.2c_Windows.7z"
 $Commands = [PSCustomObject]@{
      "ethash" = " -di $($($Config.SelGPUCC).Replace(',',''))" #Ethash
 }
@@ -12,7 +12,7 @@ $Algo = Get-Algorithm($_)
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "-esm 3 -allpools 1 -allcoins 1 -platform 2 -mport -$($Variables.NVIDIAMinerAPITCPPort) -epool $($Pools.Ethash.Host):$($Pools.Ethash.Port) -ewal $($Pools.Ethash.User) -epsw $($Pools.Ethash.Pass)$($Commands.$_)"
+        Arguments = "-nvdo 1 -esm 3 -allpools 1 -allcoins 1 -platform 2 -mport -$($Variables.NVIDIAMinerAPITCPPort) -epool $($Pools.Ethash.Host):$($Pools.Ethash.Port) -ewal $($Pools.Ethash.User) -epsw $($Pools.Ethash.Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * .9935} # substract 0.65% devfee
         API       = "ethminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort #3333
