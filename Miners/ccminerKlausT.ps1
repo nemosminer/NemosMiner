f2d8344df6a3be56e3d99aeaffaa56c2fa06f472
nemo <slbaker@hotmail.co.nz>
2019-03-25 12:12:26 +1300

    Update ccminerKlausT.ps1

--- Miners/ccminerKlausT.ps1
+++ Miners/ccminerKlausT.ps1
@@ -4,14 +4,14 @@ $Path = ".\Bin\NVIDIA-CcminerKlausTv3\ccminer.exe"
 $Uri = "https://github.com/nemosminer/ccminer-KlausT-8.21-mod-r18-src-fix/releases/download/v3/ccminerKlausT.7z"
 
 $Commands = [PSCustomObject]@{
-    #"neoscrypt" = " -a neoscrypt -d $($Config.SelGPUCC)" #NeoScrypt (CryptoDredge slighty faster)
-    #"yescrypt" = " -a yescrypt -d $($Config.SelGPUCC)" #Yescrypt (fastest)
-    #"yescryptR16" = " -i 13.25 -a yescryptr16 -d $($Config.SelGPUCC)" #YescryptR16 (fastest)
-    #"yescryptR16v2" = " -a yescryptr16v2 -d $($Config.SelGPUCC)" #YescryptR16v2 (fastest)
-    #"yescryptR24" = " -a yescryptr24 -d $($Config.SelGPUCC)" #YescryptR24 (fastest)
-    #"yescryptR8" = " -a yescryptr8 -d $($Config.SelGPUCC)" #YescryptR8 (fastest)
-    "lyra2v3" = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2v3 NICEHASH(fastest)
-    "lyra2rev3" = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2rev3 YIIMP(fastest)
+     "neoscrypt" = " -a neoscrypt -d $($Config.SelGPUCC)" #NeoScrypt
+    #"yescrypt" = " -a yescrypt -d $($Config.SelGPUCC)" #Yescrypt 
+    #"yescryptR16" = " -i 13.25 -a yescryptr16 -d $($Config.SelGPUCC)" #YescryptR16
+    #"yescryptR16v2" = " -a yescryptr16v2 -d $($Config.SelGPUCC)" #YescryptR16v2
+    #"yescryptR24" = " -a yescryptr24 -d $($Config.SelGPUCC)" #YescryptR24
+    #"yescryptR8" = " -a yescryptr8 -d $($Config.SelGPUCC)" #YescryptR8 
+    "lyra2v3" = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2v3 NICEHASH
+    "lyra2rev3" = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2rev3 YIIMP
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
