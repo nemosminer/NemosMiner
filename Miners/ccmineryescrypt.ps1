cdd070b226e3e3dfd59ec5fe3e1a6185a9542626
nemo <slbaker@hotmail.co.nz>
2019-03-25 12:19:19 +1300

    Update ccmineryescrypt.ps1

--- Miners/ccmineryescrypt.ps1
+++ Miners/ccmineryescrypt.ps1
@@ -9,7 +9,12 @@ $Commands = [PSCustomObject]@{
     "yescryptR16v2" = " -d $($Config.SelGPUCC)" #YescryptR16v2
     "yescryptR24" = " -d $($Config.SelGPUCC)" #YescryptR24 
     "yescryptR8" = " -d $($Config.SelGPUCC)" #YescryptR8
+    "yescryptR32" = " -i 12.51 -d $($Config.SelGPUCC)" #YescryptR32
 }
+   switch ($_) {
+      "yescryptR32" {$staleshares = 0.14} # account for 14% stale shares
+            default {$staleshares = 0.00}
+ }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
@@ -19,7 +24,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
         Type = "NVIDIA"
         Path = $Path
         Arguments = "--cpu-priority 4 -b $($Variables.NVIDIAMinerAPITCPPort) -N 2 -R 1 -a $_ -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * (1 - $staleshares)}
         API = "ccminer"
         Port = $Variables.NVIDIAMinerAPITCPPort
         Wrap = $false
