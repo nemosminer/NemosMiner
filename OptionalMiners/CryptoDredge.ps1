06b13516f6571369ebfa90061d5ad2a849b7800d
nemo <slbaker@hotmail.co.nz>
2019-03-30 08:55:18 +1300

    Update CryptoDredge.ps1

--- OptionalMiners/CryptoDredge.ps1
+++ OptionalMiners/CryptoDredge.ps1
@@ -7,7 +7,6 @@ $Commands = [PSCustomObject]@{
     "allium"            = " --intensity 8 -a allium" #Allium (fastest)
     "lyra2v3"           = " --intensity 8 -a lyra2v3" #Lyra2v3
     "lyra2REv3"         = " --intensity 8 -a lyra2v3" #lyra2REv3
-    "lyra2re3"          = " --intensity 8 -a lyra2v3" #lyra2re3
     "lyra2zz "          = " --intensity 8 -a lyra2zz" #Lyra2zz (Testing)
     "neoscrypt"         = " --intensity 6 -a neoscrypt" #NeoScrypt (fastest)
     "phi"               = " --intensity 8 -a phi" #Phi (fastest)
