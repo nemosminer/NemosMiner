b8be19d71aae39a3c63ee90c9645192afe7f73e0
nemo <slbaker@hotmail.co.nz>
2019-04-09 05:50:21 +1200

    revert back to v0.17
    back to stable v0.17..  after several days testing v0.18 on multiple agos i come to conclusions that CryptoDrege v0.18 is un stable for on my test rigs v017 is rock solid

--- OptionalMiners/CryptoDredge.ps1
+++ OptionalMiners/CryptoDredge.ps1
@@ -1,42 +1,45 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
-$Path = ".\Bin\NVIDIA-CryptoDredge0180\CryptoDredge.exe"
-$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.18.0/CryptoDredge_0.18.0_cuda_10.0_windows.zip"
+$Path = ".\Bin\NVIDIA-CryptoDredge0170\CryptoDredge.exe"
+$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.17.0/CryptoDredge_0.17.0_cuda_10.0_windows.zip"
 
 $Commands = [PSCustomObject]@{
-     "argon2ddyn"        = " --intensity 5 -a argon2d" #Argon2d-dyn
-     "allium"            = " --intensity 7 -a allium" #Allium
-     "lyra2v3"           = " --intensity 7 -a lyra2v3" #Lyra2v3
-     "lyra2REv3"         = " --intensity 7 -a lyra2v3" #lyra2REv3
-     "lyra2zz "          = " --intensity 7 -a lyra2zz" #Lyra2zz
-     "neoscrypt"         = " --intensity 5 -a neoscrypt"
-     "phi"               = " --intensity 7 -a phi" #Phi
-     "phi2"              = " --intensity 6 -a phi2" #Phi2 
-     "lyra2vc0ban"       = " --intensity 7 -a lyra2vc0ban" #Lyra2vc0banHash
-    #"cryptonightheavy"  = " --intensity 7 -a cryptonightheavy" # CryptoNightHeavy
-    #"x22i"              = " --intensity 7 -a x22i" # X22i 
-    #"tribus"            = " --intensity 7 -a tribus" #Tribus 
-    #"cnv8"              = " --intensity 7 -a cnv8" #CryptoNightv8
-    #"c11"               = " --intensity 7 -a c11" #C11 
-     "skunk"             = " --intensity 7 -a skunk" #Skunk 
-     "mtp"               = " --intensity 6 -a mtp" #Mtp
-    #"bcd"               = " --intensity 7 -a bcd" #Bcd 
-    #"x16rt"             = " --intensity 7 -a x16rt" #X16rt
-    #"x21s"              = " --intensity 7 -a x21s" #X21s 
-    #"x16s"              = " --intensity 7 -a x16s" #X16s 
-    #"x17"               = " --intensity 7 -a x17" #X17 
-    #"bitcore"           = " --intensity 7 -a bitcore" #Bitcore 
-     "hmq1725"           = " --intensity 7 -a hmq1725" #Hmq1725
-    #"dedal"             = " --intensity 7 -a dedal" #Dedal
-     "pipe"              = " --intensity 7 -a pipe" #Pipe 
-    #"x16r"              = " --intensity 7 -a x16r" #x16r
-     "grincuckaroo29"    = " --intensity 6 -a cuckaroo29" #Grincuckaroo29
+    "allium"            = " --intensity 8 -a allium" #Allium
+    #"lyra2v3"           = " --intensity 8 -a lyra2v3" #Lyra2v3
+    #"lyra2REv3"         = " --intensity 8 -a lyra2v3" #lyra2REv3 
+    "lyra2zz "          = " --intensity 8 -a lyra2zz" #Lyra2zz
+    "neoscrypt"         = " --intensity 6 -a neoscrypt" #NeoScrypt
+    "phi"               = " --intensity 8 -a phi" #Phi
+    "phi2"              = " --intensity 8 -a phi2" #Phi2
+    "lyra2vc0ban"       = " --intensity 8 -a lyra2vc0ban" #Lyra2vc0banHash 
+    #"cryptonightheavy"  = " --intensity 8 -a cryptonightheavy" # CryptoNightHeavy
+    #"x22i"             = " --intensity 8 -a x22i" # X22i (trex faster)
+     "tribus"           = " --intensity 8 -a tribus" #Tribus
+    #"cnv8"              = " --intensity 8 -a cnv8" #CryptoNightv8 
+    #"cryptonightmonero" = " --intensity 8 -a cnv8" # Cryptonightmonero MPH 
+    #"c11"              = " --intensity 8 -a c11" #C11
+    "skunk"             = " --intensity 8 -a skunk" #Skunk 
+    #"mtp"               = " --intensity 8 -a mtp" #Mtp
+    #"bcd"              = " --intensity 8 -a bcd" #Bcd 
+    #"x16rt"             = " --intensity 8 -a x16rt" #X16rt 
+    #"x21s"              = " --intensity 8 -a x21s" #X21s
+    #"x16s"             = " --intensity 8 -a x16s" #X16s
+    #"x17"              = " --intensity 8 -a x17" #X17 
+    #"bitcore"          = " --intensity 8 -a bitcore" #Bitcore
+    "hmq1725"           = " --intensity 8 -a hmq1725" #Hmq1725 
+    #"dedal"            = " --intensity 8 -a dedal" #Dedal 
+    "pipe"             = " --intensity 8 -a pipe" #Pipe
+    #"x16r"             = " --intensity 8 -a x16r" #x16r
+    #"grincuckaroo29"   = " --intensity 8 -a cuckaroo29" #Grincuckaroo29
+    #"exosis"           = " --intensity 8 -a exosis" #Exosis
+  
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
     $Algo = Get-Algorithm($_)
+    If ($Algo -eq "mtp" -and $Pools.($Algo).Host -like "*nicehash*") {return}
     switch ($_) {
         "mtp" {$Fee = 0.02}
         default {$Fee = 0.01}
@@ -45,7 +48,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "--no-nvml --api-type ccminer-tcp --cpu-priority 3 --no-crashreport --no-watchdog -r -1 -R 1 -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        Arguments = "--no-nvml --api-type ccminer-tcp --cpu-priority 5 --no-crashreport --no-watchdog -r -1 -R 1 -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * (1 - $Fee)} # substract 1% devfee
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort
