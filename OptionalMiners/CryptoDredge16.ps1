fe652513308ce1fc1b23752fb54b6afe76627cfd
nemo <slbaker@hotmail.co.nz>
2019-04-09 08:49:32 +1200

    Update CryptoDredge16.ps1

--- OptionalMiners/CryptoDredge16.ps1
+++ OptionalMiners/CryptoDredge16.ps1
@@ -11,17 +11,12 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
     $Algo = Get-Algorithm($_)
-    If ($Algo -eq "mtp" -and $Pools.($Algo).Host -like "*nicehash*") {return}
-    switch ($_) {
-        "mtp" {$Fee = 0.02}
-        default {$Fee = 0.01}
-    }
 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
         Arguments = "--no-nvml --api-type ccminer-tcp --cpu-priority 4 --no-watchdog -r -1 -R 1 -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * (1 - $Fee)} # substract 1% devfee
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * .99} # substract 1% devfee
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort
         Wrap      = $false
