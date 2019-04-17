2d1bad988e61d86181698e2fa5da7705f1eeba60
nemo <slbaker@hotmail.co.nz>
2019-04-17 21:49:07 +1200

    Update Core.ps1

--- Core.ps1
+++ Core.ps1
@@ -1,7 +1,5 @@
 <#
-Copyright (c) 2019 Nemo
-Copyright (c) 2019 MrPlus
-
+Copyright (c) 2018-2019 Nemo and MrPlus
 NemosMiner is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
