4e9870926431e2ef78b72393d4b5bd3af1420f38
nemo <slbaker@hotmail.co.nz>
2019-04-18 21:49:54 +1200

    Create ccminersupr.ps1

---
+++ Miners/ccminersupr.ps1
@@ -0,0 +1,28 @@
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
+
+$Path = ".\Bin\NVIDIA-ccminersupr207\ccminer.exe"
+$Uri = "https://github.com/ocminer/suprminer/releases/download/2.0/suprminer-2.0.7z"
+
+$Commands = [PSCustomObject]@{
+    "veil" = " -a x16rt -i 21" #veil
+    "neoscrypt" = " -a neoscrypt -i 21" #neoscrypt
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    $Algo = Get-Algorithm($_)
+    [PSCustomObject]@{
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "--cpu-priority 4 -N 2 -R 1 -b $($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
+        API       = "ccminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort #4068
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.($Algo).User
+        Host      = $Pools.($Algo).Host
+        Coin      = $Pools.($Algo).Coin
+    }
+}
