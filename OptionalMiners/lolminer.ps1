e9ed3348d277220bcac7f1fcb8158bdd994922b1
nemo <slbaker@hotmail.co.nz>
2019-04-19 00:06:08 +1200

    Update lolminer.ps1

--- OptionalMiners/lolminer.ps1
+++ OptionalMiners/lolminer.ps1
@@ -1,13 +1,14 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
-$Path = ".\Bin\NVIDIA-AMD-lolMiner07\lolMiner.exe"
-$Uri = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/0.7/lolMiner_v07_Win64.zip"
+$Path = ".\Bin\NVIDIA-AMD-lolMiner08\lolMiner.exe"
+$Uri = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/0.8/lolMiner_v08_Win64.zip"
 
 $Commands = [PSCustomObject]@{
-    #"equihash96" = " --coin MNX" #Equihash 96,5
-    #"Equihash21x9" = "--coin AION" #Equihash 210,9
-    #"equihash144" = " --coin AUTO144_5" #Equihash 144,5
-     "beam" = " --coin BEAM" #Equihash 150,5
+    "equihash96" = " --coin MNX" #Equihash 96,5
+    "Equihash21x9" = "--coin AION" #Equihash 210,9
+    "equihash144" = " --coin AUTO144_5" #Equihash 144,5
+    "beam" = " --coin BEAM" #Equihash 150,5
+    "grincuckatoo31" = " --coin GRIN-AT31" #grincuckatoo31
     }
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
