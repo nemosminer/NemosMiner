c7e232a3373808b8261aa2d2c7c6b34a30e27c4c
nemo <slbaker@hotmail.co.nz>
2019-04-22 11:54:37 +1200

    Update ccminerx22i.ps1

--- Miners/ccminerx22i.ps1
+++ Miners/ccminerx22i.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
 
 $Path = ".\Bin\NVIDIA-Ccminerx22i2311\ccminer.exe"
-$Uri = "https://github.com/nemosminer/ccminer-x22i/releases/download/v2.3.1.1/ccminerx22i.7z"
+$Uri = "https://github.com/nemosminer/ccminer-x22i/releases/download/v2.3.1.1/ccminerx22i2311.7z"
 
 $Commands = [PSCustomObject]@{
      "x22i" = " -a x22i -i 20 -d $($Config.SelGPUCC)" #x22i
