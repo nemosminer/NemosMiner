4c2d65b659e2e1f800ba64b31f8813e3c30b428c
nemo <slbaker@hotmail.co.nz>
2019-04-22 16:44:12 +1200

    Create ccminersupr16.ps1

---
+++ Miners/ccminersupr16.ps1
@@ -0,0 +1,30 @@
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
+
+$Path = ".\Bin\NVIDIA-ccminersupr161\ccminer.exe"
+$Uri = "https://github.com/nemosminer/ccminer-x16r/releases/download/v1.6.1/ccminer-patch3-161.7z"
+
+$Commands = [PSCustomObject]@{
+     "x16r" = " -a x16r -i 23" #x16r
+     "x16s" = " -a x16s -i 23" #x16s
+     "x17"  = " -a x17 -i 23" #x17
+     "c11"  = " -a c11 -i 23" #c11
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    $Algo = Get-Algorithm($_)
+    [PSCustomObject]@{
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "-N 2 -R 1 -b $($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
+        API       = "ccminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort #4068
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.($Algo).User
+        Host      = $Pools.($Algo).Host
+        Coin      = $Pools.($Algo).Coin
+    }
+}
