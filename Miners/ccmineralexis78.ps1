570eb08704c324d1fad17c64cfb57c77381a3d11
nemo <slbaker@hotmail.co.nz>
2019-04-23 09:17:47 +1200

    Update ccmineralexis78.ps1

--- Miners/ccmineralexis78.ps1
+++ Miners/ccmineralexis78.ps1
@@ -10,7 +10,7 @@ $Commands = [PSCustomObject]@{
     #"poly" = " -N 1 -d $($Config.SelGPUCC)" #polytimos
     #"skein" = " -i 28 -N 2 -d $($Config.SelGPUCC)" #Skein
     #"skein2" = " -i 28 -N 2 -d $($Config.SelGPUCC)" #Skein2
-    #"x11evo" = " -N 1 -i 21 -d $($Config.SelGPUCC) " #X11evo
+     "x11evo" = " -i 20 -d $($Config.SelGPUCC) " #X11evo
     #"bitcore" = "" #Bitcore
     #"blake2s" = " -r 0 -d $($Config.SelGPUCC)" #Blake2s
     #"c11" = " -N 1 -i 21 -d $($Config.SelGPUCC)" #C11
@@ -47,7 +47,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = "-b $($Variables.NVIDIAMinerAPITCPPort) -R 1 -a $_ -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        Arguments = "-N 1 -b $($Variables.NVIDIAMinerAPITCPPort) -R 1 -a $_ -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
         API = "ccminer"
         Port = $Variables.NVIDIAMinerAPITCPPort #4068
