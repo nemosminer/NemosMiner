858ed2be702a89972da0dd254be80ba17d2cefe8
nemo <slbaker@hotmail.co.nz>
2019-05-06 12:17:11 +1200

    Update bminerf.ps1

--- OptionalMiners/bminerf.ps1
+++ OptionalMiners/bminerf.ps1
@@ -3,9 +3,9 @@ if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps
 $Path = ".\Bin\NVIDIA-Bminerf1531\bminer.exe"
 $Uri = "https://www.bminercontent.com/releases/bminer-lite-v15.3.1-8887ee1-amd64.zip"
 $Commands = [PSCustomObject]@{
-   #"equihashBTG" = " -uri zhash://" #EquihashBTG
-   #"equihash144" = " -pers auto -uri equihash1445://" #Equihash144
-   #"zhash" = " -pers auto -uri equihash1445://" #Zhash
+    "equihashBTG" = " -uri zhash://" #EquihashBTG
+    "equihash144" = " -pers auto -uri equihash1445://" #Equihash144
+    "zhash" = " -pers auto -uri equihash1445://" #Zhash
     "ethash" = " -uri ethstratum://" #Ethash 
    #"cuckoocycle" = " -uri aeternity://" #aeternity
     "beam" = " -uri beam://" #beam
@@ -18,12 +18,18 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
     $Algo = Get-Algorithm($_)
+        switch ($_) {
+        "ethash" {$Fee = 0.0065} # substract devfee
+        "grincuckaroo29" {$Fee = 0.01} # substract devfee
+        "grincuckatoo31" {$Fee = 0.01} # substract devfee
+        default {$Fee = 0.02} # substract devfee
+    }
     $Pass = If ($Pools.($Algo).Pass -like "*,*") {$Pools.($Algo).Pass.ToString().replace(',','%2C')} else {$Pools.($Algo).Pass}
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
         Arguments = "$($Commands.$_)$($Pools.($Algo).User):$($Pass)@$($Pools.($Algo).Host):$($Pools.($Algo).Port) -max-temperature 94 -devices $($Config.SelGPUCC) -api 127.0.0.1:$Port"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * (1 - $Fee)} # substract devfee
         API       = "bminer"
         Port      = $Port
         Wrap      = $false
