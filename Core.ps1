08aec83cb3848aac0e827875cc5d07799b2b9af1
nemo <slbaker@hotmail.co.nz>
2019-05-08 05:16:35 +1200

    Update Core.ps1

--- Core.ps1
+++ Core.ps1
@@ -15,8 +15,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 <#
 Product:        NemosMiner
 File:           Core.ps1
-version:        3.7.9.5
-version date:   07 May 2019
+version:        3.7.9.6
+version date:   08 May 2019
 #>
 
 Function InitApplication {
