5af7df225d6b1b6141ec015ec5296b31dbd98591
nemo <slbaker@hotmail.co.nz>
2019-05-07 12:18:58 +1200

    Update NemosMiner.ps1

--- NemosMiner.ps1
+++ NemosMiner.ps1
@@ -17,8 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 <#
 Product:        NemosMiner
 File:           NemosMiner.ps1
-version:        3.7.9.3
-version date:   06 May 2019
+version:        3.7.9.4
+version date:   07 May 2019
 #>
 
 param(
@@ -27,7 +27,7 @@ param(
     [Parameter(Mandatory = $false)]
     [String]$UserName = "nemo", 
     [Parameter(Mandatory = $false)]
-    [String]$WorkerName = "ID=NemosMiner-v3.7.9.2", 
+    [String]$WorkerName = "ID=NemosMiner-v3.7.9.4", 
     [Parameter(Mandatory = $false)]
     [Int]$API_ID = 0, 
     [Parameter(Mandatory = $false)]
