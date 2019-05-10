fc5797eec3d8949516c61c78211ee0dd5f1f73b2
nemo <slbaker@hotmail.co.nz>
2019-05-10 17:43:51 +1200

    Update nbminer.ps1

--- OptionalMiners/nbminer.ps1
+++ OptionalMiners/nbminer.ps1
@@ -6,8 +6,7 @@ $Uri = "https://nemosminer.com/data/optional/NBMiner_Win23.2.zip"
 $Commands = [PSCustomObject]@{
     #"grincuckatoo31" = " -a cuckatoo -o stratum+tcp://" #grincuckatoo31 (8gb cards work win7,8, 8.1 & Linux. Win10 requires 10gb+vram)
      "grincuckaroo29" = " -a cuckaroo -o stratum+tcp://" #grincuckaroo29
-     "cuckoocycle" = " -a cuckoo_ae -o stratum+tcp://" #grincuckaroo29
-     
+     "cuckoocycle" = " -a cuckoo_ae -o stratum+tcp://" #grincuckaroo29  
     #"ethash" = " -a ethash -o ethnh+tcp://" #ethash
 }
 
