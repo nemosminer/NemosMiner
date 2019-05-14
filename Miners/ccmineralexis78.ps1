e2c1f041577d25bacf5753005915005fdf18cabd
nemo <slbaker@hotmail.co.nz>
2019-05-15 09:10:22 +1200

    $Stat.Hour
    beta_testing

--- Miners/ccmineralexis78.ps1
+++ Miners/ccmineralexis78.ps1
@@ -48,7 +48,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
         Type = "NVIDIA"
         Path = $Path
         Arguments = "-N 1 -b $($Variables.NVIDIAMinerAPITCPPort) -R 1 -a $_ -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Live}
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Hour}
         API = "ccminer"
         Port = $Variables.NVIDIAMinerAPITCPPort #4068
         Wrap = $false
