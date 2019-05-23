80d969e796d437ea8a06f89508819ec60600ce9c
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-05-24 11:07:16 +1200

    Update gminer.ps1
    v1.44

--- OptionalMiners/gminer.ps1
+++ OptionalMiners/gminer.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
-$Path = ".\Bin\NVIDIA-Gminer143\miner.exe"
-$Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.43/gminer_1_43_windows64.zip"
+$Path = ".\Bin\NVIDIA-Gminer144\miner.exe"
+$Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.44/gminer_1_44_windows64.zip"
 $Commands = [PSCustomObject]@{
      "equihash144"    = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Equihash144
      "zhash"          = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Zhash
