bd1ea00518737d256159ef1ef8879c3335ab639f
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-06-01 10:43:16 +1200

    v1.3.3

--- OptionalMiners/nanominer.ps1
+++ OptionalMiners/nanominer.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) { . .\Include.ps1; RegisterLoaded(".\Include.ps1") }
 
-$Path = ".\Bin\NVIDIA-nanominer132\cmdline_launcher.bat"
-$Uri = "https://github.com/nanopool/nanominer/releases/download/v1.3.2/nanominer-windows-1.3.2.zip"
+$Path = ".\Bin\NVIDIA-nanominer133\cmdline_launcher.bat"
+$Uri = "https://github.com/nanopool/nanominer/releases/download/v1.3.3/nanominer-windows-1.3.3.zip"
 
 $Commands = [PSCustomObject]@{
     "cryptonightr" = "" #cryptonight/r (NiceHash)
