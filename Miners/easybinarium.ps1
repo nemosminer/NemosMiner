40a0d1d8d756fd8512bedb323f8ef4e2f7abc947
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-06-15 15:48:41 +1200

    Update and rename easy-binarium.ps1 to easybinarium.ps1

--- Miners/easybinarium.ps1
+++ Miners/easybinarium.ps1
@@ -4,44 +4,7 @@ $Path = ".\Bin\CPU-bubasikBinarium\cpuminer-aes-sse42.exe"
 $Uri = "https://github.com/bubasik/cpuminer-easy-binarium/releases/download/1.0/cpuminer-easy-binarium-win_x64.zip"
 
 $Commands = [PSCustomObject]@{
-    # "allium" = "" #Allium
-    #"bitcore" = "" #Bitcore
-    #"blake2s" = "" #Blake2s
-    #"blakecoin" = "" #Blakecoin
     #"binarium-v1" = "" #binarium-v1
-    #"vanilla" = "" #BlakeVanilla
-    #"c11" = "" #C11
-    # "cryptonight" = "" #CryptoNight
-    #"cryptonightv7" = "" #cryptonightv7
-    #"decred" = "" #Decred
-    #"equihash" = "" #Equihash
-    #"ethash" = "" #Ethash
-    #"groestl" = "" #Groestl
-    # "hmq1725" = "" #HMQ1725
-    # "hodl" = "" #Hodl
-    #"jha" = "" #JHA
-    #"keccak" = "" #Keccak
-    #"lbry" = "" #Lbry
-    #"lyra2v2" = "" #Lyra2RE2
-    # "lyra2z" = "" #Lyra2z
-    # "m7m" = "" #m7m
-    #"myr-gr" = "" #MyriadGroestl
-    #"neoscrypt" = "" #NeoScrypt
-    #"nist5" = "" #Nist5
-    #"pascal" = "" #Pascal
-    #"sib" = "" #Sib
-    #"skein" = "" #Skein
-    #"skunk" = "" #Skunk
-    #"timetravel" = "" #Timetravel
-    #"tribus" = "" #Tribus
-    #"veltor" = "" #Veltor
-    #"x11evo" = "" #X11evo
-    #"x17" = "" #X17
-    # "x16r" = "" #X16r
-    # "yescrypt" = "" #Yescrypt
-    # "yespower" = "" #Yespower
-    # "yescryptr16" = "" #YescryptR16
-    # "yescryptr32" = "" #YescryptR32
 }
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
@@ -59,8 +22,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
         Type = "CPU"
         Path = $Path
         Arguments = "--cpu-affinity AAAA -q -t $($ThreadCount) -b $($Variables.CPUMinerAPITCPPort) -a $($Algo) -o $($Pools.($Algo).Protocol)://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Week * 0.68 } # Account for rejected share. Work with pool ops to fix.
-        API = "Ccminer"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
         Port = $Variables.CPUMinerAPITCPPort
         Wrap = $false
         URI = $Uri
