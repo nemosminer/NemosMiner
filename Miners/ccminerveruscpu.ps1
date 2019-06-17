bdf8c96caf7d6e166c438dba493e3a6b134bdc96
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-06-17 12:53:15 +1200

    faster verushash CPU

---
+++ Miners/ccminerveruscpu.ps1
@@ -0,0 +1,32 @@
+if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+
+$Path = ".\Bin\NVIDIA-ccminerverushash35\ccminer.exe"
+$Uri = "https://github.com/Minerx117/ccminer/releases/download/v3.5/VerushashccminerCpu.7z"
+
+$Commands = [PSCustomObject]@{
+    "verushash" = ",d=6" #Verushash
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+
+    switch ($_) {
+        default { $ThreadCount = $Variables.ProcessorCount - 2 }
+    }
+
+    $Algo = Get-Algorithm($_)
+    [PSCustomObject]@{
+        Type      = "CPU"
+        Path      = $Path
+        Arguments = "-t $($ThreadCount) --cpu-priority 4 -N 2 -R 1 -b $($Variables.NVIDIAMinerAPITCPPort) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -a verus -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day }
+        API       = "ccminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort #4068
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.($Algo).User
+        Host      = $Pools.($Algo).Host
+        Coin      = $Pools.($Algo).Coin
+    }
+}
