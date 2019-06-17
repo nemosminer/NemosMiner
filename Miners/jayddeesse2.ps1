b00d9e2a65e95501630434f313b5ba476d03c8e3
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-06-18 09:32:34 +1200

    Update jayddeesse2.ps1

--- Miners/jayddeesse2.ps1
+++ Miners/jayddeesse2.ps1
@@ -38,7 +38,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
         Path = $Path
         Arguments = "--cpu-affinity AAAA -q -t $($ThreadCount) -b $($Variables.CPUMinerAPITCPPort) -a $_ -o $($Pools.($Algo).Protocol)://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
-        API = "Ccminer"
+        API = "ccminer"
         Port = $Variables.CPUMinerAPITCPPort
         Wrap = $false
         URI = $Uri
