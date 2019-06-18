805f502cb77cac352b6be92820d4053198a0b742
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-06-18 13:07:52 +1200

    tidy2

--- Miners/ccminerklaust.ps1
+++ Miners/ccminerklaust.ps1
@@ -1,4 +1,4 @@
-if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1;RegisterLoaded(".\Includes\include.ps1")}
+if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
 $Path = ".\Bin\NVIDIA-CcminerKlausTv3\ccminer.exe"
 $Uri = "https://github.com/nemosminer/ccminer-KlausT-8.21-mod-r18-src-fix/releases/download/v3/ccminerKlausT.7z"
@@ -10,8 +10,8 @@ $Commands = [PSCustomObject]@{
     #"yescryptR16v2" = " -a yescryptr16v2 -d $($Config.SelGPUCC)" #YescryptR16v2
     #"yescryptR24" = " -a yescryptr24 -d $($Config.SelGPUCC)" #YescryptR24
     #"yescryptR8" = " -a yescryptr8 -d $($Config.SelGPUCC)" #YescryptR8 
-    "lyra2v3"   = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2v3 NICEHASH
-    "lyra2rev3" = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2rev3 YIIMP   
+    "lyra2v3"   = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2v3
+    "lyra2rev3" = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2rev3 
 }
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
@@ -21,7 +21,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
         Type      = "NVIDIA"
         Path      = $Path
         Arguments = "--cpu-priority 4 -b $($Variables.NVIDIAMinerAPITCPPort) -N 2 -R 1 -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day }
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort
         Wrap      = $false
@@ -30,3 +30,4 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
         Host      = $Pools.($Algo).Host
         Coin      = $Pools.($Algo).Coin
     }
+}
