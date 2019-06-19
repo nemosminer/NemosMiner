bc2ec5025e3bb30adbcbce461f9225d69582e6d7
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-06-19 22:02:37 +1200

    Create ccminerhoney.ps1

---
+++ Miners/ccminerhoney.ps1
@@ -0,0 +1,27 @@
+if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+
+$Path = ".\Bin\NVIDIA-ccminerhoney121560\ccminer_x64_v0012_1560.exe"
+$Uri = "https://github.com/bee-group/ccminer/releases/download/v0012_1560/ccminer_x64_v0012_1560.exe"
+
+$Commands = [PSCustomObject]@{
+    "honeycomb" = "" #Honeycomb
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    $Algo = Get-Algorithm($_)
+    [PSCustomObject]@{
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "--cpu-priority 4 -N 2 -R 1 -b $($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -a honeycomb -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day }
+        API       = "ccminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort #4068
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.($Algo).User
+        Host      = $Pools.($Algo).Host
+        Coin      = $Pools.($Algo).Coin
+    }
+}
