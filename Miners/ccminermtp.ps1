a999d852f83b31749993e8d606fb813788773e8e
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-06-24 11:26:25 +1200

    improve mtp

--- Miners/ccminermtp.ps1
+++ Miners/ccminermtp.ps1
@@ -1,22 +1,22 @@
-if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1")}
+if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
 $Path = ".\Bin\NVIDIA-ccminermtp1121\ccminer.exe"
 $Uri = "https://github.com/zcoinofficial/ccminer/releases/download/1.1.21/ccminer.exe"
 
 $Commands = [PSCustomObject]@{
-    "mtp" = " -i 17 -d $($Config.SelGPUCC)" #mtp
+    "mtp" = ",strict,verbose -i 16 -d $($Config.SelGPUCC)" #mtp
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
-        $Algo = Get-Algorithm($_)
-        If ($Algo -eq "mtp" -and $Pools.($Algo).Host -like "*nicehash*") {return}
+    $Algo = Get-Algorithm($_)
+    If ($Algo -eq "mtp" -and $Pools.($Algo).Host -like "*nicehash*") { return }
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
         Arguments = "--cpu-priority 4 -R 1 -b $($Variables.NVIDIAMinerAPITCPPort) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -a $Algo -u $($Pools.($Algo).User) --no-donation -p $($Pools.($Algo).Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day }
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort #4068
         Wrap      = $false
