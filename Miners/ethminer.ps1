216c31ac71aa5c5b18b54fffecde7c30a954b672
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-06-30 23:58:23 +1200

    Update ethminer.ps1

--- Miners/ethminer.ps1
+++ Miners/ethminer.ps1
@@ -1,27 +1,27 @@
-if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1")}
+if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
  
 $Path = ".\Bin\NVIDIA-Ethminer0171\ethminer.exe"
 $Uri = "https://github.com/ethereum-mining/ethminer/releases/download/v0.17.1/ethminer-0.17.1-cuda10.0-windows-amd64.zip"
 $Commands = [PSCustomObject]@{
-      #"ethash" = "" #Ethash
+    "ethash" = "" #Ethash
 }
 $Port = $Variables.NVIDIAMinerAPITCPPort
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
-	$Algo = Get-Algorithm($_)
-	If ($Pools.($Algo).Host -notlike "*nicehash*") {return}
+    $Algo = Get-Algorithm($_)
+    If ($Pools.($Algo).Host -notlike "*nicehash*") { return }
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "--cuda-devices $($Config.SelGPUDSTM) --api-port -$Port -U -P stratum://$($Pools.($Algo).User):x@$($Pools.($Algo).Host):$($Pools.($Algo).Port)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day} 
+        Arguments = "--cuda-devices $($Config.SelGPUDSTM) --api-port -$Port -U -P stratum2+tcp://$($Pools.($Algo).User):x@$($Pools.($Algo).Host):$($Pools.($Algo).Port)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day } 
         API       = "ethminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort
         Wrap      = $false
         URI       = $Uri    
         User      = $Pools.($Algo).User
-        Host = $Pools.($Algo).Host
-        Coin = $Pools.($Algo).Coin
+        Host      = $Pools.($Algo).Host
+        Coin      = $Pools.($Algo).Coin
     }
 }
