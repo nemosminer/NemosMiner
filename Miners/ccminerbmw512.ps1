187db8edcb2fb064ab82513f9d1269d1370fb4c4
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-07-08 11:17:16 +1200

    Enable bmw512 ZergPool

---
+++ Miners/ccminerbmw512.ps1
@@ -0,0 +1,28 @@
+if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+
+$Path = ".\Bin\NVIDIA-ccminerbmwv1\ccminer.exe"
+$Uri = "https://github.com/Minerx117/ccminer-bmw512/releases/download/v2.2.5/ccminerbmw.7z"
+
+$Commands = [PSCustomObject]@{
+    "bmw512" = " -a bmw512" #Bmw512
+
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    $Algo = Get-Algorithm($_)
+    [PSCustomObject]@{
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "--cpu-priority 4 -R 1 -b $($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day }
+        API       = "ccminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort #4068
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.($Algo).User
+        Host      = $Pools.($Algo).Host
+        Coin      = $Pools.($Algo).Coin
+    }
+}
