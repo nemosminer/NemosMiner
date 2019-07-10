eca5efca1e2d0ed4d0af330bb446d20fcb182ae6
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-07-10 23:37:05 +1200

    Update cpuopt409w.ps1

--- Miners/cpuopt409w.ps1
+++ Miners/cpuopt409w.ps1
@@ -1,6 +1,7 @@
-if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1;RegisterLoaded(".\Includes\include.ps1")}
+if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
 $Path = ".\Bin\CPU-Opt409w\cpuminer-sse2.exe"
+#$Path = ".\Bin\CPU-Opt409w\cpuminer-ryzen.exe"
 $Uri = "https://pool.rplant.xyz/miners/cpuminer-opt-win.zip"
 
 $Commands = [PSCustomObject]@{
@@ -12,22 +13,22 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
 
     switch ($_) {
-        "honeycomb" {$ThreadCount = $Variables.ProcessorCount - 1 }
-        default {$ThreadCount = $Variables.ProcessorCount - 2 }
+        "honeycomb" { $ThreadCount = $Variables.ProcessorCount - 1 }
+        default { $ThreadCount = $Variables.ProcessorCount - 2 }
     }
 
-	$Algo = Get-Algorithm($_)
+    $Algo = Get-Algorithm($_)
     [PSCustomObject]@{
-        Type = "CPU"
-        Path = $Path
+        Type      = "CPU"
+        Path      = $Path
         Arguments = "--cpu-affinity AAAA -q -t $($ThreadCount) -b $($Variables.CPUMinerAPITCPPort) -a $_ -o $($Pools.($Algo).Protocol)://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
-        API = "ccminer"
-        Port = $Variables.CPUMinerAPITCPPort
-        Wrap = $false
-        URI = $Uri
-        User = $Pools.($Algo).User
-        Host = $Pools.($Algo).Host
-        Coin = $Pools.($Algo).Coin
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day }
+        API       = "ccminer"
+        Port      = $Variables.CPUMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.($Algo).User
+        Host      = $Pools.($Algo).Host
+        Coin      = $Pools.($Algo).Coin
     }
 }
