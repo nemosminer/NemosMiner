007b667525319f52ed5526945f9338125830c3c1
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-07-11 01:46:28 +1200

    Update nicehash.ps1
    NEW NiceHash platform

--- Pools/nicehash.ps1
+++ Pools/nicehash.ps1
@@ -1,43 +1,32 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-
 try {
-    $Request = Invoke-WebRequest "https://api.nicehash.com/api?method=simplemultialgo.info" -TimeoutSec 15 -UseBasicParsing -Headers @{"Cache-Control" = "no-cache" } | ConvertFrom-Json 
+    $Request = Invoke-WebRequest "https://api2.nicehash.com/main/api/v2/public/simplemultialgo/info/" -TimeoutSec 15 -UseBasicParsing -Headers @{"Cache-Control" = "no-cache" } | ConvertFrom-Json 
+    $RequestAlgodetails = Invoke-WebRequest "https://api2.nicehash.com/main/api/v2/mining/algorithms/" -TimeoutSec 15 -UseBasicParsing -Headers @{"Cache-Control" = "no-cache" } | ConvertFrom-Json 
+    $Request.miningAlgorithms | ForEach-Object { $Algo = $_.Algorithm ; $_ | Add-Member -force @{algodetails = $RequestAlgodetails.miningAlgorithms | Where-Object { $_.Algorithm -eq $Algo } } }
 }
 catch { return }
-
 if (-not $Request) { return }
-
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
-
-# Placed here for Perf (Disk reads)
 $ConfName = if ($Config.PoolsConfig.$Name -ne $Null) { $Name }else { "default" }
 $PoolConf = $Config.PoolsConfig.$ConfName
-
-
-$Request.result.simplemultialgo | ForEach-Object {
-    $Algo = $_.Name
-    $NiceHash_Port = $_.port
-    $NiceHash_Algorithm = Get-Algorithm $_.name
+$Request.miningAlgorithms | ForEach-Object {
+    $Algo = $_.Algorithm
+    $NiceHash_Port = $_.algodetails.port
+    $NiceHash_Algorithm = Get-Algorithm $_.Algorithm
     $NiceHash_Coin = ""
-
-    $Divisor = 1000000000
-
+    $DivisorMultiplier = 1000000000
+    $Divisor = $DivisorMultiplier * [Double]$_.Algodetails.marketFactor
+    $Divisor = 100000000
     $Stat = Set-Stat -Name "$($Name)_$($NiceHash_Algorithm)_Profit" -Value ([Double]$_.paying / $Divisor)
-
-    $Locations = "eu", "usa", "hk", "jp", "in", "br"
+    $Locations = "eu", "usa", "jp"
     $Locations | ForEach-Object {
         $NiceHash_Location = $_
-        
         switch ($NiceHash_Location) {
             "eu" { $Location = "EU" }
             "usa" { $Location = "US" }
             "jp" { $Location = "JP" }
-            "hk" { $Location = "HK" }
-            "in" { $Location = "IN" }
-            "br" { $Location = "BR" }
         }
-        $NiceHash_Host = "$($Algo).$NiceHash_Location.nicehash.com"
-
+        $NiceHash_Host = "$($Algo).$($NiceHash_Location)-new.nicehash.com"
         if ($PoolConf.Wallet) {
             [PSCustomObject]@{
                 Algorithm     = $NiceHash_Algorithm
@@ -53,7 +42,6 @@ $Request.result.simplemultialgo | ForEach-Object {
                 Location      = $Location
                 SSL           = $false
             }
-
             [PSCustomObject]@{
                 Algorithm     = $NiceHash_Algorithm
                 Info          = $NiceHash_Coin
