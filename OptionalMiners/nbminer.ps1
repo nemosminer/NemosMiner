efe53b40b0db8fe6dbd3290c55d5181b5c9cc4af
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-07-15 20:24:45 +1200

    Support Grin29 fork on 17th, July
    Add new method to turn on NiceHash protocol, nicehash+tcp://
    Add option -no-nvml to close the periodical query for GPU status.
    Improve performance on Grin29 & AE.
    Support Grin29 fork on 17th, July

--- OptionalMiners/nbminer.ps1
+++ OptionalMiners/nbminer.ps1
@@ -1,12 +1,12 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1;RegisterLoaded(".\Includes\include.ps1")}
 
-$Path = ".\Bin\NVIDIA-nbminer233\nbminer.exe"
-$Uri = "https://github.com/NebuTech/NBMiner/releases/download/v23.3/NBMiner_23.3_Win.zip"
+$Path = ".\Bin\NVIDIA-nbminer24\nbminer.exe"
+$Uri = "https://github.com/NebuTech/NBMiner/releases/download/v24.0/NBMiner_24.0_Win.zip"
 
 $Commands = [PSCustomObject]@{
-    #"grincuckatoo31" = " -a cuckatoo -o stratum+tcp://" #grincuckatoo31 (8gb cards work win7,8, 8.1 & Linux. Win10 requires 10gb+vram)
-     "grincuckaroo29" = " -a cuckaroo -o stratum+tcp://" #grincuckaroo29
-    #"cuckoocycle" = " -a cuckoo_ae -o stratum+tcp://" #cuckoocycle  
+    #"grincuckatoo31" = " -a cuckatoo -o nicehash+tcp://" #grincuckatoo31 (8gb cards work win7,8, 8.1 & Linux. Win10 requires 10gb+vram)
+     "grincuckaroo29" = " -a cuckaroo -o nicehash+tcp://" #grincuckaroo29
+     "cuckoocycle" = " -a cuckoo_ae -o nicehash+tcp://" #cuckoocycle  
     #"ethash" = " -a ethash -o ethnh+tcp://" #ethash
 }
 
@@ -22,7 +22,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "$($Commands.$_)$($Pools.($Algo).Host):$($Pools.($Algo).Port) --cuckoo-intensity 0 --api 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -u $($Pools.($Algo).User):$($Pools.($Algo).Pass)"
+        Arguments = "$($Commands.$_)$($Pools.($Algo).Host):$($Pools.($Algo).Port) -no-nvml --cuckoo-intensity 0 --api 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -u $($Pools.($Algo).User):$($Pools.($Algo).Pass)"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * (1 - $Fee)} # substract devfee
         API       = "NBMiner"
         Port      = $Variables.NVIDIAMinerAPITCPPort #4068
