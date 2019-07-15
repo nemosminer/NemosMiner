764921dff4e940cd3bae51bacfe46adb885e3a30
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-07-15 12:02:13 +1200

    Update NemosMiner.ps1

--- NemosMiner.ps1
+++ NemosMiner.ps1
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Product:        NemosMiner
 File:           NemosMiner.ps1
 version:        3.8.0.4
-version date:   13 July 2019
+version date:   15 July 2019
 #>
 
 param(
