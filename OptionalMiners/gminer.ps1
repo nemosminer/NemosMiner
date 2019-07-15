170b1b38fd3ba7325f42bc880eb74b84beb75b19
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-07-16 08:05:18 +1200

    151beta
        added Cuckarood29 algorithm for Nvidia cards (use --algo grin29 for autoswitch after hardfork)
        added BeamHashII algorithm for Nvidia cards (use --algo BeamHashII to enable it or use --algo BeamHash for autoswitch after hardfork)
        added option to print miner version (--version)
        added option to enable/disable NVML (--nvml 0/1)
        bug fixes and stability improvements

--- OptionalMiners/gminer.ps1
+++ OptionalMiners/gminer.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1")}
  
-$Path = ".\Bin\NVIDIA-Gminer150\miner.exe"
-$Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.50/gminer_1_50_windows64.zip"
+$Path = ".\Bin\NVIDIA-Gminer151b\miner.exe"
+$Uri = "https://github.com/develsoftware/GMinerBetaRelease/releases/download/1.51/gminer_1_51_windows64.zip"
 $Commands = [PSCustomObject]@{
      "equihash125"    = " --devices $($Config.SelGPUDSTM) --algo 125_4 --pers auto" #Equihash125
      "equihash144"    = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Equihash144
@@ -23,7 +23,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "--intensity 100 --watchdog 0 --api $($Variables.NVIDIAMinerAPITCPPort) --server $($Pools.($Algo).Host) --port $($Pools.($Algo).Port) --user $($Pools.($Algo).User) --pass $($Pools.($Algo).Pass)$($Commands.$_)"
+        Arguments = "--intensity 100 --watchdog 0 --nvml 0 --api $($Variables.NVIDIAMinerAPITCPPort) --server $($Pools.($Algo).Host) --port $($Pools.($Algo).Port) --user $($Pools.($Algo).User) --pass $($Pools.($Algo).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * .98} # substract 2% devfee
         API       = "gminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort
