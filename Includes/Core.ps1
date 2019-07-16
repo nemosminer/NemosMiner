b93aa691da742ad7360535adc1f76e4520a0321e
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-07-16 16:03:58 +1200

    Update Core.ps1

--- Includes/Core.ps1
+++ Includes/Core.ps1
@@ -18,8 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 <#
 Product:        NemosMiner
 File:           Core.ps1
-version:        3.8.0.5
-version date:   15 July 2019
+version:        3.8.0.6
+version date:   16 July 2019
 #>
 
 Function InitApplication {
