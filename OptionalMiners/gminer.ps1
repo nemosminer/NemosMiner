ec30d9c6701197085851c0f9083b6adad4604466
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-07-16 18:43:27 +1200

    Update gminer.ps1

--- OptionalMiners/gminer.ps1
+++ OptionalMiners/gminer.ps1
@@ -3,17 +3,8 @@ if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1; RegisterLo
 $Path = ".\Bin\NVIDIA-Gminer151b\miner.exe"
 $Uri = "https://github.com/develsoftware/GMinerBetaRelease/releases/download/1.51/gminer_1_51_windows64.zip"
 $Commands = [PSCustomObject]@{
-     "equihash125"    = " --devices $($Config.SelGPUDSTM) --algo 125_4 --pers auto" #Equihash125
-     "equihash144"    = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Equihash144
-     "zhash"          = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Zhash
-     "equihash192"    = " --devices $($Config.SelGPUDSTM) --algo 192_7 --pers auto" #Equihash192
-     "equihash-btg"   = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers BgoldPoW " # Equihash-btg MPH
      "equihash96"     = " --devices $($Config.SelGPUDSTM) --algo 96_5 --pers auto" #Equihash96
      "beam"           = " --devices $($Config.SelGPUDSTM) --algo 150_5 --pers auto" #Equihash150
-     "grincuckaroo29" = " --devices $($Config.SelGPUDSTM) --algo grin29 --pers auto" #Grincuckaroo29
-     "cuckoocycle"    = " --devices $($Config.SelGPUDSTM) --algo aeternity --pers auto" #Aeternity 
-    #"grincuckatoo31" = " --devices $($Config.SelGPUDSTM) --algo grin31 --pers auto" #Grincuckatoo31 (8gb cards work win7,8, 8.1 & Linux. Win10 requires 10gb+vram)
- 
 }
 $Port = $Variables.NVIDIAMinerAPITCPPort
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
