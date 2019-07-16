6e959b4d956023d03364ca637366e2040e44e6e5
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-07-16 20:56:09 +1200

    Update gminer.ps1
    stable

--- OptionalMiners/gminer.ps1
+++ OptionalMiners/gminer.ps1
@@ -5,6 +5,11 @@ $Uri = "https://github.com/develsoftware/GMinerBetaRelease/releases/download/1.5
 $Commands = [PSCustomObject]@{
      "equihash96"     = " --devices $($Config.SelGPUDSTM) --algo 96_5 --pers auto" #Equihash96
      "beam"           = " --devices $($Config.SelGPUDSTM) --algo BeamHash --pers auto" #Equihash150
+     "equihash125"    = " --devices $($Config.SelGPUDSTM) --algo 125_4 --pers auto" #Equihash125
+     "equihash144"    = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Equihash144
+     "zhash"          = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Zhash
+     "equihash192"    = " --devices $($Config.SelGPUDSTM) --algo 192_7 --pers auto" #Equihash192
+     "equihash-btg"   = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers BgoldPoW " # Equihash-btg MPH
 }
 $Port = $Variables.NVIDIAMinerAPITCPPort
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
