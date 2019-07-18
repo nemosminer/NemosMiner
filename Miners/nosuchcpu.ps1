58ce025d915d9d6dfee1f42abc50f9375210222f
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-07-18 15:40:10 +1200

    Create nosuchcpu.ps1

---
+++ Miners/nosuchcpu.ps1
@@ -0,0 +1,34 @@
+if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+
+$Path = ".\Bin\CPU-nosuch\cpuminer-aes-sse2.exe"
+$Uri = "https://github.com/patrykwnosuch/cpuminer-3.8.8.1-nosuch/releases/download/3.8.8.1-m2/cpuminer-nosuch-m2-win64.7z"
+
+$Commands = [PSCustomObject]@{
+    "m7m"         = " -a m7m" #Binarium-v1
+    "binarium-v1" = " -a Binarium_hash_v1" #Binarium-v1
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+
+    switch ($_) {
+        "binarium-v1" { $ThreadCount = $Variables.ProcessorCount - 1 }
+        default { $ThreadCount = $Variables.ProcessorCount - 1 }
+    }
+
+    $Algo = Get-Algorithm($_)
+    [PSCustomObject]@{
+        Type      = "CPU"
+        Path      = $Path
+        Arguments = "--cpu-affinity AAAA -q -t $($ThreadCount) -b $($Variables.CPUMinerAPITCPPort) -o $($Pools.($Algo).Protocol)://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day }
+        API       = "ccminer"
+        Port      = $Variables.CPUMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.($Algo).User
+        Host      = $Pools.($Algo).Host
+        Coin      = $Pools.($Algo).Coin
+    }
+}
