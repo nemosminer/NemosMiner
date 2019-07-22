ae336a572f9734381cb146c1c77478d5455fad6b
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-07-23 09:53:20 +1200

    v4.5b
    improve hash rate of Ethash and ProgPow cuda 10.0

--- OptionalMiners/ethashphoenix.ps1
+++ OptionalMiners/ethashphoenix.ps1
@@ -1,24 +1,25 @@
-if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1")}
+if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-$Path = ".\Bin\Ethash-Phoenix42c\PhoenixMiner.exe"
-$Uri = "https://nemosminer.com/data/optional/PhoenixMiner_4.2c_Windows.7z"
+$Path = ".\Bin\Ethash-Phoenix45b\PhoenixMiner.exe"
+$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/4.5b/PhoenixMiner_4.5b_Windows.7z"
 $Commands = [PSCustomObject]@{
-     "ethash" = " -di $($($Config.SelGPUCC).Replace(',',''))" #Ethash fastest and Epoch 267 ready
+    "ethash"  = " -di $($($Config.SelGPUCC).Replace(',',''))" #Ethash
+    "progpow" = " -coin bci -di $($($Config.SelGPUCC).Replace(',',''))" #Progpow 
 }
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
-$Algo = Get-Algorithm($_)
+    $Algo = Get-Algorithm($_)
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "-nvdo 1 -esm 3 -allpools 1 -allcoins 1 -platform 2 -mport -$($Variables.NVIDIAMinerAPITCPPort) -epool $($Pools.Ethash.Host):$($Pools.Ethash.Port) -ewal $($Pools.Ethash.User) -epsw $($Pools.Ethash.Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * .9935} # substract 0.65% devfee
+        Arguments = "-nvdo 1 -esm 3 -allpools 1 -allcoins 1 -platform 2 -mport -$($Variables.NVIDIAMinerAPITCPPort) -epool $($Pools.($Algo).Host):$($Pools.($Algo).Port) -ewal $($Pools.($Algo).User) -epsw $($Pools.($Algo).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * .9935 } # substract 0.65% devfee
         API       = "ethminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort #3333
         Wrap      = $false
         URI       = $Uri
-        User = $Pools.($Algo).User
-        Host = $Pools.($Algo).Host
-        Coin = $Pools.($Algo).Coin
+        User      = $Pools.($Algo).User
+        Host      = $Pools.($Algo).Host
+        Coin      = $Pools.($Algo).Coin
     }
 }
