78e97d2c820107e7a878a66b3e53e01305ada080
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-07-22 16:12:25 +1200

    Update nanominer.ps1

--- OptionalMiners/nanominer.ps1
+++ OptionalMiners/nanominer.ps1
@@ -4,8 +4,8 @@ $Path = ".\Bin\NVIDIA-nanominer152\cmdline_launcher.bat"
 $Uri = "https://github.com/nanopool/nanominer/releases/download/v1.5.2/nanominer-windows-1.5.2.zip"
 
 $Commands = [PSCustomObject]@{
-    "cryptonightr"       = "cryptonightr" #cryptonight/r (NiceHash)
-    "cryptonight-monero" = "cryptonightr" #monero (Mining Pool Hub)
+    #"cryptonightr"       = "cryptonightr" #cryptonight/r (NiceHash)
+    #"cryptonight-monero" = "cryptonightr" #monero (Mining Pool Hub)
 }
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
