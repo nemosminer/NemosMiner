318a08508850aff75ff20e59521bcb856b9a8e4b
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-07-24 19:16:34 +1200

    Update ccminerlyra2v3.ps1
    lyra2z330 enable for zpool

--- Miners/ccminerlyra2v3.ps1
+++ Miners/ccminerlyra2v3.ps1
@@ -12,7 +12,6 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
      $Algo = Get-Algorithm($_)
-        If ($Algo -eq "lyra2z330" -and $Pools.($Algo).Host -like "*zpool*") {return}
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
