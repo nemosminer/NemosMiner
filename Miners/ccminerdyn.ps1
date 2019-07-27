37f7b1688b47383d046fbd60032da652f7cfc088
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-07-28 10:01:03 +1200

    Update ccminerdyn.ps1

--- Miners/ccminerdyn.ps1
+++ Miners/ccminerdyn.ps1
@@ -4,8 +4,8 @@ $Path = ".\Bin\NVIDIA-ccminerdyn102\ccminer.exe"
 $Uri = "https://github.com/nemosminer/Dynamic-GPU-Miner-Nvidia/releases/download/v1.0.2/ccminerdyn.7z"
 
 $Commands = [PSCustomObject]@{
-    "argon2d-dyn" = "" #argon2d-dyn 
-    "argon2d500" = "" #argon2d-dyn
+    #"argon2d-dyn" = "" #argon2d-dyn 
+    #"argon2d500" = "" #argon2d-dyn
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
