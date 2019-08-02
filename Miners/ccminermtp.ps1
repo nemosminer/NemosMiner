070281986d8c7b2e5988d05bc05b0cff4552a6a6
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-08-02 14:22:13 +1200

    Update ccminermtp.ps1

--- Miners/ccminermtp.ps1
+++ Miners/ccminermtp.ps1
@@ -4,7 +4,7 @@ $Path = ".\Bin\NVIDIA-ccminermtp1126\ccminer.exe"
 $Uri = "https://github.com/zcoinofficial/ccminer/releases/download/1.1.26/ccminer.exe"
 
 $Commands = [PSCustomObject]@{
-     "mtp" = " -d $($Config.SelGPUCC)" #mtp
+     #"mtp" = " -d $($Config.SelGPUCC)" #mtp
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
