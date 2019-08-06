9c7787406eca19c0dc0cbe809da79bdbc1863be3
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-08-06 17:42:26 +1200

    Update miniz.ps1

--- OptionalMiners/miniz.ps1
+++ OptionalMiners/miniz.ps1
@@ -4,7 +4,7 @@ $Path = ".\Bin\NVIDIA-miniZ14o\miniZ.exe"
 $Uri = "https://github.com/Minerx117/miniZ-/releases/download/v1.4o/miniz14o.7z"
 $Commands = [PSCustomObject]@{
     #"equihash144"  = " --par=144,5 --pers auto" #Equihash144
-     "equihash125"  = " --par=125,4 --pers ZelProof" #Equihash125
+    #"equihash125"  = " --par=125,4 --pers ZelProof" #Equihash125
     #"zhash"        = " --par=144,5 --pers auto" #Zhash
     #"equihash192"  = " --par=192,7 --pers auto" #Equihash192
     #"equihash-btg" = " --par=144,5 --pers BgoldPoW " # Equihash-btg MPH
