d34ad5b764bb0ceeb5d4a2fecb123e688a91b14e
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-08-10 05:48:35 +1200

    Update cryptodredgephi2.ps1

--- OptionalMiners/cryptodredgephi2.ps1
+++ OptionalMiners/cryptodredgephi2.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1;RegisterLoaded(".\Includes\include.ps1")}
 
-$Path = ".\Bin\NVIDIA-CryptoDredge0201phi2\CryptoDredge.exe"
-$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.20.1/CryptoDredge_0.20.1_cuda_10.1_windows.zip"
+$Path = ".\Bin\NVIDIA-CryptoDredge021phi2\CryptoDredge.exe"
+$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.21.0/CryptoDredge_0.21.0_cuda_10.1_windows.zip"
 
 $Commands = [PSCustomObject]@{
     "phi2" = ",mc=SPDR/AGM --intensity 8 -a phi2" #Phi2 
