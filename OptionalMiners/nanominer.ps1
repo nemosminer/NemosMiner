b8f4e55859e8287cc2ba1d565930626f7f910d74
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-08-11 12:17:11 +1200

    Update nanominer.ps1

--- OptionalMiners/nanominer.ps1
+++ OptionalMiners/nanominer.ps1
@@ -4,8 +4,8 @@ $Path = ".\Bin\NVIDIA-nanominer153\cmdline_launcher.bat"
 $Uri = "https://github.com/nanopool/nanominer/releases/download/v1.5.3/nanominer-windows-1.5.3.zip"
 
 $Commands = [PSCustomObject]@{
-    #"cryptonightr"       = "cryptonightr" #cryptonight/r (NiceHash)
-    #"cryptonight-monero" = "cryptonightr" #monero (Mining Pool Hub)
+    "cryptonightr"       = "cryptonightr" #cryptonight/r (NiceHash)
+    "cryptonight-monero" = "cryptonightr" #monero (Mining Pool Hub)
 }
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
@@ -20,7 +20,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "-mport -$($Variables.NVIDIAMinerAPITCPPort) -algo $($Commands.$_) -wallet $($Pools.($Algo).User) -rigName $($Pools.($Algo).Pass) -pool1 $($Pools.($Algo).Host):$($Pools.($Algo).Port)"
+        Arguments = "-mport -$($Variables.NVIDIAMinerAPITCPPort) -algo $($Commands.$_) -wallet $($Pools.($Algo).User) -rigName $($Pools.($Algo).Pass) -pool1 $($Pools.($Algo).Host):$($Pools.($Algo).Port) -devices $($Config.SelGPUCC)"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * (1 - $Fee) } # substract devfee
         API       = "nanominer"
         Port      = $Variables.NVIDIAMinerAPITCPPort
