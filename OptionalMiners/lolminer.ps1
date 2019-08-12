f06817581497c8a7394f543e792836ff2d500709
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-08-13 09:08:51 +1200

    Update lolminer.ps1

--- OptionalMiners/lolminer.ps1
+++ OptionalMiners/lolminer.ps1
@@ -1,14 +1,14 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
  
-$Path = ".\Bin\NVIDIA-lolMiner085\lolMiner.exe"
-$Uri = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/0.8.5/lolMiner_v085_Win64.zip"
+$Path = ".\Bin\NVIDIA-lolMiner086\lolMiner.exe"
+$Uri = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/0.8.6/lolMiner_v086_Win64.zip"
 
 $Commands = [PSCustomObject]@{
-    #"equihash96"   = " --coin MNX" #Equihash 96,5
-    #"Equihash21x9" = "--coin AION" #Equihash 210,9
-    #"equihash144"  = " --coin AUTO144_5" #Equihash 144,5
+     "equihash96"   = " --coin MNX" #Equihash 96,5
+     "Equihash21x9" = "--coin AION" #Equihash 210,9
+     "equihash144"  = " --coin AUTO144_5" #Equihash 144,5
      "equihash125"  = " --coin ZEL" #Equihash 125,4
-    #"beam"         = " --coin BEAM" #Equihash 150,5
+     "beam"         = " --coin BEAM" #Equihash 150,5
 }
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
@@ -18,7 +18,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments =  "--user $($Pools.($Algo).User) --pool $($Pools.($Algo).Host) --port $($Pools.($Algo).Port) --devices $($Config.SelGPUCC) --apiport $($Variables.NVIDIAMinerAPITCPPort) --tls 0 --digits 2 --longstats 120 --shortstats 30 --connectattempts 5 --pass $($Pools.($Algo).Pass)$($Commands.$_)"
+        Arguments =  "--user $($Pools.($Algo).User) --pool $($Pools.($Algo).Host) --port $($Pools.($Algo).Port) --devices $($Config.SelGPUCC) --apiport $($Variables.NVIDIAMinerAPITCPPort) --tls 0 --digits 2 --longstats 240 --shortstats 60 --connectattempts 5 --pass $($Pools.($Algo).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * 0.99} # 1% dev fee
         API       = "LOL"
         Port      = $Variables.NVIDIAMinerAPITCPPort
@@ -32,7 +32,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "AMD"
         Path      = $Path
-        Arguments =  "--user $($Pools.($Algo).User) --pool $($Pools.($Algo).Host) --port $($Pools.($Algo).Port) --devices $($Config.SelGPUCC) --apiport $($Variables.AMDMinerAPITCPPort) --tls 0 --digits 2 --longstats 120 --shortstats 30 --connectattempts 5 --pass $($Pools.($Algo).Pass)$($Commands.$_)"
+        Arguments =  "--user $($Pools.($Algo).User) --pool $($Pools.($Algo).Host) --port $($Pools.($Algo).Port) --devices $($Config.SelGPUCC) --apiport $($Variables.AMDMinerAPITCPPort) --tls 0 --digits 2 --longstats 240 --shortstats 60 --connectattempts 5 --pass $($Pools.($Algo).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * 0.99} # 1% dev fee
         API       = "LOL"
         Port      = $Variables.AMDMinerAPITCPPort
