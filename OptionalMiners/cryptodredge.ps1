b26161cbc2762ff4da6f3a4cfe86dfd71ad61f8b
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-08-23 09:05:22 +1200

    cuda9.2 prefered

--- OptionalMiners/cryptodredge.ps1
+++ OptionalMiners/cryptodredge.ps1
@@ -1,8 +1,8 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1;RegisterLoaded(".\Includes\include.ps1")}
 
 
-$Path = ".\Bin\NVIDIA-CryptoDredge021\CryptoDredge.exe"
-$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.21.0/CryptoDredge_0.21.0_cuda_10.1_windows.zip"
+$Path = ".\Bin\NVIDIA-CryptoDredge02192\CryptoDredge.exe"
+$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.21.0/CryptoDredge_0.21.0_cuda_9.2_windows.zip"
 
 $Commands = [PSCustomObject]@{
     "argon2d250"  = " --intensity 8 -a argon2d250" #argon2d250
