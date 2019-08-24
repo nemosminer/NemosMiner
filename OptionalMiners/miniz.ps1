b3c99106c6317e3734fcb48d6c69d18359454ba3
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-08-25 08:35:23 +1200

    Update miniz.ps1
        Adjusted oc modes for 125,4. Major improvements for 1080Ti (>7%).
        Adjusted oc modes for 150,5,3 with improvements.
        Improvements for 144,5, for RTX cards.
        Further attempts to support ZEL mining on 3GB GPU, on Windows 10.
        Fixed support for 144,5 for 1050 2GB, on Windows 10.
        Fixed slow start for some GPUs/rigs.
        Other minor bug fixes.

--- OptionalMiners/miniz.ps1
+++ OptionalMiners/miniz.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-$Path = ".\Bin\NVIDIA-miniZ15p\miniZ.exe"
-$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v1.5p/miniZ_v1.5p_cuda10_win-x64.7z"
+$Path = ".\Bin\NVIDIA-miniZ15q\miniZ.exe"
+$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v1.5q/miniZv1.5qcuda10.7z"
 
 $Commands = [PSCustomObject]@{
     "equihash144"  = " --par=144,5 --pers auto" #Equihash144
