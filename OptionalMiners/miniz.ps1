d03dee52f57ff35cc0ca01abc84ae29c2f023c45
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-08-25 09:37:52 +1200

    Update miniz.ps1
    --oc1 + --f11=0 
    
    improved equihash125,equihash192,equihash144 by 4-8% on all my rigs (tested with 1070,1660ti,2080)
    same power draw from the wall

--- OptionalMiners/miniz.ps1
+++ OptionalMiners/miniz.ps1
@@ -20,7 +20,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "--f11=0 --stat-int 60 --nonvml --latency --nocolor --extra --tempunits C -cd $($Config.SelGPUDSTM) --telemetry $($Variables.NVIDIAMinerAPITCPPort) --url $($Pools.($Algo).User)@$($Pools.($Algo).Host):$($Pools.($Algo).Port) --pass $($Pools.($Algo).Pass)$($Commands.$_)"
+        Arguments = "--oc1 --f11=0 --stat-int 60 --nonvml --latency --nocolor --extra --tempunits C -cd $($Config.SelGPUDSTM) --telemetry $($Variables.NVIDIAMinerAPITCPPort) --url $($Pools.($Algo).User)@$($Pools.($Algo).Host):$($Pools.($Algo).Port) --pass $($Pools.($Algo).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * .98 } # substract 2% devfee
         API       = "miniZ"
         Port      = $Variables.NVIDIAMinerAPITCPPort
