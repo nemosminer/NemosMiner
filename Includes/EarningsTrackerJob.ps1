af26619d19922998b7c8bad8aae22cc9521bb250
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-09-02 09:43:17 +1200

    Update EarningsTrackerJob.ps1

--- Includes/EarningsTrackerJob.ps1
+++ Includes/EarningsTrackerJob.ps1
@@ -19,8 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 <#
 Product:        NemosMiner
 File:           EarningsTrackerJob.ps1
-version:        3.8.0.9
-version date:   14 August 2019
+version:        3.8.1.1
+version date:   02 Septemper 2019
 #>
 
 # To start the job one could use the following
