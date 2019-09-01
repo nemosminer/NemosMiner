9deee25478c8bf5e183d69a5779b96b06822abc2
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-09-02 09:45:12 +1200

    Update Charting.ps1

--- Includes/Charting.ps1
+++ Includes/Charting.ps1
@@ -19,8 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 <#
 Product:        NemosMiner
 File:           Charting.ps1
-version:        3.8.0.6
-version date:   16 July 2019
+version:        3.8.1.1
+version date:   02 Septemper 2019
 #>
 
 
