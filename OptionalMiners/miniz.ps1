66feea9fa1c564a1423b6039cf62eb975c2a599f
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-09-02 09:23:56 +1200

    faster beam / V2

--- OptionalMiners/miniz.ps1
+++ OptionalMiners/miniz.ps1
@@ -5,11 +5,12 @@ $Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v1.5q3beta
 
 $Commands = [PSCustomObject]@{
     "equihash144"  = " --par=144,5 --pers auto" #Equihash144
-    "equihash125"  = " --par=125,4" #Equihash125
+    "equihash125"  = " --par=125,4 --pers auto" #Equihash125
     "zhash"        = " --par=144,5 --pers auto" #Zhash
-   #"beamv2"       = " --par=beam2" #Beamv2
+    "beamv2"       = " --par=beam --pers auto" #Beamv2
+    "beam"         = " --par=150,5 --pers auto" #Beamv2
     "equihash192"  = " --par=192,7 --pers auto" #Equihash192
-   #"equihash96"   = " --par=96,5 --pers auto" #Equihash192
+    #"equihash96"   = " --par=96,5 --pers auto" #Equihash192
     "equihash-btg" = " --par=144,5 --pers BgoldPoW" # Equihash-btg MPH
 }
 
