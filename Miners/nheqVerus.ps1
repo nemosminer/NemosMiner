aa9014912b0ff7673bd2c617b71b3ead10bbbb7e
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-09-03 18:34:56 +1200

    +100% verushash hasrate
    add 100% as pool reports / pays exactly double console output 
    tested on nlpool over 96 hours on 2 x amd ryzen 7 3700x cpu's

--- Miners/nheqVerus.ps1
+++ Miners/nheqVerus.ps1
@@ -1,4 +1,4 @@
-if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1;RegisterLoaded(".\Includes\include.ps1")}
+if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
 $Path = ".\Bin\CPU-nheqVerus072\nheqminer.exe"
 $Uri = "https://github.com/VerusCoin/nheqminer/releases/download/0.7.2/nheqminer-Windows-v0.7.2.zip"
@@ -12,22 +12,22 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
 
     switch ($_) {
-        "hodl" {$ThreadCount = $Variables.ProcessorCount}
-        default {$ThreadCount = $Variables.ProcessorCount - 2}
+        "hodl" { $ThreadCount = $Variables.ProcessorCount }
+        default { $ThreadCount = $Variables.ProcessorCount - 2 }
     }
 
-	$Algo = Get-Algorithm($_)
+    $Algo = Get-Algorithm($_)
     [PSCustomObject]@{
-        Type = "CPU"
-        Path = $Path
+        Type      = "CPU"
+        Path      = $Path
         Arguments = "-t $($ThreadCount) -a $($Variables.CPUMinerAPITCPPort) -v -l $($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
-        API = "nheq"
-        Port = $Variables.CPUMinerAPITCPPort
-        Wrap = $false
-        URI = $Uri
-        User = $Pools.($Algo).User
-        Host = $Pools.($Algo).Host
-        Coin = $Pools.($Algo).Coin
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day } # add 100% as pool reports exactly double console output
+        API       = "nheq"
+        Port      = $Variables.CPUMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.($Algo).User
+        Host      = $Pools.($Algo).Host
+        Coin      = $Pools.($Algo).Coin
     }
 }
