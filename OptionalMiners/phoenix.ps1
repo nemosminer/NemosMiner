428012308df68ab672ec2cf6b2582b7fad3cd984
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-09-13 09:07:17 +1200

    Update phoenix.ps1

--- OptionalMiners/phoenix.ps1
+++ OptionalMiners/phoenix.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-$Path = ".\Bin\NVIDIA-Phoenix45c\PhoenixMiner.exe"
-$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/4.5c/PhoenixMiner_4.5c_Windows.7z"
+$Path = ".\Bin\NVIDIA-Phoenix46c\PhoenixMiner.exe"
+$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/4.6c/PhoenixMiner_4.6c_Windows.7z"
 $Commands = [PSCustomObject]@{
     "ethash"  = " -di $($($Config.SelGPUCC).Replace(',',''))" #Ethash
     "progpow" = " -coin bci -di $($($Config.SelGPUCC).Replace(',',''))" #Progpow 
