f2a4f68e02da9a85d3a3bf63df4f6e207bd8083a
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-09-16 10:22:24 +1200

    Update and rename ccminerlyra2v3.ps1 to ccmineryescryptlyra2v3.ps1

--- Miners/ccmineryescryptlyra2v3.ps1
+++ Miners/ccmineryescryptlyra2v3.ps1
@@ -1,17 +1,21 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-$Path = ".\Bin\NVIDIA-Ccminerklaustv3\ccminer.exe"
+$Path = ".\Bin\NVIDIA-CcminerYescryptLyra2v3\ccminer.exe"
 $Uri = "https://github.com/nemosminer/ccminer-KlausT-8.21-mod-r18-src-fix/releases/download/v3/ccminerKlausT.7z"
 
 $Commands = [PSCustomObject]@{
-    "lyra2z330" = " -i 10 -t 1 -a lyra2z330" #Lyra2z330 single GPU only, needs work for multicards rigs
-    "lyra2v3"   = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2v3
-    "lyra2rev3" = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2rev3 
+    "lyra2z330"   = " -i 10 -t 1 -a lyra2z330" #Lyra2z330 single GPU only, needs work for multicards rigs
+    "lyra2v3"     = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2v3
+    "lyra2rev3"   = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2rev3 
+    "yescrypt"    = " -a yescrypt -d $($Config.SelGPUCC)" #Yescrypt
+    "yescryptR16" = " -i 13.25 -a yescryptr16 -d $($Config.SelGPUCC)" #YescryptR16
+    "yescryptR8"  = " -a yescryptr8 -d $($Config.SelGPUCC)" #YescryptR8
+    "yescryptR32" = " -i 12.49 -a yescryptr32 -d $($Config.SelGPUCC)" #YescryptR32
 }
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
-     $Algo = Get-Algorithm($_)
+    $Algo = Get-Algorithm($_)
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
