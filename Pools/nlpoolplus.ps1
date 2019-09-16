e242282e147141a33729de5634571d6ad28aaff0
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-09-17 08:59:46 +1200

    correct nlpool equihash estimates

--- Pools/nlpoolplus.ps1
+++ Pools/nlpoolplus.ps1
@@ -26,6 +26,14 @@ $Request | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty N
 
     $Divisor = 1000000 * [Double]$Request.$_.mbtc_mh_factor
 
+    
+    switch ($PoolAlgorithm) {
+        "equihash125" { $Divisor *= 100 } #temp fix
+        "equihash144" { $Divisor *= 100 } #temp fix
+        "equihash192" { $Divisor *= 100 } #temp fix
+
+    }
+
     $Stat = Set-Stat -Name "$($Name)_$($PoolAlgorithm)_Profit" -Value ([Double]$Request.$_.$PriceField / $Divisor * (1 - ($Request.$_.fees / 100)))
 
     $PwdCurr = if ($PoolConf.PwdCurrency) { $PoolConf.PwdCurrency }else { $Config.Passwordcurrency }
