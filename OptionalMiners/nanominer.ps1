4f1bd9f8a34f07341296bad0ed7f20550c68a5c5
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-10-02 06:27:58 +1300

    Update nanominer.ps1

--- OptionalMiners/nanominer.ps1
+++ OptionalMiners/nanominer.ps1
@@ -1,11 +1,12 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-$Path = ".\Bin\NVIDIA-nanominer153\cmdline_launcher.bat"
-$Uri = "https://github.com/nanopool/nanominer/releases/download/v1.5.3/nanominer-windows-1.5.3.zip"
+$Path = ".\Bin\NVIDIA-nanominer160\cmdline_launcher.bat"
+$Uri = "https://github.com/nanopool/nanominer/releases/download/v1.6.0/nanominer-windows-1.6.0.zip"
 
 $Commands = [PSCustomObject]@{
-    #"cryptonightr"       = "cryptonightr" #cryptonight/r (NiceHash)
-    #"cryptonight-monero" = "cryptonightr" #monero (Mining Pool Hub)
+    #"cryptonightr"       = "-algo cryptonightr" #cryptonight/r (NiceHash)
+    #"grincuckarood29"    = "-algo cuckarood29" #grincuckarood29 (NiceHash)
+    #"cryptonight-monero" = "-algo cryptonightr" #monero (Mining Pool Hub)
 }
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
@@ -13,14 +14,14 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     $Algo = Get-Algorithm($_)
     
     switch ($_) {
-        "grincuckaroo29" { $Fee = 0.02 } # substract devfee
+        "grincuckarood29" { $Fee = 0.02 } # substract devfee
         default { $Fee = 0.01 } # substract devfee
     }
 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "-mport -$($Variables.NVIDIAMinerAPITCPPort) -algo $($Commands.$_) -wallet $($Pools.($Algo).User) -rigName $($Pools.($Algo).Pass) -pool1 $($Pools.($Algo).Host):$($Pools.($Algo).Port)"
+        Arguments = "-mport -$($Variables.NVIDIAMinerAPITCPPort) -wallet $($Pools.($Algo).User) -rigName $($Pools.($Algo).Pass) -pool1 $($Pools.($Algo).Host):$($Pools.($Algo).Port)"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * (1 - $Fee) } # substract devfee
         API       = "nanominer"
         Port      = $Variables.NVIDIAMinerAPITCPPort
