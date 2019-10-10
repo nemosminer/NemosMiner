9b08043b584769f12018a0fd36551d3b30e71e84
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-10-11 05:21:46 +1300

    Update rplantcpu.ps1
    update rplant add power2b

--- Miners/rplantcpu.ps1
+++ Miners/rplantcpu.ps1
@@ -1,11 +1,13 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-$Path = ".\Bin\CPU-Opt4012\cpuminer-sse42.exe" #Intel
-#$Path = ".\Bin\CPU-Opt4012\cpuminer-ryzen.exe" #AMD
-$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v4.0.12/cpuminer-rplant-4.0.12-win.zip"
+$Path = ".\Bin\CPU-Opt4020\cpuminer-sse42.exe" #Intel
+#$Path = ".\Bin\CPU-Opt4020\cpuminer-ryzen.exe" #AMD
+$Uri = "https://github.com/rplant8/cpuminer-opt-rplant/releases/download/4.0.20/cpuminer-opt-win.zip"
 
 $Commands = [PSCustomObject]@{
     "yescryptr8" = " -a yescryptr8" #YescryptR8
+    "power2b" = " -a power2b" #power2b
+    
 }
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
