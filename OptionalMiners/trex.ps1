04760715cf90edbaf70c68ed1a291b95491e2041
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-10-12 14:58:07 +1300

    Update trex.ps1
    Bug fixes:
     High CPU usage on NVIDIA drivers 431+

--- OptionalMiners/trex.ps1
+++ OptionalMiners/trex.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-$Path = ".\Bin\NVIDIA-trex0144\t-rex.exe"
-$Uri = "https://github.com/trexminer/T-Rex/releases/download/0.14.4/t-rex-0.14.4-win-cuda10.0.zip"
+$Path = ".\Bin\NVIDIA-trex0146\t-rex.exe"
+$Uri = "https://github.com/trexminer/T-Rex/releases/download/0.14.6/t-rex-0.14.6-win-cuda10.0.zip"
 
 $Commands = [PSCustomObject]@{
     "balloon"    = " -a balloon -i 23" #Balloon
