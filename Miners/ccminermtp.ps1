4dad4656a00c06fba74f28098fad890f049d42f6
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-10-26 08:39:31 +1300

    Update ccminermtp.ps1

--- Miners/ccminermtp.ps1
+++ Miners/ccminermtp.ps1
@@ -1,10 +1,10 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-$Path = ".\Bin\NVIDIA-ccminermtp123\ccminer.exe"
-$Uri = "https://github.com/zcoinofficial/ccminer/releases/download/1.2.3/ccminer.exe"
+$Path = ".\Bin\NVIDIA-ccminermtp130\ccminer.exe"
+$Uri = "https://github.com/zcoinofficial/ccminer/releases/download/1.3.0/ccminer.exe"
 
 $Commands = [PSCustomObject]@{
-      #"mtp" = " -d $($Config.SelGPUCC)" #mtp
+      #"mtp" = " -d $($Config.SelGPUCC)" #mtp needs work for 6 or more GPU rigs
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
