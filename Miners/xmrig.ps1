4c9f525f13823919323803014d2299ba1bf90526
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-10-29 13:58:16 +1300

    Update xmrig.ps1

--- Miners/xmrig.ps1
+++ Miners/xmrig.ps1
@@ -4,10 +4,10 @@ $Path = ".\Bin\NVIDIA-XMRig2145\xmrig-nvidia.exe"
 $Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v2.14.5/xmrig-nvidia-2.14.5-msvc-win64.7z"
 
 $Commands = [PSCustomObject]@{
-    #"cryptonightr"       = " --nicehash" #cryptonight/r (NiceHash)
-    #"cryptonight-monero" = "" #cryptonight/r (Mining Pool Hub)
+    "cryptonightr"       = " --nicehash" #cryptonight/r (NiceHash)
+    "cryptonight-monero" = "" #cryptonight/r (Mining Pool Hub)
 }
-$Port = 4068 #2222
+$Port = $Variables.NVIDIAMinerAPITCPPort
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
