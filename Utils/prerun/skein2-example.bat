bb67ab84f9ed87cebfa77f1fb5b0d3db8ebd6061
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-11-04 11:51:35 +1300

    Update skein2-example.bat

--- Utils/prerun/skein2-example.bat
+++ Utils/prerun/skein2-example.bat
@@ -3,8 +3,8 @@ REM This file and nvidia inspector is disabled by default to Enable them, rename
 REM Below is a example for a 6 GPU setup
 
 SET PL=61
-SET MEMORY=-500
-SET CORE=125
+SET MEMORY=-750
+SET CORE=150
 SET TEMP=90
 
 SET GPU0=-setBaseClockOffset:0,0,%CORE% -setMemoryClockOffset:0,0,%MEMORY% -setPowerTarget:0,%PL% -setTempTarget:0,0,%TEMP%
