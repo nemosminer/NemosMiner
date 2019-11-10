2ce8b0bd75604ea282bbca4619994c1a791b72a6
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-11-11 11:15:36 +1300

    Update and rename xmrig.ps1 to xmrigcuda.ps1

--- Miners/xmrigcuda.ps1
+++ Miners/xmrigcuda.ps1
@@ -1,12 +1,13 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-$Path = ".\Bin\NVIDIA-XMRig2145\xmrig-nvidia.exe"
-$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v2.14.5/xmrig-nvidia-2.14.5-msvc-win64.7z"
+$Path = ".\Bin\NVIDIA-XMRig461b\xmrig.exe"
+$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v4.6.1-beta/xmrig-4.6.1-beta-win64.7z"
 
 $Commands = [PSCustomObject]@{
     "cryptonightr"       = " --nicehash" #cryptonight/r (NiceHash)
     "cryptonight-monero" = "" #cryptonight/r (Mining Pool Hub)
 }
+
 $Port = $Variables.NVIDIAMinerAPITCPPort
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
@@ -15,14 +16,12 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "-R 1 --cuda-devices=$($Config.SelGPUCC) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_) -a cryptonight/r --keepalive --api-port=$($Variables.NVIDIAMinerAPITCPPort) --donate-level=0"
+        Arguments = "--no-cpu --cuda --cuda-loader=xmrig-cuda.dll --no-nvml -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -a cryptonight/r -p $($Pools.($Algo).Pass)$($Commands.$_) --keepalive --http-port=$($Variables.NVIDIAMinerAPITCPPort) --donate-level 0"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day }
         API       = "XMRig"
         Port      = $Variables.NVIDIAMinerAPITCPPort
         Wrap      = $false
-        URI       = $Uri
+        URI       = $Uri    
         User      = $Pools.($Algo).User
-        Host      = $Pools.($Algo).Host
-        Coin      = $Pools.($Algo).Coin
     }
 }
