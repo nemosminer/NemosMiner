c63ecd3a984c783f4426c6bd8f7ad6c4766dec50
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-11-11 11:27:14 +1300

    Update xmrigcuda.ps1
    fix cuda device selection

--- Miners/xmrigcuda.ps1
+++ Miners/xmrigcuda.ps1
@@ -16,7 +16,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "--no-cpu --cuda --cuda-loader=xmrig-cuda.dll --no-nvml -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -a cryptonight/r -p $($Pools.($Algo).Pass)$($Commands.$_) --keepalive --http-port=$($Variables.NVIDIAMinerAPITCPPort) --donate-level 0"
+        Arguments = "-R 1 --threads=$($Config.SelGPUCC) --no-cpu --cuda --cuda-loader=xmrig-cuda.dll --no-nvml -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -a cryptonight/r -p $($Pools.($Algo).Pass)$($Commands.$_) --keepalive --http-port=$($Variables.NVIDIAMinerAPITCPPort) --donate-level 0"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day }
         API       = "XMRig"
         Port      = $Variables.NVIDIAMinerAPITCPPort
