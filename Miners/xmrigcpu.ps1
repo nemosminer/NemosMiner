7e1b4adc57e1e39e4c6fa789bdf3de2db4314654
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-11-11 13:12:28 +1300

    Update xmrigcpu.ps1

--- Miners/xmrigcpu.ps1
+++ Miners/xmrigcpu.ps1
@@ -4,8 +4,8 @@ $Path = ".\Bin\CPU-XMRig461b\xmrig.exe"
 $Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v4.6.1-beta/xmrig-4.6.1-beta-win64.7z"
 
 $Commands = [PSCustomObject]@{
-    "cryptonightr"       = " --nicehash" #cryptonight/r (NiceHash)
-    "cryptonight-monero" = "" #cryptonight/r (Mining Pool Hub)
+    "cryptonightr"       = " -a cryptonight/r --nicehash" #cryptonight/r (NiceHash)
+    "cryptonight-monero" = " -a cryptonight/r" #cryptonight/r (Mining Pool Hub)
 }
 
 $ThreadCount = $ThreadCount = $Variables.ProcessorCount - 1
@@ -17,7 +17,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "CPU"
         Path      = $Path
-        Arguments = "-t $($ThreadCount) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -a cryptonight/r -p $($Pools.($Algo).Pass)$($Commands.$_) --keepalive --http-port=$($Variables.CPUMinerAPITCPPort) --donate-level 0"
+        Arguments = "-t $($ThreadCount) -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_) --keepalive --http-port=$($Variables.CPUMinerAPITCPPort) --donate-level 0"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day }
         API       = "XMRig"
         Port      = $Variables.CPUMinerAPITCPPort
