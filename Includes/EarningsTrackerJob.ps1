ba4b5311ed313a71b2b69eab8d288ebb7f18880c
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-11-11 14:44:15 +1300

    Update EarningsTrackerJob.ps1
    zergpoolcoins earnings tracker support

--- Includes/EarningsTrackerJob.ps1
+++ Includes/EarningsTrackerJob.ps1
@@ -46,7 +46,7 @@ while ($true) {
     $Interval = $EarningsTrackerConfig.PollInterval
     
     #Filter pools variants
-    $TrackPools = (($EarningsTrackerConfig.pools | sort -Unique).replace("plus", "")).replace("24hr", "")
+    $TrackPools = (($EarningsTrackerConfig.pools | sort -Unique).replace("plus", "")).replace("24hr", "").replace("coins", "")
 
     # Get pools api ref
     If (-not $poolapi -or ($LastAPIUpdateTime -le (Get-Date).AddDays(-1))) {
