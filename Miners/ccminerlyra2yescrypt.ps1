40f7644fc3eba4bb1693519b8cf41e9d115f2ad2
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-11-13 10:52:12 +1300

    Update and rename ccminerlyra2.ps1 to ccminerlyra2yescrypt.ps1
    cuda 10.0
    vs 2015
    Windows x64
    
    Version 3 of 8.21 (3rd compile, has better stale share detection) for Yescrypt32 compared to 8.23 V9 and V10
    
    Lyra2z330 (single Gpu only),
    Lyra2v3 (fastest open or closed source, same or faster than latest CryptoDredge)
    Yescrypt
    YescryptR8
    YescryptR16
    YescryptR32

--- Miners/ccminerlyra2yescrypt.ps1
+++ Miners/ccminerlyra2yescrypt.ps1
@@ -1,14 +1,15 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-$Path = ".\Bin\NVIDIA-CcminerYescryptLyra2-V3\ccminer.exe"
-$Uri = "https://github.com/Minerx117/ccminer-KlausT-8.21-mod-r18-src-fix/releases/download/8.2.1-v3/ccminerKlausT-8.21.3-v3.7z"
+$Path = ".\Bin\NVIDIA-CcminerLyra2yescrypt821v3\ccminer.exe"
+$Uri = "https://github.com/Minerx117/ccminer-KlausT-8.21-mod-r18-src-fix/releases/download/8.21-v3/ccminerLyra2yescrypt-8.21-v3.7z"
 
 $Commands = [PSCustomObject]@{
     "lyra2z330"   = " -i 10 -t 1 -a lyra2z330" #Lyra2z330 single GPU only, needs work for multicards rigs
     "lyra2v3"     = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2v3
-    "lyra2rev3"   = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2rev3 
-    "yescryptR16" = " -i 13.25 -a yescryptr16 -d $($Config.SelGPUCC)" #YescryptR16
+    "lyra2rev3"   = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2rev3
+    "yescrypt"    = " -d $($Config.SelGPUCC)" #Yescrypt
     "yescryptR8"  = " -a yescryptr8 -d $($Config.SelGPUCC)" #YescryptR8
+    "yescryptR16" = " -i 13.25 -a yescryptr16 -d $($Config.SelGPUCC)" #YescryptR16
     "yescryptR32" = " -i 12.49 -a yescryptr32 -d $($Config.SelGPUCC)" #YescryptR32
 }
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
