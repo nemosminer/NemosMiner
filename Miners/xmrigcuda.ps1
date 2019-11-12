2de8e958d1b70ccd7ffe74eda808ab848ce9b2f6
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-11-13 12:16:00 +1300

    Update xmrigcuda.ps1

--- Miners/xmrigcuda.ps1
+++ Miners/xmrigcuda.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-$Path = ".\Bin\NVIDIA-XMRig461b\xmrig.exe"
-$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v4.6.1-beta/xmrig-4.6.1-beta-win64.7z"
+$Path = ".\Bin\NVIDIA-XMRig462b\xmrig.exe"
+$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v4.6.2-beta/xmrig-4.6.2-beta-msvc-cuda10_1-win64.7z"
 
 $Commands = [PSCustomObject]@{
     "cryptonightr"       = " -a cryptonight/r --nicehash" #cryptonight/r (NiceHash)
@@ -16,7 +16,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "-R 1 --threads=$($Config.SelGPUCC) --no-cpu --cuda --cuda-loader=xmrig-cuda.dll --no-nvml -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_) --keepalive --http-port=$($Variables.NVIDIAMinerAPITCPPort) --donate-level 0"
+        Arguments = "-R 1 --cuda-devices=$($Config.SelGPUCC) --no-cpu --cuda --cuda-loader=xmrig-cuda.dll --no-nvml -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_) --keepalive --http-port=$($Variables.NVIDIAMinerAPITCPPort) --donate-level 0"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day }
         API       = "XMRig"
         Port      = $Variables.NVIDIAMinerAPITCPPort
