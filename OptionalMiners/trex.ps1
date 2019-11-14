7cdfe65df81a490b6a86e78e54167517f3516c5f
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-11-15 10:23:05 +1300

    decrease stratum timeout
     (40,000 = mtp + a poor internet connection)

--- OptionalMiners/trex.ps1
+++ OptionalMiners/trex.ps1
@@ -39,7 +39,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "--no-watchdog --no-nvml --gpu-report-interval 25 -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_) --quiet -R 1 -T 50000 --cpu-priority 4"
+        Arguments = "--no-watchdog --no-nvml --gpu-report-interval 25 -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_) --quiet -R 1 -T 40000 --cpu-priority 4"
         HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Day * .99 } # substract 1% devfee
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort
