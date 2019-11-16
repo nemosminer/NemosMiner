4f4109bc5f6951c9d748c6ba9cb1ef61042d9c9b
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-11-16 15:36:21 +1300

    Update and rename ccminerlyra2yescryptr.ps1 to ccmineryescryptr.ps1

--- Miners/ccmineryescryptr.ps1
+++ Miners/ccmineryescryptr.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
 $Path = ".\Bin\NVIDIA-CcmineryescryptrV4\ccminer.exe"
-$Uri = "https://github.com/Minerx117/ccminer-KlausT-8.21-mod-r18-src-fix/releases/download/v4/ccmineryescryptrv4.7z"
+$Uri = "https://github.com/Minerx117/ccminer-KlausT-8.21-mod-r18-src-fix/releases/download/v4/ccmineryescryptrv4.zip"
 
 $Commands = [PSCustomObject]@{
     "lyra2z330"   = " -i 10 -t 1 -a lyra2z330" #Lyra2z330 single GPU only, needs work for multicards rigs
