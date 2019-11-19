3bcffa5c777c0b603656adaa8f959986326d0c73
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-11-20 10:46:49 +1300

    Create ccminerlyra2z330.ps1
    fix single GPU problem (now runs on all GPU's)
    
    fix intensity (default should be optimal)
    
    increase hash-rate by 100% over previous single GPU compiles (tested on GTX 1070 before 1.33khs after 2.66khs
    
    vc++ 2015 cuda 10.0
    
    compiled for windows 7,8, & 10
    
    Test on RTX 2080 RTX 2060 GTX 1660ti and 1070
    
    remove un used compute code (reduce compile size)

---
+++ Miners/ccminerlyra2z330.ps1
@@ -0,0 +1,26 @@
+if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+
+$Path = ".\Bin\NVIDIA-Ccminerlyra2z330v1\ccminer.exe"
+$Uri = "https://github.com/Minerx117/ccminer8.21r9-lyra2z330/releases/download/v1/ccminerlyra2z330v1.zip"
+
+$Commands = [PSCustomObject]@{
+    "lyra2z330" = " -a lyra2z330" #Lyra2z330
+}
+
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object {
+    [PSCustomObject]@{
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "--cpu-priority 4 -b $($Variables.NVIDIAMinerAPITCPPort) -T 50000 -R 1 -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Day }
+        API       = "ccminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.$Algo.User
+        Host      = $Pools.$Algo.Host
+        Coin      = $Pools.$Algo.Coin
+    }
+}
