65679e3a355344d8d3369dce86590b63626b9719
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-11-26 20:06:01 +1300

    Update lolminer.ps1

--- OptionalMiners/lolminer.ps1
+++ OptionalMiners/lolminer.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
  
-$Path = ".\Bin\NVIDIA-lolMiner091\lolMiner.exe"
-$Uri = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/0.91/lolMiner_v091_Win64.zip"
+$Path = ".\Bin\NVIDIA-lolMiner091hf\lolMiner.exe"
+$Uri = "https://github.com/Lolliedieb/lolMiner-preview/releases/download/0.9.1hotfix/lolMiner_v091_hotfix_Win64.zip"
 
 $Commands = [PSCustomObject]@{
      "equihash144"    = " --coin AUTO144_5" #Equihash 144,5
