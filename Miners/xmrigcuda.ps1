4546d54bcab3ced5abd9431cf3a0d6fd09fb1efb
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-11-28 11:57:24 +1300

    Update xmrigcuda.ps1

--- Miners/xmrigcuda.ps1
+++ Miners/xmrigcuda.ps1
@@ -4,9 +4,10 @@ $Path = ".\Bin\NVIDIA-XMRig501\xmrig.exe"
 $Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v5.0.1/xmrig-5.0.1-msvc-cuda10_1-win64.7z"
 
 $Commands = [PSCustomObject]@{
-   #"randomx"            = " --coin=monero --nicehash" #RandomX
-    "cryptonightr"       = " -a cryptonight/r --nicehash" #cryptonight/r (NiceHash)
-    "cryptonight-monero" = " -a cryptonight/r" #cryptonight/r (Mining Pool Hub)
+    #"randomx"            = " --coin=monero --nicehash" #RandomX
+    #"randomx"            = " -a rx/0 --nicehash" #RandomX
+    "cryptonightr"        = " -a cryptonight/r --nicehash" #cryptonight/r (NiceHash)
+    "cryptonight-monero"  = " -a cryptonight/r" #cryptonight/r (Mining Pool Hub)
 }
 
 $Port = $Variables.NVIDIAMinerAPITCPPort
