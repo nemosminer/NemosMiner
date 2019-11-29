90e0174d6cdf152ba3d550aca375960b7193fbf3
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-11-30 08:28:13 +1300

    v0.9.2
    Improved performance on GRIN-AT31 for 8 and 16G cards by (2-8%)

--- OptionalMiners/lolminer.ps1
+++ OptionalMiners/lolminer.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
  
 $Path = ".\Bin\NVIDIA-lolMiner091hf\lolMiner.exe"
-$Uri = "https://github.com/Lolliedieb/lolMiner-preview/releases/download/0.9.1hotfix/lolMiner_v091_hotfix_Win64.zip"
+$Uri = "https://github.com/Lolliedieb/lolMiner-preview/releases/download/0.9.2beta/lolMiner_v092_beta_Win64.zip"
 
 $Commands = [PSCustomObject]@{
      "equihash144"    = " --coin AUTO144_5" #Equihash 144,5
@@ -9,7 +9,7 @@ $Commands = [PSCustomObject]@{
      "equihash96"     = " --coin AUTO96_5" #Equihash 144,5
      "equihash125"    = " --coin AUTO125_4" #Equihash 125,4
      "beamv2"         = " --coin BEAM-II" #Equihash 150,5 (NiceHash)
-    #"grincuckatoo31" = " --coin GRIN-AT31" 
+     "grincuckatoo31" = " --coin GRIN-AT31" 
 }
 
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
