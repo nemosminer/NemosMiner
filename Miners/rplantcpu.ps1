ecbd76834de0b398850fc2f7e8ae3074aabc1eee
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-12-18 18:36:50 +1300

    Update rplantcpu.ps1

--- Miners/rplantcpu.ps1
+++ Miners/rplantcpu.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-#$Path = ".\Bin\CPU-Opt4028\cpuminer-sse42.exe" #Intel
-$Path = ".\Bin\CPU-Opt4028\cpuminer-ryzen.exe" #AMD
+$Path = ".\Bin\CPU-Opt4028\cpuminer-sse42.exe" #Intel
+#$Path = ".\Bin\CPU-Opt4028\cpuminer-ryzen.exe" #AMD
 $Uri = "https://github.com/rplant8/cpuminer-opt-rplant/releases/download/4.0.28/cpuminer-opt-win.zip"
 
 $Commands = [PSCustomObject]@{
