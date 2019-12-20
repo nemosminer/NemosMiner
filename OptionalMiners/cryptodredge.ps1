19cb760523b88f81b1e94d1602b1f00860e5b8b2
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-12-21 11:42:52 +1300

    Update cryptodredge.ps1
    update to cuda 10.1 for CD

--- OptionalMiners/cryptodredge.ps1
+++ OptionalMiners/cryptodredge.ps1
@@ -2,22 +2,22 @@ if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1;RegisterLoa
 
 
 $Path = ".\Bin\NVIDIA-CryptoDredge022\CryptoDredge.exe"
-$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.22.0/CryptoDredge_0.22.0_cuda_10.0_windows.zip"
+$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.22.0/CryptoDredge_0.22.0_cuda_10.1_windows.zip"
 
 $Commands = [PSCustomObject]@{
     "argon2d250"  = " --intensity 8 -a argon2d250" #argon2d250
    #"argon2d500"  = " --intensity 6 -a argon2d-dyn" #Argon2d-dyn
     "argon2d4096" = " --intensity 8 -a argon2d4096" #argon2d4096
-    #"argon2ddyn"  = " --intensity 6 -a argon2d-dyn" #Argon2d-dyn
-    #"allium"      = " --intensity 8 -a allium" #Allium
+   #"argon2ddyn"  = " --intensity 6 -a argon2d-dyn" #Argon2d-dyn
+    "allium"      = " --intensity 8 -a allium" #Allium
     "lyra2zz "    = " --intensity 8 -a lyra2zz" #Lyra2zz
-    #"neoscrypt"   = " --intensity 6 -a neoscrypt" #Neoscrypt
-    #"phi2"        = " --intensity 8 -a phi2" #Phi2 
-    #"phi2-lux"    = " --intensity 8 -a phi2" #Phi2-lux   
+    "neoscrypt"   = " --intensity 6 -a neoscrypt" #Neoscrypt
+    "phi2"        = " --intensity 8 -a phi2" #Phi2 
+    "phi2-lux"    = " --intensity 8 -a phi2" #Phi2-lux   
     "lux"         = " --intensity 8 -a phi2" #Lux
     "lyra2vc0ban" = " --intensity 8 -a lyra2vc0ban" #Lyra2vc0banHash
     "skunk"       = " --intensity 8 -a skunk" #Skunk 
-    #"hmq1725"     = " --intensity 8 -a hmq1725" #Hmq1725
+    "hmq1725"     = " --intensity 8 -a hmq1725" #Hmq1725
     "pipe"        = " --intensity 8 -a pipe" #Pipe 
 }
 
@@ -34,7 +34,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "--timeout 60 --api-type ccminer-tcp --cpu-priority 3 --no-watchdog -r -1 -R 1 -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
+        Arguments = "--timeout 60 --api-type ccminer-tcp --no-watchdog -r -1 -R 1 -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Day * (1 - $Fee) } # substract devfee
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort
