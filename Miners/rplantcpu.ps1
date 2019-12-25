585ee5d4addd288aa055a69c25250dd697244982
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-12-26 08:39:28 +1300

    Update rplantcpu.ps1

--- Miners/rplantcpu.ps1
+++ Miners/rplantcpu.ps1
@@ -1,15 +1,16 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-$Path = ".\Bin\CPU-Opt4028\cpuminer-sse42.exe" #Intel
-#$Path = ".\Bin\CPU-Opt4028\cpuminer-ryzen.exe" #AMD
-$Uri = "https://github.com/rplant8/cpuminer-opt-rplant/releases/download/4.0.28/cpuminer-opt-win.zip"
+$Path = ".\Bin\CPU-Opt4029\cpuminer-sse42.exe" #Intel
+#$Path = ".\Bin\CPU-Opt4029\cpuminer-ryzen.exe" #AMD
+$Uri = "https://github.com/rplant8/cpuminer-opt-rplant/releases/download/4.0.29/cpuminer-opt-win.zip"
 
 $Commands = [PSCustomObject]@{
     "yespoweriots" = " -a yespoweriots" #yespoweriots
     "yescryptr8"   = " -a yescryptr8" #YescryptR8 
     "yescryptr16"  = " -a yescryptr16" #YescryptR16 
-    "yescryptr32"  = " -a yescryptr32" #YescryptR32 
-    "yescryptr8g"  = " -a yescryptr8g" #YescryptR8g     
+    "yescryptr32"  = " -a yescryptr32" #YescryptR32  
+    "yescryptr8g"  = " -a yescryptr8g" #YescryptR8g  
+    "yespoweritc"  = " -a yespoweritc" #Yespoweritc  
 }
 
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
