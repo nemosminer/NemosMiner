15bbc5274297ca8bfac1fa33d632f5bbab35befe
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-12-31 11:27:42 +1300

    Update gminer.ps1

--- OptionalMiners/gminer.ps1
+++ OptionalMiners/gminer.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
  
-$Path = ".\Bin\NVIDIA-Gminer191\miner.exe"
-$Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.91/gminer_1_91_windows64.zip"
+$Path = ".\Bin\NVIDIA-Gminer192\miner.exe"
+$Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.92/gminer_1_92_windows64.zip"
 $Commands = [PSCustomObject]@{
     #"beamv2"         = " --devices $($Config.SelGPUDSTM) -a BeamHashII" #Equihash150 (NiceHash)
     #"equihash125"  = " --devices $($Config.SelGPUDSTM) -a 125_4" #Equihash125
