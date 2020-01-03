d6ea7270c98e38b3ee2ded57a1184b74228b0347
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-01-03 19:58:45 +1300

    Update and rename ccminerklaust.ps1 to cudaminerneoscrypt.ps1
    OG

--- Miners/cudaminerneoscrypt.ps1
+++ Miners/cudaminerneoscrypt.ps1
@@ -1,10 +1,10 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-$Path = ".\Bin\NVIDIA-ccminerklaust825\ccminer.exe"
-$Uri = "https://github.com/Minerx117/ccminerklaust/releases/download/8.25/ccminerklaust.zip"
+$Path = ".\Bin\NVIDIA-cudaminerneo101\cudaminer-x64.exe"
+$Uri = "https://github.com/ghostlander/cudaminer-neoscrypt/releases/download/v1.0.1/cudaminer-neoscrypt-win-1.0.1.zip"
 
 $Commands = [PSCustomObject]@{
-     "neoscrypt" = " -d $($Config.SelGPUCC)" 
+     "neoscrypt" = " -i 16.999 -d $($Config.SelGPUCC)" 
 }
 
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
@@ -14,7 +14,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "--cpu-priority 4 -R 1 -b $($Variables.NVIDIAMinerAPITCPPort) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -a $Algo -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
+        Arguments = "--cpu-priority 4 -N 2 -R 1 -b $($Variables.NVIDIAMinerAPITCPPort) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Day }
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort #4068
