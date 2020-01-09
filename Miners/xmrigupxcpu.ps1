292ac239e2ecf32c34a96bdac89601a8bbdd951d
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-01-09 15:04:51 +1300

    Create xmrigupxcpu.ps1
    add recompiled xmrig-upx   MSVC 2017 
    
    -Add cryptonight_upx support ZergPool

---
+++ Miners/xmrigupxcpu.ps1
@@ -0,0 +1,26 @@
+if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+
+$Path = ".\Bin\CPU-XMRigUPX020\xmrig.exe"
+$Uri = "https://github.com/Minerx117/xmrig-upx/releases/download/0.2.0/XMRigUPX020.zip"
+
+$Commands = [PSCustomObject]@{
+  "cryptonight_upx" = " -a cryptonight-upx/2 --nicehash"
+}
+
+$ThreadCount = $Variables.ProcessorCount - 1
+$Port = $Variables.CPUMinerAPITCPPort
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object {
+  [PSCustomObject]@{
+    Type      = "CPU"
+    Path      = $Path
+    Arguments = "-t $($ThreadCount) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_) --keepalive --api-port=$($Variables.CPUMinerAPITCPPort) --donate-level 0"
+    HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Day } #Recompiled 0% fee
+    API       = "XMRig"
+    Port      = $Variables.CPUMinerAPITCPPort
+    Wrap      = $false
+    URI       = $Uri    
+    User      = $Pools.$Algo.User
+  }
+}
