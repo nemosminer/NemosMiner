a3bd707dd6ef63030e80e798f14850f14c6fc999
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-01-10 17:35:52 +1300

    Update xmrigcpu.ps1

--- Miners/xmrigcpu.ps1
+++ Miners/xmrigcpu.ps1
@@ -1,6 +1,6 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\CPU-XMRig550\xmrig.exe"
-$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/5.5.0/xmrig-5.5.0-msvc-cuda10_1-win64.7z"
+$Path = ".\Bin\CPU-XMRigcc250\xmrigDaemon.exe"
+$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/2.5.0/xmrigcc.zip"
 $Commands = [PSCustomObject]@{
     "randomxmonero"      = " -a rx/0 --nicehash" #RandomX
     "randomarq"          = " -a rx/arq --nicehash" #Randomarq
@@ -13,6 +13,7 @@ $Commands = [PSCustomObject]@{
     "cryptonight_saber"  = " -a cn-heavy/0 --nicehash" #cryptonightGPU
     "cryptonight_fast"   = " -a cn/half --nicehash" #cryptonightFast
     "cryptonight_haven"  = " -a cn-heavy/xhv --nicehash" #cryptonightFast
+    "cryptonight_upx"    = " -a cryptonight-upx/2 --nicehash" #cryptonightupx
 }
 $ThreadCount = $Variables.ProcessorCount - 1
 $Port = $Variables.CPUMinerAPITCPPort
@@ -21,7 +22,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type      = "CPU"
         Path      = $Path
-        Arguments = "-t $($ThreadCount) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_) --keepalive --http-port=$($Variables.CPUMinerAPITCPPort) --donate-level 0"
+        Arguments = "-t $($ThreadCount) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_) --keepalive --http-enabled --http-port=$($Variables.CPUMinerAPITCPPort) --donate-level 0"
         HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Day } #Recompiled 0% fee
         API       = "XMRig"
         Port      = $Variables.CPUMinerAPITCPPort
