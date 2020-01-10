e39acd9065953fc889bc515224bc8590790942a5
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-01-11 11:26:57 +1300

    cryptonight_upx cuda
    MSVC 2017 CUDA 10.1 update2 
    Recompiled

---
+++ Miners/xmrigcudaupx.ps1
@@ -0,0 +1,21 @@
+if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+$Path = ".\Bin\NVIDIA-XMRig2144upx\xmrig-nvidia.exe"
+$Uri = "https://github.com/Minerx117/xmrig-nvidia/releases/download/2.14.4%2Bupx/xmrigcudaupx.zip"
+$Commands = [PSCustomObject]@{
+    "cryptonight_upx" = " -a cn-extremelite --nicehash" #cryptonight_upx
+}
+$Port = $Variables.NVIDIAMinerAPITCPPort
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object {
+    [PSCustomObject]@{
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "-R 1 --cuda-devices=$($Config.SelGPUCC) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_) --api-port=$($Variables.NVIDIAMinerAPITCPPort) --donate-level 0"
+        HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Day } #Recompiled 0% fee
+        API       = "XMRig"
+        Port      = $Variables.NVIDIAMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri    
+        User      = $Pools.$Algo.User
+    }
+}
