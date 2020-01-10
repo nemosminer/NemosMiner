5a83e0629035acc40bac3c4176b0d69eeca51ae7
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-01-10 22:09:19 +1300

    Update cudaminerneoscrypt.ps1

--- Miners/cudaminerneoscrypt.ps1
+++ Miners/cudaminerneoscrypt.ps1
@@ -2,7 +2,7 @@ if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterL
 $Path = ".\Bin\NVIDIA-cudaminerneo101\cudaminer-x64.exe"
 $Uri = "https://github.com/ghostlander/cudaminer-neoscrypt/releases/download/v1.0.1/cudaminer-neoscrypt-win-1.0.1.zip"
 $Commands = [PSCustomObject]@{
-    "neoscrypt" = " -i 16.999 -d $($Config.SelGPUCC)" 
+   #"neoscrypt" = " -i 16.999 -d $($Config.SelGPUCC)" 
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object {
