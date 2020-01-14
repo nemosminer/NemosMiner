8919a622ddbad9c42aa08206a705a51acd5e84e7
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-01-15 06:03:43 +1300

    Update gminer.ps1
    added Cuckaroom29 algorithm (DevFee is 3%), use --algo grin29 for hardfork autoswitch (Watchdog should be enabled)
    added Cuckarood29v algorithm (MoneroV coin, DevFee is 10%)
    fixed "No shares" bug for Eaglesong when extranonce length not equal to 4

--- OptionalMiners/gminer.ps1
+++ OptionalMiners/gminer.ps1
@@ -1,6 +1,6 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\NVIDIA-Gminer192\miner.exe"
-$Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.92/gminer_1_92_windows64.zip"
+$Path = ".\Bin\NVIDIA-Gminer193\miner.exe"
+$Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.93/gminer_1_93_windows64.zip"
 $Commands = [PSCustomObject]@{
     #"beamv2"         = " --devices $($Config.SelGPUDSTM) -a BeamHashII" #Equihash150 (NiceHash)
     #"equihash125"  = " --devices $($Config.SelGPUDSTM) -a 125_4" #Equihash125
