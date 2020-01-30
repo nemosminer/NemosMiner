e564337d6d2c2be132fc639f1aafe8decf63826a
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-01-31 05:34:04 +1300

    Update jayddeecpu.ps1

--- Miners/jayddeecpu.ps1
+++ Miners/jayddeecpu.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") } 
-#$Path = ".\Bin\CPU-JayDDee3117\cpuminer-zen.exe" #AMD
-$Path = ".\Bin\CPU-JayDDee3117\cpuminer-aes-sse42.exe" #Intel
-$Uri = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.11.7/cpuminer-opt-3.11.7-windows.zip"
+#$Path = ".\Bin\CPU-JayDDee3118\cpuminer-zen.exe" #AMD
+$Path = ".\Bin\CPU-JayDDee3118\cpuminer-aes-sse42.exe" #Intel
+$Uri = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.11.8/cpuminer-opt-3.11.8-windows.zip"
 $Commands = [PSCustomObject]@{ 
     "lyra2z330"   = " -a lyra2z330" #Lyra2z330
     "sha3d"   = " -a sha3d" #sha3d
