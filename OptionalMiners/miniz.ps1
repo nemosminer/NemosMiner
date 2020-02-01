0b1b3184a7911a49a481404540177722ed54d7f6
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-02-01 15:43:34 +1300

    Update miniz.ps1

--- OptionalMiners/miniz.ps1
+++ OptionalMiners/miniz.ps1
@@ -1,5 +1,5 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\NVIDIA-miniZ15s\miniZ.exe"
+$Path = ".\Bin\NVIDIA-miniZ15t\miniZ.exe"
 $Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v1.5t/miniZ_v1.5t_cuda10_win-x64.zip"
 $Commands = [PSCustomObject]@{ 
     "equihash144"  = " --par=144,5 --pers auto" #Equihash144
