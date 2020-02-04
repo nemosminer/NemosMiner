609e4119b29ddba009ac07036a530fb7cdbfe230
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-02-05 09:32:57 +1300

    add scryptn2 support ZergPool
    disable for now as pool dev is working on fixing implementation

---
+++ Miners/cpuscryptn2miner.ps1
@@ -0,0 +1,27 @@
+If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+
+$Path = ".\Bin\CPU-Verium14\cpuminer.exe"
+$Uri = "https://github.com/fireworm71/veriumMiner/releases/download/v1.4/cpuminer_1.4_windows_x64_O2_GCC7.zip"
+$Commands = [PSCustomObject]@{ 
+     #"scryptn2" = "" #Verushash
+}
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { 
+    switch ($_) { 
+        default { $ThreadCount = $Variables.ProcessorCount - 1 }
+    }
+    $Algo = Get-Algorithm($_)
+    [PSCustomObject]@{ 
+        Type      = "CPU"
+        Path      = $Path
+        Arguments = " -t $($ThreadCount) -R 1 -b $($Variables.CPUMinerAPITCPPort) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{$Algo = $Stats."$($Name)_$($Algo)_HashRate".Day }
+        API       = "ccminer"
+        Port      = $Variables.CPUMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.$Algo.User
+        Host      = $Pools.$Algo.Host
+        Coin      = $Pools.$Algo.Coin
+    }
+}
