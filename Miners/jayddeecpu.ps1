b2394c0fb6467ba6fbec683aeb7faee8d67b6819
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-02-06 09:01:01 +1300

    Update jayddeecpu.ps1

--- Miners/jayddeecpu.ps1
+++ Miners/jayddeecpu.ps1
@@ -1,7 +1,7 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 #$Path = ".\Bin\CPU-JayDDee3118\cpuminer-zen.exe" #AMD
-$Path = ".\Bin\CPU-JayDDee3118\cpuminer-aes-sse42.exe" #Intel
-$Uri = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.11.8/cpuminer-opt-3.11.8-windows.zip"
+$Path = ".\Bin\CPU-JayDDee3119\cpuminer-aes-sse42.exe" #Intel
+$Uri = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.11.9/cpuminer-opt-3.11.9-windows.zip"
 $Commands = [PSCustomObject]@{ 
     "lyra2z330"   = " -a lyra2z330" #Lyra2z330
     "sha3d"   = " -a sha3d" #sha3d
@@ -12,7 +12,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{ 
         Type      = "CPU"
         Path      = $Path
-        Arguments = "--cpu-affinity AAAA --hash-meter -q -t $($ThreadCount) -b $($Variables.CPUMinerAPITCPPort) -o $($Pools.$Algo.Protocol)://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
+        Arguments = "--cpu-affinity AAAA --hash-meter -q -t $($ThreadCount) -b 127.0.0.1:$($Variables.CPUMinerAPITCPPort) -o $($Pools.$Algo.Protocol)://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Day }
         API       = "ccminer"
         Port      = $Variables.CPUMinerAPITCPPort
