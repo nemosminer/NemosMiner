3ad31678a2236ca5abf8e978ede0f401bbf7af07
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-02-07 09:12:48 +1300

    Update jayddeecpu.ps1

--- Miners/jayddeecpu.ps1
+++ Miners/jayddeecpu.ps1
@@ -1,10 +1,10 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-#$Path = ".\Bin\CPU-JayDDee3119\cpuminer-zen.exe" #AMD
-$Path = ".\Bin\CPU-JayDDee3119\cpuminer-aes-sse42.exe" #Intel
-$Uri = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.11.9/cpuminer-opt-3.11.9-windows.zip"
+#$Path = ".\Bin\CPU-JayDDee3120\cpuminer-zen.exe" #AMD
+$Path = ".\Bin\CPU-JayDDee3120\cpuminer-aes-sse42.exe" #Intel
+$Uri = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.12.0/cpuminer-opt-3.12.0-windows.zip"
 $Commands = [PSCustomObject]@{ 
-    "lyra2z330"   = " -a lyra2z330" #Lyra2z330
-    "sha3d"   = " -a sha3d" #sha3d
+    "lyra2z330" = " -a lyra2z330" #Lyra2z330
+    "sha3d"     = " -a sha3d" #sha3d
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
@@ -12,7 +12,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{ 
         Type      = "CPU"
         Path      = $Path
-        Arguments = "--cpu-affinity AAAA --hash-meter -q -t $($ThreadCount) -b 127.0.0.1:$($Variables.CPUMinerAPITCPPort) -o $($Pools.$Algo.Protocol)://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
+        Arguments = "--hash-meter -q -t $($ThreadCount) --api-bind=$($Variables.CPUMinerAPITCPPort) -o $($Pools.$Algo.Protocol)://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Day }
         API       = "ccminer"
         Port      = $Variables.CPUMinerAPITCPPort
