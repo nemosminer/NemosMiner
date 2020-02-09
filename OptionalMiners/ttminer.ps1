50d03608b1ed278623667f69bc00a799efd0d47c
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-02-10 09:30:28 +1300

    Create ttminer.ps1

---
+++ OptionalMiners/ttminer.ps1
@@ -0,0 +1,19 @@
+If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+$Path = ".\Bin\NVIDIA-TTMiner3.2.3b1\TT-Miner.exe"
+$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v3.2.3b1/TT-Miner-3.2.3-beta1.zip"
+$Commands = [PSCustomObject]@{ 
+    "mtp" = " -a MTP" #mtp  
+}
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
+    [PSCustomObject]@{ 
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "--nvidia -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Day * .99 } # substract 1% devfee
+        API       = "ccminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri
+    }
+}
