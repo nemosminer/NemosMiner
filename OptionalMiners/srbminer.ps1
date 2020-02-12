f607c3a3b19280e2309015afc8ce1f34a8c2a01c
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-02-13 10:13:23 +1300

    SRBMiner-Multi-0-3-2
    Added algorithm 'bl2bsha3' (Handshake (HNS) coin - 6block.com pool only) - Fee 3%
    RandomX fixes
    Added '--gpu-platform' parameter to cmd config, and 'gpu_platform' to configuration files to force opencl platform
    Added '--randomx-use-1gb-pages' parameter to config, whiche enables 1GB sized page files if supported by CPU

--- OptionalMiners/srbminer.ps1
+++ OptionalMiners/srbminer.ps1
@@ -1,10 +1,10 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\cpu-SRBMiner-Multi-0-3-1/SRBMiner-MULTI.exe"
-$Uri = "https://github.com/doktor83/SRBMiner-Multi/releases/download/0.3.1/SRBMiner-Multi-0-3-1-win64.zip"
+$Path = ".\Bin\cpu-SRBMiner-Multi-0-3-2/SRBMiner-MULTI.exe"
+$Uri = "https://github.com/doktor83/SRBMiner-Multi/releases/download/0.3.2/SRBMiner-Multi-0-3-2-win64.zip"
 $Commands = [PSCustomObject]@{ 
-    "randomx"      = " --algorithm randomx" #randomx 
-    "randomarq"    = " --algorithm randomarq" #randomarq  
-    "randomsfx"    = " --algorithm randomsfx" #randomsfx  
+    "randomx"      = " --algorithm randomx --randomx-use-1gb-pages" #randomx 
+    "randomarq"    = " --algorithm randomarq --randomx-use-1gb-pages" #randomarq  
+    "randomsfx"    = " --algorithm randomsfx --randomx-use-1gb-pages" #randomsfx  
     "eaglesong"    = " --algorithm eaglesong" #eaglesong  
     "yescrypt"     = " --algorithm yescrypt" #yescrypt    
     "yescryptR16"  = " --algorithm yescryptR16" #yescryptR16  
