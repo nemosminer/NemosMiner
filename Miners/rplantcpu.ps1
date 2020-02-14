5d08dc1ff1471ee3a9e0e1f0273774be6e8ead07
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-02-15 06:02:44 +1300

    Update rplantcpu.ps1
    add lyra2tdc algo

--- Miners/rplantcpu.ps1
+++ Miners/rplantcpu.ps1
@@ -1,7 +1,7 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\CPU-Opt4029\cpuminer-sse42.exe" #Intel
-#$Path = ".\Bin\CPU-Opt4029\cpuminer-ryzen.exe" #AMD
-$Uri = "https://github.com/rplant8/cpuminer-opt-rplant/releases/download/4.0.29/cpuminer-opt-win.zip"
+$Path = ".\Bin\CPU-Opt4030\cpuminer-sse42.exe" #Intel
+#$Path = ".\Bin\CPU-Opt4030\cpuminer-ryzen.exe" #AMD
+$Uri = "https://github.com/rplant8/cpuminer-opt-rplant/releases/download/4.0.30/cpuminer-opt-win.zip"
 $Commands = [PSCustomObject]@{ 
     "yespoweriots" = " -a yespoweriots" #yespoweriots
     "yescryptr8"   = " -a yescryptr8" #YescryptR8 
