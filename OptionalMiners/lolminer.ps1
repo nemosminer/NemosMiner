90a968b107ee595fc9959ccef1f69680ad856154
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-02-24 14:23:21 +1300

    Create lolminer.ps1
    add grincuckatoo31 & grincuckatoo31 (Nicehash) support 4gb solvers for Nvidia cards works on 6gb cards windows 10

---
+++ OptionalMiners/lolminer.ps1
@@ -0,0 +1,20 @@
+If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+$Path = ".\Bin\NVIDIA-lolminer098\lolMiner.exe"
+$Uri = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/0.98/lolMiner_v098_Win64.zip"
+$Commands = [PSCustomObject]@{ 
+    "grincuckatoo31" = " --coin GRIN-AT31" #grincuckatoo31
+    "grincuckatoo32" = " --coin GRIN-AT32" #grincuckatoo32
+}
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
+    [PSCustomObject]@{ 
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "--tls 0 --digits 2 --longstats 60 --shortstats 5 --connectattempts 3 --devices $($Config.SelGPUCC) --apiport $($Variables.NVIDIAMinerAPITCPPort) --pool $($Pools.$Algo.Host) --port $($Pools.$Algo.Port) --user $($Pools.$Algo.User) --pass $($Pools.$Algo.Pass)$($Commands.$_)" #
+        HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Day * .99 } # substract 2% devfee
+        API       = "lol"
+        Port      = $Variables.NVIDIAMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri
+    }
+}
