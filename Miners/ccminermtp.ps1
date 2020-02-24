725a3c98008d95498000c4b3f9835825b8010bc1
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-02-25 11:09:55 +1300

    Update ccminermtp.ps1
    reduce cpu usage on latest nvidia drivers. The cpu should have at least 2 cores or 2 threads (if single core).
    This version allows to have a stable 100% gpu usage on all gpu while using --cpu-affinity 1
    
    You need to add to the ccminer command line: --cpu-affinity 1 (or 2 or 4).
    This will assign all the mining threads to a single thread (it is mandatory that this thread is different from thread 0, hence the cpu-affinity)
    
    The cpu usage will be the one you would get if your system had only one gpu.
    
    This version may also improve the running on multigpu system (testing to confirm)

--- Miners/ccminermtp.ps1
+++ Miners/ccminermtp.ps1
@@ -1,8 +1,9 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\NVIDIA-ccminermtp131\ccminer.exe"
-$Uri = "https://github.com/Minerx117/ccminer-2/releases/download/1.3.1/ccminermtp131.zip"
+$Path = ".\Bin\NVIDIA-ccminermtp128\ccminer.exe"
+$Uri = "https://github.com/tecracoin/ccminer/releases/download/1.2.8/ccminer.exe"
 $Commands = [PSCustomObject]@{ 
-     #"mtp" = " -d $($Config.SelGPUCC)" #mtp -i 19 -i 20 -i 21 -i 22
+     #"mtp" = " -d $($Config.SelGPUCC) -a mtp-classic -i 21" #mtp -i 19 -i 20 -i 21 -i 22
+     #"mtptrc" = " -d $($Config.SelGPUCC) -a mtp-tcr -i 21" #mtptrc -i 19 -i 20 -i 21 -i 22
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
@@ -10,7 +11,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{ 
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "--cpu-affinity 1 --cpu-priority 4 -R 1 -b $($Variables.NVIDIAMinerAPITCPPort) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -a $Algo -u $($Pools.$Algo.User) --no-donation -p $($Pools.$Algo.Pass)$($Commands.$_)"
+        Arguments = "--cpu-affinity 1 --cpu-priority 4 -R 1 -b $($Variables.NVIDIAMinerAPITCPPort) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) --no-donation -p $($Pools.$Algo.Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week }
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort #4068
