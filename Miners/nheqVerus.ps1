08bbc48d7f9134fbd0e3216ed74e0a0665d7b12b
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-02-25 10:52:15 +1300

    Update nheqVerus.ps1

--- Miners/nheqVerus.ps1
+++ Miners/nheqVerus.ps1
@@ -14,7 +14,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
         Type      = "CPU"
         Path      = $Path
         Arguments = "-t $($ThreadCount) -a $($Variables.CPUMinerAPITCPPort) -v -l $($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Day }
+        HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week }
         API       = "nheq"
         Port      = $Variables.CPUMinerAPITCPPort
         Wrap      = $false
