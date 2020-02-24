3542bfa9426e0bbec87377c9bbc77bbde0da3a32
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-02-24 21:27:59 +1300

    Update ccminertpruvot.ps1
    scryptn11 stability lower cpu usuage

--- Miners/ccminertpruvot.ps1
+++ Miners/ccminertpruvot.ps1
@@ -2,16 +2,16 @@ If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; Regis
 $Path = ".\Bin\NVIDIA-ccminertpruvot231x64\ccminer-x64.exe"
 $Uri = "https://github.com/tpruvot/ccminer/releases/download/2.3.1-tpruvot/ccminer-2.3.1-cuda10.7z"
 $Commands = [PSCustomObject]@{ 
-    #"blake2b" = " -a blake2b" #blake2b
-    "x12"       = " -a x12 -i 20" #X12
-    "scryptn11" = " -a scrypt:10" #scryptn11
+    #"blake2b" = " --cpu-priority 4 -a blake2b" #blake2b
+    "x12"       = " --cpu-priority 4 -a x12 -i 20" #X12
+    "scryptn11" = " --cpu-priority 1 -a scrypt:10" #scryptn11
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
     [PSCustomObject]@{ 
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "--cpu-priority 4 -T 50000 -R 1 -b $($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
+        Arguments = "-T 50000 -R 1 -b $($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Day }
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort #4068
