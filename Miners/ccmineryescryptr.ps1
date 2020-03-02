2e515b5694a6cf548d41918f691e788f29b2b89f
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-03-03 08:55:49 +1300

    Update ccmineryescryptr.ps1
    faster benchmarks

--- Miners/ccmineryescryptr.ps1
+++ Miners/ccmineryescryptr.ps1
@@ -3,12 +3,12 @@ $Path = ".\Bin\NVIDIA-CcmineryescryptrV5\ccminer.exe"
 $Uri = "https://github.com/Minerx117/ccminer/releases/download/8.21-r18-v5/ccmineryescryptrV5.zip"
 $Commands = [PSCustomObject]@{ 
     "yescryptr8"  = " -a yescryptr8 -d $($Config.SelGPUCC)" #YescryptR8
-    "yescryptr16" = " -i 13.2 -a yescryptr16 -d $($Config.SelGPUCC)" #YescryptR16
+    #"yescryptr16" = " -i 13.2 -a yescryptr16 -d $($Config.SelGPUCC)" #YescryptR16
     "yescryptr32" = " -i 12.23 -a yescryptr32 -d $($Config.SelGPUCC)" #YescryptR32 recommend -i 12.49
     "yescryptr8g" = " -a yescrypt -d $($Config.SelGPUCC)" #YescryptR8g
     #"lyra2z330"   = " -t 1 -a lyra2z330" #Lyra2z330
-    "lyra2v3"     = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2v3
-    "lyra2rev3"   = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2rev3
+    #"lyra2v3"     = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2v3
+    #"lyra2rev3"   = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2rev3
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
