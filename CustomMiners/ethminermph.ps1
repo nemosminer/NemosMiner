b20ef790f1e5fa5495bdd49c08bd0db15f94faab
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-03-02 17:38:20 +1300

    Create ethminermph.ps1
    ethash Mining Pool Hub Support (Nvidia)

---
+++ CustomMiners/ethminermph.ps1
@@ -0,0 +1,20 @@
+If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+$Path = ".\Bin\NVIDIA-ethminer19a1fix\ethminer.exe"
+$Uri = "https://github.com/Minerx117/ethminer/releases/download/v0.19.0-alpha.1/ethminer0.19.0alpha1-ApiFix.zip"
+$Commands = [PSCustomObject]@{ 
+    "ethash" = "" #ethash
+}
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object {
+    If ($Algo -eq "ethash" -and $Pools.$Algo.Host -like "*zergpool*" -or $Pools.$Algo.Host -like "*nicehash*") { return }
+    [PSCustomObject]@{ 
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "--cuda-devices $($Config.SelGPUDSTM) --api-port -$($Variables.NVIDIAMinerAPITCPPort) -U -P stratum+tcp://$($Pools.$Algo.User):$($Pools.$Algo.Pass)@$($Pools.$Algo.Host):$($Pools.$Algo.Port)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week }
+        API       = "ethminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort #4068
+        Wrap      = $false
+        URI       = $Uri
+    }
+}
