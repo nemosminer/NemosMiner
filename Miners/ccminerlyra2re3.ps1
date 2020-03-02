5118336e32540f45d0272335b3e40f9a72117d5a
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-03-03 08:56:48 +1300

    Update ccminerlyra2re3.ps1
    never profitable/ faster benchmarks

--- Miners/ccminerlyra2re3.ps1
+++ Miners/ccminerlyra2re3.ps1
@@ -2,7 +2,7 @@ If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; Regis
 $Path = ".\Bin\NVIDIA-CcminerLyra2RE3\ccminer.exe"
 $Uri = "https://github.com/Minerx117/ccminer/releases/download/8.21-r18-v5/ccmineryescryptrV5.zip"
 $Commands = [PSCustomObject]@{ 
-    "lyra2re3" = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2RE3 (mining pool hub)
+    #"lyra2re3" = " -i 24 -a lyra2v3 -d $($Config.SelGPUCC)" #Lyra2RE3 (mining pool hub)
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
