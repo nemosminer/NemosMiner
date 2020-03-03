cd37bc5dc551fb358b2d4730a3cabfdd01d42763
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-03-03 22:41:14 +1300

    Update ethminer.ps1

--- CustomMiners/ethminer.ps1
+++ CustomMiners/ethminer.ps1
@@ -1,6 +1,6 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 $Path = ".\Bin\NVIDIA-ethminer19a1fix\ethminer.exe"
-$Uri = "https://github.com/Minerx117/ethminer/releases/download/v0.19.0-alpha.1/ethminer-0.19.0-alpha.1-cuda10.0-windows-amd64.zip.zip"
+$Uri = "https://github.com/Minerx117/ethminer/releases/download/v0.19.0-alpha.1/ethminer-0.19.0-alpha.1-cuda10.0-windows-amd64.zip"
 $Commands = [PSCustomObject]@{ 
     "ethash" = "" #ethash
 }
