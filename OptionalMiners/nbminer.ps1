2f98ded78612ecfad7c979ae45ea799ca34b476a
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-03-16 09:07:05 +1300

    Update nbminer.ps1

--- OptionalMiners/nbminer.ps1
+++ OptionalMiners/nbminer.ps1
@@ -2,11 +2,11 @@ If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; Regis
 $Path = ".\Bin\NVIDIA-nbminer277\nbminer.exe"
 $Uri = "https://github.com/NebuTech/NBMiner/releases/download/v27.7/NBMiner_27.7_Win.zip"
 $Commands = [PSCustomObject]@{ 
-    #"grincuckatoo31"   = "-a cuckatoo -o nicehash+tcp://" #grincuckatoo31 (8gb cards work win7,8, 8.1 & Linux. Win10 requires 10gb+vram)
-    #"grincuckarood29"  = "-a cuckarood -o nicehash+tcp://" #grincuckaroo29
-    #"grincuckaroo29"   = "-a cuckaroo -o nicehash+tcp://" #grincuckaroo29
-    #"cuckoocycle"      = "-a cuckoo_ae --cuckoo-intensity 0 -o nicehash+tcp://" #cuckoocycle
-     "eaglesong+ethash" = "-a eaglesong_ethash -di 24 -o stratum+tcp://" #eaglesong + ethash
+    #"grincuckatoo31"   = "-a cuckatoo --fee 1 -o nicehash+tcp://" #grincuckatoo31 (8gb cards work win7,8, 8.1 & Linux. Win10 requires 10gb+vram)
+    #"grincuckarood29"  = "-a cuckarood --fee 1 -o nicehash+tcp://" #grincuckaroo29
+    #"grincuckaroo29"   = "-a cuckaroo --fee 1 -o nicehash+tcp://" #grincuckaroo29
+    #"cuckoocycle"      = "-a cuckoo_ae --fee 1 --cuckoo-intensity 0 -o nicehash+tcp://" #cuckoocycle
+    "eaglesong+ethash" = "-a eaglesong_ethash --fee 1 -di 24 -o stratum+tcp://" #eaglesong + ethash
     #"ethash"           = "-a ethash -o stratum+tcp://" #ethash (ZergPool Yiimp Auto Exchange) 
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
@@ -14,8 +14,8 @@ $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm ($_ -split '\+' | Select-Object -Index 0); $Algo2 = Get-Algorithm ($_ -split '\+' | Select-Object -Index 1); $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
     switch ($_) { 
         "ethash" { $Fee = 0.0065 }
-        "eaglesong_ethash" { $Fee = 0.03 }
-        default { $Fee = 0.02 }
+        "eaglesong_ethash" { $Fee = 0.01 }
+        default { $Fee = 0.01 }
     }
 
     If ($Algo2) { 
