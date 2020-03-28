602b9a49eea116bd30a878f08f9eb1ee33fe0861
Useless Guru <useless.guru@gmail.com>
2020-03-28 09:11:07 +0100

    Update nbminer.ps1
    Ethash: use ethproxy for all non-nicehash pools (nicehash+tcp on zergpool does not produce any shares)

--- OptionalMiners/nbminer.ps1
+++ OptionalMiners/nbminer.ps1
@@ -1,35 +1,52 @@
-if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
-
-$Path = ".\Bin\NVIDIA-nbminer2321\nbminer.exe"
-$Uri = "https://nemosminer.com/data/optional/NBMiner_Win23.2.1.7z"
-
-$Commands = [PSCustomObject]@{
-    #"grincuckatoo31" = " -a cuckatoo -o stratum+tcp://" #grincuckatoo31 (8gb cards work win7,8, 8.1 & Linux. Win10 requires 10gb+vram)
-     "grincuckaroo29" = " -a cuckaroo -o stratum+tcp://" #grincuckaroo29
-     "cuckoocycle" = " -a cuckoo_ae -o stratum+tcp://" #grincuckaroo29  
-    #"ethash" = " -a ethash -o ethnh+tcp://" #ethash
+If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+$Path = ".\Bin\NVIDIA-nbminer277\nbminer.exe"
+$Uri = "https://github.com/NebuTech/NBMiner/releases/download/v27.7/NBMiner_27.7_Win.zip"
+$Commands = [PSCustomObject]@{ 
+    #"grincuckatoo31"   = "-a cuckatoo --fee 1 -o nicehash+tcp://" #grincuckatoo31 (8gb cards work win7,8, 8.1 & Linux. Win10 requires 10gb+vram)
+    #"grincuckarood29"  = "-a cuckarood --fee 1 -o nicehash+tcp://" #grincuckaroo29
+    #"grincuckaroo29"   = "-a cuckaroo --fee 1 -o nicehash+tcp://" #grincuckaroo29
+    #"cuckoocycle"      = "-a cuckoo_ae --fee 1 --cuckoo-intensity 0 -o nicehash+tcp://" #cuckoocycle
+    "eaglesong+ethash" = "-a eaglesong_ethash --fee 1 -di 24 -o stratum+tcp://" #eaglesong + ethash
+    #"ethash"           = "-a ethash -o stratum+tcp://" #ethash (ZergPool Yiimp Auto Exchange) 
 }
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 
-$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
-
-$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
-    $Algo = Get-Algorithm($_)
-     switch ($_) {
-        "ethash" {$Fee = 0.0065}
-        default {$Fee = 0.02}
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm ($_ -split '\+' | Select-Object -Index 0); $Algo2 = Get-Algorithm ($_ -split '\+' | Select-Object -Index 1); $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
+    switch ($_) { 
+        "ethash" { $Fee = 0.0065 }
+        "eaglesong_ethash" { $Fee = 0.01 }
+        default { $Fee = 0.01 }
     }
-    If ($Pools.($Algo).Host -notlike "*nicehash*") {return}
-    [PSCustomObject]@{
+
+    If ($Algo2) { 
+        $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)-$Algo"
+        $HashRates = [PSCustomObject]@{ $Algo2 = $Stats."$($Name)_$($Algo2)_HashRate".Week * (1 - $Fee); $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week * (1 - $Fee) }
+        # Ethash: use ethproxy for all non-nicehash pools (nicehash+tcp on zergpool does not produce any shares)
+        If ($Pools.$($Algo2).Name -eq "NiceHash") { 
+            $Algo2Parameter = " -do nicehash+tcp://$($Pools.$($Algo2).Host):$($Pools.$($Algo2).Port) -du $($Pools.$($Algo2).User)"
+        }
+        Else { 
+            $Algo2Parameter = " -do ethproxy+tcp://$($Pools.$($Algo2).Host):$($Pools.$($Algo2).Port) -du $($Pools.$($Algo2).User)"
+        }
+        If ($Pools.$Algo2.SSL) { $Algo2Parameter = $Algo2Parameter -replace '\+tcp\://$', '+ssl://' }
+    }
+    Else { 
+        $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+        $HashRates = [PSCustomObject]@{ $Algo = ($Stats."$($Name)_$($Algo)_HashRate".Week) * (1 - $Fee) }
+        $Algo2Parameter = ""
+    }
+
+    If ($Pools.$Algo.SSL) { $_ = $_ -replace '\+tcp\://$', '+ssl://' }
+
+    [PSCustomObject]@{ 
         Type      = "NVIDIA"
+        Name      = $Name
         Path      = $Path
-        Arguments = "$($Commands.$_)$($Pools.($Algo).Host):$($Pools.($Algo).Port) --cuckoo-intensity 0 --api 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -u $($Pools.($Algo).User):$($Pools.($Algo).Pass)"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * (1 - $Fee)} # substract devfee
-        API       = "NBMiner"
-        Port      = $Variables.NVIDIAMinerAPITCPPort #4068
+        Arguments = "$($Commands.$_)$($Pools.$Algo.Host):$($Pools.$Algo.Port) -no-nvml --api 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -u $($Pools.$Algo.User):$($Pools.$Algo.Pass)$Algo2Parameter"
+        HashRates = $HashRates
+        API       = "nbminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort
         Wrap      = $false
         URI       = $Uri
-        User      = $Pools.($Algo).User
-        Host      = $Pools.($Algo).Host
-        Coin      = $Pools.($Algo).Coin
     }
 }
