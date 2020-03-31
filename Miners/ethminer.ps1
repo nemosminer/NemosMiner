5d6f3cb4b483bc8a93d6476b266f7b4e9224beb6
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-03-31 13:30:46 +1300

    Update ethminer.ps1

--- Miners/ethminer.ps1
+++ Miners/ethminer.ps1
@@ -6,15 +6,23 @@ $Commands = [PSCustomObject]@{
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object {
-    If ($Algo -eq "ethash" -and $Pools.$Algo.Host -like "*miningpoolhub*") { return }
-    [PSCustomObject]@{ 
-        Type      = "NVIDIA"
-        Path      = $Path
-        Arguments = "--farm-recheck 5000 --farm-retries 40 --work-timeout 920 --response-timeout 360 --cuda-devices $($Config.SelGPUDSTM) --api-port -$($Variables.NVIDIAMinerAPITCPPort) -U -P stratum2+tcp://$($Pools.$Algo.User):$($Pools.$Algo.Pass)@$($Pools.$Algo.Host):$($Pools.$Algo.Port)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week }
-        API       = "ethminer"
-        Port      = $Variables.NVIDIAMinerAPITCPPort #4068
-        Wrap      = $false
-        URI       = $Uri
+    If ($Algo) { 
+        $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)-$Algo"
+        If ($Pools.$($Algo).Name -eq "MPH") { 
+            $AlgoParameter = " -P stratum+tcp://$($Pools.$Algo.User):$($Pools.$Algo.Pass)@$($Pools.$Algo.Host):$($Pools.$Algo.Port)"
+        }
+        Else { 
+            $AlgoParameter = " -P stratum2+tcp://$($Pools.$Algo.User):$($Pools.$Algo.Pass)@$($Pools.$Algo.Host):$($Pools.$Algo.Port)"
+        }
+        [PSCustomObject]@{ 
+            Type      = "NVIDIA"
+            Path      = $Path
+            Arguments = "--farm-recheck 5000 --farm-retries 40 --work-timeout 920 --response-timeout 360 --cuda-devices $($Config.SelGPUDSTM) --api-port -$($Variables.NVIDIAMinerAPITCPPort) -U $AlgoParameter$($Commands.$_)"
+            HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week }
+            API       = "ethminer"
+            Port      = $Variables.NVIDIAMinerAPITCPPort #4068
+            Wrap      = $false
+            URI       = $Uri
+        }
     }
 }
