57634f026d1b3b998e67d3a6f55f6c4a9c258368
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-04-23 08:33:37 +1200

    Update ccminermtp.ps1

--- Miners/ccminermtp.ps1
+++ Miners/ccminermtp.ps1
@@ -1,9 +1,8 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\NVIDIA-ccminermtp129\ccminer.exe"
-$Uri = "https://github.com/tecracoin/ccminer/releases/download/1.2.9/ccminer.exe"
+$Path = ".\Bin\NVIDIA-ccminermtp132\ccminer.exe"
+$Uri = "https://github.com/zcoinofficial/ccminer/releases/download/1.3.2/ccminer.exe"
 $Commands = [PSCustomObject]@{ 
-     #"mtp" = " -d $($Config.SelGPUCC) -a mtp-classic -i 21" # memeroy allocation error when running 7 or more GPU's work well with upto 6 GPU's 
-     #"mtptrc" = " -d $($Config.SelGPUCC) -a mtp-tcr -i 21" #mtptrc -i 19 -i 20 -i 21 -i 22
+     #"mtp" = " -d $($Config.SelGPUCC) -a mtp -i 21" # memeroy allocation error when running 7 or more GPU's work well with upto 6 GPU's 
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
@@ -11,7 +10,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{ 
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "--cpu-affinity 1 --cpu-priority 4 -R 1 -b $($Variables.NVIDIAMinerAPITCPPort) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) --no-donation -p $($Pools.$Algo.Pass)$($Commands.$_)"
+        Arguments = "--cpu-priority 4 -R 1 -b $($Variables.NVIDIAMinerAPITCPPort) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) --no-donation -p $($Pools.$Algo.Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week }
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort #4068
