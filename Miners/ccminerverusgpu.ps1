b75a332eae9ddd3079f33a85792af1bd65b2093b
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-04-22 20:05:59 +1200

    Update ccminerverusgpu.ps1
    verus zpool.ca

--- Miners/ccminerverusgpu.ps1
+++ Miners/ccminerverusgpu.ps1
@@ -3,6 +3,7 @@ $Path = ".\Bin\NVIDIA-Ccminerverus375\ccminer.exe"
 $Uri = "https://github.com/Minerx117/miner-binaries/releases/download/3.7.5/ccminerverus375.7z"
 $Commands = [PSCustomObject]@{ 
     "verushash" = " -a verus -i 21 -d $($Config.SelGPUCC)" #verus
+    "verus" = " -a verus -i 21 -d $($Config.SelGPUCC)" #verus
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
