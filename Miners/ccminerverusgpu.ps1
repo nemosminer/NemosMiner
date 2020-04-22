1cec7f6301c0eadabbad85d1b36be5ea53aeb555
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-04-22 18:01:44 +1200

    Update ccminerverusgpu.ps1
    faster verushash gpu

--- Miners/ccminerverusgpu.ps1
+++ Miners/ccminerverusgpu.ps1
@@ -1,8 +1,8 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\NVIDIA-Ccminerverus3611\ccminer.exe"
-$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/3.611revD/ccminerverus-3.611cuda-revD-win.7z"
+$Path = ".\Bin\NVIDIA-Ccminerverus375\ccminer.exe"
+$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/3.7.5/ccminerverus375.7z"
 $Commands = [PSCustomObject]@{ 
-    "verushash" = " -a verus -i 20 -d $($Config.SelGPUCC)" #verus
+    "verushash" = " -a verus -i 21 -d $($Config.SelGPUCC)" #verus
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
