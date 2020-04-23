026a823b8946baa8a547a16b4a479a2a477c3afd
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-04-24 09:45:06 +1200

    Update xmrigcuda.ps1

--- Miners/xmrigcuda.ps1
+++ Miners/xmrigcuda.ps1
@@ -1,6 +1,6 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\NVIDIA-XMRig5111\xmrig.exe"
-$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/5.11.1/XMRig5111.7z"
+$Path = ".\Bin\NVIDIA-XMRigv5111\xmrig.exe"
+$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v5.11.1/XMRig5111.zip"
 $Commands = [PSCustomObject]@{ 
     #"randomxmonero" = " -a rx/0 --nicehash" #RandomX
     #"randomarq"     = " -a rx/arq --nicehash" #Randomarq
