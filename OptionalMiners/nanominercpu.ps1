62b6d120aa4637e5d3fde5ab127c403da781aa6b
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-05-08 08:46:10 +1200

    Update nanominercpu.ps1

--- OptionalMiners/nanominercpu.ps1
+++ OptionalMiners/nanominercpu.ps1
@@ -1,6 +1,6 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\CPU-nanominer191\nanominer.exe"
-$Uri = "https://github.com/nanopool/nanominer/releases/download/v1.9.1/nanominer-windows-1.9.1.zip"
+$Path = ".\Bin\CPU-nanominer192\nanominer.exe"
+$Uri = "https://github.com/nanopool/nanominer/releases/download/v1.9.2/nanominer-windows-1.9.2.zip"
 $Commands = [PSCustomObject]@{ 
     #"Ethash" = "" #GPU Only
     #"Ubqhash" = "" #GPU Only
