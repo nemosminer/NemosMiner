eaff3175264446899c10e94c8d4946d9e75c120d
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-05-10 19:17:09 +1200

    Create kawpowminer.ps1
    ttminer faster

---
+++ Miners/kawpowminer.ps1
@@ -0,0 +1,27 @@
+If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+$Path = ".\Bin\NVIDIA-kawpowminer122\kawpowminer.exe"
+$Uri = "https://github.com/RavenCommunity/kawpowminer/releases/download/1.2.2/kawpowminer-windows-1.2.2.zip"
+$Commands = [PSCustomObject]@{ 
+    "kawpow" = "" #kawpow
+}
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object {
+    If ($Algo) { 
+        If ($Pools.$($Algo).Name -eq "nicehashV2") { 
+            $AlgoParameter = " -P stratum2+tcp://$($Pools.$Algo.User):$($Pools.$Algo.Pass)@$($Pools.$Algo.Host):$($Pools.$Algo.Port)"
+        }
+        Else { 
+            $AlgoParameter = " -P stratum+tcp://$($Pools.$Algo.User):$($Pools.$Algo.Pass)@$($Pools.$Algo.Host):$($Pools.$Algo.Port)"
+        }
+        [PSCustomObject]@{ 
+            Type      = "NVIDIA"
+            Path      = $Path
+            Arguments = "--farm-recheck 10000 --farm-retries 40 --work-timeout 100000 --response-timeout 720 --cuda-devices $($Config.SelGPUDSTM) --api-port -$($Variables.NVIDIAMinerAPITCPPort) -U $AlgoParameter$($Commands.$_)"
+            HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week }
+            API       = "ethminer"
+            Port      = $Variables.NVIDIAMinerAPITCPPort #4068
+            Wrap      = $false
+            URI       = $Uri
+        }
+    }
+}
