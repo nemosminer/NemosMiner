fb1be0c19f56839efabd7ce8e3be54a67ad8f972
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-05-24 17:07:22 +1200

    Update trex.ps1

--- OptionalMiners/trex.ps1
+++ OptionalMiners/trex.ps1
@@ -1,6 +1,6 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\NVIDIA-trex0156\t-rex.exe"
-$Uri = "https://github.com/trexminer/T-Rex/releases/download/0.15.6/t-rex-0.15.6-win-cuda10.0.zip"
+$Path = ".\Bin\NVIDIA-trex0157\t-rex.exe"
+$Uri = "https://github.com/trexminer/T-Rex/releases/download/0.15.7/t-rex-0.15.7-win-cuda10.0.zip"
 $Commands = [PSCustomObject]@{ 
     "balloon"    = " -a balloon -i 23" #Balloon
    #"astralhash" = " -a astralhash -i 23" #Astralhash
