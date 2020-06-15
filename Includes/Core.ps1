a666925b107ebbda6cbcacffbf6365e716187095
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-06-16 10:30:18 +1200

    Update Core.ps1
    wait 30minutes for bin folder to download..

--- Includes/Core.ps1
+++ Includes/Core.ps1
@@ -472,9 +472,9 @@ Function Start-Cycle {
         }
 
         If (-not ($Variables.Miners | Where-Object Enabled -EQ $true)) { 
-            Write-Message -Level Warn "No miners available$(If ($Variables.Downloader.State -eq "Running") { " waiting 10 minutes for downloader to install binaries.." })."
+            Write-Message -Level Warn "No miners available$(If ($Variables.Downloader.State -eq "Running") { " waiting 30 minutes for downloader to install binaries.." })."
             $Variables.EndLoop = $true
-            Start-Sleep -Seconds 600
+            Start-Sleep -Seconds 1800
             Continue
         }
 
