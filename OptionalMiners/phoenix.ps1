faacb04e93d71d06c0fa9f00fb30bab909a655c5
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-06-24 11:07:17 +1200

    enable ethash on AMD GPU's

--- OptionalMiners/phoenix.ps1
+++ OptionalMiners/phoenix.ps1
@@ -1,20 +1,19 @@
 using module ..\Includes\Include.psm1
 
-$Path = ".\Bin\NVIDIA-Phoenix50d\PhoenixMiner.exe"
-$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/5.0d/PhoenixMiner_5.0d_Windows.zip"
+$Path = ".\Bin\AMD-Phoenix50e\PhoenixMiner.exe"
+$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/5.0e/PhoenixMiner_5.0e_Windows.7z"
 $Commands = [PSCustomObject]@{ 
-   #"ethash"  = " -di $($($Config.SelGPUCC).Replace(',',''))" #Ethash
-   #"progpow" = " -coin bci -di $($($Config.SelGPUCC).Replace(',',''))" #Progpow 
+    "ethash"  = "" #Ethash
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
     [PSCustomObject]@{ 
-        Type      = "NVIDIA"
+        Type      = "AMD"
         Path      = $Path
-        Arguments = "-gbase 0 -nvdo 1 -esm 3 -allpools 1 -allcoins 1 -platform 2 -mport -$($Variables.NVIDIAMinerAPITCPPort) -epool $($Pools.$Algo.Host):$($Pools.$Algo.Port) -ewal $($Pools.$Algo.User) -epsw $($Pools.$Algo.Pass)$($Commands.$_)"
+        Arguments = "-proto 4 -amd -mport -$($Variables.AMDMinerAPITCPPort) -epool $($Pools.$Algo.Host):$($Pools.$Algo.Port) -ewal $($Pools.$Algo.User) -epsw $($Pools.$Algo.Pass)$($Commands.$_)"
         Algorithm = $Algo
         API       = "ethminer"
-        Port      = $Variables.NVIDIAMinerAPITCPPort
+        Port      = $Variables.AMDMinerAPITCPPort
         Wrap      = $false
         URI       = $Uri
         Fee       = 0.0065 # Dev fee
