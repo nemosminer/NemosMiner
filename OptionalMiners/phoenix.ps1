9e9db363467670ef2c7eb4a24700cc7bf7392b65
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-06-24 11:05:20 +1200

    enable ethash on AMD GPU's

--- OptionalMiners/phoenix.ps1
+++ OptionalMiners/phoenix.ps1
@@ -1,19 +1,18 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\NVIDIA-Phoenix50d\PhoenixMiner.exe"
-$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/5.0d/PhoenixMiner_5.0d_Windows.zip"
+$Path = ".\Bin\AMD-Phoenix50e\PhoenixMiner.exe"
+$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/5.0e/PhoenixMiner_5.0e_Windows.7z"
 $Commands = [PSCustomObject]@{ 
-   #"ethash"  = " -di $($($Config.SelGPUCC).Replace(',',''))" #Ethash
-   #"progpow" = " -coin bci -di $($($Config.SelGPUCC).Replace(',',''))" #Progpow 
+    "ethash"  = "" #Ethash
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
     [PSCustomObject]@{ 
-        Type      = "NVIDIA"
+        Type      = "AMD"
         Path      = $Path
-        Arguments = "-gbase 0 -nvdo 1 -esm 3 -allpools 1 -allcoins 1 -platform 2 -mport -$($Variables.NVIDIAMinerAPITCPPort) -epool $($Pools.$Algo.Host):$($Pools.$Algo.Port) -ewal $($Pools.$Algo.User) -epsw $($Pools.$Algo.Pass)$($Commands.$_)"
+        Arguments = "-proto 4 -amd -mport -$($Variables.AMDMinerAPITCPPort) -epool $($Pools.$Algo.Host):$($Pools.$Algo.Port) -ewal $($Pools.$Algo.User) -epsw $($Pools.$Algo.Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week * .9935 } # substract 0.65% devfee
         API       = "ethminer"
-        Port      = $Variables.NVIDIAMinerAPITCPPort #3333
+        Port      = $Variables.AMDMinerAPITCPPort #3333
         Wrap      = $false
         URI       = $Uri
     }
