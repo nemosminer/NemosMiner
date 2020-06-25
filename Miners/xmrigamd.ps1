84398d3f5caaaa3f03c2882a53c7da082668c46a
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-06-25 20:59:51 +1200

    Create xmrigamd.ps1

---
+++ Miners/xmrigamd.ps1
@@ -0,0 +1,21 @@
+If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+$Path = ".\Bin\AMD-XMRigv622\xmrig.exe"
+$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v6.2.2/XMRigv622.7z"
+$Commands = [PSCustomObject]@{ 
+    "kawpow"              = " -a kawpow --nicehash" #kawpow
+    "cryptonight_conceal" = " -a cn/ccx --nicehash" #cryptonight_conceal
+}
+$Port = $Variables.AMDMinerAPITCPPort
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
+   [PSCustomObject]@{ 
+        Type      = "AMD"
+        Path      = $Path
+        Arguments = "-R 1 --no-cpu --opencl -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_) --keepalive --http-port=$($Variables.AMDMinerAPITCPPort) --donate-level 0"
+        HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week } #Recompiled 0% fee
+        API       = "XMRig"
+        Port      = $Variables.AMDMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri    
+    }
+}
