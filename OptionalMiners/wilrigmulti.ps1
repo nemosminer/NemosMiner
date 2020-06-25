786d7b9c0de2502863af1522dce701842dfba3c5
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-06-25 21:00:34 +1200

    Create wilrigmulti.ps1

---
+++ OptionalMiners/wilrigmulti.ps1
@@ -0,0 +1,27 @@
+if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1")}
+
+$Path = ".\Bin\AMD-WildRigMulti0251\wildrig.exe"
+$Uri = "https://github.com/andru-kun/wildrig-multi/releases/download/0.25.1/wildrig-multi-windows-0.25.1.7z"
+
+$Commands = [PSCustomObject]@{
+    "mtp"           = " --algo mtp" #Mtp
+    "kawpow"        = " --algo kawpow" #KawPow
+}
+
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object {
+    [PSCustomObject]@{
+        Type = "AMD"
+        Path = $Path
+        Arguments = "--api-port=$($Variables.AMDMinerAPITCPPort) --url=$($Pools.$Algo.Host):$($Pools.$Algo.Port) --opencl-platforms amd --opencl-threads auto --opencl-launch auto --user=$($Pools.$Algo.User) --pass=$($Pools.$Algo.Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * .99} # substract 1% devfee
+        API = "Xmrig"
+        Port = $Variables.AMDMinerAPITCPPort
+        Wrap = $false
+        URI = $Uri
+        User = $Pools.$Algo.User
+        Host = $Pools.$Algo.Host
+        Coin = $Pools.$Algo.Coin
+    }
+}
