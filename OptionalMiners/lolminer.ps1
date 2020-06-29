ab1969923d988a8d6d1ff8ee234f8378ae2771c1
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-06-29 13:27:30 +1200

    Update lolminer.ps1

--- OptionalMiners/lolminer.ps1
+++ OptionalMiners/lolminer.ps1
@@ -1,23 +1,25 @@
-using module ..\Includes\Include.psm1
-
-$Path = ".\Bin\NVIDIA-lolminer101\lolMiner.exe"
+If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+$Path = ".\Bin\AMD-lolminer101\lolMiner.exe"
 $Uri = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.01/lolMiner_v1.01_Win64.zip"
 $Commands = [PSCustomObject]@{ 
-    #"grincuckatoo31" = " --coin GRIN-AT31" #grincuckatoo31
-    #"grincuckatoo32" = " --coin GRIN-AT32" #grincuckatoo32
-    #"beamv3"         = " --coin BEAM-III"  #beamv3
+    "grincuckatoo31"  = " -a C31" #grincuckatoo31
+    "grincuckatoo32"  = " -a C32" #grincuckatoo32
+    "equihash144"     = " -a EQUI144_5 -c AUTO144_5"  #equihash144
+    "equihash125"     = " --coin ZEL"  #equihash125
+    "equihash192"     = " -a EQUI192_7 --coin AUTO192_7"  #equihash192
+    "grincuckarood29" = " -a C29D"  #grincuckarood29
+    "cuckaroom"       = " -a C29M"  #cuckaroom
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
     [PSCustomObject]@{ 
-        Type      = "NVIDIA"
+        Type      = "AMD"
         Path      = $Path
-        Arguments = "--tls 0 --digits 2 --longstats 60 --shortstats 5 --connectattempts 3 --devices $($Config.SelGPUCC) --apiport $($Variables.NVIDIAMinerAPITCPPort) --pool $($Pools.$Algo.Host) --port $($Pools.$Algo.Port) --user $($Pools.$Algo.User) --pass $($Pools.$Algo.Pass)$($Commands.$_)" #
-        Algorithm = $Algo
+        Arguments = "--devices AMD --longstats 120 --shortstats 10 --apiport $($Variables.AMDMinerAPITCPPort) -p $($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) --pass $($Pools.$Algo.Pass)$($Commands.$_)" #
+        HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week * .99 } # substract 2% devfee
         API       = "lol"
-        Port      = $Variables.NVIDIAMinerAPITCPPort
+        Port      = $Variables.AMDMinerAPITCPPort
         Wrap      = $false
         URI       = $Uri
-        Fee       = 0.02 #Dev fee
     }
 }
