a609eba9149022e2c6adbf2725c155a0ee3d1b50
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-06-29 16:06:36 +1200

    Update claymore.ps1
    ethash AMD GPU's

--- OptionalMiners/claymore.ps1
+++ OptionalMiners/claymore.ps1
@@ -1,19 +1,19 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\Ethash-Claymore150\EthDcrMiner64.exe"
+$Path = ".\Bin\AMD-Claymore150\EthDcrMiner64.exe"
 $Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v15.0/Claymoresethereumv15.0.7z"
 $Commands = [PSCustomObject]@{ 
-    #"ethash" = " -di $($($Config.SelGPUCC).Replace(',',''))" #Ethash -strap 1 -strap 2 -strap 3 -strap 4 -strap 5 -strap 6
+    "ethash" = "" #Ethash -strap 1 -strap 2 -strap 3 -strap 4 -strap 5 -strap 6
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
     If ($Algo -eq "ethash" -and $Pools.$Algo.Host -like "*zergpool*") { return }
     [PSCustomObject]@{ 
-        Type      = "NVIDIA"
+        Type      = "AMD"
         Path      = $Path
-        Arguments = "-wd 0 -esm 3 -allpools 1 -allcoins 1 -platform 2 -mode 1 -mport -$($Variables.NVIDIAMinerAPITCPPort) -epool $($Pools.$Algo.Host):$($Pools.$Algo.Port) -ewal $($Pools.$Algo.User) -epsw $($Pools.$Algo.Pass)$($Commands.$_)"
+        Arguments = "-wd 0 -esm 3 -allpools 1 -allcoins 1 -platform 1 -mode 1 -mport -$($Variables.AMDMinerAPITCPPort) -epool $($Pools.$Algo.Host):$($Pools.$Algo.Port) -ewal $($Pools.$Algo.User) -epsw $($Pools.$Algo.Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week * .99 } # substract 1% devfee
         API       = "ethminer"
-        Port      = $Variables.NVIDIAMinerAPITCPPort #3333
+        Port      = $Variables.AMDMinerAPITCPPort #3333
         Wrap      = $false
         URI       = $Uri
     }
