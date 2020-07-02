e544dd7152445610cb2a2cd5206b41158e207e45
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-07-03 09:32:22 +1200

    Update wilrigmulti.ps1

--- OptionalMiners/wilrigmulti.ps1
+++ OptionalMiners/wilrigmulti.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1")}
 
-$Path = ".\Bin\AMD-WildRigMulti0251\wildrig.exe"
-$Uri = "https://github.com/andru-kun/wildrig-multi/releases/download/0.25.1/wildrig-multi-windows-0.25.1.7z"
+$Path = ".\Bin\AMD-WildRigMulti0252b\wildrig.exe"
+$Uri = "https://github.com/andru-kun/wildrig-multi/releases/download/0.25.2/wildrig-multi-windows-0.25.2.7z"
 
 $Commands = [PSCustomObject]@{
     "mtp"           = " --algo mtp" #Mtp
