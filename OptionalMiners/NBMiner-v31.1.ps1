14a0635826c06247bff0d229bc8baaf055987c5b
Useless Guru <useless.guru@gmail.com>
2020-08-23 01:18:56 +0200

    Update NBMiner-v31.1.ps1
    Ethash does not work with MPH

--- OptionalMiners/NBMiner-v31.1.ps1
+++ OptionalMiners/NBMiner-v31.1.ps1
@@ -6,24 +6,24 @@ $Uri = "https://github.com/NebuTech/NBMiner/releases/download/v31.1/NBMiner_31.1
 $DeviceEnumerator = "Type_Slot"
 
 $Commands = [PSCustomObject[]]@(
-#   [PSCustomObject]@{ Algorithm = @("Ethash");       Fee = @(0.0065);     MinMemGB = 4; Type = "AMD"   ; Command = " --algo ethash" } #BMiner & PhoenixMiner are fastest
+#   [PSCustomObject]@{ Algorithm = @("Ethash");              Fee = @(0.0065);     MinMemGB = 4; Type = "AMD"   ; Command = " --algo ethash" } #BMiner & PhoenixMiner are fastest
     [PSCustomObject]@{ Algorithm = @("Eaglesong", "Ethash"); Fee = @(0.01, 0.01); MinMemGB = 4; Type = "AMD"   ; Command = " --algo eaglesong_ethash --fee 1" }
     [PSCustomObject]@{ Algorithm = @("Handshake", "Ethash"); Fee = @(0.01, 0.01); MinMemGB = 4; Type = "AMD"   ; Command = " --algo hns_ethash --fee 1" }
-    [PSCustomObject]@{ Algorithm = @("Handshake");    Fee = @(0.01);       MinMemGB = 4; Type = "AMD"   ; Command = " --algo hns --fee 1" }
-    [PSCustomObject]@{ Algorithm = @("KawPoW");       Fee = @(0.01);       MinMemGB = 4; Type = "AMD"   ; Command = " --algo kawpow --fee 1" }
+    [PSCustomObject]@{ Algorithm = @("Handshake");           Fee = @(0.01);       MinMemGB = 4; Type = "AMD"   ; Command = " --algo hns --fee 1" }
+    [PSCustomObject]@{ Algorithm = @("KawPoW");              Fee = @(0.01);       MinMemGB = 4; Type = "AMD"   ; Command = " --algo kawpow --fee 1" }
 
-    [PSCustomObject]@{ Algorithm = @("Cuckatoo31");   Fee = @(0.01);       MinMemGB = 8; Type = "NVIDIA"; Command = " --algo cuckatoo --fee 1" }
-    [PSCustomObject]@{ Algorithm = @("Cuckatoo32");   Fee = @(0.01);       MinMemGB = 8; Type = "NVIDIA"; Command = " --algo cuckatoo32 --fee 1" }
-    [PSCustomObject]@{ Algorithm = @("CuckarooD29");  Fee = @(0.01);       MinMemGB = 4; Type = "NVIDIA"; Command = " --algo cuckarood --fee 1" }
-    [PSCustomObject]@{ Algorithm = @("Cuckaroo29s");  Fee = @(0.01);       MinMemGB = 4; Type = "NVIDIA"; Command = " --algo cuckaroo_swap --fee 1" }
-    [PSCustomObject]@{ Algorithm = @("CuckooCycle");  Fee = @(0.01);       MinMemGB = 4; Type = "NVIDIA"; Command = " --algo cuckoo_ae --fee 1" }
-#   [PSCustomObject]@{ Algorithm = @("Ethash");       Fee = @(0.0065);     MinMemGB = 4; Type = "NVIDIA"; Command = " --algo ethash" } #BMiner & PhoenixMiner are fastest
+    [PSCustomObject]@{ Algorithm = @("Cuckatoo31");          Fee = @(0.01);       MinMemGB = 8; Type = "NVIDIA"; Command = " --algo cuckatoo --fee 1" }
+    [PSCustomObject]@{ Algorithm = @("Cuckatoo32");          Fee = @(0.01);       MinMemGB = 8; Type = "NVIDIA"; Command = " --algo cuckatoo32 --fee 1" }
+    [PSCustomObject]@{ Algorithm = @("CuckarooD29");         Fee = @(0.01);       MinMemGB = 4; Type = "NVIDIA"; Command = " --algo cuckarood --fee 1" }
+    [PSCustomObject]@{ Algorithm = @("Cuckaroo29s");         Fee = @(0.01);       MinMemGB = 4; Type = "NVIDIA"; Command = " --algo cuckaroo_swap --fee 1" }
+    [PSCustomObject]@{ Algorithm = @("CuckooCycle");         Fee = @(0.01);       MinMemGB = 4; Type = "NVIDIA"; Command = " --algo cuckoo_ae --fee 1" }
+#   [PSCustomObject]@{ Algorithm = @("Ethash");              Fee = @(0.0065);     MinMemGB = 4; Type = "NVIDIA"; Command = " --algo ethash" } #BMiner & PhoenixMiner are fastest
     [PSCustomObject]@{ Algorithm = @("Eaglesong", "Ethash"); Fee = @(0.01, 0.01); MinMemGB = 4; Type = "NVIDIA"; Command = " --algo eaglesong_ethash --fee 1" }
     [PSCustomObject]@{ Algorithm = @("Handshake", "Ethash"); Fee = @(0.01, 0.01); MinMemGB = 4; Type = "NVIDIA"; Command = " --algo hns_ethash --fee 1" }
     [PSCustomObject]@{ Algorithm = @("Tensority", "Ethash"); Fee = @(0.01, 0.01); MinMemGB = 4; Type = "NVIDIA"; Command = " --algo tensority_ethash --fee 1" }
-    [PSCustomObject]@{ Algorithm = @("Handshake");    Fee = @(0.01)      ; MinMemGB = 4; Type = "NVIDIA"; Command = " --algo hns --fee 1" }
-    [PSCustomObject]@{ Algorithm = @("KawPoW");       Fee = @(0.01)      ; MinMemGB = 4; Type = "NVIDIA"; Command = " --algo kawpow --fee 1" }
-    [PSCustomObject]@{ Algorithm = @("Tensority");    Fee = @(0.01)      ; MinMemGB = 4; Type = "NVIDIA"; Command = " --algo tensority --fee 1" }
+    [PSCustomObject]@{ Algorithm = @("Handshake");           Fee = @(0.01)      ; MinMemGB = 4; Type = "NVIDIA"; Command = " --algo hns --fee 1" }
+    [PSCustomObject]@{ Algorithm = @("KawPoW");              Fee = @(0.01)      ; MinMemGB = 4; Type = "NVIDIA"; Command = " --algo kawpow --fee 1" }
+    [PSCustomObject]@{ Algorithm = @("Tensority");           Fee = @(0.01)      ; MinMemGB = 4; Type = "NVIDIA"; Command = " --algo tensority --fee 1" }
 )
 
 If ($Commands = $Commands | Where-Object { $Pools.($_.Algorithm[1]).Host -and (-not $_.Algorithm[0] -or $Pools.($_.Algorithm[0]).Host) }) { 
@@ -52,7 +52,7 @@ If ($Commands = $Commands | Where-Object { $Pools.($_.Algorithm[1]).Host -and (-
         If ($SelectedDevices = @($Devices | Where-Object Type -EQ $_.Type | Where-Object Model -EQ $_.Model)) { 
             $MinerAPIPort = [UInt16]($Config.APIPort + ($SelectedDevices | Sort-Object Id | Select-Object -First 1 -ExpandProperty Id) + 1)
 
-            $Commands | Where-Object Type -EQ $_.Type | ForEach-Object { $Algo = ($_.Algorithm[1]); $_ } | ForEach-Object { 
+            $Commands | Where-Object Type -EQ $_.Type | ForEach-Object { 
                 $MinMemGB = $_.MinMemGB
                 
                 #Cuckatoo3? on windows 10 requires 3.5 GB extra
@@ -63,10 +63,21 @@ If ($Commands = $Commands | Where-Object { $Pools.($_.Algorithm[1]).Host -and (-
                     #Get commands for active miner devices
                     #$_.Command = Get-CommandPerDevice -Command $_.Command -ExcludeParameters @("algo") -DeviceIDs $Miner_Devices.$DeviceEnumerator
 
-                    $Protocol = If ($Pools.($_.Algorithm[0]).SSL) { "stratum+ssl://" } Else { "stratum+tcp://" }
+                    If ($_.Algorithm[0] -like "Ethash*" -and $Pools.($_.Algorithm[0]).Name -match "MPH*") { Return } #temp fix
+
+                    $Miner_Name = (@($Name) + @($Miner_Devices.Model | Sort-Object -Unique | ForEach-Object { $Model = $_; "$(@($Miner_Devices | Where-Object Model -eq $Model).Count)x$Model" }) + @($_.Algorithm[0]) + @($_.Intensity2) | Select-Object) -join '-'
+
+                    If ($_.Algorithm[0] -like "Ethash*") { 
+                        $Protocol = If ($Pools.($_.Algorithm[0]).Name -match "MPH*|NiceHash*") { "nicehash+tcp://" } Else { "ethproxy+tcp://" } 
+                    }
+                    Else { $Protocol = "stratum+tcp://" }
+                    If ($Pools.($_.Algorithm[0]).SSL) { $Protocol = $Protocol -replace '\+tcp\://$', '+ssl://' }
+
                     $_.Command += " --url $($Protocol)$($Pools.($_.Algorithm[0]).Host):$($Pools.($_.Algorithm[0]).Port) --user $($Pools.($_.Algorithm[0]).User):$($Pools.($_.Algorithm[0]).Pass)"
 
                     If ($_.Algorithm[1]) { 
+                        If ($_.Algorithm[1] -like "Ethash*" -and $Pools.($_.Algorithm[1]).Name -match "MPH*") { Return } #temp fix
+
                         $Miner_Name = (@($Name) + @($Miner_Devices.Model | Sort-Object -Unique | ForEach-Object { $Model = $_; "$(@($Miner_Devices | Where-Object Model -eq $Model).Count)x$Model" }) + @($_.Algorithm[0]) + @($_.Intensity2) | Select-Object) -join '-'
 
                         If ($_.Algorithm[1] -like "Ethash*") { 
@@ -74,6 +85,7 @@ If ($Commands = $Commands | Where-Object { $Pools.($_.Algorithm[1]).Host -and (-
                         }
                         Else { $Protocol2 = "stratum+tcp://" }
                         If ($Pools.($_.Algorithm[1]).SSL) { $Protocol2 = $Protocol2 -replace '\+tcp\://$', '+ssl://' }
+
                         $_.Command += " --secondary-url $($Protocol2)$($Pools.$($_.Algorithm[1]).Host):$($Pools.$($_.Algorithm[1]).Port) --secondary-user $($Pools.$($_.Algorithm[1]).User):$($Pools.($_.Algorithm[0]).Pass)$(If($_.Intensity2 -ge 0) { " --secondary-intensity $($_.Intensity2)" })"
                     }
                     Else { 
