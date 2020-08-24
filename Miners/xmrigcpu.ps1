1f6b530f3d3b0973c05dfd8168b327d3ef439f71
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-08-24 12:07:05 +1200

    Update xmrigcpu.ps1
    visual studio 2019
    cuda 10.1

--- Miners/xmrigcpu.ps1
+++ Miners/xmrigcpu.ps1
@@ -1,6 +1,6 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\CPU-XMRigv631\xmrig.exe"
-$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v6.3.1/XMRigv631.7z"
+$Path = ".\Bin\CPU-XMRigv632\xmrig.exe"
+$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v6.3.2/XMRigv632.7z"
 $Commands = [PSCustomObject]@{ 
     "randomxmonero"       = " -a rx/0 --nicehash" #RandomX
     "randomx"             = " -a rx/0 --nicehash" #RandomX 
