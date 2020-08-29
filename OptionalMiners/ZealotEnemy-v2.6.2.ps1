c4ee2a182b7ed99ecdfb6b742873b7ff1cfee087
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-08-30 09:06:41 +1200

    Update ZealotEnemy-v2.6.2.ps1

--- OptionalMiners/ZealotEnemy-v2.6.2.ps1
+++ OptionalMiners/ZealotEnemy-v2.6.2.ps1
@@ -6,10 +6,10 @@ $Uri = "https://github.com/zealot-rvn/z-enemy/releases/download/kawpow262/z-enem
 $DeviceEnumerator = "Type_Vendor_Index"
 
 $Commands = [PSCustomObject[]]@(
-    [PSCustomObject]@{ Algorithm = "Aergo";  MinMemGB = 1; Command = " -a aergo --intensity 23" }
-    [PSCustomObject]@{ Algorithm = "Xevan";  MinMemGB = 2; Command = " -a xevan --intensity 22" }
-    [PSCustomObject]@{ Algorithm = "Hex";    MinMemGB = 1; Command = " -a --intensity 24" }
-    [PSCustomObject]@{ Algorithm = "KawPoW"; MinMemGB = 3; Command = " -a kawpow --intensity 23" } 
+    [PSCustomObject]@{ Algorithm = "Aergo";  MinMemGB = 1; Command = " -a aergo -i 23" }
+    [PSCustomObject]@{ Algorithm = "Xevan";  MinMemGB = 2; Command = " -a xevan -i 22" }
+    [PSCustomObject]@{ Algorithm = "Hex";    MinMemGB = 1; Command = " -a hex -i 24" }
+    [PSCustomObject]@{ Algorithm = "KawPoW"; MinMemGB = 3; Command = " -a kawpow -i 23" } 
 )
 
 If ($Commands = $Commands | Where-Object { $Pools.($_.Algorithm).Host }) { 
