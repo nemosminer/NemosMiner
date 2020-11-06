9e0c110f8d3152e498e06f0865358c579a2afce9
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-11-07 02:32:57 +1300

    Update xmrigcuda.ps1

--- Miners/xmrigcuda.ps1
+++ Miners/xmrigcuda.ps1
@@ -1,6 +1,6 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\Nvidia-XMRigv635\xmrig.exe"
-$Uri = "https://github.com/Minerx117/miners/releases/download/xmrig/XMRigv635.7z"
+$Path = ".\Bin\Nvidia-XMRigv650\xmrig.exe"
+$Uri = "https://github.com/Minerx117/miners/releases/download/xmrig/XMRigv650.7z"
 $Commands = [PSCustomObject]@{ 
      "kawpow" = " -a kawpow --nicehash" #kawpow
 }
