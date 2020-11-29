311a6cd688080ed7ad1d618c9904b641222c674f
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-11-29 19:04:51 +1300

    Update nanomineramd.ps1

--- OptionalMiners/nanomineramd.ps1
+++ OptionalMiners/nanomineramd.ps1
@@ -1,5 +1,5 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\CPU-nanominer312\nanominer.exe"
+$Path = ".\Bin\AMD-nanominer312\nanominer.exe"
 $Uri = "https://github.com/nanopool/nanominer/releases/download/v3.1.2/nanominer-windows-3.1.2-cuda11.zip"
 $Commands = [PSCustomObject]@{ 
      "Ubqhash" = "" #
