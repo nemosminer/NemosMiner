2c40bbf9482a4fed6d1202b730cd4bfd6dd59f04
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-12-19 13:56:23 +1300

    Update ethminer.ps1
    Windows Build
    Compiled Using Visual Studio 2019 & Cuda 11.1
    Support RTX 30 Series GPUS

--- Miners/ethminer.ps1
+++ Miners/ethminer.ps1
@@ -1,6 +1,6 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\NVIDIA-ethminer0190r4\ethminer.exe"
-$Uri = "https://github.com/Minerx117/ethminer/releases/download/v0.19.0-r4/ethminer0190r4.7z"
+$Path = ".\Bin\NVIDIA-ethminer0190r5\ethminer.exe"
+$Uri = "https://github.com/Minerx117/ethminer/releases/download/v0.19.0-r5/ethminer0190r5.7z"
 $Commands = [PSCustomObject]@{ 
     "ethash" = "" #ethash
 }
