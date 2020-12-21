bff5cc36f88d2456e5a9f1446ec4163c3265f86a
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-12-22 11:32:19 +1300

    Update phoenix.ps1

--- OptionalMiners/phoenix.ps1
+++ OptionalMiners/phoenix.ps1
@@ -1,5 +1,5 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\AMD-Phoenix52e\PhoenixMiner.exe"
+$Path = ".\Bin\AMD-Phoenix54c\PhoenixMiner.exe"
 $Uri = "https://github.com/Minerx117/miners/releases/download/PhoenixMiner/PhoenixMiner_5.4c_Windows.zip"
 $Commands = [PSCustomObject]@{ 
     "ethash"  = "" #Ethash
