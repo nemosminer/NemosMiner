a3d8053412c5bcd06e3158b335e1a29f9bc025ac
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-12-23 12:54:15 +1300

    Update xmrigamd.ps1

--- Miners/xmrigamd.ps1
+++ Miners/xmrigamd.ps1
@@ -1,6 +1,6 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\AMD-XMRigv650\xmrig.exe"
-$Uri = "https://github.com/Minerx117/miners/releases/download/xmrig/XMRigv650.7z"
+$Path = ".\Bin\AMD-XMRigv670\xmrig.exe"
+$Uri = "https://github.com/Minerx117/miners/releases/download/xmrig/xmrig-6.7.0.7z"
 $Commands = [PSCustomObject]@{ 
     "kawpow"              = " -a kawpow --nicehash" #kawpow
     "cryptonight_conceal" = " -a cn/ccx --nicehash" #cryptonight_conceal
