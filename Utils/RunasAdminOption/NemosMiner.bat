b3267af1e5d5ac80ee6bb0b3ca105f70529561d5
Useless Guru <useless.guru@gmail.com>
2020-12-26 12:10:20 +0100

    PowerShell 7.x is required, older PowerShell versions are not longer supported

--- Utils/RunasAdminOption/NemosMiner.bat
+++ Utils/RunasAdminOption/NemosMiner.bat
@@ -23,4 +23,8 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 ::--------------------------------------
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -version 5.0 -executionpolicy bypass -windowstyle maximized -command ".\NemosMiner.ps1 -ConfigFile '.\Config\config.json'"
+
+PWSH -executionpolicy bypass -windowstyle maximized -command ".\NemosMiner.ps1 -ConfigFile '.\Config\config.json'"
+
+
+
