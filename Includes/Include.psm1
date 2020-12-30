4bf804defcfb22ad29a318e77471623a6e2a3d3a
Useless Guru <useless.guru@gmail.com>
2020-12-30 18:51:22 +0100

    Update Include.psm1

--- Includes/Include.psm1
+++ Includes/Include.psm1
@@ -2675,7 +2675,7 @@ Function Initialize-Autoupdate {
     # Use PostUpdateActions.ps1 in new release to place code
     If (Test-Path ".\$UpdateFilePath\PostUpdateActions.ps1" -PathType Leaf) { 
         Write-Message -Level Verbose  "Running post update actions..."
-        Invoke-Expression "$UpdateFilePath\PostUpdateActions.ps1 $Config $($Variables.ConfigFile) $($UpdateVersion.Version) $AllCommandLineParameters"
+        Invoke-Expression  "$UpdateFilePath\PostUpdateActions.ps1 @(Config = $Config, ConfigFile = $($Variables.ConfigFile), Version = $($UpdateVersion.Version), AllCommandLieParameters = $AllCommandLineParameters)"
     }
 
     # Remove temp files
