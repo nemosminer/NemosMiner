bfc88ce302af1fcd851f4e2aff09cb1375438cba
Useless Guru <useless.guru@gmail.com>
2020-12-30 16:00:49 +0100

    Update Include.psm1

--- Includes/Include.psm1
+++ Includes/Include.psm1
@@ -2664,7 +2664,7 @@ Function Initialize-Autoupdate {
     # Use PostUpdateActions.ps1 in new release to place code
     If (Test-Path ".\$UpdateFileName\NemosMiner-Testing\PostUpdateActions.ps1" -PathType Leaf) { 
         Write-Message -Level Verbose  "Running post update actions..."
-        & ".\$UpdateFileName\NemosMiner-Testing\PostUpdateActions.ps1 "
+        & ".\$UpdateFileName\NemosMiner-Testing\PostUpdateActions.ps1"
     }
 
     # Remove temp files
