fa8d2967e2fae046f289c019f24ca96faefa0207
Useless Guru <useless.guru@gmail.com>
2021-01-06 10:34:24 +0100

    Bad / share ratio calulation not working

--- Includes/Core.ps1
+++ Includes/Core.ps1
@@ -19,8 +19,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 <#
 Product:        NemosMiner
 File:           core.ps1
-version:        3.9.9.11
-version date:   04 Januar 2021
+version:        3.9.9.12
+version date:   06 Januar 2021
 #>
 
 using module .\Include.psm1
@@ -1121,7 +1121,7 @@ While ($true) {
                         Write-Message -Level Verbose "$($Miner.Name) data sample retrieved: [$(($Miner.WorkersRunning.Pool.Algorithm | ForEach-Object { "$_ = $(($Sample.Hashrate.$_ | ConvertTo-Hash) -replace ' ')$(If ($Miner.AllowedBadShareRatio) { " / Shares Total = $($Sample.Shares.$_[2]), Rejected = $($Sample.Shares.$_[1])" })" }) -join ' & ')$(If ($Sample.PowerUsage) { " / Power = $($Sample.PowerUsage.ToString("N2"))W" })] ($(($Miner.Data).Count) sample$(If (($Miner.Data).Count -ne 1) { "s"} ))"
                         If ($Miner.AllowedBadShareRatio) { 
                             $Miner.WorkersRunning.Pool.Algorithm | ForEach-Object { 
-                                If ((-not $Sample.Shares.$_[0] -and $Sample.Shares.$_[1] -ge 3) -or ($Sample.Shares.$_[0] -and ($Sample.Shares.$_[1] * $Miner.AllowedBadShareRatio -gt $Sample.Shares.$_[0]))) { 
+                                If ((-not $Sample.Shares.$_[0] -and $Sample.Shares.$_[1] -ge 3) -or ($Sample.Shares.$_[0] -and ($Sample.Shares.$_[1] / $Sample.Shares.$_[0] -gt $Miner.AllowedBadShareRatio))) { 
                                     Write-Message -Level Error "Miner '$($Miner.Info)' stopped. Reason: Too many bad shares (Shares Total = $($Sample.Shares.$_[2]), Rejected = $($Sample.Shares.$_[1]))." 
                                     $Miner.SetStatus([MinerStatus]::Failed)
                                     $Miner.StatusMessage = "too many bad shares."
