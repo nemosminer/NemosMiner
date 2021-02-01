0bd3ab4926b28f7ac7add618681eb1d34132fed3
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2021-02-02 10:47:05 +1300

    Update teamred.ps1

--- OptionalMiners/teamred.ps1
+++ OptionalMiners/teamred.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-$Path = ".\Bin\AMD-teamred0721\teamredminer.exe"
-$Uri = "https://github.com/todxx/teamredminer/releases/download/0.7.21/teamredminer-v0.7.21-win.zip"
+$Path = ".\Bin\AMD-teamred080\teamredminer.exe"
+$Uri = "https://github.com/todxx/teamredminer/releases/download/0.8.0/teamredminer-v0.8.0-win.zip"
 
 $Commands = [PSCustomObject]@{
     "etchash"= " --algo etchash" #etchash
