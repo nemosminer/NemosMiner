915291599f74061150a7542b26d799b197fc017c
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2021-02-08 10:14:21 +1300

    Revert "Update ethminer.ps1"
    This reverts commit 8192427ab45baf3d38e66f1d1acb24764ee839dd.

--- Miners/ethminer.ps1
+++ Miners/ethminer.ps1
@@ -1,6 +1,6 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\NVIDIA-nsfminer132\nsfminer.exe"
-$Uri = "https://github.com/no-fee-ethereum-mining/nsfminer/releases/download/v1.3.2/nsfminer_1.3.2-windows_10-cuda_11.2-opencl.zip"
+$Path = ".\Bin\NVIDIA-ethminer0190r5\ethminer.exe"
+$Uri = "https://github.com/Minerx117/ethminer/releases/download/v0.19.0-r5/ethminer0190r5.7z"
 $Commands = [PSCustomObject]@{ 
     "ethash" = "" #ethash
 }
