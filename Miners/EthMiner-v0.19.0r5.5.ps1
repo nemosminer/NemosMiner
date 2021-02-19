cd474dc654b034bbcc301cb9f0ed18272ed10daf
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2021-02-19 18:12:43 +1300

    Update EthMiner-v0.19.0r5.5.ps1

--- Miners/EthMiner-v0.19.0r5.5.ps1
+++ Miners/EthMiner-v0.19.0r5.5.ps1
@@ -7,7 +7,7 @@ $DeviceEnumerator = "Type_Vendor_Slot"
 $DAGmemReserve = [Math]::Pow(2, 23) * 17 # Number of epochs 
 
 $AlgorithmDefinitions = [PSCustomObject[]]@(
-    [PSCustomObject]@{ Algorithm = "Ethash"; Type = "AMD";    MinerSet = 0; Arguments = " --opencl --cl-devices" } # May need https://github.com/ethereum-mining/ethminer/issues/2001
+    [PSCustomObject]@{ Algorithm = "Ethash"; Type = "AMD"; MinerSet = 0; Arguments = " --opencl --cl-devices" } # May need https://github.com/ethereum-mining/ethminer/issues/2001
     [PSCustomObject]@{ Algorithm = "Ethash"; Type = "NVIDIA"; MinerSet = 0; Arguments = " --cuda --cu-devices" } # PhoenixMiner-v5.5c is fastest but has dev fee
 )
 
@@ -27,22 +27,24 @@ $Devices | Where-Object Type -in @("AMD", "NVIDIA") | Select-Object Type, Model 
                 # Get arguments for active miner devices
                 # $_.Arguments= Get-ArgumentsPerDevice -Command $_.Arguments-ExcludeParameters @() -DeviceIDs $Miner_Devices.$DeviceEnumerator
 
-                If ($Pools.($_.Algorithm).SSL) { $Protocol = "stratums://" }
-                Else { $Protocol = "stratum://" }
+                If ($Pools.($_.Algorithm).Name -match "^NiceHash$|^MiningPoolHub(|Coins)$") { $Protocol = "-P stratum2+tcp" }
+                Else { $Protocol = "-P stratum+tcp" }
+
+                If ($Pools.($_.Algorithm).SSL) { $Protocol = $Protocol -replace "tcp", "ssl" }
 
                 [PSCustomObject]@{ 
                     Name        = $Miner_Name
                     DeviceName  = $Miner_Devices.Name
                     Type        = $_.Type
                     Path        = $Path
-                    Arguments   = ("-P $Protocol$(If ($Pools.($_.Algorithm).Name -like "MiningPoolHub*") { $($Pools.($_.Algorithm).User -replace "\.", "%2e") } Else { $($Pools.($_.Algorithm).User) }):$($Pools.($_.Algorithm).Pass)@$($Pools.($_.Algorithm).Host):$($Pools.($_.Algorithm).Port) --api-port -$MinerAPIPort $($_.Arguments) $(($Miner_Devices | Sort-Object $DeviceEnumerator -Unique | ForEach-Object { '{0:x}' -f $_.$DeviceEnumerator }) -join ' ')" -replace "\s+", " ").trim()
+                    Arguments   = ("$($Protocol)://$($Pools.($_.Algorithm).User):$($Pools.($_.Algorithm).Pass)@$($Pools.($_.Algorithm).Host):$($Pools.($_.Algorithm).Port) --api-port -$MinerAPIPort $($_.Arguments) $(($Miner_Devices | Sort-Object $DeviceEnumerator -Unique | ForEach-Object { '{0:x}' -f $_.$DeviceEnumerator }) -join ' ')" -replace "\s+", " ").trim()
                     Algorithm   = $_.Algorithm
                     API         = "EthMiner"
                     Port        = $MinerAPIPort
                     Wrap        = $false
                     URI         = $Uri
                     MinerUri    = "http://localhost:$($MinerAPIPort)"
-                    WaitForData = 45
+                    WaitForData = 60
                 }
             }
         }
