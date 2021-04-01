8fdf2e59dbcdfe68d1f277ac5a844135a118cad9
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2021-04-02 12:00:46 +1300

    Update ccminermtp.ps1
    recompile of 1.3.2
    CUDA: 11.2.2
    DRIVER: 461.33+
    VS: VC++2019
    Support Recent 30x GPU's

--- Miners/ccminermtp.ps1
+++ Miners/ccminermtp.ps1
@@ -1,8 +1,8 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 $Path = ".\Bin\NVIDIA-ccminermtp132\ccminer.exe"
-$Uri = "https://github.com/zcoinofficial/ccminer/releases/download/1.3.2/ccminer.exe"
+$Uri = "https://github.com/Minerx117/ccminer-2/releases/download/1.3.2/ccminermtp132.7z"
 $Commands = [PSCustomObject]@{ 
-     #"mtp" = " -d $($Config.SelGPUCC) -a mtp -i 21" # memeroy allocation error when running 7 or more GPU's work well with upto 6 GPU's 
+      "mtp" = " -d $($Config.SelGPUCC) -a mtp -i 21" # memeroy allocation error when running 7 or more GPU's work well with upto 6 GPU's 
 }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
