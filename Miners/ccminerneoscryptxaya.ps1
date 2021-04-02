87f0c65007d5f9bed7b2730667540f6c12b85a73
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2021-04-03 12:00:05 +1300

    Update ccminerneoscryptxaya.ps1
    CUDA: 11.1.1
    REQUIRES DRIVER: 461.33+
    VS: VC++2013
    Support Recent Nvidia GPU's

--- Miners/ccminerneoscryptxaya.ps1
+++ Miners/ccminerneoscryptxaya.ps1
@@ -1,15 +1,15 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\NVIDIA-neoscryptxaya01\ccminer-64bit.exe"
-$Uri = "https://github.com/xaya/ccminer/releases/download/v0.1/ccminer-64bit.exe"
+$Path = ".\Bin\NVIDIA-neoscryptxaya02\ccminer.exe"
+$Uri = "https://github.com/Minerx117/ccminer/releases/download/v0.2/neoscryptxayaV02.7z"
 $Commands = [PSCustomObject]@{ 
-    "neoscrypt-xaya" = " -a neoscrypt-xaya" #neoscrypt-xaya 8gb and above gpu's can use -i 21 for 35-45% performance increase over default -i 13
+    "neoscrypt-xaya" = " -a neoscrypt-xaya"
  }
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object {
     [PSCustomObject]@{ 
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "--cpu-priority 4 -T 50000 -R 1 -i 19 -b $($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://neoscrypt-xaya.mine.zergpool.com:4238 -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
+        Arguments = "--cpu-priority 4 -T 50000 -R 1 -i 21 -b $($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://neoscrypt-xaya.mine.zergpool.com:4238 -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week }
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort #4068
