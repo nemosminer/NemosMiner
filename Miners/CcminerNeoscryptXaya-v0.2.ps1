5a35f97d7e03dc4c4bee5791402d1ef38d88cae4
Useless Guru <useless.guru@gmail.com>
2021-04-04 00:15:45 +0200

    Create CcminerNeoscryptXaya-v0.2.ps1

---
+++ Miners/CcminerNeoscryptXaya-v0.2.ps1
@@ -0,0 +1,48 @@
+using module ..\Includes\Include.psm1
+
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+$Path = ".\Bin\$($Name)\ccminer.exe"
+$Uri = "https://github.com/Minerx117/ccminer/releases/download/v0.2/neoscryptxayaV02.7z"
+$DeviceEnumerator = "Type_Vendor_Index"
+
+$AlgorithmDefinitions = [PSCustomObject[]]@(
+    [PSCustomObject]@{ Algorithm = "NeoscryptXaya"; MinMemGB = 2; MinerSet = 0; Arguments = " --algo neoscrypt-xaya --cpu-priority 4 --retries 1 --retry-pause 1 --intensity 21" }
+)
+
+If ($AlgorithmDefinitions = $AlgorithmDefinitions | Where-Object MinerSet -LE $Config.MinerSet | Where-Object { $Pools.($_.Algorithm).Host }) { 
+
+    $Devices | Where-Object Type -EQ "NVIDIA" | Select-Object Model -Unique | ForEach-Object { 
+
+        If ($SelectedDevices = @($Devices | Where-Object Model -EQ $_.Model)) { 
+
+            $MinerAPIPort = [UInt16]($Config.APIPort + ($SelectedDevices | Sort-Object Id | Select-Object -First 1 -ExpandProperty Id) + 1)
+            $Miner_Name = (@($Name) + @($SelectedDevices.Model | Sort-Object -Unique | ForEach-Object { $Model = $_; "$(@($SelectedDevices | Where-Object Model -eq $Model).Count)x$Model" }) | Select-Object) -join '-'
+
+            $AlgorithmDefinitions | Where-Object { -not $Pools.($_.Algorithm).SSL } | ForEach-Object {
+
+                $MinMemGB = $_.MinMemGB
+
+                If ($SelectedDevices | Where-Object { ($_.OpenCL.GlobalMemSize / 1GB) -le 2 }) { $_.Arguments = $_.Arguments -replace " --intensity 21", " --intensity 20" }
+
+                If ($Miner_Devices = @($SelectedDevices | Where-Object { ($_.OpenCL.GlobalMemSize / 1GB) -ge $MinMemGB })) { 
+
+                    # Get arguments for active miner devices
+                    # $_.Arguments= Get-ArgumentsPerDevice -Command $_.Arguments-ExcludeParameters @("algo", "statsavg") -DeviceIDs $Miner_Devices.$DeviceEnumerator
+
+                    [PSCustomObject]@{ 
+                        Name       = $Miner_Name
+                        DeviceName = $Miner_Devices.Name
+                        Type       = "NVIDIA"
+                        Path       = $Path
+                        Arguments  = ("$($_.Arguments) --url stratum+tcp://$($Pools.($_.Algorithm).Host):$($Pools.($_.Algorithm).Port) --user $($Pools.($_.Algorithm).User) --pass $($Pools.($_.Algorithm).Pass) --api-bind $MinerAPIPort --devices $(($Miner_Devices | Sort-Object $DeviceEnumerator -Unique | ForEach-Object { '{0:x}' -f $_.$DeviceEnumerator }) -join ',')" -replace "\s+", " ").trim()
+                        Algorithm  = $_.Algorithm
+                        API        = "Ccminer"
+                        Port       = $MinerAPIPort
+                        URI        = $Uri
+                        WarmupTime = 75 # Seconds, additional wait time until first data sample
+                    }
+                }
+            }
+        }
+    }
+}
