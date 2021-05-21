ec68ab97de30d70d9b71262e1b22d233faa95af7
Useless Guru <useless.guru@gmail.com>
2021-05-21 13:49:48 +0200

    Add VertHash miner
    Read https://github.com/Minerx117/miners/releases/tag/Verthash

---
+++ Includes/MinerAPIs/VertHashMiner.ps1
@@ -0,0 +1,63 @@
+<#
+Copyright (c) 2018-2021 Nemo, MrPlus & UselessGuru
+
+NemosMiner is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+NemosMiner is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+#>
+
+<#
+Product:        NemosMiner
+File:           LogFile.ps1
+Version:        3.9.9.45
+Version date:   17 May 2021
+#>
+
+using module ..\Include.psm1
+
+class VertHashMiner : Miner { 
+    [Object]UpdateMinerData () { 
+
+        [Double]$HashRate_Value = 0
+
+        # This will 'fake' has rate data, VertMiner currently has no API to retrieve live stat information
+        $StaticHashrates = Get-Content "$(Split-Path $this.Path)\Hashrates.json" -ErrorAction Stop | ConvertFrom-Json
+        $HashRate_Value = $StaticHashrates.($this.DeviceName -join ',')
+
+        $PowerUsage = [Double]0
+        $Sample = [PSCustomObject]@{ }
+        $HashRate = [PSCustomObject]@{ }
+
+        $HashRate_Name = $this.Algorithm | Select-Object -Last 1
+        $HashRate | Add-Member @{ $HashRate_Name = [Double]$HashRate_Value }
+
+        $Shares = [PSCustomObject]@{ }
+        $Shares_Accepted = [Int64]0
+        $Shares_Rejected = [Int64]0
+        $Shares | Add-Member @{ $HashRate_Name = @($Shares_Accepted, $Shares_Rejected, ($Shares_Accepted + $Shares_Rejected)) }
+
+        If ($this.CalculatePowerCost) { 
+            $PowerUsage = $this.GetPowerUsage()
+        }
+
+        If ($HashRate.PSObject.Properties.Value -gt 0) { 
+            $Sample = [PSCustomObject]@{ 
+                Date       = (Get-Date).ToUniversalTime()
+                HashRate   = $HashRate
+                PowerUsage = $PowerUsage
+                Shares     = $Shares
+            }
+            Return $Sample
+        }
+        Return $null
+    }
+}
---
+++ OptionalMiners/VertHashMiner-v0.7.2.ps1
@@ -0,0 +1,54 @@
+using module ..\Includes\Include.psm1
+
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+$Path = ".\Bin\$($Name)\verthashminer.exe"
+$Uri = "https://github.com/Minerx117/miners/releases/download/Verthash/VertHashMiner_0.7.2.zip"
+$DeviceEnumerator = "Type_Vendor_Index"
+
+$AlgorithmDefinitions = [PSCustomObject[]]@(
+    [PSCustomObject]@{ Algorithm = "VertHash"; MinMemGB = 2; Type = "AMD";    MinerSet = 0; WarmupTime = 0; Arguments = " --verthash-data ..\..\Cache\VertHash.dat --cl-devices" }
+    [PSCustomObject]@{ Algorithm = "VertHash"; MinMemGB = 2; Type = "NVIDIA"; MinerSet = 0; WarmupTime = 0; Arguments = " --verthash-data ..\..\Cache\VertHash.dat --cu-devices" }
+)
+
+If ($AlgorithmDefinitions = $AlgorithmDefinitions | Where-Object MinerSet -LE $Config.MinerSet | Where-Object { $Pools.($_.Algorithm).Host }) { 
+
+    $Devices | Where-Object Type -in @($AlgorithmDefinitions.Type) | Select-Object Type, Model -Unique | ForEach-Object { 
+
+        If ($SelectedDevices = @($Devices | Where-Object Type -EQ $_.Type | Where-Object Model -EQ $_.Model)) { 
+
+            $MinerAPIPort = 0 # Wrapper has no API Port #[UInt16]($Config.APIPort + ($SelectedDevices | Sort-Object Id | Select-Object -First 1 -ExpandProperty Id) + 1)
+
+            $AlgorithmDefinitions | Where-Object Type -EQ $_.Type | ForEach-Object { 
+
+                $MinMemGB = $_.MinMemGB
+
+                If ($_.Algorithm -eq "VertHash" -and (-not (Test-Path -Path ".\Cache\VertHash.dat" -PathType Leaf ))) { 
+                    If (-not (Test-Path -Path ".\Cache" -PathType Container)) { New-Item -Path . -Name "Cache" -ItemType Directory | Out-Null }
+                    $_.WarmupTime += 420 # Seconds, max. wait time until first data sample, allow extra time to build verthash.dat}
+                }
+
+                If ($Miner_Devices = @($SelectedDevices | Where-Object { ($_.OpenCL.GlobalMemSize / 1GB) -ge $MinMemGB } )) { 
+
+                    $Miner_Name = (@($Name) + @($Miner_Devices.Model | Sort-Object -Unique | ForEach-Object { $Model = $_; "$(@($Miner_Devices | Where-Object Model -eq $Model).Count)x$Model" }) | Select-Object) -join '-'
+
+                    # Get arguments for active miner devices
+                    # $_.Arguments= Get-ArgumentsPerDevice -Command $_.Arguments-ExcludeParameters @("algo") -DeviceIDs $Miner_Devices.$DeviceEnumerator
+
+                    [PSCustomObject]@{ 
+                        Name       = $Miner_Name
+                        DeviceName = $Miner_Devices.Name
+                        Type       = $_.Type
+                        Path       = $Path
+                        Arguments  = ("$($_.Arguments) $(($Miner_Devices | Sort-Object $DeviceEnumerator -Unique | ForEach-Object { '{0:x}' -f $_.$DeviceEnumerator }) -join ',') --url stratum+tcp://$($Pools.($_.Algorithm).Host):$($Pools.($_.Algorithm).Port) --user $($Pools.($_.Algorithm).User) --pass $($Pools.($_.Algorithm).Pass)" -replace "\s+", " ").trim()
+                        Algorithm  = $_.Algorithm
+                        API        = "VertHashMiner"
+                        Port       = $MinerAPIPort
+                        URI        = $Uri
+                        Fee        = 0.01
+                        WarmupTime = $_.WarmupTime # Seconds, additional wait time until first data sample
+                    }
+                }
+            }
+        }
+    }
+}
