38bfea682438c6bb1079be0946e23269091a9636
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2021-06-15 22:41:12 +0200

    v3.9.9.51

--- Brains/blockmasters/Brains.ps1
+++ Brains/blockmasters/Brains.ps1
@@ -18,8 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 <#
 Product:        NemosMiner
 File:           Brains.ps1
-version:        3.9.9.50
-version date:   13 June 2021 
+version:        3.9.9.51
+version date:   15 June 2021 
 #>
 
 Set-Location ($args[0])
