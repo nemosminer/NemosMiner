dc1d6b355d72f64558e8e2faa21ccaae967344b0
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2021-06-27 10:59:42 +1200

    panthera support xmrig variant support 0%fee

---
+++ Miners/xlarig.ps1
@@ -0,0 +1,22 @@
+If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+$Path = ".\Bin\CPU-XLArigv522\xlarig.exe"
+$Uri = "https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-win64.zip"
+$Commands = [PSCustomObject]@{ 
+    "panthera"       = " -a panthera --nicehash" #RandomX
+
+}
+$ThreadCount = $Variables.ProcessorCount - 1
+$Port = $Variables.CPUMinerAPITCPPort
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object { 
+    [PSCustomObject]@{ 
+        Type      = "CPU"
+        Path      = $Path
+        Arguments = "-t $($ThreadCount) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_) --keepalive --http-port=$($Variables.CPUMinerAPITCPPort) --donate-level 0 --retries=90 --retry-pause=1 --cpu-priority 3"
+        HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week } #Recompiled 0% fee
+        API       = "XMRig"
+        Port      = $Variables.CPUMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri    
+    }
+}
