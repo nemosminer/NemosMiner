d46051dfe0d0aa2937c88f51a996ea1fc08cbb94
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2021-06-27 17:52:23 +1200

    Update xmrigcpu.ps1
    update xmrig  zergpool urls

--- Miners/xmrigcpu.ps1
+++ Miners/xmrigcpu.ps1
@@ -1,21 +1,11 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
-$Path = ".\Bin\CPU-XMRigv6100\xmrig.exe"
-$Uri = "https://github.com/Minerx117/miners/releases/download/xmrig/XMRig-v6.10.0.7z"
+$Path = ".\Bin\CPU-XMRigv6122\xmrig.exe"
+$Uri = "https://github.com/Minerx117/miners/releases/download/XMRig/XMRigv6122.7z"
 $Commands = [PSCustomObject]@{ 
-    "randomxmonero"       = " -a rx/0 --nicehash" #RandomX
-    "randomx"             = " -a rx/0 --nicehash" #RandomX 
-    "cryptonight-monero"  = " -a rx/0 --nicehash" #Cryptonight-Monero
-    "randomsfx"           = " -a rx/sfx --nicehash" #Randomsfx
-    "randomwow"           = " -a rx/wow --nicehash" #Randomwow
-    "randomarq"           = " -a rx/arq --nicehash" #Randomarq
-    "cryptonightv7"       = " -a cn/1 --nicehash" #cryptonightv7
-    "cryptonight_heavy"   = " -a cn-heavy/0 --nicehash" #cryptonight_heavyx
-    "cryptonight_heavyx"  = " -a cn/double --nicehash" #cryptonight_heavyx
-    "cryptonight_saber"   = " -a cn-heavy/0 --nicehash" #cryptonightGPU
-    "cryptonight_fast"    = " -a cn/half --nicehash" #cryptonightFast
-    "cryptonight_haven"   = " -a cn-heavy/xhv --nicehash" #cryptonightFast
-    "chukwa"              = " -a argon2/chukwa --nicehash" #chukwa
-    "cryptonight_conceal" = " -a cn/ccx --nicehash" #cryptonight_conceal
+    "randomxmonero"       = " -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380 -a rx/0 --nicehash" #RandomX
+    "randomwow"           = " -o stratum+tcp://randomwow.mine.zergpool.com:4460 -a rx/wow --nicehash" #Randomwow
+    "cryptonight_haven"   = " -o stratum+tcp://cryptonight_haven.mine.zergpool.com:4452 -a cn-heavy/xhv --nicehash" #cryptonight_haven
+    "cryptonight_upx"     = " -o stratum+tcp://cryptonight_upx.mine.zergpool.com:4457 -a cn/upx2 --nicehash" #cryptonight_upx
 }
 $ThreadCount = $Variables.ProcessorCount - 1
 $Port = $Variables.CPUMinerAPITCPPort
@@ -24,7 +14,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{ 
         Type      = "CPU"
         Path      = $Path
-        Arguments = "-t $($ThreadCount) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_) --keepalive --http-port=$($Variables.CPUMinerAPITCPPort) --donate-level 0 --retries=90 --retry-pause=1 --cpu-priority 3"
+        Arguments = "-t $($ThreadCount) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_) --keepalive --http-port=$($Variables.CPUMinerAPITCPPort) --donate-level 1 --retries=90 --retry-pause=1 --cpu-priority 3"
         HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week } #Recompiled 0% fee
         API       = "XMRig"
         Port      = $Variables.CPUMinerAPITCPPort
