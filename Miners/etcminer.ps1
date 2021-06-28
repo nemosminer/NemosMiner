d6c3c9012a362e5e9c850a02bb4b1090668cbc44
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2021-06-29 00:04:28 +1200

    Create etcminer.ps1
    etcminer etchash MPH miningpoolhub and Zergpool

---
+++ Miners/etcminer.ps1
@@ -0,0 +1,27 @@
+If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+$Path = ".\Bin\NVIDIA-ethminer0190etc\ethminer.exe"
+$Uri = "https://github.com/Minerx117/ethminer/releases/download/v0.19.0-etc/etchashminer.7z"
+$Commands = [PSCustomObject]@{ 
+    "etchash" = "" #etchash
+}
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algo = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algo.Host } | ForEach-Object {
+    If ($Algo) { 
+        If ($Pools.$($Algo).Name -eq "MPH") { 
+            $AlgoParameter = "-P stratum+tcp://$($Pools.$Algo.User):$($Pools.$Algo.Pass)@$($Pools.$Algo.Host):$($Pools.$Algo.Port)"
+        }
+        Else { 
+            $AlgoParameter = "-P stratum2+tcp://$($Pools.$Algo.User):$($Pools.$Algo.Pass)@$($Pools.$Algo.Host):$($Pools.$Algo.Port)"
+        }
+        [PSCustomObject]@{ 
+            Type      = "NVIDIA"
+            Path      = $Path
+            Arguments = "--cuda-devices $($Config.SelGPUDSTM) --api-port -$($Variables.NVIDIAMinerAPITCPPort) -U $AlgoParameter$($Commands.$_)"
+            HashRates = [PSCustomObject]@{ $Algo = $Stats."$($Name)_$($Algo)_HashRate".Week }
+            API       = "ethminer"
+            Port      = $Variables.NVIDIAMinerAPITCPPort #4068
+            Wrap      = $false
+            URI       = $Uri
+        }
+    }
+}
