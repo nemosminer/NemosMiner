21a4b6f94896d474e05dcb2aa1ca022e1b4c43b4
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2021-07-30 17:05:21 +0200

    Update VertHashMiner-v0.7.2.ps1

--- OptionalMiners/VertHashMiner-v0.7.2.ps1
+++ OptionalMiners/VertHashMiner-v0.7.2.ps1
@@ -2,7 +2,8 @@ using module ..\Includes\Include.psm1
 
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Path = ".\Bin\$($Name)\verthashminer.exe"
-$Uri = "https://github.com/Minerx117/miners/releases/download/Verthash/VertHashMiner_0.7.2.zip"
+$Uri = "https://github.com/Minerx117/miners/releases/download/VertHashMiner/VertHashMiner_0.7.2.zip"
+
 $DeviceEnumerator = "Type_Vendor_Index"
 
 $AlgorithmDefinitions = [PSCustomObject[]]@(
