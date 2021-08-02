607b741ab70deddd2e3fa588bf01ccb2320b9a5a
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2021-08-02 08:16:40 +0200

    Update Downloader.ps1

--- Includes/Downloader.ps1
+++ Includes/Downloader.ps1
@@ -44,17 +44,16 @@ $Variables.DownloadList | Select-Object | ForEach-Object {
             Else { 
                 Expand-WebRequest $URI $Path -ErrorAction Stop
             }
-            Write-Message "Downloader: Installed downloaded miner binary '$($Path)'."
+            Write-Message "Downloader: Installed downloaded miner binary '$Path'."
         }
         Catch { 
-            Write-Message "Downloader: Searching '$Path' on local computer..."
             $Path_Old = $null
 
-            If ($URI) { Write-Message -Level Warn  "Downloader: Cannot download '$(Split-Path $Path -Leaf)' distributed at '$($URI)'." }
+            If ($URI) { Write-Message -Level Warn  "Downloader: Cannot download '$(Split-Path $Path -Leaf)' distributed at '$URI'." }
             Else { Write-Message -Level Warn  "Downloader: Cannot download'$(Split-Path $Path -Leaf)'." }
-                        
+
             If ($Searchable) { 
-                Write-Message "Downloader: Searching for $(Split-Path $Path -Leaf)."
+                Write-Message "Downloader: Searching for $(Split-Path $Path -Leaf) on local computer..."
 
                 $Path_Old = Get-PSDrive -PSProvider FileSystem | ForEach-Object { Get-ChildItem -Path $_.Root -Include (Split-Path $Path -Leaf) -Recurse -ErrorAction Ignore } | Sort-Object LastWriteTimeUtc -Descending | Select-Object -First 1
                 $Path_New = $Path
@@ -63,11 +62,11 @@ $Variables.DownloadList | Select-Object | ForEach-Object {
             If ($Path_Old) { 
                 If (Test-Path (Split-Path $Path_New) -PathType Container) { (Split-Path $Path_New) | Remove-Item -Recurse -Force }
                 (Split-Path $Path_Old) | Copy-Item -Destination (Split-Path $Path_New) -Recurse -Force
-                Write-Message -Level Verbose "Downloader: Copied '$($Path)' from local repository '$($PathOld)'."
+                Write-Message -Level Verbose "Downloader: Copied '$Path' from local repository '$PathOld'."
             }
             Else { 
-                If ($URI) { Write-Message -Level Warn "Downloader: Cannot find '$($Path)' distributed at '$($URI)'." }
-                Else { Write-Message -Level Warn "Downloader: Cannot find '$($Path)'." }
+                If ($URI) { Write-Message -Level Warn "Downloader: Cannot find '$Path' distributed at '$URI'." }
+                Else { Write-Message -Level Warn "Downloader: Cannot find '$Path'." }
             }
         }
     }
