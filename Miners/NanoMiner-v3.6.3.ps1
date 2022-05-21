77b3e30ce4857e9171adeeda24a8304c02bfb859
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2022-05-21 15:15:31 +0200

    Update NanoMiner-v3.6.3

--- Miners/NanoMiner-v3.6.3.ps1
+++ Miners/NanoMiner-v3.6.3.ps1
@@ -3,8 +3,8 @@ using module ..\Includes\Include.psm1
 If (-not ($Devices = $Variables.EnabledDevices | Where-Object { $_.Type -ne "NVIDIA" -or $_.OpenCL.ComputeCapability -gt 5.0 })) { Return }
 
 $Uri = Switch ($Variables.DriverVersion.CUDA) { 
-    { $_ -ge "11.0" } { "https://github.com/nanopool/nanominer/releases/download/v3.6.2/nanominer-windows-3.6.2-cuda11.zip"; Break }
-    Default           { "https://github.com/nanopool/nanominer/releases/download/v3.6.1/nanominer-windows-3.6.2.zip" }
+    { $_ -ge "11.0" } { "https://github.com/nanopool/nanominer/releases/download/v3.6.3/nanominer-windows-3.6.3-cuda11.zip"; Break }
+    Default           { "https://github.com/nanopool/nanominer/releases/download/v3.6.3/nanominer-windows-3.6.3.zip" }
 }
 $Name = (Get-Item $MyInvocation.MyCommand.Path).BaseName
 $Path = ".\Bin\$($Name)\nanominer.exe"
