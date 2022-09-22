1fea879974328e1f950b7bc7a3aeaff2e988fb56
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2022-09-23 09:09:05 +1200

    Update Raptor3umCpu-v2.0.ps1

--- Miners/Raptor3umCpu-v2.0.ps1
+++ Miners/Raptor3umCpu-v2.0.ps1
@@ -12,7 +12,7 @@ $Algorithms = [PSCustomObject[]]@(
 
 If ($Algorithms = $Algorithms | Where-Object MinerSet -LE $Config.MinerSet | Where-Object { $MinerPools[0].($_.Algorithm).PoolPorts }) { 
 
-    ElseIf ($AvailableMiner_Devices.CpuFeatures -match "avx2")   { $Path = ".\Bin\$($Name)\cpuminer-avx2.exe" }
+        If ($AvailableMiner_Devices.CpuFeatures -match "avx2")   { $Path = ".\Bin\$($Name)\cpuminer-avx2.exe" }
     ElseIf ($AvailableMiner_Devices.CpuFeatures -match "avx")    { $Path = ".\Bin\$($Name)\cpuminer-avx.exe" }
     ElseIf ($AvailableMiner_Devices.CpuFeatures -match "aes")    { $Path = ".\Bin\$($Name)\cpuminer-aes-sse42.exe" }
     ElseIf ($AvailableMiner_Devices.CpuFeatures -match "sse2")   { $Path = ".\Bin\$($Name)\cpuminer-sse2.exe" }
