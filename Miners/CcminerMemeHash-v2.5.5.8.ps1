9fa6cca10814c2c43ec38de4575e968df4c15d81
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2023-05-24 16:56:20 +0200

    Create CcminerMemeHash-v2.5.5.8.ps1

---
+++ Miners/CcminerMemeHash-v2.5.5.8.ps1
@@ -0,0 +1,51 @@
+If (-not ($Devices = $Variables.EnabledDevices | Where-Object Type -EQ "NVIDIA")) { Return }
+
+$Uri = "https://github.com/Minerx117/miners/releases/download/CcminerMemeHash/CcminerMemeHash-2.5.5.8.zip"
+$Name = (Get-Item $MyInvocation.MyCommand.Path).BaseName
+$Path = ".\Bin\$($Name)\ccminer.exe"
+$DeviceEnumerator = "Type_Vendor_Index"
+
+$Algorithms = [PSCustomObject[]]@(
+    [PSCustomObject]@{ Algorithm = "MemeHash";  MinMemGiB = 1; MinerSet = 1; WarmupTimes = @(30, 0); ExcludeGPUArchitecture = @("Other"); Arguments = " --algo memehash" }
+)
+
+$Algorithms = $Algorithms | Where-Object MinerSet -LE $Config.MinerSet
+$Algorithms = $Algorithms | Where-Object { $MinerPools[0].($_.Algorithm) }
+$Algorithms = $Algorithms | Where-Object { $MinerPools[0].($_.Algorithm).PoolPorts[0] }
+
+If ($Algorithms) { 
+    $Devices | Select-Object Model -Unique | ForEach-Object { 
+
+        $Miner_Devices = @($Devices | Where-Object Model -EQ $_.Model)
+        $MinerAPIPort = [UInt16]($Config.APIPort + ($Miner_Devices.Id | Sort-Object -Top 1) + 1)
+
+        $Algorithms | ForEach-Object { 
+
+            $Arguments = $_.Arguments
+
+            If ($AvailableMiner_Devices = $Miner_Devices | Where-Object MemoryGiB -GE $_.MinMemGiB | Where-Object Architecture -notin $_.ExcludeGPUArchitecture) { 
+
+                $Miner_Name = "$($Name)-$($AvailableMiner_Devices.Count)x$($AvailableMiner_Devices.Model)" -replace ' '
+
+                If ($AvailableMiner_Devices | Where-Object MemoryGiB -le 2) { $Arguments = $Arguments -replace " --intensity [0-9\.]+" }
+
+                # Get arguments for available miner devices
+                # $Arguments = Get-ArgumentsPerDevice -Arguments $Arguments -ExcludeArguments @("algo") -DeviceIDs $AvailableMiner_Devices.$DeviceEnumerator
+
+                [PSCustomObject]@{ 
+                    API         = "Ccminer"
+                    Arguments   = ("$($Arguments) --url stratum+tcp://$($MinerPools[0].($_.Algorithm).Host):$($MinerPools[0].($_.Algorithm).PoolPorts[0]) --user $($MinerPools[0].($_.Algorithm).User)$(If ($MinerPools[0].($_.Algorithm).WorkerName) { ".$($MinerPools[0].($_.Algorithm).WorkerName)" }) --pass $($MinerPools[0].($_.Algorithm).Pass) --retry-pause 1 --api-bind $MinerAPIPort --cuda-schedule 2 --devices $(($AvailableMiner_Devices.$DeviceEnumerator | Sort-Object -Unique | ForEach-Object { '{0:x}' -f $_ }) -join ',')" -replace "\s+", " ").trim()
+                    Algorithms  = @($_.Algorithm)
+                    DeviceNames = $AvailableMiner_Devices.Name
+                    MinerSet    = $_.MinerSet
+                    Name        = $Miner_Name
+                    Path        = $Path
+                    Port        = $MinerAPIPort
+                    Type        = "NVIDIA"
+                    URI         = $Uri
+                    WarmupTimes = $_.WarmupTimes # First value: Seconds until miner must send first sample, if no sample is received miner will be marked as failed; Second value: Seconds from first sample until miner sends stable hashrates that will count for benchmarking
+                }
+            }
+        }
+    }
+}
