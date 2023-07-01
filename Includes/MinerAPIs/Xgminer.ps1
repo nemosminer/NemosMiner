23e0c06e3e69f3c25cb731b0d3ed0621e1d29136
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2023-07-01 19:17:48 +0200

    Update Xgminer.ps1

--- Includes/MinerAPIs/Xgminer.ps1
+++ Includes/MinerAPIs/Xgminer.ps1
@@ -19,10 +19,10 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 Product:        NemosMiner
 File:           Xgminer.ps1
 Version:        4.3.5.0
-Version date:   27 June 2023
+Version date:   01 July 2023
 #>
 
-class Xgminer : Miner { 
+class XgMiner : Miner { 
     [Object]GetMinerData () { 
         $Timeout = 5 #seconds
         $Data = [PSCustomObject]@{ }
